40f579ab17cc8fe6060474d17ae7418c0ec58478
rgalina <rgalina@i-novus.ru>
2019-01-09 17:05:39 +0300

    [bf] Не корректно отображается дата LocalDateTime (NNO-2698)

---
+++ backend/n2o/n2o-spring-boot-starter/src/main/java/net/n2oapp/framework/boot/json/DateTimeModule.java
@@ -0,0 +1,21 @@
+package net.n2oapp.framework.boot.json;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeModule extends SimpleModule {
+
+    public DateTimeModule(String dateTimePattern) {
+        addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimePattern)));
+        addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
+        addSerializer(LocalTime.class, LocalTimeSerializer.INSTANCE);
+    }
+}
+
