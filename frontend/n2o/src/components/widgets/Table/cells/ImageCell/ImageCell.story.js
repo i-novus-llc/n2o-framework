8242349a647d49a8d30b18e3f8d2c6e103113a73
I-NOVUS\dmusin <dmusin@i-novus.ru>
2019-01-10 11:31:22 +0300

    Реализовано отправка экшена при клике на изображение, обновлены тесты

--- frontend/n2o/src/components/widgets/Table/cells/ImageCell/ImageCell.story.js
+++ frontend/n2o/src/components/widgets/Table/cells/ImageCell/ImageCell.story.js
@@ -17,48 +17,98 @@ stories.addDecorator(withTests('ImageCell'));
 const IMAGEURL =
   'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNjJiNDE2OGJmYSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE2MmI0MTY4YmZhIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA1NDY4NzUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=';
 
-stories.add('Метаданные', () => {
-  const props = {
-    id: text('id', ImageCellJson.id),
-    shape: select('shape', Object.values(imageShapes), ImageCellJson.shape),
-    style: object('style', ImageCellJson.style),
-    className: text('className', ImageCellJson.className),
-    title: text('title', ImageCellJson.title),
-    model: {
-      url:
-        'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNjJiNDE2OGJmYSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE2MmI0MTY4YmZhIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA1NDY4NzUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4='
-    }
-  };
+stories
+  .add('Метаданные', () => {
+    const props = {
+      id: text('id', ImageCellJson.id),
+      shape: select('shape', Object.values(imageShapes), ImageCellJson.shape),
+      style: object('style', ImageCellJson.style),
+      className: text('className', ImageCellJson.className),
+      title: text('title', ImageCellJson.title),
+      model: {
+        url:
+          'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNjJiNDE2OGJmYSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE2MmI0MTY4YmZhIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA1NDY4NzUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4='
+      }
+    };
 
-  const tableProps = {
-    headers: [
-      {
-        id: 'knobs',
-        component: TextTableHeader,
-        label: 'Ячейка с картинкой'
-      }
-    ],
-    cells: [
-      {
-        component: ImageCell,
-        ...props
-      }
-    ],
-    datasource: [
-      {
-        id: 'data',
-        rounded: IMAGEURL,
-        circle: IMAGEURL,
-        thumbnail: IMAGEURL
-      }
-    ]
-  };
+    const tableProps = {
+      headers: [
+        {
+          id: 'knobs',
+          component: TextTableHeader,
+          label: 'Ячейка с картинкой'
+        }
+      ],
+      cells: [
+        {
+          component: ImageCell,
+          ...props
+        }
+      ],
+      datasource: [
+        {
+          id: 'data',
+          rounded: IMAGEURL,
+          circle: IMAGEURL,
+          thumbnail: IMAGEURL
+        }
+      ]
+    };
 
-  return (
-    <Table
-      headers={tableProps.headers}
-      cells={tableProps.cells}
-      datasource={tableProps.datasource}
-    />
-  );
-});
+    return (
+      <Table
+        headers={tableProps.headers}
+        cells={tableProps.cells}
+        datasource={tableProps.datasource}
+      />
+    );
+  })
+  .add('Вызов действия при клике на картинку', () => {
+    const action = {
+      src: 'perform',
+      options: {
+        type: 'n2o/modals/INSERT',
+        payload: {
+          pageUrl: '/page/widget/create',
+          size: 'sm',
+          visible: true,
+          closeButton: true,
+          title: 'Новое модальное окно',
+          pageId: 'undefined'
+        }
+      }
+    };
+
+    const tableProps = {
+      headers: [
+        {
+          id: 'knobs',
+          component: TextTableHeader,
+          label: 'Ячейка с картинкой'
+        }
+      ],
+      cells: [
+        {
+          component: ImageCell,
+          action,
+          ...ImageCellJson
+        }
+      ],
+      datasource: [
+        {
+          id: 'data',
+          rounded: IMAGEURL,
+          circle: IMAGEURL,
+          thumbnail: IMAGEURL
+        }
+      ]
+    };
+
+    return (
+      <Table
+        headers={tableProps.headers}
+        cells={tableProps.cells}
+        datasource={tableProps.datasource}
+      />
+    );
+  });
