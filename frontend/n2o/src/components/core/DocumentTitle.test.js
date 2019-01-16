75854f9ef7f49e32965664640bdc65b0af4afd7c
I-NOVUS\dmusin <dmusin@i-novus.ru>
2019-01-16 11:30:56 +0300

    Добавлен тест для DocumentTitle

---
+++ frontend/n2o/src/components/core/DocumentTitle.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import DocumentTitle from './DocumentTitle';
+
+const setupContainer = propOverrides => {
+  const props = Object.assign(
+    {
+      // use this to assign some default props
+    },
+    propOverrides
+  );
+  const initialState = {
+    models: {
+      test: {
+        test: 'test'
+      }
+    }
+  };
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+  const wrapper = mount(
+    <Provider store={store}>
+      <DocumentTitle {...props} />
+    </Provider>
+  );
+
+  return {
+    props,
+    wrapper
+  };
+};
+
+describe('<DocumentTitle />', () => {
+  it('тестирует резолв в DocumentTitle', () => {
+    const { wrapper } = setupContainer({
+      title: "`test ? test : 'dummy'`",
+      modelLink: {
+        link: 'models.test'
+      }
+    });
+    expect(wrapper.find(Helmet).props().title).toBe('test');
+  });
+});
