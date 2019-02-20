3f6078ecdd3babc6e14832d818a82ac98ef4daf6
sliskov <sliskov@i-novus.ru>
2019-02-20 11:03:41 +0300

    [NNO-2827] - перенес стили

--- frontend/n2o/src/components/controls/TextEditor/TextEditor.jsx
+++ frontend/n2o/src/components/controls/TextEditor/TextEditor.jsx
@@ -6,7 +6,6 @@ import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import PropTypes from 'prop-types';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 /**
  * Компонент TextEditor
