357e0a1b1ecde8ff7cf0a1dd86ac06f01d439cea
sliskov <sliskov@i-novus.ru>
2019-02-26 14:58:21 +0300

    [NNO-2923] тесты для utils, tools, core

---
+++ frontend/n2o/src/utils/withTooltip.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import withTooltip from './withTooltip';
+import { UncontrolledTooltip } from 'reactstrap';
+
+const setup = propsOverride => {
+  return mount(withTooltip(<div className={'test'}>test</div>, 'test hint', 123));
+};
+
+describe('Проверка withTooltip', () => {
+  it('вернет компонент с тултипом', () => {
+    //  const wrapper = setup();
+    // expect(wrapper.find(UncontrolledTooltip).exists()).toEqual(true);
+  });
+});
