8ca8835fc17ea47b4f1e87e7c37011dcfb9728d1
estartcev <estartcev@i-novus.ru>
2019-02-26 15:29:17 +0300

    [NNO-2855] добавлена компиляция контролов внутри field

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/CustomFieldCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/CustomFieldCompiler.java
@@ -3,9 +3,12 @@ package net.n2oapp.framework.config.metadata.compile.control;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
+import net.n2oapp.framework.api.metadata.control.N2oComponent;
 import net.n2oapp.framework.api.metadata.control.N2oCustomField;
 import net.n2oapp.framework.api.metadata.meta.control.CustomField;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 /**
  * Компиляция кастомного поля для ввода
@@ -19,14 +22,20 @@ public class CustomFieldCompiler extends FieldCompiler<CustomField, N2oCustomFie
     }
 
     @Override
-    public CustomField compile(N2oCustomField source, CompileContext<?,?> context, CompileProcessor p) {
+    public CustomField compile(N2oCustomField source, CompileContext<?, ?> context, CompileProcessor p) {
         CustomField customField = new CustomField();
         compileField(customField, source, context, p);
+        if (source.getControls() != null) {
+            if (source.getControls().length > 1) {
+                customField.setControls(new ArrayList<>());
+                for (N2oComponent component : source.getControls()) {
+                    customField.getControls().add(p.compile(component, context));
+                }
+            } else if (source.getControls().length == 1) {
+                customField.setControl(p.compile(source.getControls()[0], context));
+            }
+        }
         return customField;
     }
 
-    @Override
-    protected String getSrcProperty() {
-        return "n2o.api.field.src";
-    }
 }
