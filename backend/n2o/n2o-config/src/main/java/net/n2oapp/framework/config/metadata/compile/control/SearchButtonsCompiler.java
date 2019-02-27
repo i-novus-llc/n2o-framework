27242066a61c5ee1826ab100afe8460e7b42eee8
estartcev <estartcev@i-novus.ru>
2019-02-27 10:28:37 +0300

    [NNO-2856] сборка search-buttons перенесена в StandardField

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/SearchButtonsCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/SearchButtonsCompiler.java
@@ -5,16 +5,15 @@ import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.control.N2oSearchButtons;
 import net.n2oapp.framework.api.metadata.meta.control.SearchButtons;
-import net.n2oapp.framework.config.metadata.compile.ComponentCompiler;
+import net.n2oapp.framework.api.metadata.meta.control.StandardField;
 import org.springframework.stereotype.Component;
 
-import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
 
 /**
  * Компиляция компонента SearchButtons (кнопки фильтра)
  */
 @Component
-public class SearchButtonsCompiler extends ComponentCompiler<SearchButtons, N2oSearchButtons> {
+public class SearchButtonsCompiler extends StandardFieldCompiler<SearchButtons, N2oSearchButtons> {
 
     @Override
     public Class<? extends Source> getSourceClass() {
@@ -22,16 +21,22 @@ public class SearchButtonsCompiler extends ComponentCompiler<SearchButtons, N2oS
     }
 
     @Override
-    public SearchButtons compile(N2oSearchButtons source, CompileContext<?, ?> context, CompileProcessor p) {
+    public StandardField<SearchButtons> compile(N2oSearchButtons source, CompileContext<?, ?> context, CompileProcessor p) {
         SearchButtons field = new SearchButtons();
 
-        compileComponent(field, source, context, p);
-
-        field.setId(source.getId());
-        field.setSrc(p.cast(source.getSrc(), p.resolve(property("n2o.api.control.searchButtons.src"), String.class)));
         field.setResetLabel(source.getResetLabel());
         field.setSearchLabel(source.getSearchLabel());
 
-        return field;
+        return compileStandardField(field, source, context, p);
+    }
+
+//    @Override
+//    protected String getSrcProperty() {
+//        return "n2o.api.control.searchButtons.src";
+//    }
+
+    @Override
+    protected String getControlSrcProperty() {
+        return "n2o.api.control.searchButtons.src";
     }
 }
