ab834e6b7e117304c0531b1f526b071900f7a7a4
btsyganov <btsyganov@i-novus.ru>
2019-03-04 14:38:04 +0300

    [NNO-2950][nf] атрибуты для ограничения количества отображаемых данных в дереве

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectTreeCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectTreeCompileTest.java
@@ -1,5 +1,6 @@
 package net.n2oapp.framework.config.metadata.compile.control;
 
+import net.n2oapp.framework.api.metadata.control.list.CheckingStrategy;
 import net.n2oapp.framework.api.metadata.meta.control.InputSelectTree;
 import net.n2oapp.framework.api.metadata.meta.control.StandardField;
 import net.n2oapp.framework.api.metadata.meta.widget.form.Form;
@@ -39,5 +40,7 @@ public class InputSelectTreeCompileTest extends SourceCompileTestBase {
                 .get(0).getRows().get(0).getCols().get(0).getFields().get(0)).getControl();
         assertThat(ist.getParentFieldId(), is("testParentFieldId"));
         assertThat(ist.getHasChildrenFieldId(), is("testHasChildrenFieldId"));
+        assertThat(ist.getCheckingStrategy(), is(CheckingStrategy.child));
+        assertThat(ist.getMaxTagCount(), is(5));
     }
 }
