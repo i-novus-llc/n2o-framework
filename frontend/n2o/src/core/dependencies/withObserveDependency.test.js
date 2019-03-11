05bcba5fb94f303eec9fa9bd8d9ff027d305d499
sliskov <sliskov@i-novus.ru>
2019-03-11 16:26:27 +0300

    [NNO-2923] тесты для utils, tools, core

--- frontend/n2o/src/core/dependencies/withObserveDependency.test.js
+++ frontend/n2o/src/core/dependencies/withObserveDependency.test.js
@@ -47,4 +47,72 @@ describe('Проверка хока withObserveDependency', () => {
     expect(isEmpty(wrapper.find('ReRenderComponent').instance()._observers)).toEqual(false);
     expect(typeof wrapper.find('ReRenderComponent').instance()._observers[0]).toEqual('function');
   });
+
+  it('срабатывает onChange reRender', () => {
+    const onChange = sinon.spy();
+    const store = createStore(reducers);
+    store.dispatch(
+      registerFieldDependency('testForm', 'testField', [
+        {
+          type: 'reRender',
+          on: ['anotherTestField']
+        }
+      ])
+    );
+    store.dispatch(setModel('resolve', 'widgetName', { anotherValue: 'value' }));
+    const wrapper = setup(
+      store,
+      {
+        id: 'testField',
+        form: 'testForm',
+        dependency: [
+          {
+            type: 'reRender',
+            on: ['testField']
+          }
+        ]
+      },
+      onChange
+    );
+
+    wrapper
+      .find('ReRenderComponent')
+      .instance()
+      .reRenderDependencyAction();
+    expect(onChange.called).toEqual(true);
+  });
+
+  it('срабатывает onChange fetch', () => {
+    const onChange = sinon.spy();
+    const store = createStore(reducers);
+    store.dispatch(
+      registerFieldDependency('testForm', 'testField', [
+        {
+          type: 'reRender',
+          on: ['anotherTestField']
+        }
+      ])
+    );
+    store.dispatch(setModel('resolve', 'widgetName', { anotherValue: 'value' }));
+    const wrapper = setup(
+      store,
+      {
+        id: 'testField',
+        form: 'testForm',
+        dependency: [
+          {
+            type: 'fetch',
+            on: ['testField']
+          }
+        ]
+      },
+      onChange
+    );
+
+    wrapper
+      .find('ReRenderComponent')
+      .instance()
+      .fetchDependencyAction();
+    expect(onChange.called).toEqual(true);
+  });
 });
