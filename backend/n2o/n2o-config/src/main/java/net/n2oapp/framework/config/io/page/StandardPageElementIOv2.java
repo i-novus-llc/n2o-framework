5709a1ca278d1f7b1683d0ed9c5244f3309fa799
iryabov <iryabov@i-novus.ru>
2019-03-18 13:27:12 +0300

    [NNO-2988] [NF] Атрибуты в <regions>

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/io/page/StandardPageElementIOv2.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/io/page/StandardPageElementIOv2.java
@@ -32,22 +32,23 @@ public class StandardPageElementIOv2 implements NamespaceIO<N2oStandardPage> {
         p.attribute(e, "route", m::getRoute, m::setRoute);
         p.attribute(e, "layout", m::getLayout, m::setLayout);
         p.attribute(e, "modal-size", m::getModalSize, m::setModalSize);
-        p.anyChildren(e,"regions",m::getN2oRegions,m::setN2oRegions,p.anyOf(N2oRegion.class),regionDefaultNamespace);
+        p.anyChildren(e, "regions", m::getN2oRegions, m::setN2oRegions, p.anyOf(N2oRegion.class), regionDefaultNamespace);
+        p.childAnyAttributes(e, "regions", m::getLayoutExtAttributes, m::setLayoutExtAttributes);
         p.children(e, "actions", "action", m::getActions, m::setActions, ActionsBar::new, this::action);
-        p.childAttributeEnum(e,"actions", "generate", m::getActionGenerate, m::setActionGenerate, GenerateType.class);
+        p.childAttributeEnum(e, "actions", "generate", m::getActionGenerate, m::setActionGenerate, GenerateType.class);
         p.children(e, null, "toolbar", m::getToolbars, m::setToolbars, new ToolbarIO());
         p.extensionAttributes(e, m::getExtAttributes, m::setExtAttributes);
-}
+    }
 
     private void action(Element e, ActionsBar a, IOProcessor p) {
-        p.attribute(e,"id",a::getId,a::setId);
-        p.attribute(e,"name",a::getLabel,a::setLabel);
-        p.attribute(e,"widget-id",a::getWidgetId,a::setWidgetId);
-        p.attributeEnum(e,"model",a::getModel,a::setModel, ReduxModel.class);
-        p.attribute(e,"icon",a::getIcon,a::setIcon);
-        p.attribute(e,"hotkey",a::getHotkey,a::setHotkey);
-        p.attribute(e,"visible", a::getVisible,a::setVisible);
-        p.attribute(e,"enabled", a::getEnabled, a::setEnabled);
+        p.attribute(e, "id", a::getId, a::setId);
+        p.attribute(e, "name", a::getLabel, a::setLabel);
+        p.attribute(e, "widget-id", a::getWidgetId, a::setWidgetId);
+        p.attributeEnum(e, "model", a::getModel, a::setModel, ReduxModel.class);
+        p.attribute(e, "icon", a::getIcon, a::setIcon);
+        p.attribute(e, "hotkey", a::getHotkey, a::setHotkey);
+        p.attribute(e, "visible", a::getVisible, a::setVisible);
+        p.attribute(e, "enabled", a::getEnabled, a::setEnabled);
         p.anyChild(e, null, a::getAction, a::setAction, p.anyOf(N2oAction.class), actionDefaultNamespace);
     }
 
