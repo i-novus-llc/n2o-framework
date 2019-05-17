a5d30f326c91bbc094fe0c143179c160bfe28dca
I-NOVUS\dmusin <dmusin@i-novus.ru>
2019-05-17 19:08:56 +0300

    [NNO-3165] fix

--- frontend/n2o/src/components/errors/index.js
+++ frontend/n2o/src/components/errors/index.js
@@ -1,13 +1,10 @@
-import NotFoundPage from './NotFoundPage';
-import ForbiddenPage from './ForbiddenPage';
-import ServerErrorPage from './ServerErrorPage';
-
-export { NotFoundPage, ForbiddenPage, ServerErrorPage };
+import React from 'react';
+import ErrorPage from './ErrorPage';
 
 export default ({
-  notFound = NotFoundPage,
-  forbidden = ForbiddenPage,
-  serverError = ServerErrorPage,
+  notFound = () => <ErrorPage status={404} error="Страница не найдена" />,
+  forbidden = () => <ErrorPage status={403} error="Доступ запрещён" />,
+  serverError = () => <ErrorPage status={500} error="Сервер не доступен" />,
 } = {}) => [
   {
     status: 403,
