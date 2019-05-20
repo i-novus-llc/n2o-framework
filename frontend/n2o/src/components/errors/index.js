bac14448bdb95c5064e56c10e2a0148ee530d30b
I-NOVUS\dmusin <dmusin@i-novus.ru>
2019-05-20 11:24:28 +0300

    [NNO-3165] fix

--- frontend/n2o/src/components/errors/index.js
+++ frontend/n2o/src/components/errors/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ErrorPage from './ErrorPage';
 
-export default ({
-  notFound = () => <ErrorPage status={404} error="Страница не найдена" />,
-  forbidden = () => <ErrorPage status={403} error="Доступ запрещён" />,
-  serverError = () => <ErrorPage status={500} error="Сервер не доступен" />,
-} = {}) => [
-  {
-    status: 403,
-    path: '/403',
-    component: forbidden,
-  },
-  {
-    status: 404,
-    path: '/404',
-    component: notFound,
-  },
-  {
-    status: 500,
-    path: '/500',
-    component: serverError,
-  },
-];
+const NotFoundPage = () => (
+  <ErrorPage status={404} error="Страница не найдена" />
+);
+const ForbiddenPage = () => <ErrorPage status={403} error="Доступ запрещён" />;
+const ServerErrorPage = () => (
+  <ErrorPage status={500} error="Сервер не доступен" />
+);
+
+const createErrorPages = ({
+  notFound = NotFoundPage,
+  forbidden = ForbiddenPage,
+  serverError = ServerErrorPage,
+} = {}) => {
+  return [
+    {
+      status: 403,
+      path: '/403',
+      component: forbidden,
+    },
+    {
+      status: 404,
+      path: '/404',
+      component: notFound,
+    },
+    {
+      status: 500,
+      path: '/500',
+      component: serverError,
+    },
+  ];
+};
+
+export default createErrorPages;
