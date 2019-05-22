fd1a529dacd653a0e0313bb54153130e6085b075
I-NOVUS\dmusin <dmusin@i-novus.ru>
2019-05-22 15:06:37 +0300

    [NNO-3172] стили + story + test + docs

--- frontend/n2o/src/components/controls/Slider/Slider.jsx
+++ frontend/n2o/src/components/controls/Slider/Slider.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
+import cx from 'classnames';
 import BaseSlider, { createSliderWithTooltip } from 'rc-slider';
-import { stringConverter } from './untils';
+import { stringConverter, prepareStyle } from './utils';
 import { propTypes, defaultProps } from './allProps';
-import 'rc-slider/assets/index.css';
+/**
+ * Компонент Slider
+ * @reactProps {boolean} multiple - Множественный выбор
+ * @reactProps {boolean} showTooltip - Показать тултип
+ * @reactProps {string} tooltipPlacement - Позиция тултипа
+ * @reactProps {string} tooltipFormatter - Форматированный вывод тултипа
+ * @reactProps {boolean} vertical - Отобразить slider вертикально
+ * @reactProps {boolean} style - Стили root компонента
+ * @reactProps {boolean} className - Дополнительный класс
+ * @reactProps {string|number} step - шаг ползунка
+ * @reactProps {boolean} disabled - Нередактаруем
+ * @reactProps {boolean} dots - Показать шкалу
+ * @reactProps {number|string} min - Начало шкалы
+ * @reactProps {number|string} max - Конец шкалы
+ * @reactProps {object} marks - Подписи к шкале
+ * @reactProps {boolean} pushable - В мульти режиме блокирует смену несколькох ползунков
+ * @reactProps {object} trackStyle - стиль трека
+ * @reactProps {object} railStyle - стиль непройденной части шкалы
+ * @reactProps {object} dotStyle - стиль шкалы
+ * @returns {*}
+ * @constructor
+ */
+function Slider(props) {
+  const {
+    multiple,
+    showTooltip,
+    tooltipPlacement,
+    tooltipFormatter,
+    vertical,
+    style,
+    className,
+    ...rest
+  } = props;
 
-function Slider({
-  multiple,
-  showTooltip,
-  tooltipPlacement,
-  tooltipFormatter,
-  ...rest
-}) {
   const expressionFn = tooltipFormatter
     ? value => new Function('', 'return ' + tooltipFormatter).bind(value)()
     : value => value;
@@ -26,14 +52,23 @@ function Slider({
 
   return (
     <RenderSlider
+      className={cx('n2o-slider', className)}
       tipProps={tooltipProps}
       tipFormatter={expressionFn}
+      vertical={vertical}
+      style={prepareStyle(vertical, style)}
       {...rest}
     />
   );
 }
 
-const WrapSlider = stringConverter(['value', 'max', 'min', 'step'])(Slider);
+const WrapSlider = stringConverter([
+  'value',
+  'max',
+  'min',
+  'step',
+  'stoppingValue',
+])(Slider);
 
 WrapSlider.propTypes = propTypes;
 WrapSlider.defaultProps = defaultProps;
--- frontend/n2o/src/components/controls/Slider/Slider.story.js
+++ frontend/n2o/src/components/controls/Slider/Slider.story.js
@@ -106,8 +106,135 @@ stories
           defaultValue={[20, '50.5']}
           step="5.5"
           dots={true}
-          onChange={console.log}
         />
+        <div>pushable=true</div>
+        <Slider
+          multiple={true}
+          min={0}
+          max={100}
+          defaultValue={[20, '50.5']}
+          pushable={true}
+          step="5.5"
+          dots={true}
+        />
+        <div>value=[10, 50, 90]</div>
+        <Slider
+          multiple={true}
+          min={0}
+          max={100}
+          defaultValue={[10, 50, 90]}
+          pushable={true}
+          step="5.5"
+          dots={true}
+        />
+      </Fragment>
+    );
+  })
+  .add('Вертикальный режим', () => {
+    return (
+      <Fragment>
+        <h4>vertical=true</h4>
+        <div style={{ height: '400px' }}>
+          <Slider
+            marks={{ '0': '0℃', '100': '100℃' }}
+            max={100}
+            min={0}
+            step={10}
+            vertical={true}
+          />
+          <Slider
+            marks={{ '0': '0℃', '100': '100℃' }}
+            max={100}
+            min={0}
+            step={10}
+            dots={true}
+            multiple={true}
+            showTooltip={true}
+            defaultValue={[20, 60]}
+            vertical={true}
+          />
+        </div>
+      </Fragment>
+    );
+  })
+  .add('Кастомные стили', () => {
+    return (
+      <Fragment>
+        <h4>Стиль трекера</h4>
+        <div>
+          trackStyle=<code>{`{ backgroundColor: 'blue' }`}</code>
+        </div>
+        <Slider
+          marks={{ '0': '0℃', '100': '100℃' }}
+          trackStyle={{ backgroundColor: 'blue' }}
+          max={100}
+          min={0}
+          step={10}
+          defaultValue={50}
+        />
+        <br />
+        <div>
+          trackStyle=
+          <code>{`[{ backgroundColor: 'red', height: '6px' }]`}</code>
+          multiple=<code>true</code>
+        </div>
+        <Slider
+          marks={{
+            '0': '0K',
+            '100': '100K',
+            '20': '20K',
+            '40': '40K',
+            '60': '60K',
+            '80': '80K',
+          }}
+          trackStyle={[{ backgroundColor: 'red', height: '6px' }]}
+          multiple={true}
+          defaultValue={[20, 60]}
+          max={100}
+          min={0}
+          step={10}
+        />
+        <br />
+        <div>
+          railStyle=<code>{`{ backgroundColor: 'orange' }`}</code>
+        </div>
+        <Slider
+          railStyle={{ backgroundColor: 'blue' }}
+          max={100}
+          min={0}
+          step={10}
+          defaultValue={50}
+        />
+        <br />
+        <h4>Стиль ползунка</h4>
+        <div>
+          handleStyle=
+          <code>{`{ backgroundColor: 'blue', border: '2px solid red' }`}</code>
+        </div>
+        <Slider
+          marks={{ '0': '0℃', '100': '100℃' }}
+          handleStyle={{ backgroundColor: 'blue', border: '2px solid red' }}
+          max={100}
+          min={0}
+          step={10}
+          defaultValue={50}
+        />
+        <br />
+        <h4>Стиль меток</h4>
+        <div>
+          dotStyle=
+          <code>{`{ backgroundColor: 'blue', border: '2px solid red' }`}</code>
+          dots=<code>true</code>
+        </div>
+        <Slider
+          dots={true}
+          dotStyle={{ backgroundColor: 'blue', border: '2px solid red' }}
+          max={100}
+          min={0}
+          step={10}
+          defaultValue={50}
+        />
+        <br />
       </Fragment>
     );
   });
