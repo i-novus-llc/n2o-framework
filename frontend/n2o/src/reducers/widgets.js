8ddcd6c5a67ebe01f8ba32a6cb177a2431a951d0
Sergey <lalkatralka1488>
2019-05-31 17:39:04 +0300

    [NNO-3021] - починил selectedId

--- frontend/n2o/src/reducers/widgets.js
+++ frontend/n2o/src/reducers/widgets.js
@@ -1,4 +1,4 @@
-import { omit, mapValues, isEmpty, isNumber } from 'lodash';
+import { omit, mapValues, isEmpty, isNaN } from 'lodash';
 import merge from 'deepmerge';
 import {
   REGISTER,
@@ -137,7 +137,9 @@ function resolve(state = widgetState, action) {
     case RESET_STATE:
       return Object.assign({}, state, { isInit: false });
     case SET_TABLE_SELECTED_ID:
-      return Object.assign({}, state, { selectedId: resolveSelectedId(action.payload.value)});
+      return Object.assign({}, state, {
+        selectedId: resolveSelectedId(action.payload.value),
+      });
     case SET_ACTIVE:
       return Object.assign({}, state, { isActive: true });
     default:
@@ -146,7 +148,7 @@ function resolve(state = widgetState, action) {
 }
 
 function resolveSelectedId(selectedId) {
-  if (selectedId !== '' && isNumber(+selectedId)) {
+  if (selectedId !== '' && !isNaN(+selectedId)) {
     selectedId = +selectedId;
   }
 
