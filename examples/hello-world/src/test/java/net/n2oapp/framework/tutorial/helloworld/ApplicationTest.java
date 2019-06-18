561b0d83d876f723f5f90c448a7f62878b46a34d
ilebedenko <ilebedenko@i-novus.ru>
2019-06-18 17:58:51 +0300

    [CF][NNO-3238] - добавил тест для helloworld

---
+++ examples/hello-world/src/test/java/net/n2oapp/framework/tutorial/helloworld/ApplicationTest.java
@@ -0,0 +1,16 @@
+package net.n2oapp.framework.tutorial.helloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
