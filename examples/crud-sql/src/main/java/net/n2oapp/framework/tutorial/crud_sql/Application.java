47de04534452bc6f1fc0c734a64a7fd164ac2bfb
ilebedenko <ilebedenko@i-novus.ru>
2019-06-20 15:59:23 +0300

    [CF][NNO-3238] - пример crud-sql обновлен

--- examples/crud-sql/src/main/java/net/n2oapp/framework/tutorial/crud_sql/Application.java
+++ examples/crud-sql/src/main/java/net/n2oapp/framework/tutorial/crud_sql/Application.java
@@ -1,6 +1,5 @@
-package net.n2oapp.framework.tutorial.crud.sql;
+package net.n2oapp.framework.tutorial.crud_sql;
 
-import net.n2oapp.framework.engine.sql.jdbc.EnableRoutingDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
