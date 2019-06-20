f84e07972164b9852f79df5c422c639f0a5c7574
ilebedenko <ilebedenko@i-novus.ru>
2019-06-20 12:52:52 +0300

    [CF][NNO-3238] - обновлен пример master-detail

--- examples/master-detail/src/main/java/net/n2oapp/framework/tutorial/master_detail/Application.java
+++ examples/master-detail/src/main/java/net/n2oapp/framework/tutorial/master_detail/Application.java
@@ -1,13 +1,11 @@
-package net.n2oapp.framework.examples.master_detail;
+package net.n2oapp.framework.tutorial.master_detail;
 
-import net.n2oapp.framework.engine.sql.jdbc.EnableRoutingDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRoutingDataSource
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
