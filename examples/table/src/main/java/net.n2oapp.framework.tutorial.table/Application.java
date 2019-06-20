94995254c66a2e03a554ef01ca832c7b752aa907
ilebedenko <ilebedenko@i-novus.ru>
2019-06-20 10:39:13 +0300

    [NF][NNO-3238] - добавлен пример с таблицей

---
+++ examples/table/src/main/java/net.n2oapp.framework.tutorial.table/Application.java
@@ -0,0 +1,12 @@
+package net.n2oapp.framework.tutorial.table;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
