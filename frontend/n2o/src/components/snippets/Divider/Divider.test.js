a2fe32b99f2d9edf2c9d1ae9ed3c07514d16c3c9
asokolov <asokolov@i-novus.ru>
2019-08-06 17:36:54 +0300

    Добавлен компонент Divider

---
+++ frontend/n2o/src/components/snippets/Divider/Divider.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Divider } from './Divider';
+import { mount, shallow } from 'enzyme/build';
+
+const setup = propOverrides => {
+  const props = Object.assign(
+    {
+      children: 'title',
+      // use this to assign some default props
+    },
+    propOverrides
+  );
+
+  const wrapper = mount(<Divider {...props} />);
+
+  return wrapper;
+};
+
+describe('<Divider />', () => {
+  it('проверяет создание элемента Divider', () => {
+    let wrapper = shallow(<Divider />);
+    expect(wrapper.find('.divider-h').exists()).toBeTruthy();
+    expect(wrapper.find('.divider_horizontal').exists()).toBeTruthy();
+  });
+  it('проверяет наличие children', () => {
+    let wrapper = setup();
+    expect(wrapper.find('span').exists()).toBeTruthy();
+  });
+  it('проверка на соответствие заголовка = title', () => {
+    let wrapper = setup();
+    expect(wrapper.text()).toBe('title');
+  });
+  it('snapshot', () => {
+    let wrapper = setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
