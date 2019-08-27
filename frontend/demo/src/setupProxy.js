8e5c200e17e46de81321fd3c4a914136f1647246
aahmetov <aahmetov@i-novus.ru>
2019-08-27 14:28:56 +0300

    [NNO-3404] - удален стиль n2o-checkbox

--- frontend/demo/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(proxy('/n2o/data', { target: 'https://n2o.i-novus.ru/dev/', changeOrigin: true }));
-    app.use(proxy('/n2o', { target: 'https://n2o.i-novus.ru/dev/', changeOrigin: true }));
-};
