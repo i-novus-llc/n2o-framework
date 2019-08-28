8625496cfd5af50eae1e2143d6acb309ebbf5f6b
emamoshin <emamoshin@i-novus.ru>
2019-08-28 16:08:45 +0300

    rf добавил прокси файлы

---
+++ frontend/demo/src/setupProxy.js
@@ -0,0 +1,10 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(proxy('/n2o',
+        {
+            target: "https://n2o.i-novus.ru/dev/",
+            changeOrigin: true
+        }
+    ));
+};
