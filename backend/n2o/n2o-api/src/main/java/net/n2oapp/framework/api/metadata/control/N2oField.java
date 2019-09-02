7c7d836e15ac788b53044ff1b6a066c7bef1b8cc
rgalina <rgalina@i-novus.ru>
2019-09-02 10:46:15 +0300

    [NNO-3373] [nf] Xml Зависимость обновления модели

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/control/N2oField.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/control/N2oField.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.aware.IdAware;
+import net.n2oapp.framework.api.metadata.global.dao.N2oPreFilter;
 import net.n2oapp.framework.api.metadata.global.dao.validation.N2oValidation;
 import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.N2oToolbar;
 
@@ -88,6 +89,14 @@ public abstract class N2oField extends N2oComponent implements IdAware {
     }
 
     public static class FetchDependency extends Dependency {
+    }
+
+    @Getter
+    @Setter
+    public static class FetchValueDependency extends Dependency {
+        private String queryId;
+        private String valueFieldId;
+        private N2oPreFilter[] preFilters;
     }
 
     @Getter
