4ddc92e9d8195e2e780f7cbb55a6afebecffb74b
emamoshin <emamoshin@i-novus.ru>
2019-09-05 15:35:15 +0300

    [rf] удалил antd-n2o-ui и исправил все package.json

--- frontend/antd-n2o-ui/src/controls/InputNumber/InputNumber.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { InputNumber as BaseNumberBase } from "antd";
-
-export default ({ style, ...rest }) => (
-  <BaseNumberBase style={{ width: "100%", ...style }} {...rest} />
-);
