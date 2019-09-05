e707656d61ffd4eea6ed0134b5389dce74d6bafe
sliskov <sliskov@i-novus.ru>
2019-09-05 17:07:51 +0300

    [NNO-3359] - переделал компонент под InputSelect

--- frontend/n2o/src/components/controls/AutoComplete/AutoComplete.jsx
+++ frontend/n2o/src/components/controls/AutoComplete/AutoComplete.jsx
@@ -1,105 +1,291 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  compose,
-  withState,
-  lifecycle,
-  setDisplayName,
-  withHandlers,
-  withProps,
-  defaultProps,
-} from 'recompose';
+import { find, isEqual, isFunction, get, filter, includes } from 'lodash';
+import { compose } from 'recompose';
+import listContainer from '../listContainer';
+import onClickOutside from 'react-onclickoutside';
 
-import ReactAutocomplete from 'react-autocomplete';
-import AutoCompleteItem from './AutoCompleteItem';
+import InputContent from '../InputSelect/InputContent';
 
-import { get, isFunction } from 'lodash';
-import cn from 'classnames';
+import { Manager, Reference, Popper } from 'react-popper';
+import { MODIFIERS } from '../DatePicker/utils';
+import PopupList from '../InputSelect/PopupList';
+import Alert from '../../snippets/Alerts/Alert';
 
-function AutoComplete({
-  value,
-  onChange,
-  options,
-  getItemValue,
-  shouldItemRender,
-  renderItem,
-  className,
-  style,
-  inputProps,
-  onSelect,
-  ...rest
-}) {
-  return (
-    <div className={cn('n2o-autocomplete', className)}>
-      <ReactAutocomplete
-        value={value}
-        onChange={onChange}
-        items={options}
-        getItemValue={getItemValue}
-        shouldItemRender={shouldItemRender}
-        wrapperStyle={style}
-        renderItem={renderItem}
-        inputProps={inputProps}
-        onSelect={onSelect}
-      />
-    </div>
-  );
+class AutoComplete extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+      value: [],
+      activeValueId: null,
+    };
+
+    this._input = null;
+    this._textarea = null;
+
+    this.setInputRef = this.setInputRef.bind(this);
+    this._setIsExpanded = this._setIsExpanded.bind(this);
+    this._handleClick = this._handleClick.bind(this);
+    this.calcPopperWidth = this.calcPopperWidth.bind(this);
+    this.setSelectedListRef = this.setSelectedListRef.bind(this);
+    this.setTextareaRef = this.setTextareaRef.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this._setActiveValueId = this._setActiveValueId.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { value } = this.props;
+
+    if (prevProps.value !== value) {
+      this.setState({ value: [value] });
+    }
+  }
+
+  handleClickOutside() {
+    const { onBlur } = this.props;
+    const { isExpanded } = this.state;
+    if (isExpanded) {
+      this._setIsExpanded(false);
+      onBlur();
+    }
+  }
+
+  calcPopperWidth() {
+    const { _input, _textarea } = this;
+    const { popupAutoSize } = this.props;
+    if ((_input || _textarea) && !popupAutoSize) {
+      return _input
+        ? _input.getBoundingClientRect().width
+        : _textarea.getBoundingClientRect().width;
+    }
+  }
+
+  _setIsExpanded(isExpanded) {
+    this.setState({ isExpanded });
+  }
+
+  setInputRef(popperRef) {
+    return r => {
+      this._input = r;
+      popperRef(r);
+    };
+  }
+
+  setTextareaRef(poperRef) {
+    return r => {
+      this._textarea = r;
+      poperRef(r);
+    };
+  }
+
+  setSelectedListRef(selectedList) {
+    this._selectedList = selectedList;
+  }
+
+  _handleClick() {
+    this._setIsExpanded(true);
+  }
+
+  onFocus() {
+    const { openOnFocus } = this.props;
+
+    if (openOnFocus) {
+      this._setIsExpanded(true);
+    }
+  }
+
+  onClick() {
+    this._setIsExpanded(true);
+  }
+
+  onChange(value) {
+    this.setState({ value: [value] });
+  }
+
+  onBlur() {
+    const { onBlur } = this.props;
+
+    if (isFunction(onBlur)) {
+      onBlur();
+    }
+  }
+
+  onSelect(item) {
+    const { valueFieldId, onChange, closePopupOnSelect } = this.props;
+    const value = get(item, valueFieldId);
+
+    this.setState(
+      {
+        value: [value],
+      },
+      () => {
+        if (closePopupOnSelect) {
+          this._setIsExpanded(false);
+        }
+
+        onChange(value);
+      }
+    );
+  }
+
+  _setActiveValueId(activeValueId) {
+    this.setState({ activeValueId });
+  }
+
+  render() {
+    const { isExpanded, value, activeValueId } = this.state;
+    const {
+      loading,
+      className,
+      valueFieldId,
+      labelFieldId,
+      iconFieldId,
+      disabled,
+      placeholder,
+      multiSelect,
+      disabledValues,
+      imageFieldId,
+      groupFieldId,
+      hasCheckboxes,
+      format,
+      badgeFieldId,
+      badgeColorFieldId,
+      onScrollEnd,
+      expandPopUp,
+      style,
+      alerts,
+      flip,
+      autoFocus,
+      options,
+    } = this.props;
+    const needAddFilter = !find(
+      this.state.value,
+      item => item[labelFieldId] === this.state.input
+    );
+
+    const filteredOptions = filter(options, item =>
+      includes(item[labelFieldId], value)
+    );
+
+    return (
+      <div>
+        <Manager>
+          <Reference>
+            {({ ref }) => (
+              <div style={{ position: 'relative' }}>
+                <InputContent
+                  options={filteredOptions}
+                  setRef={this.setInputRef(ref)}
+                  onInputChange={this.onChange}
+                  setActiveValueId={this._setActiveValueId}
+                  closePopUp={() => this._setIsExpanded(false)}
+                  openPopUp={() => this._setIsExpanded(true)}
+                  selected={value}
+                  value={value}
+                  onFocus={this.onFocus}
+                  onClick={this.onClick}
+                  onBlur={this.onBlur}
+                  isExpanded={isExpanded}
+                  setTextareaRef={this.setTextareaRef(ref)}
+                  setSelectedListRef={this.setSelectedListRef}
+                  valueFieldId={valueFieldId}
+                  activeValueId={activeValueId}
+                  onSelect={this.onSelect}
+                  loading={loading}
+                  disabled={disabled}
+                  disabledValues={disabledValues}
+                  placeholder={placeholder}
+                  iconFieldId={iconFieldId}
+                  imageFieldId={imageFieldId}
+                  labelFieldId={labelFieldId}
+                  autoFocus={autoFocus}
+                  _textarea={this._textarea}
+                  _selectedList={this._selectedList}
+                />
+              </div>
+            )}
+          </Reference>
+          {isExpanded && (
+            <Popper
+              placement="bottom-start"
+              modifiers={MODIFIERS}
+              positionFixed={true}
+            >
+              {({ ref, style, placement }) => (
+                <div
+                  ref={ref}
+                  style={{
+                    ...style,
+                    minWidth: this.calcPopperWidth(),
+                    maxWidth: 600,
+                  }}
+                  data-placement={placement}
+                  className="n2o-pop-up"
+                >
+                  <PopupList
+                    isExpanded={isExpanded}
+                    activeValueId={activeValueId}
+                    setActiveValueId={this._setActiveValueId}
+                    onScrollEnd={onScrollEnd}
+                    needAddFilter={needAddFilter}
+                    options={filteredOptions}
+                    valueFieldId={valueFieldId}
+                    labelFieldId={labelFieldId}
+                    iconFieldId={iconFieldId}
+                    imageFieldId={imageFieldId}
+                    badgeFieldId={badgeFieldId}
+                    badgeColorFieldId={badgeColorFieldId}
+                    onSelect={this.onSelect}
+                    selected={value}
+                    disabledValues={disabledValues}
+                    groupFieldId={groupFieldId}
+                    hasCheckboxes={hasCheckboxes}
+                    format={format}
+                  >
+                    <div className="n2o-alerts">
+                      {alerts &&
+                        alerts.map(alert => (
+                          <Alert
+                            key={alert.id}
+                            onDismiss={() => this.props.onDismiss(alert.id)}
+                            {...alert}
+                          />
+                        ))}
+                    </div>
+                  </PopupList>
+                </div>
+              )}
+            </Popper>
+          )}
+        </Manager>
+      </div>
+    );
+  }
 }
 
 AutoComplete.propTypes = {
-  value: PropTypes.string,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
   onChange: PropTypes.func,
-  options: PropTypes.array,
-  style: PropTypes.object,
-  labelFieldId: PropTypes.string,
+  closePopupOnSelect: PropTypes.bool,
 };
 
 AutoComplete.defaultProps = {
-  options: [],
-  style: {},
-  labelFieldId: 'label',
+  onFocus: () => {},
+  onBlur: () => {},
+  onChange: () => {},
+  closePopupOnSelect: true,
 };
 
 const enhance = compose(
-  setDisplayName('AutoComplete'),
-  defaultProps({
-    labelFieldId: 'label',
-  }),
-  withProps(() => ({
-    inputProps: {
-      className: 'form-control',
-    },
-  })),
-  withState('value', 'setValue', ''),
-  withHandlers({
-    getItemValue: ({ labelFieldId }) => item => get(item, labelFieldId, ''),
-    renderItem: ({ labelFieldId }) => (item, isHighlighted) => (
-      <AutoCompleteItem
-        value={get(item, labelFieldId)}
-        isHighlighted={isHighlighted}
-      />
-    ),
-    shouldItemRender: ({ labelFieldId }) => (item, value) =>
-      !!~get(item, labelFieldId, '')
-        .toLowerCase()
-        .indexOf(value.toLowerCase()),
-    onChange: ({ onChange, setValue }) => event => {
-      const value = get(event, 'target.value', '');
-
-      setValue(value);
-
-      if (isFunction(onChange)) {
-        onChange(value);
-      }
-    },
-    onSelect: ({ onChange, setValue }) => value => {
-      setValue(value);
-
-      if (isFunction(onChange)) {
-        onChange(value);
-      }
-    },
-  })
+  listContainer,
+  onClickOutside
 );
 
 export { AutoComplete };
--- frontend/n2o/src/components/controls/AutoComplete/AutoComplete.story.js
+++ frontend/n2o/src/components/controls/AutoComplete/AutoComplete.story.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import withForm from 'N2oStorybook/decorators/withForm';
 import AutoComplete from './AutoComplete';
 
 const stories = storiesOf('Контролы/AutoComplete', module);
 
+const form = withForm({ src: 'AutoComplete' });
+
 const options = [
   {
     id: 1,
-    label: 'test1',
+    name: 'test1',
   },
   {
     id: 2,
-    label: 'test2',
+    name: 'test2',
   },
   {
     id: 3,
-    label: 'test3',
+    name: 'test3',
   },
 ];
 
 stories.add('Компонент', () => {
-  return <AutoComplete options={options} />;
+  return (
+    <AutoComplete valueFieldId="name" labelFieldId="name" options={options} />
+  );
 });
