6c32b66f2abdb619326a0ed2c5bc070beeb98de4
aahmetov <aahmetov@i-novus.ru>
2019-09-17 13:04:17 +0300

    [NNO-3440] - Покрытие сниппетов

--- frontend/n2o/src/components/snippets/Typography/ContentEditable.test.js
+++ frontend/n2o/src/components/snippets/Typography/ContentEditable.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import sinon from 'sinon';
 import ContentEditable from './ContentEditable';
 
 const setup = props => {
@@ -7,16 +6,8 @@ const setup = props => {
 };
 
 describe('Тесты ContentEditable', () => {
-  // it('componentDidUpdate', () => {
-  //   const focus = sinon.spy();
-  //   const wrapper = setup({ focus: focus });
-  //   console.log(wrapper.debug())
-  //   expect(wrapper.props().editable).toBe(false);
-  //   wrapper.setProps({ editable: true});
-  //   expect(focus.calledOnce).toEqual(true); ///// ??????????????
-  // });
   it('editable = true', () => {
-    const wrapper = setup({ editable: true});
+    const wrapper = setup({ editable: true });
     expect(wrapper.find('.editable').exists()).toEqual(true);
   });
   it('editable = false', () => {
