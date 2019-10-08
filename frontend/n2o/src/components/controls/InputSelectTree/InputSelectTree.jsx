c60a7b98d5d04b22cbe5ab2330bbc10c7c19dbb9
Мамошин Егор Андреевич <emamoshin@i-novus.ru>
2019-10-08 10:32:09 +0000

    Merge branch 'bugfix/NNO-3458' into 'develop'
    bugfix/NNO-3458 добавил title к item в InputSelectTree
    
    Closes NNO-3458
    
    See merge request open/n2o-framework!655
    
    (cherry picked from commit f1f57925b24c075897b047b1a86dc5393f2d8f79)
    
    6cbfe944 bugfix/NNO-3458 добавил title к item в InputSelectTree
    a7d958b8 bf NNO-3458  исправил зашитость
    33640978 Merge remote-tracking branch 'origin/develop' into bugfix/NNO-3458

--- frontend/n2o/src/components/controls/InputSelectTree/InputSelectTree.jsx
+++ frontend/n2o/src/components/controls/InputSelectTree/InputSelectTree.jsx
@@ -1,26 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import TreeSelect from 'rc-tree-select';
-import ReactDOM from 'react-dom';
 import {
   difference,
   filter as filterF,
-  eq,
   every,
   find,
   isArray,
-  isNumber,
-  isString,
   isEmpty,
   keys,
   forEach,
   map,
-  reduce,
   memoize,
   some,
-  uniq,
-  uniqBy,
-  unionWith,
-  isEqual,
 } from 'lodash';
 import Icon from '../../snippets/Icon/Icon';
 import InlineSpinner from '../../snippets/Spinner/InlineSpinner';
@@ -380,6 +371,8 @@ function InputSelectTree({
       treeData={createTree(data)}
       filterTreeNode={handlerFilter}
       treeNodeFilterProp={labelFieldId}
+      treeNodeLabelProp={labelFieldId}
+      maxTagTextLength="10"
       removeIcon={clearIcon}
       clearIcon={clearIcon}
       onChange={handleChange}
