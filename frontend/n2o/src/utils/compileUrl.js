ec7894245c0268d8631e0e713ef48fd86985af7e
sliskov <sliskov@i-novus.ru>
2019-10-23 16:32:05 +0300

    [BF][NNO-3505] - исправил обработку пути с доменом и портом

--- frontend/n2o/src/utils/compileUrl.js
+++ frontend/n2o/src/utils/compileUrl.js
@@ -1,4 +1,4 @@
-import { each, isEmpty } from 'lodash';
+import { each, isEmpty, get } from 'lodash';
 import pathToRegexp from 'path-to-regexp';
 import queryString from 'query-string';
 import linkResolver from './linkResolver';
@@ -18,17 +18,19 @@ export default function compileUrl(
   state,
   { extraPathParams = {}, extraQueryParams = {} } = {}
 ) {
+  const domainWithProtocol = get(url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i), 0);
+  const urlForCompile = url.split(domainWithProtocol);
   const pathParams = getParams(pathMapping, state);
   const queryParams = getParams(queryMapping, state);
-  let compiledUrl = url;
+  let compiledUrl = domainWithProtocol && get(urlForCompile, '[1]') ? urlForCompile[1] : url;
   if (!isEmpty(pathParams)) {
-    compiledUrl = pathToRegexp.compile(url)({
+    compiledUrl = (domainWithProtocol || '') + pathToRegexp.compile(compiledUrl)({
       ...pathParams,
       ...extraPathParams,
     });
   }
   if (!isEmpty(queryParams)) {
-    compiledUrl = `${compiledUrl}?${queryString.stringify({
+    compiledUrl = (domainWithProtocol || '') + `${compiledUrl}?${queryString.stringify({
       ...queryParams,
       ...extraQueryParams,
     })}`;
