03154ea2527854496ab6dd7d79fc3fa0d48a7b8f
aahmetov <aahmetov@i-novus.ru>
2019-11-15 14:42:46 +0300

    [NNO-3561] - реализация CodeViewer

--- frontend/n2o-framework/src/components/controls/CodeViewer/CodeViewer.jsx
+++ frontend/n2o-framework/src/components/controls/CodeViewer/CodeViewer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import compose from 'recompose/compose';
+import withState from 'recompose/withState';
+import withHandlers from 'recompose/withHandlers';
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism, darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import cx from 'classnames';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 /**
  * Компонент CodeViewer
@@ -11,6 +16,8 @@ import { prism, darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
  * @reactProps {string} theme - цвет фона
  * @reactProps {boolean} showLineNumbers - отображать нумерацию строк
  * @reactProps {number} startingLineNumber - если включен showLineNumbers, нумерация строк начнется c указанного числа
+ * @reactProps {string} title - закголовок CodeViewer
+ * @reactProps {string} text - описание CodeViewer
  */
 
 const colorTheme = {
@@ -18,15 +25,48 @@ const colorTheme = {
   DARK: 'dark',
 };
 
-function CodeViewer({ visible, children, theme, ...rest }) {
+function CodeViewer({
+  visible,
+  children,
+  theme,
+  copied,
+  handleCopy,
+  handleShow,
+  show,
+  title,
+  text,
+  ...rest
+}) {
   return (
     visible && (
-      <SyntaxHighlighter
-        style={theme === colorTheme.DARK ? darcula : prism}
-        {...rest}
-      >
-        {children}
-      </SyntaxHighlighter>
+      <div className="n2o-code-viewer">
+        <h4>{title}</h4>
+        <p>{text}</p>
+        <div className="code-viewer-actions">
+          <CopyToClipboard text={children} onCopy={handleCopy}>
+            <i
+              className={cx(
+                'code-viewer-actions-copy',
+                copied
+                  ? 'n2o-icon fa fa-check text-success'
+                  : 'n2o-icon fa fa-clipboard'
+              )}
+            />
+          </CopyToClipboard>
+          <i
+            className="n2o-icon code-viewer-actions-code fa fa-code"
+            onClick={handleShow}
+          />
+        </div>
+        {show ? (
+          <SyntaxHighlighter
+            style={theme === colorTheme.DARK ? darcula : prism}
+            {...rest}
+          >
+            {children}
+          </SyntaxHighlighter>
+        ) : null}
+      </div>
     )
   );
 }
@@ -56,11 +96,36 @@ CodeViewer.propTypes = {
    * Если включен showLineNumbers, нумерация строк начнется c указанного числа
    */
   startingLineNumber: PropTypes.number,
+  /**
+   * Заголовок CodeViewer
+   */
+  title: PropTypes.string,
+  /**
+   * Описание CodeViewer
+   */
+  text: PropTypes.string,
 };
 
 CodeViewer.defaultProps = {
   visible: true,
   theme: colorTheme.LIGHT,
+  title: '',
+  text: '',
 };
 
-export default CodeViewer;
+export { CodeViewer };
+export default compose(
+  withState('show', 'setShow', ({ visible }) => visible),
+  withState('copied', 'setCopy', false),
+  withHandlers({
+    handleCopy: ({ setCopy }) => {
+      setCopy(true);
+      setTimeout(() => {
+        setCopy(false);
+      }, 2000);
+    },
+    handleShow: ({ show, setShow }) => {
+      setShow(!show);
+    },
+  })
+)(CodeViewer);
