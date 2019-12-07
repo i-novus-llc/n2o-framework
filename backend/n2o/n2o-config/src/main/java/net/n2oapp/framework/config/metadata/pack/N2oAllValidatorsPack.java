9b86ffdde3a63a45fa38aeb6fd66e022451dfa5d
Старцев Егор <estartcev@i-novus.ru>
2019-12-07 12:41:55 +0300

    [NNO-3264] Добавлена валидация id в field

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/pack/N2oAllValidatorsPack.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/pack/N2oAllValidatorsPack.java
@@ -4,10 +4,16 @@ import net.n2oapp.framework.api.pack.MetadataPack;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.metadata.compile.header.SimpleHeaderValidator;
 import net.n2oapp.framework.config.metadata.compile.menu.SimpleMenuValidator;
+import net.n2oapp.framework.config.metadata.validation.standard.fieldset.FieldSetColumnValidator;
+import net.n2oapp.framework.config.metadata.validation.standard.fieldset.FieldSetRowValidator;
+import net.n2oapp.framework.config.metadata.validation.standard.fieldset.FieldSetValidator;
 import net.n2oapp.framework.config.metadata.validation.standard.object.ObjectValidator;
 import net.n2oapp.framework.config.metadata.validation.standard.page.PageValidator;
 import net.n2oapp.framework.config.metadata.validation.standard.query.QueryValidator;
+import net.n2oapp.framework.config.metadata.validation.standard.widget.FormValidator;
+import net.n2oapp.framework.config.metadata.validation.standard.widget.TableValidator;
 import net.n2oapp.framework.config.metadata.validation.standard.widget.WidgetValidator;
+import org.springframework.core.env.PropertyResolver;
 
 /**
  * Набор стандартных валидаторов метаданных
@@ -15,7 +21,9 @@ import net.n2oapp.framework.config.metadata.validation.standard.widget.WidgetVal
 public class N2oAllValidatorsPack implements MetadataPack<N2oApplicationBuilder> {
     @Override
     public void build(N2oApplicationBuilder b) {
+        PropertyResolver prop = b.getEnvironment().getSystemProperties();
         b.validators(new ObjectValidator(), new QueryValidator(), new PageValidator(),
-                new SimpleHeaderValidator(), new SimpleMenuValidator(), new WidgetValidator());
+                new SimpleHeaderValidator(), new SimpleMenuValidator(), new WidgetValidator(), new FieldSetValidator(prop)
+        ,new FieldSetColumnValidator(prop), new FieldSetRowValidator(prop), new FormValidator(prop), new TableValidator(prop));
     }
 }
---
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/IdValidationUtils.java
@@ -0,0 +1,45 @@
+package net.n2oapp.framework.config.metadata.validation.standard;
+
+import net.n2oapp.framework.api.metadata.aware.IdAware;
+import net.n2oapp.framework.api.metadata.aware.NamespaceUriAware;
+import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
+import org.springframework.core.env.PropertyResolver;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Утилиты проверки содержания в id зарезервированных слов
+ */
+public class IdValidationUtils {
+
+    private final Set<String> forbiddenId;
+
+    public IdValidationUtils(PropertyResolver propertyResolver) {
+        String prop = propertyResolver.getProperty("n2o.config.field.forbidden_ids");
+        forbiddenId = new HashSet<>();
+        if (prop != null) {
+            for (String item : prop.split(",")) {
+                forbiddenId.add(item.trim());
+            }
+        }
+    }
+
+    public void checkId(IdAware idAware) {
+        String id = idAware.getId();
+        if (id != null && forbiddenId != null && forbiddenId.contains(id.trim())) {
+            throw new N2oMetadataValidationException("Field ID contains forbidden word: \'" + id + "\'");
+        }
+    }
+
+    public void checkIds(NamespaceUriAware[] items) {
+        if (items != null && forbiddenId != null) {
+            for (NamespaceUriAware item : items) {
+                if (item instanceof IdAware) {
+                    checkId((IdAware) item);
+                }
+            }
+        }
+    }
+
+}
