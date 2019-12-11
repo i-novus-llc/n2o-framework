4b0771e0603cbb9620e990436bd41f3df96d9f75
sliskov <sliskov@i-novus.ru>
2019-12-11 12:14:18 +0300

    [NNO-3523] - сделал в демо полный список компонентов

--- frontend/demo/src/App.js
+++ frontend/demo/src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 import N2O from "n2o-framework/lib/N2o";
 import { authProvider } from "n2o-auth";
-import createFactoryConfigLight from "n2o-framework/lib/core/factory/createFactoryConfigLight";
+import createFactoryConfig from "n2o-framework/lib/core/factory/createFactoryConfig";
 import functions from "n2o-framework/lib/utils/functions";
-import {
-  handleApi,
-  defaultApiProvider,
-  FETCH_APP_CONFIG
-} from "n2o-framework/lib/core/api";
 import Route from "n2o-framework/lib/components/core/Route";
 import Page from "n2o-framework/lib/components/core/Page";
 
-import Template from "./components/core/Template";
 import DashboardV2 from "./pages/DashboardV2";
-import TableTree from "./pages/TableTree";
 import Select from "./pages/Select";
 import DataGrid from "./components/widgets/DataGrid/DataGrid";
 import AvatarCell from "./components/cells/Avatar/AvatarCell";
@@ -49,7 +42,7 @@ const config = {
 class App extends Component {
   render() {
     return (
-      <N2O {...createFactoryConfigLight(config)}>
+      <N2O {...createFactoryConfig(config)}>
         {/* 1 полный кастом */}
         <Route path="/custom/v1" exact component={DashboardV2} />
         {/* 2 обертка, без метаданных */}
