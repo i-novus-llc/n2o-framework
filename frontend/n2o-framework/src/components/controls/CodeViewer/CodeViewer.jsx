b36ebde5700c2eb360da22560a0d103b30036967
aahmetov <aahmetov@i-novus.ru>
2019-12-13 14:32:22 +0300

    [NNO-3561] - реализация CodeViewer

--- frontend/n2o-framework/src/components/controls/CodeViewer/CodeViewer.jsx
+++ frontend/n2o-framework/src/components/controls/CodeViewer/CodeViewer.jsx
@@ -4,7 +4,7 @@ import withState from 'recompose/withState';
 import withHandlers from 'recompose/withHandlers';
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { prism, darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vs, xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import cx from 'classnames';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
@@ -13,54 +13,50 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
  * @reactProps {boolean} visible - отображается / не отображается
  * @reactProps {string} children - код для отображения
  * @reactProps {string} language - язык программирования
- * @reactProps {string} theme - цвет фона
+ * @reactProps {string} darkTheme - темная тема компонента
  * @reactProps {boolean} showLineNumbers - отображать нумерацию строк
  * @reactProps {number} startingLineNumber - если включен showLineNumbers, нумерация строк начнется c указанного числа
- * @reactProps {string} title - закголовок CodeViewer
- * @reactProps {string} text - описание CodeViewer
+ * @reactProps {boolean} hideButtons - скрыть кнопки
+ * @reactProps {boolean} hideOverflow - скрыть вертикальную прокрутку
  */
-
-const colorTheme = {
-  LIGHT: 'light',
-  DARK: 'dark',
-};
 
 function CodeViewer({
   visible,
   children,
-  theme,
+  darkTheme,
   copied,
   handleCopy,
   handleShow,
   show,
-  title,
-  text,
+  hideOverflow,
+  hideButtons,
   ...rest
 }) {
   return (
     visible && (
       <div className="n2o-code-viewer">
-        <h4>{title}</h4>
-        <p>{text}</p>
-        <div className="code-viewer-actions">
-          <CopyToClipboard text={children} onCopy={handleCopy}>
+        {!hideButtons ? (
+          <div className="code-viewer-actions">
+            <CopyToClipboard text={children} onCopy={handleCopy}>
+              <i
+                className={cx(
+                  'code-viewer-actions-copy',
+                  copied
+                    ? 'n2o-icon fa fa-check text-success'
+                    : 'n2o-icon fa fa-clipboard'
+                )}
+              />
+            </CopyToClipboard>
             <i
-              className={cx(
-                'code-viewer-actions-copy',
-                copied
-                  ? 'n2o-icon fa fa-check text-success'
-                  : 'n2o-icon fa fa-clipboard'
-              )}
+              className="n2o-icon code-viewer-actions-code fa fa-code"
+              onClick={handleShow}
             />
-          </CopyToClipboard>
-          <i
-            className="n2o-icon code-viewer-actions-code fa fa-code"
-            onClick={handleShow}
-          />
-        </div>
+          </div>
+        ) : null}
         {show ? (
           <SyntaxHighlighter
-            style={theme === colorTheme.DARK ? darcula : prism}
+            className={!hideOverflow ? 'code-viewer-body' : null}
+            style={darkTheme ? xonokai : vs}
             {...rest}
           >
             {children}
@@ -85,9 +81,9 @@ CodeViewer.propTypes = {
    */
   language: PropTypes.string,
   /**
-   * Цвет фона
+   * Темная тема компонента
    */
-  theme: PropTypes.string,
+  darkTheme: PropTypes.bool,
   /**
    * Отображать нумерацию строк
    */
@@ -97,20 +93,20 @@ CodeViewer.propTypes = {
    */
   startingLineNumber: PropTypes.number,
   /**
-   * Заголовок CodeViewer
+   * Cкрыть кнопки
    */
-  title: PropTypes.string,
+  hideButtons: PropTypes.hideButtons,
   /**
-   * Описание CodeViewer
+   * Скрыть вертикальную прокрутку
    */
-  text: PropTypes.string,
+  hideOverflow: PropTypes.overflow,
 };
 
 CodeViewer.defaultProps = {
   visible: true,
-  theme: colorTheme.LIGHT,
-  title: '',
-  text: '',
+  hideButtons: false,
+  darkTheme: false,
+  hideOverflow: false,
 };
 
 export { CodeViewer };
