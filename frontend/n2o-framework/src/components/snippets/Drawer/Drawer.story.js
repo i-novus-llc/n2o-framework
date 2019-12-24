b388c46664dd29904c8479af76d8bca761e2a17e
aahmetov <aahmetov@i-novus.ru>
2019-12-24 17:46:18 +0300

    [NNO-3380] - правки Drawer

--- frontend/n2o-framework/src/components/snippets/Drawer/Drawer.story.js
+++ frontend/n2o-framework/src/components/snippets/Drawer/Drawer.story.js
@@ -3,7 +3,6 @@ import { forceReRender, storiesOf } from '@storybook/react';
 import Drawer from './Drawer';
 import { StateDecorator, Store } from '@sambego/storybook-state';
 import { Button } from 'reactstrap';
-import MetaJSON from './Drawer.meta';
 
 const stories = storiesOf('UI Компоненты/Drawer', module);
 
@@ -21,33 +20,34 @@ stories.addParameters({
   },
 });
 
-stories
-  .add(
-    'Компонент',
-    () => (
-      <React.Fragment>
-        <Drawer
-          title={<h3>Title</h3>}
-          footer="footer"
-          visible={store.get('visible')}
-          onClose={() => store.set({ visible: !store.get('visible') })}
-          onHandleClick={() => store.set({ visible: !store.get('visible') })}
-          backdropClosable={false}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Drawer>
-        <div className="d-flex justify-content-center">
-          <Button onClick={() => store.set({ visible: !store.get('visible') })}>
-            {store.get('visible') ? 'Close' : 'Open'}
-          </Button>
-        </div>
-      </React.Fragment>
-    ),
-    {
-      info: {
-        text: `
+stories.add(
+  'Компонент',
+  () => (
+    <React.Fragment>
+      <Drawer
+        title={<h3>Title</h3>}
+        footer="footer"
+        visible={store.get('visible')}
+        onClose={() => store.set({ visible: !store.get('visible') })}
+        onHandleClick={() => store.set({ visible: !store.get('visible') })}
+        backdropClosable={false}
+        width={'10%'}
+        height={'100%'}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+      <div className="d-flex justify-content-center">
+        <Button onClick={() => store.set({ visible: !store.get('visible') })}>
+          {store.get('visible') ? 'Close' : 'Open'}
+        </Button>
+      </div>
+    </React.Fragment>
+  ),
+  {
+    info: {
+      text: `
     Компонент 'Drawer'
     ~~~js
     import Drawer from 'n2o-framework/frontend/n2o-framework/lib/components/snippets/Drawer';
@@ -56,49 +56,17 @@ stories
       title="title"
       footer="footer"
       visible={true}
-      onClose={onClose}
-      onHandleClick={onHandleClick}
+      animation={true}
+      closable={true},
+      backdropClosable={false},
+      visible={true},
+      backdrop={true},
       backdropClosable={false}
+      width={'10%'},
+      height={'100%'}
     />
     ~~~
     `,
-      },
-    }
-  )
-  .add(
-    'Метаданные',
-    () => (
-      <React.Fragment>
-        <Drawer
-          {...MetaJSON}
-          visible={store.get('visible')}
-          backdropClosable={false}
-          onHandleClick={() => store.set({ visible: !store.get('visible') })}
-          onClose={() => store.set({ visible: !store.get('visible') })}
-        />
-        <div className="d-flex justify-content-center">
-          <Button onClick={() => store.set({ visible: !store.get('visible') })}>
-            Open
-          </Button>
-        </div>
-      </React.Fragment>
-    ),
-    {
-      info: {
-        text: `
-    Компонент 'Drawer'
-    ~~~js
-    import Drawer from 'n2o-framework/frontend/n2o-framework/lib/components/snippets/Drawer';
-      
-    <Drawer
-      {...props}
-      visible={true}
-      onClose={onClose}
-      onHandleClick={onHandleClick}
-      backdropClosable={false}
-    />
-    ~~~
-    `,
-      },
-    }
-  );
+    },
+  }
+);
