038a5d458a5e74bf3e8a129ce9bc5f9a832a0336
btsyganov <btsyganov@i-novus.ru>
2020-01-13 18:08:22 +0300

    [nf][NNO-3753] selenide тест демо

---
+++ demo/server/src/test/java/net/n2oapp/demo/DemoIntegrationTest.java
@@ -0,0 +1,46 @@
+package net.n2oapp.demo;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.headless;
+import static com.codeborne.selenide.Selenide.open;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private ProtoPage protoPage;
+
+    @BeforeClass
+    public static void configure() {
+        browser = "chrome";
+        headless = true;
+    }
+
+    @Before
+    public void openProtoPage() {
+        protoPage = open("http://localhost:" + port, ProtoPage.class)
+                .findBySurname("Иванов");
+    }
+
+    @Test
+    public void testTableSize() {
+        protoPage.tableShouldHaveSize(1);
+    }
+
+    @Test
+    public void testSurname() {
+        protoPage.assertSurname("Иванова", 0);
+    }
+}
---
+++ demo/server/src/test/java/net/n2oapp/demo/ProtoPage.java
@@ -0,0 +1,28 @@
+package net.n2oapp.demo;
+
+import com.codeborne.selenide.Selectors;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * Главная страница демо приложения
+ */
+public class ProtoPage {
+
+    public ProtoPage findBySurname(String query) {
+        $(".n2o-filter").$$("input").get(0).val(query);
+        $(".n2o-filter").$(Selectors.byText("Найти")).click();
+        return page(ProtoPage.class);
+    }
+
+    public ProtoPage tableShouldHaveSize(int size) {
+        $$("tbody").shouldHaveSize(size);
+        return page(ProtoPage.class);
+    }
+
+    public ProtoPage assertSurname(String surname, Integer rowIndex) {
+        $$("tbody").get(rowIndex).$("button").shouldHave(text(surname));
+        return page(ProtoPage.class);
+    }
+}
