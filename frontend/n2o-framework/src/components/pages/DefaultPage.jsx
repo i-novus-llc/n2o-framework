036d5d42c32b2a62a8a8b00b70b3b13524d7829c
sliskov <sliskov@i-novus.ru>
2020-01-22 15:52:56 +0300

    [NNO-2847] - заменил Actions на Toolbar

--- frontend/n2o-framework/src/components/pages/DefaultPage.jsx
+++ frontend/n2o-framework/src/components/pages/DefaultPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import isEmpty from 'lodash/isEmpty';
+import cn from 'classnames';
 import Alert from '../snippets/Alerts/Alert';
 import DocumentTitle from '../core/DocumentTitle';
 import DefaultBreadcrumb from '../core/Breadcrumb/DefaultBreadcrumb';
 import BreadcrumbContainer from '../core/Breadcrumb/BreadcrumbContainer';
-import Actions from '../actions/Actions';
-import cn from 'classnames';
+import Toolbar from '../buttons/Toolbar';
 
 /**
  * Стандартное наполнение страницы
@@ -51,35 +51,15 @@ function DefaultPage(
       )}
       {toolbar && (toolbar.topLeft || toolbar.topRight) && (
         <div className="n2o-page-actions">
-          <Actions
-            toolbar={toolbar.topLeft}
-            actions={actions}
-            containerKey={containerKey}
-            pageId={pageId}
-          />
-          <Actions
-            toolbar={toolbar.topRight}
-            actions={actions}
-            containerKey={containerKey}
-            pageId={pageId}
-          />
+          <Toolbar entityKey={containerKey} toolbar={toolbar.topLeft} />
+          <Toolbar entityKey={containerKey} toolbar={toolbar.topRight} />
         </div>
       )}
       {children}
       {toolbar && (toolbar.bottomLeft || toolbar.bottomRight) && (
         <div className="n2o-page-actions">
-          <Actions
-            toolbar={toolbar.bottomLeft}
-            actions={actions}
-            containerKey={containerKey}
-            pageId={pageId}
-          />
-          <Actions
-            toolbar={toolbar.bottomRight}
-            actions={actions}
-            containerKey={containerKey}
-            pageId={pageId}
-          />
+          <Toolbar entityKey={containerKey} toolbar={toolbar.bottomLeft} />
+          <Toolbar entityKey={containerKey} toolbar={toolbar.bottomRight} />
         </div>
       )}
     </div>
