2b8cf5700608345dcb71f3bc7f8ae155b1361f85
asokolov <asokolov@i-novus.ru>
2020-01-31 10:36:21 +0300

    sub/NNO-3845 добавил классы к breadcrumbs

--- frontend/n2o-framework/src/components/core/Breadcrumb/DefaultBreadcrumb.jsx
+++ frontend/n2o-framework/src/components/core/Breadcrumb/DefaultBreadcrumb.jsx
@@ -3,22 +3,35 @@ import { Link } from 'react-router-dom';
 import isUndefined from 'lodash/isUndefined';
 import last from 'lodash/last';
 import filter from 'lodash/filter';
+import cn from 'classnames';
 import Breadcrumb from 'reactstrap/lib/Breadcrumb';
 import BreadcrumbItem from 'reactstrap/lib/BreadcrumbItem';
 
-function DefaultBreadcrumb({ items }) {
+function DefaultBreadcrumb({ items }, props) {
   const itemsWithLabels = filter(items, item => !isUndefined(item.label));
   const title = last(itemsWithLabels).title;
-  const renderTitle = !isUndefined(title) && <h1>{title}</h1>;
+  const renderTitle = !isUndefined(title) && (
+    <h1 className="n2o-breadcrumb-title">{title}</h1>
+  );
+
   const crumbs = itemsWithLabels.map(({ label, path }, index) => {
+    const lastCrumb = index === itemsWithLabels.length - 1;
     return (
-      <BreadcrumbItem key={index} active={index === itemsWithLabels.length - 1}>
-        {path && index !== itemsWithLabels.length - 1 ? (
-          <Link to={path} key={index}>
+      <BreadcrumbItem key={index} active={lastCrumb}>
+        {path && !lastCrumb ? (
+          <Link className="n2o-breadcrumb-link" to={path} key={index}>
             {label}
           </Link>
         ) : (
-          label
+          <span
+            className={cn({
+              'n2o-breadcrumb-link n2o-breadcrumb-link_active': lastCrumb,
+              'n2o-breadcrumb-link n2o-breadcrumb-link_disabled': !lastCrumb,
+            })}
+            key={index}
+          >
+            {label}
+          </span>
         )}
       </BreadcrumbItem>
     );
