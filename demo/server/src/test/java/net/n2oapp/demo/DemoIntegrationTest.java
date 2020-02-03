f5c2bf06e51aa884294894290fbd835d7e61d92f
aarifullin <aarifullin@i-novus.ru>
2020-02-03 16:23:07 +0300

    [NF][NNO-3903] junit 5

--- demo/server/src/test/java/net/n2oapp/demo/DemoIntegrationTest.java
+++ demo/server/src/test/java/net/n2oapp/demo/DemoIntegrationTest.java
@@ -1,20 +1,29 @@
 package net.n2oapp.demo;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static com.codeborne.selenide.Configuration.*;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.headless;
 import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = DemoApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DemoIntegrationTest {
 
     @LocalServerPort
@@ -22,34 +31,46 @@ public class DemoIntegrationTest {
 
     private ProtoPage protoPage;
 
-    @BeforeClass
+    @BeforeAll
     public static void configure() {
         browser = "chrome";
         headless = true;
     }
 
-    @Before
+    @BeforeEach
     public void openProtoPage() {
         protoPage = open("http://localhost:" + port, ProtoPage.class);
     }
 
     @Test
+    @Order(1)
+    public void checkStaticContent() throws IOException {
+        HttpUriRequest request = new HttpGet("http://localhost:" + port + "/index.html");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+    }
+
+    @Test
     @Primary
+    @Order(2)
     public void checkAllElementsExists() {
         protoPage.checkAllElementsExists();
     }
 
     @Test
+    @Order(3)
     public void testFilterByGender() {
         protoPage.testFilterByGender();
     }
 
     @Test
+    @Order(4)
     public void testTableSorting() {
         protoPage.testTableSorting();
     }
 
     @Test
+    @Order(5)
     public void testTableEditBirthday() {
         protoPage.testTableEditBirthday();
     }
