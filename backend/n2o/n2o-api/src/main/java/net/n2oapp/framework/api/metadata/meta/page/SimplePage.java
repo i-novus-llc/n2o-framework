c4ec9588e82a9ecdab64d9c80b3440872ff33ec1
estartcev <estartcev@i-novus.ru>
2020-02-06 14:41:18 +0300

    [NNO-3921] Добавлен тулбар в SimplePage

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/page/SimplePage.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/page/SimplePage.java
@@ -3,6 +3,7 @@ package net.n2oapp.framework.api.metadata.meta.page;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import net.n2oapp.framework.api.metadata.meta.toolbar.Toolbar;
 import net.n2oapp.framework.api.metadata.meta.widget.Widget;
 
 /**
@@ -13,4 +14,6 @@ import net.n2oapp.framework.api.metadata.meta.widget.Widget;
 public class SimplePage extends Page {
     @JsonProperty
     private Widget widget;
+    @JsonProperty
+    private Toolbar toolbar;
 }
