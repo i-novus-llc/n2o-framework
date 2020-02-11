4926107d8bc55f49f2a89c05d35908baef87c68a
ilebedenko <ilebedenko@i-novus.ru>
2020-02-11 17:57:32 +0300

    [BF][NNO-3962] - фикс теста

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/io/widget/table/cell/SwitchCellXmlIOTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/io/widget/table/cell/SwitchCellXmlIOTest.java
@@ -1,6 +1,7 @@
 package net.n2oapp.framework.config.io.widget.table.cell;
 
 import net.n2oapp.framework.config.io.widget.table.TableElementIOV4;
+import net.n2oapp.framework.config.metadata.pack.N2oCellsIOPack;
 import net.n2oapp.framework.config.selective.ION2oMetadataTester;
 import org.junit.Test;
 
@@ -11,7 +12,8 @@ public class SwitchCellXmlIOTest {
     @Test
     public void testSwitchCell() {
         ION2oMetadataTester tester = new ION2oMetadataTester();
-        tester.ios(new TableElementIOV4(), new SwitchCellElementIOv2());
+        tester.ios(new TableElementIOV4())
+        .addPack(new N2oCellsIOPack());
 
         assert tester.check("net/n2oapp/framework/config/io/widget/table/cell/testSwitchCellIOv4.widget.xml");
     }
