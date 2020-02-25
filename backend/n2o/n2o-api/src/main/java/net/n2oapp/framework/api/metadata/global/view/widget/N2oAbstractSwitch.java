bb5c9e35ec645649a27b2914ae2fb8c1a9ec46ec
ilebedenko <ilebedenko@i-novus.ru>
2020-02-25 17:43:34 +0300

    [CF][NNO-3962] - исправления

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/widget/N2oAbstractSwitch.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/widget/N2oAbstractSwitch.java
@@ -4,14 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.n2oapp.framework.api.metadata.Source;
-import net.n2oapp.framework.api.metadata.aware.NamespaceUriAware;
 
 /**
  * Абстрактная модель переключателя элементов
  */
 @Getter
 @Setter
-public abstract class N2oAbstractSwitch<T> implements NamespaceUriAware {
+public abstract class N2oAbstractSwitch<T> implements Source {
     private String namespaceUri;
     private String valueFieldId;
     private Case<T>[] cases;
