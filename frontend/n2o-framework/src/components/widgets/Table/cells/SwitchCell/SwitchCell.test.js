f3876197d9d0c47caf70cf7679f99374d646ca13
asokolov <asokolov@i-novus.ru>
2020-03-10 15:25:41 +0300

    sub/NNO-3961 тесты SwitchCell

--- frontend/n2o-framework/src/components/widgets/Table/cells/SwitchCell/SwitchCell.test.js
+++ frontend/n2o-framework/src/components/widgets/Table/cells/SwitchCell/SwitchCell.test.js
@@ -1,13 +1,26 @@
-// import React from 'react';
-//
-// import SwitchCell from './SwitchCell';
-// import { model } from './switchCellStoryProps';
-//
-// const setup = shallow(<SwitchCell model={model} />);
-//
-// describe('SwitchCell tests <SwitchCell>', () => {
-//   it('Компонент отрисовывается', () => {
-//     const wrapper = setup();
-//     console.warn(wrapper);
-//   });
-// });
+import React from 'react';
+import SwitchCell from './SwitchCell';
+import {
+  model,
+  model2,
+  modelFromDefaultView,
+} from './switchCellStoryProps';
+
+const setupOptions = model => shallow(<SwitchCell model={model} />);
+
+describe('SwitchCell tests <SwitchCell>', () => {
+  it('Компонент возвращает верный cell - TextCell', () => {
+    const wrapper = setupOptions(model);
+    expect(wrapper.props().model.src).toBe('TextCell');
+  });
+
+  it('Компонент возвращает верный cell - LinkCell', () => {
+    const wrapper = setupOptions(model2);
+    expect(wrapper.props().model.src).toBe('LinkCell');
+  });
+
+  it('Компонент возвращает верный default cell - ImageCell', () => {
+    const wrapper = setupOptions(modelFromDefaultView);
+    expect(wrapper.props().model.src).toBe('ImageCell');
+  });
+});
