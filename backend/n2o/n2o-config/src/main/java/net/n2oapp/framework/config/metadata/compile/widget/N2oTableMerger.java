59814bfac53ccbd48cf43e4c37005371ac276e87
ilebedenko <ilebedenko@i-novus.ru>
2020-03-19 18:57:38 +0300

    [CF][NNO-4082] - доработка FormMerger и TableMerger

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/N2oTableMerger.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/N2oTableMerger.java
@@ -12,6 +12,14 @@ public class N2oTableMerger extends N2oWidgetMerger<N2oTable> {
     @Override
     public N2oTable merge(N2oTable source, N2oTable override) {
         setIfNotNull(source::setSelection, override::getSelection);
+        setIfNotNull(source::setScrollX, override::getScrollX);
+        setIfNotNull(source::setScrollY, override::getScrollY);
+        setIfNotNull(source::setTableSize, override::getTableSize);
+        setIfNotNull(source::setChildren, override::getChildren);
+        setIfNotNull(source::setPagination, override::getPagination);
+        setIfNotNull(source::setRows, override::getRows);
+        addIfNotNull(source, override, N2oTable::setColumns, N2oTable::getColumns);
+        addIfNotNull(source, override, N2oTable::setFilters, N2oTable::getFilters);
         return source;
     }
 
