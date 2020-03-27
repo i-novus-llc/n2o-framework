e5789167bf8fc3a64ef19198f2d0e833b4a72854
aarifullin <aarifullin@i-novus.ru>
2020-03-27 17:53:43 +0300

    [NF][NNO-1629]  перевод на junit5

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/FieldToolbarAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/FieldToolbarAT.java
@@ -20,20 +20,20 @@ import net.n2oapp.framework.config.metadata.pack.N2oDataProvidersIOPack;
 import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
 import net.n2oapp.framework.config.metadata.pack.N2oObjectsPack;
 import net.n2oapp.framework.config.selective.CompileInfo;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class FieldToolbarAT extends AutoTestBase {
 
-    @BeforeClass
+    @BeforeAll
     public static void beforeClass() {
         configureSelenide();
     }
 
-    @Before
+    @BeforeEach
     @Override
     public void setUp() throws Exception {
         super.setUp();
