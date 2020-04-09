c6ddcd7744eaea0929a4501f7faa6da51df1d8cb
ilebedenko <ilebedenko@i-novus.ru>
2020-04-09 15:05:35 +0300

    [СF][NNO-4120] - доработка автотеста

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/MultiColumnAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/MultiColumnAT.java
@@ -1,8 +1,10 @@
 package net.n2oapp.framework.autotest.widget.table;
 
 import net.n2oapp.framework.autotest.api.collection.TableHeaders;
+import net.n2oapp.framework.autotest.api.collection.Widgets;
 import net.n2oapp.framework.autotest.api.component.control.InputText;
-import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.page.StandardPage;
+import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
 import net.n2oapp.framework.autotest.api.component.widget.table.FilterHeader;
 import net.n2oapp.framework.autotest.api.component.widget.table.StandardTableHeader;
 import net.n2oapp.framework.autotest.api.component.widget.table.TableWidget;
@@ -14,13 +16,15 @@ import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * Автотест для мульти-столбца таблицы
  */
 public class MultiColumnAT extends AutoTestBase {
-    private SimplePage page;
+
+    private Widgets widgets;
 
     @BeforeAll
     public static void beforeClass() {
@@ -33,10 +37,10 @@ public class MultiColumnAT extends AutoTestBase {
         super.setUp();
 
         builder.sources(new CompileInfo("net/n2oapp/framework/autotest/widget/table/multi_column/index.page.xml"),
-                new CompileInfo("net/n2oapp/framework/autotest/widget/table/multi_column/table.widget.xml"),
                 new CompileInfo("net/n2oapp/framework/autotest/widget/table/multi_column/test.query.xml"));
-        page = open(SimplePage.class);
+        StandardPage page = open(StandardPage.class);
         page.shouldExists();
+        widgets = page.place("single").region(0, SimpleRegion.class).content();
     }
 
     @Override
@@ -48,7 +52,40 @@ public class MultiColumnAT extends AutoTestBase {
 
     @Test
     public void testMultiColumn() {
-        TableWidget table = page.single().widget(TableWidget.class);
+        TableWidget table = widgets.widget(0, TableWidget.class);
+        table.shouldExists();
+
+        TableWidget.Rows rows = table.columns().rows();
+        TableHeaders headers = table.columns().headers();
+        headers.shouldHaveSize(5);
+        rows.shouldHaveSize(4);
+
+        StandardTableHeader header1 = headers.header(0);
+        StandardTableHeader header2 = headers.header(1);
+        StandardTableHeader header3 = headers.header(2);
+        StandardTableHeader header4 = headers.header(3);
+        StandardTableHeader header5 = headers.header(4);
+        // проверка порядка столбцов
+        // столбцы первого уровня
+        header1.shouldHaveTitle("ID");
+        header2.shouldHaveTitle("Info");
+        header3.shouldHaveTitle("Birthday");
+        // столбцы второго уровня
+        header4.shouldHaveTitle("FirstName");
+        header5.shouldHaveTitle("LastName");
+
+        // проверка, что все ячейки корректно заполнены
+        rows.row(0).cell(0).textShouldHave("1");
+        rows.row(0).cell(1).textShouldHave("John");
+        rows.row(0).cell(2).textShouldHave("Smith");
+        rows.row(0).cell(3).textShouldHave("2018.12.31");
+    }
+
+    /// - TODO реализация вложенных MultiColumn и FilterColumn ожидается в будущем
+    @Disabled
+    @Test
+    public void testAdvancedMultiColumn() {
+        TableWidget table = widgets.widget(1, TableWidget.class);
         table.shouldExists();
 
         TableWidget.Rows rows = table.columns().rows();
