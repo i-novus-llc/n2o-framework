5ba4808205e8f28411f66a4d9e479821051c0a3f
Игорь Рябов <iryabov@i-novus.ru>
2020-05-06 09:27:05 +0000

    Merge branch 'fix/NNO-4418' into 'master'
    [BF][NNO-4418] - фикс задания src контролов
    
    Closes NNO-4418
    
    See merge request open/n2o-framework!1158
    
    (cherry picked from commit 7701c0c17e0284a9ee3ddeb1c5fbc8fdb0ecc212)
    
    a7446f57 [BF][NNO-4418] - фикс задания src контролов
    d86fe17d [CF][NNO-4418] - исправления
    d5a42d93 [CF][NNO-4418] - фикс
    2b69b432 Merge branch 'master' of https://git.i-novus.ru/open/n2o-framework into fix/NNO-4418

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/control/N2oStandardField.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/control/N2oStandardField.java
@@ -15,19 +15,8 @@ public abstract class N2oStandardField extends N2oField {
 
     private String placeholder;
     private Boolean copied;
-    private String controlSrc;
     private Validations validations;
     private String defaultValue;
-
-    @Override
-    public void setSrc(String src) {
-        this.controlSrc = src;
-    }
-
-    @Override
-    public String getSrc() {
-        return controlSrc;
-    }
 
     public void setActionButtons(List<N2oActionButton> buttons) {
         //todo добавлять их в тулбар
