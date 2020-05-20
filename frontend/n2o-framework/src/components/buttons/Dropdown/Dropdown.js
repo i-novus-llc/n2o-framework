84dc2590daa7a9ecf562392fad4cffce0f6d8fa0
asokolov <asokolov@i-novus.ru>
2020-05-20 12:28:30 +0300

    fix/INPORTAL-4514 Popper strategy to absolute

--- frontend/n2o-framework/src/components/buttons/Dropdown/Dropdown.js
+++ frontend/n2o-framework/src/components/buttons/Dropdown/Dropdown.js
@@ -63,7 +63,7 @@ class DropdownButton extends React.Component {
               />
             )}
           </Reference>
-          <Popper placement="bottom-start" strategy="fixed">
+          <Popper placement="bottom-start" strategy="absolute">
             {({ ref, style, placement }) => (
               <div
                 ref={ref}
