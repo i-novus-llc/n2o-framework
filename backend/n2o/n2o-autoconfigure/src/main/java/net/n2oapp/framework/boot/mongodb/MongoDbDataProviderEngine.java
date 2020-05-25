a9046e6883e8536d50f25ac0b2006b66bd7e9a02
ilebedenko <ilebedenko@i-novus.ru>
2020-05-25 14:52:36 +0300

    [NF][NNO-4537] - реализация crud MongoDbDataProviderEngine

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/mongodb/MongoDbDataProviderEngine.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/mongodb/MongoDbDataProviderEngine.java
@@ -3,12 +3,17 @@ package net.n2oapp.framework.boot.mongodb;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import lombok.Setter;
 import net.n2oapp.framework.api.data.MapInvocationEngine;
 import net.n2oapp.framework.api.exception.N2oException;
 import net.n2oapp.framework.api.metadata.dataprovider.N2oMongoDbDataProvider;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,13 +24,16 @@ public class MongoDbDataProviderEngine implements MapInvocationEngine<N2oMongoDb
     private String connectionUrl;
     private String databaseName;
 
-
     private MongoCollection<Document> getCollection(N2oMongoDbDataProvider invocation) {
         String connUrl = invocation.getConnectionUrl() != null ? invocation.getConnectionUrl() : connectionUrl;
         String dbName = invocation.getDatabaseName() != null ? invocation.getDatabaseName() : databaseName;
-        return new MongoClient(new MongoClientURI(connUrl)).getDatabase(dbName)
+
+        if (connUrl == null)
+            throw new N2oException("Need to define n2o.engine.mongodb.connection_url property");
+
+        return new MongoClient(new MongoClientURI(connUrl))
+                .getDatabase(dbName)
                 .getCollection(invocation.getCollectionName());
-        // TODO close connection
     }
 
     @Override
@@ -45,43 +53,70 @@ public class MongoDbDataProviderEngine implements MapInvocationEngine<N2oMongoDb
             case find:
                 return find(inParams, collection);
             case insertOne:
-                return insertOne(invocation, inParams);
+                return insertOne(inParams, collection);
             case updateOne:
-                return updateOne(invocation, inParams, collection);
+                return updateOne(inParams, collection);
             case updateMany:
-                return updateMany(invocation, inParams, collection);
+                return updateMany(inParams, collection);
             case deleteOne:
-                return deleteOne(invocation, inParams, collection);
+                return deleteOne(inParams, collection);
             case deleteMany:
-                return deleteMany(invocation, inParams, collection);
+                return deleteMany(inParams, collection);
             case countDocuments:
-                return collection.count();
+                return collection.countDocuments();
             default:
                 throw new N2oException("Unsupported invocation's operation");
         }
     }
 
     private Object find(Map<String, Object> inParams, MongoCollection<Document> collection) {
+        // TODO - filters, pagination, sorting
+
+        List<Document> data = new ArrayList<>();
+        Bson filter;
+        if (((List) inParams.get("filters")).get(0).equals("id :in :id")) {
+            Object[] ids = ((List) inParams.get("id")).stream().map(id -> new ObjectId((String) id)).toArray();
+            filter = Filters.in("_id", ids);
+        }
+        else
+            filter = Filters.eq("_id", new ObjectId((String) inParams.get("id")));
+
+        for (Document document : collection.find(filter))
+            data.add(document);
+        return data;
+    }
+
+    private Object insertOne(Map<String, Object> inParams, MongoCollection<Document> collection) {
+        Document document = new Document(inParams);
+        collection.insertOne(document);
+        return document.get("_id").toString();
+    }
+
+    private Object updateOne(Map<String, Object> inParams, MongoCollection<Document> collection) {
+        if (!inParams.containsKey("id"))
+            throw new N2oException("Id is required for operation \"updateOne\"");
+        String id = (String) inParams.remove("id");
+        collection.updateOne(Filters.eq("_id", new ObjectId(id)), new Document("$set", new Document(inParams)));
         return null;
     }
 
-    private Object insertOne(N2oMongoDbDataProvider invocation, Map<String, Object> inParams) {
+    private Object updateMany(Map<String, Object> inParams, MongoCollection<Document> collection) {
         return null;
     }
 
-    private Object updateOne(N2oMongoDbDataProvider invocation, Map<String, Object> inParams, MongoCollection<Document> collection) {
+    private Object deleteOne(Map<String, Object> inParams, MongoCollection<Document> collection) {
+        if (!inParams.containsKey("id"))
+            throw new N2oException("Id is required for operation \"deleteOne\"");
+        collection.deleteOne(Filters.eq("_id", new ObjectId((String) inParams.get("id"))));
         return null;
     }
 
-    private Object updateMany(N2oMongoDbDataProvider invocation, Map<String, Object> inParams, MongoCollection<Document> collection) {
-        return null;
-    }
+    private Object deleteMany(Map<String, Object> inParams, MongoCollection<Document> collection) {
+        if (!inParams.containsKey("ids"))
+            throw new N2oException("Ids is required for operation \"deleteMany\"");
 
-    private Object deleteOne(N2oMongoDbDataProvider invocation, Map<String, Object> inParams, MongoCollection<Document> collection) {
-        return null;
-    }
-
-    private Object deleteMany(N2oMongoDbDataProvider invocation, Map<String, Object> inParams, MongoCollection<Document> collection) {
+        Object[] ids = ((List) inParams.get("ids")).stream().map(id -> new ObjectId((String) id)).toArray();
+        collection.deleteMany(Filters.in("_id", ids));
         return null;
     }
 }
---
+++ backend/n2o/n2o-autoconfigure/src/test/java/net/n2oapp/framework/boot/mongodb/MongoDbDataProviderEngineTest.java
@@ -0,0 +1,299 @@
+package net.n2oapp.framework.boot.mongodb;
+
+import net.n2oapp.framework.api.metadata.dataprovider.N2oMongoDbDataProvider;
+import org.bson.Document;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Тестирование сервиса для выполнения запросов к MongoDb
+ */
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MongoDbDataProviderTestApplication.class)
+@AutoConfigureDataMongo
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MongoDbDataProviderEngineTest {
+    private MongoDbDataProviderEngine engine;
+    private N2oMongoDbDataProvider provider;
+
+    private String collectionName = "user";
+
+    @Value("${local.mongo.port}")
+    private String port;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private String id;
+
+
+    @BeforeAll
+    public void init() {
+        engine = new MongoDbDataProviderEngine();
+        provider = new N2oMongoDbDataProvider();
+
+        provider.setConnectionUrl("mongodb://localhost:" + port);
+        provider.setDatabaseName("test");
+        provider.setCollectionName(collectionName);
+
+        mongoTemplate.createCollection(collectionName);
+        mongoTemplate.getCollection(collectionName).insertMany(UserBuilder.testData());
+    }
+
+
+    @Test
+    @Order(1)
+    public void insertOneOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.insertOne);
+        HashMap<String, Object> inParams = new HashMap<>();
+        inParams.put("name", "test");
+        inParams.put("age", 99);
+        inParams.put("birthday", "01.01.1900 00:00:00");
+        inParams.put("vip", true);
+
+        id = (String) engine.invoke(provider, inParams);
+
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("id :eq :id"));
+        inParams.put("id", id);
+
+        Document document = ((List<Document>) engine.invoke(provider, inParams)).get(0);
+        assertThat(document.get("name"), is("test"));
+        assertThat(document.get("age"), is(99));
+        assertThat(document.get("birthday"), is("01.01.1900 00:00:00"));
+        assertThat(document.get("vip"), is(true));
+    }
+
+    @Test
+    @Order(2)
+    public void updateOneOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.updateOne);
+        HashMap<String, Object> inParams = new HashMap<>();
+
+        inParams.put("id", id);
+        inParams.put("name", "test2");
+        inParams.put("age", 10);
+//        inParams.put("a.b.c", "bbb");
+        inParams.put("birthday", "01.01.2000 00:00:00");
+        inParams.put("vip", false);
+
+        engine.invoke(provider, inParams);
+
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("id :eq :id"));
+        inParams.put("id", id);
+
+        Document document = ((List<Document>) engine.invoke(provider, inParams)).get(0);
+        assertThat(document.get("name"), is("test2"));
+        assertThat(document.get("age"), is(10));
+        assertThat(document.get("birthday"), is("01.01.2000 00:00:00"));
+        assertThat(document.get("vip"), is(false));
+    }
+
+    @Test
+    @Order(3)
+    public void deleteOneOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.deleteOne);
+        HashMap<String, Object> inParams = new HashMap<>();
+
+        inParams.put("id", id);
+        engine.invoke(provider, inParams);
+
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("id :eq :id"));
+        inParams.put("id", id);
+
+        List documents = (List<Document>) engine.invoke(provider, inParams);
+        assertThat(documents.size(), is(0));
+
+        id = null;
+    }
+
+    @Disabled
+    @Test
+    @Order(4)
+    public void updateManyOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.updateMany);
+        HashMap<String, Object> inParams = new HashMap<>();
+
+        engine.invoke(provider, inParams);
+    }
+
+    @Test
+    @Order(5)
+    public void deleteManyOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.insertOne);
+        HashMap<String, Object> inParams = new HashMap<>();
+        inParams.put("name", "test2");
+        String id1 = (String) engine.invoke(provider, inParams);
+
+        inParams = new HashMap<>();
+        inParams.put("name", "test3");
+        String id2 = (String) engine.invoke(provider, inParams);
+
+        // проверяем, что произошла вставка
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("id :in :id"));
+        inParams.put("id", Arrays.asList(id1, id2));
+        List documents = (List<Document>) engine.invoke(provider, inParams);
+        assertThat(documents.size(), is(2));
+
+        // удаляем
+        provider.setOperation(N2oMongoDbDataProvider.Operation.deleteMany);
+        inParams = new HashMap<>();
+        inParams.put("ids", Arrays.asList(id1, id2));
+        engine.invoke(provider, inParams);
+
+        // проверяем, что документы удалены
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("id :in :id"));
+        inParams.put("id", Arrays.asList(id1, id2));
+        documents = (List<Document>) engine.invoke(provider, inParams);
+        assertThat(documents.size(), is(0));
+    }
+
+    @Disabled
+    @Test
+    public void findOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        Object documents = engine.invoke(provider, new HashMap<>());
+    }
+
+    @Test
+    public void countDocumentsOperationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.countDocuments);
+        Long count = (Long) engine.invoke(provider, new HashMap<>());
+        assertThat(count, is(5L));
+    }
+
+    @Disabled
+    @Test
+    public void paginationTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("limit", 2);
+        inParams.put("offset", 2);
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void sortingTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("limit", 3);
+        inParams.put("offset", 0);
+        inParams.put("sorting", Arrays.asList("name :desc"));
+
+        Object documents = engine.invoke(provider, inParams);
+
+    }
+
+    @Disabled
+    @Test
+    public void eqFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("name :eq :name"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void moreFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :more :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void lessFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :less :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void likeFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :like :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void inFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :in :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void isNullFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :isNull :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void isNotNullFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :isNotNull :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+
+    @Disabled
+    @Test
+    public void eqOrIsNullFilterTest() {
+        provider.setOperation(N2oMongoDbDataProvider.Operation.find);
+        HashMap<Object, Object> inParams = new HashMap<>();
+        inParams.put("filters", Arrays.asList("age :eqOrIsNull :age"));
+        inParams.put("", "");
+
+        Object documents = engine.invoke(provider, inParams);
+    }
+}
