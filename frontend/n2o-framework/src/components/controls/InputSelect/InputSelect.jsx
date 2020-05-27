6af607b05d5e3597b583d925e11911d400c49dc1
sliskov <sliskov@i-novus.ru>
2020-05-27 15:37:20 +0300

    [BF][NNO-4583] - исправил работу popupAutoSize. Теперь при popupAutoSize: false, длина попапа фиксируется длиной инупута.

--- frontend/n2o-framework/src/components/controls/InputSelect/InputSelect.jsx
+++ frontend/n2o-framework/src/components/controls/InputSelect/InputSelect.jsx
@@ -49,6 +49,8 @@ import { MODIFIERS } from '../DatePicker/utils';
  * @reactProps {boolean} popupAutoSize - флаг включения автоматическиого расчета длины PopUp
  */
 
+const POPUP_MAX_WIDTH = 600;
+
 class InputSelect extends React.Component {
   constructor(props) {
     super(props);
@@ -474,13 +476,35 @@ class InputSelect extends React.Component {
     };
   }
 
-  calcPopperWidth() {
-    if ((this._input || this._textarea) && !this.props.popupAutoSize) {
-      return this._input
-        ? this._input.getBoundingClientRect().width
-        : this._textarea.getBoundingClientRect().width;
-    }
+  preparePopUpStyles(popperStyles) {
+    return Object.assign({}, popperStyles, this.calcPopperWidth());
   }
+
+  calcPopperWidth() {
+    const component = this._input || this._textarea;
+
+    if (!!component) {
+      if (!this.props.popupAutoSize) {
+        const width = component.getBoundingClientRect().width;
+
+        return {
+          minWidth: width,
+          maxWidth: width,
+        };
+      }
+
+      return {
+        minWidth: 'auto',
+        maxWidth: POPUP_MAX_WIDTH,
+      };
+    }
+
+    return {
+      minWidth: 'auto',
+      maxWidth: 'auto',
+    };
+  }
+
   /**
    * Рендер
    */
@@ -588,11 +612,7 @@ class InputSelect extends React.Component {
               {({ ref, style, placement, scheduleUpdate }) => (
                 <div
                   ref={ref}
-                  style={{
-                    ...style,
-                    minWidth: this.calcPopperWidth(),
-                    maxWidth: 600,
-                  }}
+                  style={this.preparePopUpStyles(style)}
                   data-placement={placement}
                   className="n2o-pop-up"
                 >
