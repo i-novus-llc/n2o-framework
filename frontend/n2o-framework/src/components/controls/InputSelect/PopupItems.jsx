f0d817926f39922d45bfb42348ee7bd79e42da5c
sliskov <sliskov@i-novus.ru>
2020-06-05 14:54:55 +0300

    [NNO-4181] - фикс

--- frontend/n2o-framework/src/components/controls/InputSelect/PopupItems.jsx
+++ frontend/n2o-framework/src/components/controls/InputSelect/PopupItems.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import isNil from 'lodash/isNil';
-
 import Badge from 'reactstrap/lib/Badge';
 import DropdownItem from 'reactstrap/lib/DropdownItem';
 import scrollIntoView from 'scroll-into-view-if-needed';
