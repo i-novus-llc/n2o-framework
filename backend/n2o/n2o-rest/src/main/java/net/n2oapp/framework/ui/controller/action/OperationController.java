caf1f961a0e1472b255bc02912158a80ac271890
iryabov <iryabov@i-novus.ru>
2020-06-17 09:37:29 +0300

    NNO-4516 Вывод userMessage в log

--- backend/n2o/n2o-rest/src/main/java/net/n2oapp/framework/ui/controller/action/OperationController.java
+++ backend/n2o/n2o-rest/src/main/java/net/n2oapp/framework/ui/controller/action/OperationController.java
@@ -59,7 +59,8 @@ public class OperationController extends SetController {
             return constructSuccessSetDataResponse(data, requestInfo, responseInfo);
         } catch (N2oException e) {
             SetDataResponse response = constructFailSetDataResponse(e, requestInfo);
-            logger.error("Error response " + response.getStatus() + " " + e.getSeverity(), e);
+            logger.error("Error response " + response.getStatus() + " " + e.getSeverity() + ": "
+                    + (e.getUserMessage() != null ? e.getUserMessage() : e.getMessage()), e);
             return response;
         }
     }
