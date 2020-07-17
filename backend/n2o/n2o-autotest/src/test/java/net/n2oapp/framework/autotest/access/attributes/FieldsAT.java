7b4f09f0822114962cfdef3b3a3644a6acc2b5d9
rgazizov <rgazizov@i-novus.ru>
2020-07-17 16:24:13 +0300

    [NNO-4775] Добавлены тесты ограничения доступа

---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/access/attributes/FieldsAT.java
@@ -0,0 +1,80 @@
+package net.n2oapp.framework.autotest.access.attributes;
+
+import net.n2oapp.framework.access.metadata.pack.AccessSchemaPack;
+import net.n2oapp.framework.autotest.api.collection.Fields;
+import net.n2oapp.framework.autotest.api.component.page.StandardPage;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FieldsAT extends AutoTestBase {
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oAllPagesPack(), new N2oHeaderPack(), new N2oAllDataPack(), new AccessSchemaPack());
+        CompileInfo.setSourceTypes(builder.getEnvironment().getSourceTypeRegister());
+        builder.sources(new CompileInfo("net/n2oapp/framework/config/default/default.header.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/attributes/fields/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/attributes/fields/default.access.xml"));
+    }
+
+    @Test
+    public void testAdminAccess() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", "Admin");
+        user.put("roles", Collections.singleton("admin"));
+        user.put("permissions", Collections.singleton("edit"));
+        setUserInfo(user);
+
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        page.header().brandNameShouldBe("N2O");
+        page.breadcrumb().titleShouldHaveText("Доступ к полям по sec атрибутам");
+
+        Fields fields = page.widgets().widget(FormWidget.class).fields();
+        fields.field("Доступно всем").shouldExists();
+        fields.field("Только с ролью admin").shouldExists();
+        fields.field("Только с правом edit").shouldExists();
+        fields.field("Только анонимам").shouldNotExists();
+    }
+
+    @Test
+    public void testAnonymousAccess() {
+        setUserInfo(null);
+
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        page.header().brandNameShouldBe("N2O");
+        page.breadcrumb().titleShouldHaveText("Доступ к полям по sec атрибутам");
+
+        Fields fields = page.widgets().widget(FormWidget.class).fields();
+        fields.field("Доступно всем").shouldExists();
+        fields.field("Только с ролью admin").shouldNotExists();
+        fields.field("Только с правом edit").shouldNotExists();
+        fields.field("Только анонимам").shouldExists();
+    }
+
+}
---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/access/schema/HeaderAT.java
@@ -0,0 +1,117 @@
+package net.n2oapp.framework.autotest.access.schema;
+
+import net.n2oapp.framework.access.metadata.pack.AccessSchemaPack;
+import net.n2oapp.framework.autotest.api.component.header.AnchorMenuItem;
+import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import net.n2oapp.framework.config.test.SimplePropertyResolver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HeaderAT extends AutoTestBase {
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oAllPagesPack(), new N2oHeaderPack(), new N2oAllDataPack(), new AccessSchemaPack());
+        CompileInfo.setSourceTypes(builder.getEnvironment().getSourceTypeRegister());
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/tutorial.header.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/menu1.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/menu2.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/menu3.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/menu4.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/access/schema/header/schema.access.xml"));
+    }
+
+    @Test
+    public void testAdminAccess() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", "Admin");
+        user.put("roles", Collections.singleton("admin"));
+        user.put("permissions", Collections.singleton("edit"));
+        setUserInfo(user);
+        ((SimplePropertyResolver) builder.getEnvironment().getSystemProperties()).setProperty("n2o.access.schema.id", "schema");
+        builder.getEnvironment().getContextProcessor().set("username", user.get("username"));
+
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        page.header().brandNameShouldBe("Хедер");
+        page.single().widget(FormWidget.class).shouldExists();
+        page.breadcrumb().titleShouldHaveText("Главная страница");
+
+        page.header().nav().shouldHaveSize(3);
+
+        AnchorMenuItem menuItem0 = page.header().nav().anchor(0);
+        menuItem0.labelShouldHave("Доступно всем");
+        menuItem0.urlShouldHave(getBaseUrl() + "/#/menu1");
+        menuItem0.click();
+        page.breadcrumb().titleShouldHaveText("Страница доступная всем");
+
+        AnchorMenuItem menuItem1 = page.header().nav().anchor(1);
+        menuItem1.labelShouldHave("Требуется роль admin");
+        menuItem1.urlShouldHave(getBaseUrl() + "/#/menu2");
+        menuItem1.click();
+        page.breadcrumb().titleShouldHaveText("Страница доступная с ролью admin");
+
+        AnchorMenuItem menuItem2 = page.header().nav().anchor(2);
+        menuItem2.labelShouldHave("Требуется право edit");
+        menuItem2.urlShouldHave(getBaseUrl() + "/#/menu3");
+        menuItem2.click();
+        page.breadcrumb().titleShouldHaveText("Страница доступная с правом edit");
+
+        page.header().extra().shouldHaveSize(1);
+        AnchorMenuItem extraLink = page.header().extra().item(0, AnchorMenuItem.class);
+        extraLink.labelShouldHave("Admin");
+    }
+
+    @Test
+    public void testAnonymousAccess() {
+        setUserInfo(null);
+
+        ((SimplePropertyResolver) builder.getEnvironment().getSystemProperties()).setProperty("n2o.access.schema.id", "schema");
+
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        page.header().brandNameShouldBe("Хедер");
+        page.single().widget(FormWidget.class).shouldExists();
+        page.breadcrumb().titleShouldHaveText("Главная страница");
+
+        page.header().nav().shouldHaveSize(2);
+
+        AnchorMenuItem menuItem0 = page.header().nav().anchor(0);
+        menuItem0.labelShouldHave("Доступно всем");
+        menuItem0.urlShouldHave(getBaseUrl() + "/#/menu1");
+        menuItem0.click();
+        page.breadcrumb().titleShouldHaveText("Страница доступная всем");
+
+        AnchorMenuItem menuItem3 = page.header().nav().anchor(1);
+        menuItem3.labelShouldHave("Только анонимам");
+        menuItem3.urlShouldHave(getBaseUrl() + "/#/menu4");
+        menuItem3.click();
+        page.breadcrumb().titleShouldHaveText("Страница доступная только анонимам");
+    }
+
+}
