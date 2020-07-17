8e635ec9eb7650c867273aa942c4492800f5645b
rgazizov <rgazizov@i-novus.ru>
2020-07-17 14:01:42 +0300

    [NNO-4775] Добавлен метод shouldHaveTitle

--- backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/api/component/region/LineRegion.java
+++ backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/api/component/region/LineRegion.java
@@ -4,4 +4,6 @@ import net.n2oapp.framework.autotest.api.collection.Widgets;
 
 public interface LineRegion extends Region, Сollapsible {
     Widgets content();
+
+    void shouldHaveTitle(String title);
 }
