f6dd50dacd0fe0264c4fafe479281d5310852679
sliskov <sliskov@i-novus.ru>
2020-08-04 17:10:04 +0300

    [BF][NNO-4900] - добавил visible в ButtonField

--- frontend/n2o-framework/src/components/widgets/Form/fields/ButtonField/ButtonField.js
+++ frontend/n2o-framework/src/components/widgets/Form/fields/ButtonField/ButtonField.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 
 import StandardButton from '../../../../buttons/StandardButton/StandardButton';
 
-function ButtonField({ className, ...rest }) {
+function ButtonField({ className, visible, ...rest }) {
   return (
-    <div className={cn('n2o-button-field n2o-form-group', className)}>
-      <StandardButton {...rest} />
-    </div>
+    visible && (
+      <div className={cn('n2o-button-field n2o-form-group', className)}>
+        <StandardButton {...rest} />
+      </div>
+    )
   );
 }
+
+ButtonField.propTypes = {
+  visible: PropTypes.bool,
+};
+
+ButtonField.defaultProps = {
+  visible: true,
+};
 
 export default ButtonField;
