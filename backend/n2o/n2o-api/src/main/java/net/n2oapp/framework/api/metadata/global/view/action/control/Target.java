287bfb132e20c982cb6e64d6e285a6cbe85eff74
estartcev <estartcev@i-novus.ru>
2020-08-06 10:10:33 +0300

    NNO-4895 исправлено открытие ссылок в новом окне

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/action/control/Target.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/action/control/Target.java
@@ -1,8 +1,23 @@
 package net.n2oapp.framework.api.metadata.global.view.action.control;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Тип сценария открытия ссылки
  */
 public enum Target {
-    self, newWindow, application
+    self("self"),
+    newWindow("_blank"),
+    application("application");
+
+    private String value;
+
+    Target(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return this.value;
+    }
 }
