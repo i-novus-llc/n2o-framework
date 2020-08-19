d3d8560571cb194fae9dd94f4f360c37b80f3f9b
ilebedenko <ilebedenko@i-novus.ru>
2020-08-19 18:09:31 +0300

    [CF][NNO-4553] - рефакторинг

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ShowModalCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ShowModalCompiler.java
@@ -3,19 +3,15 @@ package net.n2oapp.framework.config.metadata.compile.action;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
-import net.n2oapp.framework.api.metadata.compile.building.Placeholders;
 import net.n2oapp.framework.api.metadata.event.action.N2oShowModal;
 import net.n2oapp.framework.api.metadata.meta.ModelLink;
 import net.n2oapp.framework.api.metadata.meta.action.show_modal.ShowModal;
 import net.n2oapp.framework.api.metadata.meta.action.show_modal.ShowModalPayload;
-import net.n2oapp.framework.api.metadata.meta.saga.MessageSaga;
-import net.n2oapp.framework.api.metadata.meta.saga.MetaSaga;
 import net.n2oapp.framework.config.metadata.compile.context.ModalPageContext;
 import net.n2oapp.framework.config.metadata.compile.context.PageContext;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.Optional;
 
 import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
 import static net.n2oapp.framework.config.register.route.RouteUtil.convertPathToId;
@@ -66,14 +62,8 @@ public class ShowModalCompiler extends AbstractOpenPageCompiler<ShowModal, N2oSh
     private void compilePayload(ShowModal showModal, N2oShowModal source, CompileContext<?, ?> context, CompileProcessor p) {
         ShowModalPayload payload = showModal.getPayload();
         payload.setSize(source.getModalSize());
-        if (source.getScrollable() != null) {
-            payload.setScrollable(source.getScrollable());
-            Optional.ofNullable(showModal.getMeta())
-                    .map(MetaSaga::getMessages)
-                    .map(MessageSaga::getFields)
-                    .ifPresent(f -> f.values().forEach(rm -> rm.setPosition("fixed")));
-        } else
-            payload.setScrollable(p.resolve(Placeholders.property("n2o.api.action.show_modal.scrollable"), Boolean.class));
+        payload.setScrollable(p.cast(source.getScrollable(),
+                p.resolve(property("n2o.api.action.show_modal.scrollable"), Boolean.class)));
         payload.setCloseButton(true);
     }
 }
