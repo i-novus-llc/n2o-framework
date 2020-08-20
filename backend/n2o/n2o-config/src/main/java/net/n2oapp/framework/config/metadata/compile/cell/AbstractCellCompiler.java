55df45ac17beb999eb807d465cd78d55c10a8f33
rgazizov <rgazizov@i-novus.ru>
2020-08-20 15:59:48 +0300

    [NNO-5028] Компиляция tooltipFieldId перенесена в AbstractColumn

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/cell/AbstractCellCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/cell/AbstractCellCompiler.java
@@ -36,7 +36,12 @@ public abstract class AbstractCellCompiler<D extends N2oAbstractCell, S extends 
         compiled.setReactStyle(StylesResolver.resolveStyles(source.getStyle()));
         compiled.setJsonVisible(p.resolveJS(source.getVisible(), Boolean.class));
         compiled.setProperties(p.mapAttributes(source));
-        compiled.setTooltipFieldId(source.getTooltipFieldId());
+        ComponentScope componentScope = p.getScope(ComponentScope.class);
+        if (componentScope != null) {
+            AbstractColumn column = componentScope.unwrap(AbstractColumn.class);
+            if (column != null)
+                compiled.setTooltipFieldId(column.getTooltipFieldId());
+        }
         return compiled;
     }
 
