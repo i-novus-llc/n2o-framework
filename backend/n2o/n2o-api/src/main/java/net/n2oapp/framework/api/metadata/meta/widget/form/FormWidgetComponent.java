e6549374d96e8045da7fcdcb464b5ffe5067de60
ilebedenko <ilebedenko@i-novus.ru>
2020-08-21 12:53:44 +0300

    [BF][NNO-4915] - возвращена клиентская валидация формы

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/widget/form/FormWidgetComponent.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/widget/form/FormWidgetComponent.java
@@ -4,10 +4,12 @@ package net.n2oapp.framework.api.metadata.meta.widget.form;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import net.n2oapp.framework.api.data.validation.Validation;
 import net.n2oapp.framework.api.metadata.meta.fieldset.FieldSet;
 import net.n2oapp.framework.api.metadata.meta.widget.WidgetComponent;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Клиентская модель компонента формы
@@ -17,6 +19,8 @@ import java.util.List;
 public class FormWidgetComponent extends WidgetComponent {
     @JsonProperty
     private List<FieldSet> fieldsets;
+    @JsonProperty
+    private Map<String, List<Validation>> validation;
     @JsonProperty
     private String modelPrefix;
     @JsonProperty
