eb3bd9bf56c0d236948697de1ed017c72f9e4bf7
iryabov <iryabov@i-novus.ru>
2020-08-25 12:06:51 +0300

    NNO-5053 Fix params resolver for xml load 2

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/validation/WidgetValidatorTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/validation/WidgetValidatorTest.java
@@ -2,10 +2,7 @@ package net.n2oapp.framework.config.metadata.validation;
 
 import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
-import net.n2oapp.framework.config.metadata.pack.N2oPagesPack;
-import net.n2oapp.framework.config.metadata.pack.N2oQueriesPack;
-import net.n2oapp.framework.config.metadata.pack.N2oRegionsPack;
-import net.n2oapp.framework.config.metadata.pack.N2oWidgetsPack;
+import net.n2oapp.framework.config.metadata.pack.*;
 import net.n2oapp.framework.config.metadata.validation.standard.widget.WidgetValidator;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import net.n2oapp.framework.config.test.SourceValidationTestBase;
@@ -23,9 +20,10 @@ public class WidgetValidatorTest extends SourceValidationTestBase {
     @Override
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
-        builder.packs(new N2oPagesPack(), new N2oRegionsPack(), new N2oWidgetsPack(), new N2oQueriesPack());
+        builder.packs(new N2oPagesPack(), new N2oRegionsPack(), new N2oWidgetsPack(), new N2oQueriesPack(), new N2oDataProvidersPack());
         builder.validators(new WidgetValidator());
-        builder.sources(new CompileInfo("net/n2oapp/framework/config/metadata/compile/stub/utBlank.query.xml"));
+        builder.sources(new CompileInfo("net/n2oapp/framework/config/metadata/compile/stub/utBlank.query.xml"),
+                new CompileInfo("net/n2oapp/framework/config/metadata/validation/widget/testWidgetValidator.query.xml"));
     }
 
     /**
