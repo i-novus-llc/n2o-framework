c10c9bf5cf16167fad11772c2441196882dc6adf
estartcev <estartcev@i-novus.ru>
2020-09-15 13:48:28 +0300

    NNO-5124 bpm example added

--- examples/bpm/src/main/java/net/n2oapp/framework/examples/bpm/Application.java
+++ examples/bpm/src/main/java/net/n2oapp/framework/examples/bpm/Application.java
@@ -1,7 +1,11 @@
 package net.n2oapp.framework.examples.bpm;
 
+import org.camunda.bpm.engine.impl.cfg.IdGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 
 @SpringBootApplication
@@ -9,6 +13,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean   //Костыль т.к. фронт падает из за - в id
+    public IdGenerator getIdGenerator() {
+        return () -> UUID.randomUUID().toString().replaceAll("-", "_");
     }
 
 }
--- examples/bpm/src/main/java/net/n2oapp/framework/examples/bpm/BpmIntegration.java
+++ examples/bpm/src/main/java/net/n2oapp/framework/examples/bpm/BpmIntegration.java
@@ -7,10 +7,7 @@ import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class BpmIntegration {
