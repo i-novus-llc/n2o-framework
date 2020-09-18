4dedebe409cdd7e3640aa51a4719cb84a84e1da4
rgazizov <rgazizov@i-novus.ru>
2020-09-18 15:47:16 +0300

    [NNO-4918] Добавлен IndexScope в компиляцию ячейки

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/TileCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/TileCompiler.java
@@ -6,6 +6,7 @@ import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.global.view.widget.N2oTiles;
 import net.n2oapp.framework.api.metadata.meta.widget.Tiles;
 import net.n2oapp.framework.config.metadata.compile.ComponentCompiler;
+import net.n2oapp.framework.config.metadata.compile.IndexScope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,7 +24,7 @@ public class TileCompiler extends ComponentCompiler<Tiles.Tile, N2oTiles.Block> 
     public Tiles.Tile compile(N2oTiles.Block source, CompileContext<?, ?> context, CompileProcessor p) {
         Tiles.Tile tile = new Tiles.Tile();
         tile.setId(source.getId());
-        tile.setComponent(p.compile(source.getComponent(), context, p));
+        tile.setComponent(p.compile(source.getComponent(), context, p, new IndexScope()));
         source.setSrc(p.cast(source.getSrc(), tile.getComponent().getSrc()));
         compileComponent(tile, source, context, p);
         return tile;
