487aaeba4058d99141e9375bc6e12572309c7cea
asokolov <asokolov@i-novus.ru>
2020-09-30 15:43:26 +0300

    fix/NNO-5166 SearchBar in SimpleHeader fixes

--- frontend/demo/src/setupProxy.js
+++ frontend/demo/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy("/n2o", {
-      target: "http://docker.one:32439/sandbox/view/Ra1BB/",
+      target: "https://n2o.i-novus.ru/next/demo/",
       changeOrigin: true
     })
   );
