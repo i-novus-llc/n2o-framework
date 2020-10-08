d240c0267c2f4262132175d60642570cbbbcf7aa
rgazizov <rgazizov@i-novus.ru>
2020-10-08 12:04:24 +0300

    [NNO-5286] удалена проверка экшена

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/cell/RatingCellCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/cell/RatingCellCompiler.java
@@ -1,12 +1,9 @@
 package net.n2oapp.framework.config.metadata.compile.cell;
 
-import net.n2oapp.framework.api.exception.N2oException;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.column.cell.N2oRatingCell;
-import net.n2oapp.framework.api.metadata.meta.action.invoke.InvokeAction;
-import net.n2oapp.framework.config.metadata.compile.widget.MetaActions;
 import org.springframework.stereotype.Component;
 
 import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
@@ -29,14 +26,7 @@ public class RatingCellCompiler extends AbstractCellCompiler<N2oRatingCell, N2oR
         cell.setHalf(p.cast(source.getHalf(), p.resolve(property("n2o.api.cell.rating.half"), Boolean.class)));
         cell.setMax(p.cast(source.getMax(), p.resolve(property("n2o.api.cell.rating.max"), Integer.class)));
         cell.setReadonly(p.cast(source.getReadonly(), p.resolve(property("n2o.api.cell.rating.readonly"), Boolean.class)));
-        if (Boolean.FALSE.equals(source.getReadonly())) {
-            if (source.getActionId() != null) {
-                MetaActions metaActions = p.getScope(MetaActions.class);
-                if (metaActions != null && !(metaActions.get(source.getActionId()) instanceof InvokeAction))
-                    throw new N2oException("Rating cell supports only invoke action");
-            }
-            compileAction(cell, source, context, p);
-        }
+        if (Boolean.FALSE.equals(source.getReadonly())) compileAction(cell, source, context, p);
         return cell;
     }
 }
