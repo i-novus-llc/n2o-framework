445cbc8c1e260ec53d901b6bde548048a59350a9
estartcev <estartcev@i-novus.ru>
2020-10-21 14:47:48 +0300

    [NNO-5072] добавлен автотест автрибута children

---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/ChildrenColumnAT.java
@@ -0,0 +1,57 @@
+package net.n2oapp.framework.autotest.widget.table;
+
+import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.widget.table.TableWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * Автотест атрибута children="expand"
+ */
+public class ChildrenColumnAT extends AutoTestBase {
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oHeaderPack(), new N2oAllPagesPack(), new N2oAllDataPack());
+    }
+
+    @Test
+    public void testTable() {
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/widget/table/children_column/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/widget/table/children_column/test.query.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/blank.header.xml"));
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        page.breadcrumb().titleShouldHaveText("Children column");
+
+        TableWidget table = page.widget(TableWidget.class);
+        table.shouldExists();
+        table.paging().totalElementsShouldBe(4);
+        table.columns().rows().shouldHaveSize(10);
+
+        table.columns().rows().columnShouldHaveTexts(0, Arrays.asList("1", "11", "12", "13", "2", "21", "22", "23", "3", "4"));
+        table.columns().rows().columnShouldHaveTexts(1, Arrays.asList("test1", "name11", "name12", "name13", "test2", "name21", "name22", "name23", "test3", "test4"));
+    }
+
+}
