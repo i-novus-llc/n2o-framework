0268fda06012a20887aea398617aa0c3ad0ed1d9
rgazizov <rgazizov@i-novus.ru>
2020-10-22 14:07:17 +0300

    [NNO-5045] Добавлен автотест для диалога подтверждения

--- backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/component/page/N2oPage.java
+++ backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/component/page/N2oPage.java
@@ -2,8 +2,8 @@ package net.n2oapp.framework.autotest.impl.component.page;
 
 import com.codeborne.selenide.*;
 import net.n2oapp.framework.autotest.N2oSelenide;
-import net.n2oapp.framework.autotest.api.collection.Toolbar;
 import net.n2oapp.framework.autotest.api.collection.Alerts;
+import net.n2oapp.framework.autotest.api.collection.Toolbar;
 import net.n2oapp.framework.autotest.api.component.header.SimpleHeader;
 import net.n2oapp.framework.autotest.api.component.page.Page;
 import net.n2oapp.framework.autotest.impl.component.N2oComponent;
@@ -32,6 +32,11 @@ public class N2oPage extends N2oComponent implements Page {
     @Override
     public Dialog dialog(String title) {
         return new N2oDialog(element().$$(".modal-dialog").findBy(Condition.text(title)).parent());
+    }
+
+    @Override
+    public Popover popover(String title) {
+        return new N2oPopover(element().$$(".popover .popover-header").findBy(Condition.text(title)).parent());
     }
 
     @Override
@@ -134,6 +139,34 @@ public class N2oPage extends N2oComponent implements Page {
         }
     }
 
+    public static class N2oPopover implements Popover {
+        private final SelenideElement element;
+
+        public N2oPopover(SelenideElement element) {
+            this.element = element;
+        }
+
+        @Override
+        public void shouldBeVisible() {
+            element.isDisplayed();
+        }
+
+        @Override
+        public void shouldHaveText(String text) {
+            element.$(".popover-body").shouldHave(Condition.text(text));
+        }
+
+        @Override
+        public void click(String label) {
+            element.$$(".btn").findBy(Condition.text(label)).click();
+        }
+
+        @Override
+        public void shouldBeClosed(long timeOut) {
+            element.$(".popover-header .popover-body").waitWhile(Condition.exist, timeOut);
+        }
+    }
+
     public static class N2oTooltip implements Tooltip {
         private final SelenideElement element;
 
@@ -170,7 +203,7 @@ public class N2oPage extends N2oComponent implements Page {
 
         public UrlMatch(String regex) {
             super("urlMatch", true);
-            this.regex =regex;
+            this.regex = regex;
         }
 
         @Override
---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/PopoverAT.java
@@ -0,0 +1,100 @@
+package net.n2oapp.framework.autotest.control;
+
+import net.n2oapp.framework.autotest.api.component.button.StandardButton;
+import net.n2oapp.framework.autotest.api.component.control.InputText;
+import net.n2oapp.framework.autotest.api.component.field.StandardField;
+import net.n2oapp.framework.autotest.api.component.page.Page;
+import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Автотест для диалога подтверждения
+ */
+public class PopoverAT extends AutoTestBase {
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oHeaderPack(), new N2oAllPagesPack(), new N2oAllDataPack());
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/control/popover/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/control/popover/myObject.object.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/blank.header.xml"));
+    }
+
+    @Test
+    public void testDialog() {
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        FormWidget form = page.widget(FormWidget.class);
+        form.fields().shouldHaveSize(1);
+
+        StandardField input = form.fields().field("Наименование");
+        input.shouldExists();
+        input.control(InputText.class).val("testDialog");
+
+        StandardButton button = form.toolbar().bottomLeft().button("dialog");
+        button.shouldBeEnabled();
+
+        button.click();
+        Page.Dialog dialog = page.dialog("Предупреждение");
+        dialog.shouldBeVisible();
+        dialog.shouldHaveText("confirm-text");
+        dialog.click("Нет");
+        page.alerts().alert(0).shouldNotExists();
+
+        button.click();
+        dialog.shouldBeVisible();
+        dialog.shouldHaveText("confirm-text");
+        dialog.click("Да");
+        page.alerts().alert(0).shouldHaveText("Данные сохранены");
+    }
+
+    @Test
+    public void testPopover() {
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        FormWidget form = page.widget(FormWidget.class);
+        form.fields().shouldHaveSize(1);
+
+        StandardField input = form.fields().field("Наименование");
+        input.shouldExists();
+        input.control(InputText.class).val("testPopover");
+
+        StandardButton button = form.toolbar().bottomLeft().button("popover");
+        button.shouldBeEnabled();
+
+        button.click();
+        Page.Popover popover = page.popover("Предупреждение");
+        popover.shouldBeVisible();
+        popover.shouldHaveText("confirm-text");
+        popover.click("Нет");
+        page.alerts().alert(0).shouldNotExists();
+
+        button.click();
+        popover.shouldBeVisible();
+        popover.shouldHaveText("confirm-text");
+        popover.click("Да");
+        page.alerts().alert(0).shouldHaveText("Данные сохранены");
+    }
+
+}
