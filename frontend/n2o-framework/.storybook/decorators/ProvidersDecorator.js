9d33eaf7b86a110cfb6ea2e67e278a2dbfa900c6
ainur <akhmetovainur@gmail.com>
2020-10-22 17:42:28 +0300

    [BF][NNO-5343] - Настроить i18n в storybook

--- frontend/n2o-framework/.storybook/decorators/ProvidersDecorator.js
+++ frontend/n2o-framework/.storybook/decorators/ProvidersDecorator.js
@@ -11,6 +11,7 @@ import FactoryProvider from '../../src/core/factory/FactoryProvider';
 import createFactoryConfig from '../../src/core/factory/createFactoryConfig';
 import SecurityProvider from '../../src/core/auth/SecurityProvider';
 import OverlayPages from '../../src/components/core/OverlayPages';
+import "../i18n";
 
 const { store, securityConfig, history } = makeStore();
 
@@ -22,23 +23,23 @@ const OverlayPagesWithContext = compose(
   },
   props => ({
     defaultPromptMessage:
-      props.t(),
+      props.t('defaultPromptMessage'),
   })
 ))(OverlayPages);
 
 export default story => {
   return (
-      <Provider store={store}>
-        <SecurityProvider {...securityConfig}>
-          <FactoryProvider config={createFactoryConfig({})}>
-            <ConnectedRouter history={history}>
-              <div>
-                {story()}
-                <OverlayPagesWithContext />
-              </div>
-            </ConnectedRouter>
-          </FactoryProvider>
-        </SecurityProvider>
-      </Provider>
+    <Provider store={store}>
+      <SecurityProvider {...securityConfig}>
+        <FactoryProvider config={createFactoryConfig({})}>
+          <ConnectedRouter history={history}>
+            <div>
+              {story()}
+              <OverlayPagesWithContext />
+            </div>
+          </ConnectedRouter>
+        </FactoryProvider>
+      </SecurityProvider>
+    </Provider>
   );
 };
