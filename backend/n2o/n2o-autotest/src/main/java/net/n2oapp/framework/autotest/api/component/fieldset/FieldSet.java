83b63f3d8a864cab0be32966ee042fb98c1f16de
Игорь Рябов <iryabov@i-novus.ru>
2020-11-02 15:50:33 +0300

    Revert "Merge branch 'sub/NNO-5430' into 'master'"
    This reverts merge request !1772

--- backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/api/component/fieldset/FieldSet.java
+++ backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/api/component/fieldset/FieldSet.java
@@ -7,7 +7,4 @@ import net.n2oapp.framework.autotest.api.component.Component;
  */
 public interface FieldSet extends Component {
     void shouldBeEmpty();
-
-    void shouldHaveLabel(String label);
-
 }
