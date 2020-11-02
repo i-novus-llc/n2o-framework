53c619ae0928607c3e67ed1cd9fb08a1184df6cd
rgazizov <rgazizov@i-novus.ru>
2020-11-02 17:22:18 +0300

    [NNO-5446] Реализован контрол ввода чисел "плюс-минус"

---
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/NumberPickerCompiler.java
@@ -0,0 +1,39 @@
+package net.n2oapp.framework.config.metadata.compile.control;
+
+import net.n2oapp.framework.api.metadata.Source;
+import net.n2oapp.framework.api.metadata.compile.CompileContext;
+import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
+import net.n2oapp.framework.api.metadata.control.N2oNumberPicker;
+import net.n2oapp.framework.api.metadata.meta.control.NumberPicker;
+import org.springframework.stereotype.Component;
+
+import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
+
+/**
+ * Компиляция NumberPicker
+ */
+@Component
+public class NumberPickerCompiler extends FieldCompiler<NumberPicker, N2oNumberPicker> {
+
+    @Override
+    public Class<? extends Source> getSourceClass() {
+        return N2oNumberPicker.class;
+    }
+
+    @Override
+    public NumberPicker compile(N2oNumberPicker source, CompileContext<?, ?> context, CompileProcessor p) {
+        NumberPicker numberPicker = new NumberPicker();
+        compileField(numberPicker, source, context, p);
+        numberPicker.setMin(source.getMin());
+        numberPicker.setMax(source.getMax());
+        numberPicker.setStep(p.cast(source.getStep(),
+                p.resolve(property("n2o.api.control.number_picker.step"), Integer.class)));
+        //todo default-value
+        return numberPicker;
+    }
+
+    @Override
+    protected String getSrcProperty() {
+        return "n2o.api.control.number_picker.src";
+    }
+}
---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/io/control/v2/NumberPickerIOv2Test.java
@@ -0,0 +1,20 @@
+package net.n2oapp.framework.config.io.control.v2;
+
+import net.n2oapp.framework.config.io.control.NumberPickerIOv2;
+import net.n2oapp.framework.config.io.widget.form.FormElementIOV4;
+import net.n2oapp.framework.config.reader.control.N2oStandardControlReaderTestBase;
+import net.n2oapp.framework.config.selective.ION2oMetadataTester;
+import org.junit.Test;
+
+/**
+ * Тестирование чтения/записи компонента NumberPicker
+ */
+public class NumberPickerIOv2Test extends N2oStandardControlReaderTestBase {
+
+    @Test
+    public void test() {
+        ION2oMetadataTester tester = new ION2oMetadataTester();
+        tester.ios(new NumberPickerIOv2(), new FormElementIOV4());
+        assert tester.check("net/n2oapp/framework/config/io/control/v2/testNumberPicker.widget.xml");
+    }
+}
