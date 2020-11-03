859f660e3bd6c6fd7c5f68bd1332993c8295f31e
rgazizov <rgazizov@i-novus.ru>
2020-11-03 12:16:34 +0300

    [NNO-5411] fix

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ClearActionCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ClearActionCompiler.java
@@ -30,7 +30,7 @@ public class ClearActionCompiler extends AbstractActionCompiler<ClearAction, N2o
         clearAction.setType(p.resolve(property("n2o.api.action.clear.type"), String.class));
         clearAction.getPayload().setPrefixes(p.cast(source.getModel(), new String[]{ReduxModel.EDIT.getId()}));
         clearAction.getPayload().setKey(initTargetWidget(source, context, p));
-        if (source.getCloseOnSuccess() != null && Objects.equals(source.getCloseOnSuccess(), true)) {
+        if (source.getCloseOnSuccess() != null && Boolean.TRUE.equals(source.getCloseOnSuccess())) {
             if (clearAction.getMeta() == null) clearAction.setMeta(new MetaSaga());
             clearAction.getMeta().setModalsToClose(1);
         }
