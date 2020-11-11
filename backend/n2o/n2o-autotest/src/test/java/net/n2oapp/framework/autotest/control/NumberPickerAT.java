e248169d06f038fc6aa09c4d2d36d8220313db6e
rgazizov <rgazizov@i-novus.ru>
2020-11-11 07:03:49 +0300

    [NNO-5447] Добавлен автотест для number-picker

---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/NumberPickerAT.java
@@ -0,0 +1,64 @@
+package net.n2oapp.framework.autotest.control;
+
+import net.n2oapp.framework.autotest.api.component.control.InputText;
+import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.*;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Автотест компонента ввода числа из диапазона
+ */
+public class NumberPickerAT extends AutoTestBase {
+
+    private SimplePage page;
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+//        headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "false"));
+
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/control/number_picker/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/blank.header.xml"));
+
+        page = open(SimplePage.class);
+        page.shouldExists();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oPagesPack(), new N2oHeaderPack(), new N2oWidgetsPack(), new N2oFieldSetsPack(), new N2oControlsPack());
+    }
+
+    @Test
+    public void testNumberPicker() {
+        InputText input = page.widget(FormWidget.class).fields().field("numberPicker")
+                .control(InputText.class);
+//        input.shouldExists();
+//
+//        input.shouldHavePlaceholder("Введите текст");
+//        input.shouldBeEmpty();
+//        input.val("test-value");
+//        input.shouldHaveValue("test-value");
+//        // превышение указанной длины
+//        input.val("test1test2test3");
+//        input.shouldHaveValue("test1test2");
+//        // проверка меры измерения
+//        input.shouldHaveMeasure();
+//        input.measureShouldHaveText("шт.");
+    }
+
+}
