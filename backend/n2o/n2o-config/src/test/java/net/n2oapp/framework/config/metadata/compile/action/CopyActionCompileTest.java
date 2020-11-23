2fa480329c04f55050eea0695863fb77b41e9d30
ilebedenko <ilebedenko@i-novus.ru>
2020-11-23 19:14:28 +0300

    [CF][NNO-5526] - добавлен атрибут target-page-id

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/action/CopyActionCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/action/CopyActionCompileTest.java
@@ -78,6 +78,10 @@ public class CopyActionCompileTest extends SourceCompileTestBase {
         assertThat(action.getPayload().getTarget().getField(), is("dictionary.id"));
         assertThat(action.getPayload().getMode(), is(CopyMode.replace));
         assertThat(action.getMeta().getModalsToClose(), is(1));
+
+        action = (CopyAction) page.getActions().get("menuItem0");
+        assertThat(action.getPayload().getSource().getKey(), is("modal_table1"));
+        assertThat(action.getPayload().getTarget().getKey(), is("page_form"));
     }
 
     @Test
