853734e672548e7539ea572f2e42e3f17120133b
Токарев Марк Вячеславович <mtokarew@dtsoft.io>
2020-11-27 19:16:41 +0300

    Resolve NNO-5533 "Sub/" доработка ModalPage, нужны новые автотесты

--- frontend/n2o-framework/src/components/core/ModalPage.jsx
+++ frontend/n2o-framework/src/components/core/ModalPage.jsx
@@ -4,21 +4,19 @@ import Modal from 'reactstrap/lib/Modal';
 import ModalHeader from 'reactstrap/lib/ModalHeader';
 import ModalBody from 'reactstrap/lib/ModalBody';
 import ModalFooter from 'reactstrap/lib/ModalFooter';
-import { compose } from 'recompose';
 import Page from './Page';
 import cn from 'classnames';
 import Toolbar from '../buttons/Toolbar';
 import Spinner from '../snippets/Spinner/Spinner';
+import { compose } from 'recompose';
 import withOverlayMethods from './withOverlayMethods';
-
-const style = { zIndex: 10 };
-
 /**
  * Компонент, отображающий модальное окно
  * @reactProps {string} pageId - id пейджа
  * @reactProps {string} name - имя модалки
  * @reactProps {boolean} visible - отображается модалка или нет
  * @reactProps {string} size - размер('sm' или 'lg')
+ * @reactProps {string | bool} backdrop -  наличие фона модального окна  false/true/'static'
  * @reactProps {string} headerTitle - заголовок в хэдере
  * @reactProps {boolean} closeButton - Есть кнопка закрытия или нет
  * @reactProps {object} actions - объект экшнов
@@ -51,6 +49,10 @@ function ModalPage(props) {
     disabled,
     scrollable,
     prompt,
+    className,
+    backdrop,
+    style,
+    hasHeader,
     ...rest
   } = props;
 
@@ -69,16 +71,20 @@ function ModalPage(props) {
           isOpen={visible}
           toggle={() => rest.closeOverlay(prompt)}
           size={size}
-          backdrop={false}
           style={style}
           scrollable={scrollable}
+          className={className}
+          backdrop={backdrop}
         >
-          <ModalHeader
-            className={classes}
-            toggle={() => rest.closeOverlay(prompt)}
-          >
-            {headerTitle}
-          </ModalHeader>
+          {hasHeader && (
+            <ModalHeader
+              className={classes}
+              toggle={() => rest.closeOverlay(prompt)}
+            >
+              {headerTitle}
+            </ModalHeader>
+          )}
+
           <ModalBody className={classes}>
             {pageUrl ? (
               <Page
@@ -92,6 +98,7 @@ function ModalPage(props) {
               rest.renderFromSrc(src)
             ) : null}
           </ModalBody>
+
           {toolbar && (
             <ModalFooter className={classes}>
               <div
@@ -155,11 +162,30 @@ ModalPage.propTypes = {
    * Флаг активности
    */
   disabled: PropTypes.bool,
+  /**
+   * Класс модального окна
+   */
+  className: PropTypes.string,
+  /**
+   * Объект стилей
+   */
+  style: PropTypes.object,
+  /**
+   * Значение для отоборажения хедера
+   */
+  hasHeader: PropTypes.bool,
+  /**
+   * Фон модального окна
+   */
+  backdrop: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['static'])]),
 };
 
 ModalPage.defaultProps = {
   size: 'lg',
+  headerTitle: 'Модальное окно',
   disabled: false,
+  hasHeader: false,
+  backdrop: true,
 };
 
 ModalPage.contextTypes = {
