85e21100bcd221bb0f5335381fb81348159abbf7
ilebedenko <ilebedenko@i-novus.ru>
2020-12-01 19:05:31 +0300

    [CF][NNO-5440] - fix

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/ImageFieldAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/ImageFieldAT.java
@@ -1,6 +1,6 @@
 package net.n2oapp.framework.autotest.control;
 
-import net.n2oapp.framework.autotest.TextPosition;
+import net.n2oapp.framework.api.metadata.meta.control.TextPosition;
 import net.n2oapp.framework.autotest.api.component.page.SimplePage;
 import net.n2oapp.framework.autotest.api.component.snippet.Image;
 import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
@@ -50,14 +50,14 @@ public class ImageFieldAT extends AutoTestBase {
         image.shouldHaveDescription("описание");
         image.shouldHaveUrl("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAlCAMAAAADS4u8AAAAnFBMVEX////ZbFrptDj////ptDj///9YuKvZbFrptDj///9YuKvZbFrptDj///9YuKvptDj////ZbFr////ptDj///9YuKvZbFrptDj///9YuKvptDj////ZbFrptDj///9YuKvptDj///9YuKvZbFrptDj///9YuKvZbFr///9YuKvptDj///9YuKvZbFrptDj///9YuKvZbFrptDj///90gGlOAAAAMHRSTlMAEBAQICAwMDAwQEBAQFBQUGBgcHCAgICAkJCQoKCgsLCwwMDAwNDQ0ODg4PDw8PDEXJ+/AAAA+UlEQVQYGc3B61aCQBSA0U/ICtGki4ZhGKmlYXI57/9uzbgwRqHF/KnV3hBl0rTxUHrL4mjeg1BaZX3gvajNIZN2D+AXph7ygxD8wuSTSrs7uCoMnxBIq40LzIvaFAiSdVPsok2XR7f8B7O8026CNsttTFB2uY0tSm4HZZvbeEO5yW0M0AaL1y6LS36TOzJQcYaKw6lIToRoT+XBysEQyZkQeCwrKwyZnEmBfXl0QU0agPLbkFoqZxLgo6zsHWqBnMo8YFxW7jGN4rUh9tCun1fKy5i/0B91cTlwY+kWoUViI0QRKymK2EFJxUaCEoiFzEPz4nWXuI/yBYSItStrEp20AAAAAElFTkSuQmCC");
         image.shouldHaveWidth(40);
-        image.shouldHaveTextPosition(TextPosition.RIGHT);
+        image.shouldHaveTextPosition(TextPosition.right);
 
         image = page.widget(FormWidget.class).snippet(1, Image.class);
         image.shouldExists();
         image.shouldHaveTitle("Заголовок");
         image.shouldHaveDescription("описание");
         image.shouldHaveUrl("https://i-novus.ru/assets/3c502870/images/logo.png");
-        image.shouldHaveTextPosition(TextPosition.BOTTOM);
+        image.shouldHaveTextPosition(TextPosition.bottom);
     }
 
 }
