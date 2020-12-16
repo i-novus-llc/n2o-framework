21bf5a506821874240281c7ae04ac0dbc081029f
aahmetov <aahmetov@i-novus.ru>
2020-12-16 16:27:01 +0300

    sub/NNO-5753 masked-input clearOnBlur

--- frontend/n2o-framework/src/components/controls/InputMask/InputMask.test.js
+++ frontend/n2o-framework/src/components/controls/InputMask/InputMask.test.js
@@ -23,4 +23,30 @@ describe('<InputMask />', () => {
     const { wrapper } = setup();
     expect(wrapper.find('input')).toHaveLength(1);
   });
+  it('should call onBlur with prop', () => {
+    const onBlur = jest.fn();
+    const { wrapper } = setup({ onBlur });
+    const testValue = '+7999999999';
+    const blurEvent = {
+      preventDefault() {},
+      nativeEvent: {
+        target: { value: testValue },
+      },
+    };
+    const input = wrapper.find('input');
+    input.simulate('blur', blurEvent);
+    expect(onBlur).toBeCalledWith(testValue);
+  });
+  it('should call onChange with prop', () => {
+    const onChange = jest.fn();
+    const { wrapper } = setup({ onChange });
+    const testValue = '+7999999999';
+    const blurEvent = {
+      preventDefault() {},
+      target: { value: testValue },
+    };
+    const input = wrapper.find('input');
+    input.simulate('change', blurEvent);
+    expect(onChange).toBeCalledWith(testValue);
+  });
 });
