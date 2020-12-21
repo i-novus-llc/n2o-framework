150c7787a7659d34b87e2c95a982ed237db49a5c
estartcev <estartcev@i-novus.ru>
2020-12-21 11:18:10 +0300

    [NNO-5750] Переделано на использование jdbcTemplate

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/route/jdbc/JDBCRouteRepository.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/route/jdbc/JDBCRouteRepository.java
@@ -8,13 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.SerializationUtils;
 
 import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Сохранение и загрузка данных RouteRegister в БД
@@ -23,49 +23,28 @@ public class JDBCRouteRepository implements ConfigRepository<RouteInfoKey, Compi
 
     private static final Logger logger = LoggerFactory.getLogger(JDBCRouteRepository.class);
 
-    @Autowired(required = false)
-    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Value("${n2o.config.register.jdbc.table-name:route_repository}")
     private String tableName;
     @Value("${n2o.config.register.jdbc.create-table:false}")
     private Boolean createTable;
 
-    @Value("${spring.datasource.url}")
-    private String jdbcUrl;
-    @Value("${spring.datasource.username}")
-    private String jdbcUser;
-    @Value("${spring.datasource.password}")
-    private String jdbcPassword;
-
     @Override
     public CompileContext save(RouteInfoKey key, CompileContext value) {
-        String insertSQL = "INSERT INTO " + tableName + " VALUES (?, ?, ?, ?)";
-        String updateSQL = "UPDATE " + tableName + " SET context=? WHERE url=? AND class=?";
+        final String insertSQL = "INSERT INTO " + tableName + " VALUES (?, ?, ?, ?)";
+        final String updateSQL = "UPDATE " + tableName + " SET context=? WHERE url=? AND class=?";
 
-        try (Connection connection = getNewConnection()) {
-            final byte[] serialValue = SerializationUtils.serialize(value);
-            PreparedStatement statement = connection.prepareStatement(updateSQL);
-            statement.setBytes(1, serialValue);
-            statement.setString(2, key.getUrlMatching());
-            statement.setString(3, key.getCompiledClass().getName());
-            int cnt = statement.executeUpdate();
-            statement.close();
+        final byte[] serialValue = SerializationUtils.serialize(value);
+        int cnt = jdbcTemplate.update(updateSQL, serialValue, key.getUrlMatching(), key.getCompiledClass().getName());
 
-            if (cnt < 1) {
-                statement = connection.prepareStatement(insertSQL);
-                statement.setString(1, UUID.randomUUID().toString());
-                statement.setString(2, key.getUrlMatching());
-                statement.setString(3, key.getCompiledClass().getName());
-                statement.setBytes(4, serialValue);
-                statement.executeUpdate();
-                statement.close();
-                logger.info(String.format("Inserted route: '%s' to [%s]", value, key.getUrlMatching()));
-            } else {
-                logger.info(String.format("Updated route: '%s' to [%s]", value, key.getUrlMatching()));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (cnt < 1) {
+            jdbcTemplate.update(insertSQL, UUID.randomUUID().toString(), key.getUrlMatching(),
+                    key.getCompiledClass().getName(), serialValue);
+            logger.info(String.format("Inserted route: '%s' to [%s]", value, key.getUrlMatching()));
+        } else {
+            logger.info(String.format("Updated route: '%s' to [%s]", value, key.getUrlMatching()));
         }
 
         return value;
@@ -73,93 +52,56 @@ public class JDBCRouteRepository implements ConfigRepository<RouteInfoKey, Compi
 
     @Override
     public void clear(Predicate<? super RouteInfoKey> filter) {
-        String selectSQL = "SELECT id, url, class FROM " + tableName;
-        String deleteSQL = "DELETE FROM " + tableName + " WHERE id in (?)";
-        try (Connection connection = getNewConnection()) {
-            PreparedStatement statement = connection.prepareStatement(selectSQL);
-            statement.execute();
-            ResultSet resultSet = statement.getResultSet();
-            StringBuilder deleteList = new StringBuilder();
-            while (resultSet.next()) {
-                RouteInfoKey key = getKey(resultSet.getString(2), resultSet.getString(3));
-                if (filter.test(key)) deleteList.append(resultSet.getString(1)).append(",");
-            }
-            statement.close();
-            if (deleteList.length() > 1) {
-                deleteList.setLength(deleteList.length() - 1);
-                statement = connection.prepareStatement(deleteSQL);
-                statement.setString(1, deleteList.toString());
-                statement.executeUpdate();
-                statement.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        final String selectSQL = "SELECT id, url, class FROM " + tableName;
+        final String deleteSQL = "DELETE FROM " + tableName + " WHERE id in (?)";
+
+        StringBuilder deleteList = new StringBuilder();
+
+        jdbcTemplate.queryForList(selectSQL).forEach(row -> {
+            if (filter.test(getKey(row))) deleteList.append(row.get("id")).append(",");
+        });
+
+        if (deleteList.length() > 1) {
+            deleteList.setLength(deleteList.length() - 1);
+            jdbcTemplate.update(deleteSQL, deleteList.toString());
         }
     }
 
     @Override
     public Map<RouteInfoKey, CompileContext> getAll() {
-        String selectSQL = "SELECT url, class, context FROM " + tableName;
-        try (Connection connection = getNewConnection()) {
-            PreparedStatement statement = connection.prepareStatement(selectSQL);
-            statement.execute();
-            ResultSet resultSet = statement.getResultSet();
-            Map<RouteInfoKey, CompileContext> result = new HashMap<>();
-            while (resultSet.next()) {
-                RouteInfoKey key = getKey(resultSet.getString(1), resultSet.getString(2));
-                CompileContext context = (CompileContext) SerializationUtils.deserialize(resultSet.getBytes(3));
-                result.put(key, context);
-            }
-            statement.close();
-            logger.info(String.format("Returned %s routes.", result.size()));
-            return result;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return new HashMap<>();
+        final String selectSQL = "SELECT url, class, context FROM " + tableName;
+
+        List<Map<String, Object>> resultSet = jdbcTemplate.queryForList(selectSQL);
+        Map<RouteInfoKey, CompileContext> result = resultSet.stream().collect(Collectors.toMap(this::getKey, row ->
+                (CompileContext) SerializationUtils.deserialize((byte[]) row.get("context")), (a, b) -> b));
+
+        logger.info(String.format("Returned %s routes.", result.size()));
+        return result;
     }
 
     @PostConstruct
     public void createTable() {
-        if (!createTable) return;
+        if (createTable) {
+            final String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName +
+                    " (id uuid PRIMARY KEY, url char(255), class char(255), context binary)";
 
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (id uuid PRIMARY KEY, url char(255), class char(255), context binary)";
-
-        try (Connection connection = getNewConnection()) {
-            PreparedStatement statement = connection.prepareStatement(createTableSQL);
-            statement.executeUpdate();
-            statement.close();
+            jdbcTemplate.execute(createTableSQL);
             logger.info(String.format("Created table %s.", tableName));
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
-    public int getRecordCount() {
+    public Integer getRecordCount() {
         String selectSQL = "SELECT count(id) FROM " + tableName;
-        try (Connection connection = getNewConnection()) {
-            PreparedStatement statement = connection.prepareStatement(selectSQL);
-            statement.execute();
-            ResultSet resultSet = statement.getResultSet();
 
-            if (resultSet.next()) return resultSet.getInt(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return -1;
+        return jdbcTemplate.queryForObject(selectSQL, Integer.class);
     }
 
-    private RouteInfoKey getKey(String url, String className) {
+    private RouteInfoKey getKey(Map<String, Object> map) {
         try {
-            Class<? extends Compiled> clazz = Class.forName(className).asSubclass(Compiled.class);
-            return new RouteInfoKey(url, clazz);
+            Class<? extends Compiled> clazz = Class.forName("" + map.get("class")).asSubclass(Compiled.class);
+            return new RouteInfoKey("" + map.get("url"), clazz);
         } catch (ClassNotFoundException e) {
             return null;
         }
-    }
-
-    private Connection getNewConnection() throws SQLException {
-        if (dataSource != null) return dataSource.getConnection();
-        return DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPassword);
     }
 }
