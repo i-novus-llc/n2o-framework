f0c7115ecd3983477f4aa9dba6791e524cd69e06
Григорчук Константин Анатольевич <kgrigorchuk@i-novus.ru>
2021-01-22 19:42:38 +0000

    Merge branch 'fix/NNO-5769' into 'master'
    [BF][NNO-5769] не уходил запрос без валидации с model begin-end
    
    See merge request open/n2o-framework!2085
    
    (cherry picked from commit 7991c71855a36f0f7814daa28e195e254cce712b)
    
    30f4cb32 fix/NNO-5769 не уходил запрос без валидации с model begin-end
    473d0b3b fix/NNO-5769 не уходил запрос !visible field с валидацией

--- frontend/n2o-framework/src/sagas/fetch.js
+++ frontend/n2o-framework/src/sagas/fetch.js
@@ -5,6 +5,7 @@ import keys from 'lodash/keys';
 import first from 'lodash/first';
 import includes from 'lodash/includes';
 import split from 'lodash/split';
+import isEmpty from 'lodash/isEmpty';
 
 import {
   fetchStart,
@@ -33,11 +34,14 @@ export default function* fetchSaga(
 
   const widgetId = get(action, 'payload.widgetId');
   const widgetValidation = get(state, `widgets.${widgetId}.validation`);
+  const hasWidgetValidation = !isEmpty(widgetValidation);
   const validationKey = first(keys(widgetValidation));
   const treePath = includes(split(validationKey, ''), '.');
   const registeredFields = get(state, `form.${widgetId}.registeredFields`);
   const isValidForm =
     get(registeredFields, [validationKey, 'message']) === null;
+  const isVisibleForm =
+    get(registeredFields, [validationKey, 'visible']) === true;
   const treePathFields = registeredFields && treePath;
 
   const isRangeModel =
@@ -45,10 +49,12 @@ export default function* fetchSaga(
   const isValidRange = isValidRangeModel(modelValues);
 
   try {
-    if ((isRangeModel && !isValidRange) || (treePathFields && !isValidForm)) {
+    if (
+      (isRangeModel && !isValidRange && hasWidgetValidation) ||
+      (treePathFields && !isValidForm && isVisibleForm)
+    ) {
       return;
     }
-
     yield put(fetchStart(fetchType, options));
     const response = yield call(apiProvider, fetchType, options);
     yield put(fetchEnd(fetchType, options, response));
