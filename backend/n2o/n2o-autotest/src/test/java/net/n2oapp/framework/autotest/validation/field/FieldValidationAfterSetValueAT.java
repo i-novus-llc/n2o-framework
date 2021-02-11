bcd09df277a35b7619dd2f9b7f439d352b146583
Григорчук Константин Анатольевич <kgrigorchuk@i-novus.ru>
2021-02-11 08:46:14 +0000

    Merge branch 'fix/NNO-5978' into 'master'
    fix/NNO-5978 Некорректное срабатываение валидации при setValue
    
    See merge request open/n2o-framework!2235
    
    (cherry picked from commit 5fc7a82d9c80a46d7f0e3dba9b9daa35aaa92934)
    
    3ba39b6f fix/NNO-5978 Некорректное срабатываение валидации при setValue
    5cf52c49 [CF][NNO-5996] - автотест для некорректного срабатывания валидации
    96c45450 Merge branch 'fix/NNO-5978' of https://git.i-novus.ru/open/n2o-framework into fix/NNO-5978
    d59e1708 Merge branch 'sub/NNO-5996' into fix/NNO-5978

---
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/validation/field/FieldValidationAfterSetValueAT.java
@@ -0,0 +1,74 @@
+package net.n2oapp.framework.autotest.validation.field;
+
+import com.codeborne.selenide.Condition;
+import net.n2oapp.framework.autotest.api.collection.Fields;
+import net.n2oapp.framework.autotest.api.component.control.InputText;
+import net.n2oapp.framework.autotest.api.component.control.RadioGroup;
+import net.n2oapp.framework.autotest.api.component.field.StandardField;
+import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.run.AutoTestBase;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
+import net.n2oapp.framework.config.selective.CompileInfo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Автотест валидации для поля после set-value
+ */
+public class FieldValidationAfterSetValueAT extends AutoTestBase {
+
+    @BeforeAll
+    public static void beforeClass() {
+        configureSelenide();
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oHeaderPack(), new N2oAllPagesPack(), new N2oAllDataPack());
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/blank.header.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/validation/field/after_set_value/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/validation/field/after_set_value/test.query.xml"));
+    }
+
+    @Test
+    public void testValidation() {
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+
+        Fields fields = page.widget(FormWidget.class).fields();
+        RadioGroup radio = fields.field("type").control(RadioGroup.class);
+        radio.shouldExists();
+        StandardField nameField = fields.field("name");
+        InputText name = nameField.control(InputText.class);
+        name.shouldExists();
+
+        radio.shouldBeChecked("Type1");
+        name.shouldHaveValue("Test");
+        name.shouldBeDisabled();
+
+        radio.check("Type2");
+        name.shouldBeEmpty();
+        name.shouldBeEnabled();
+
+        name.clear();
+        nameField.shouldHaveValidationMessage(Condition.exist);
+
+        radio.check("Type1");
+        name.shouldHaveValue("Test");
+        name.shouldBeDisabled();
+        nameField.shouldHaveValidationMessage(Condition.empty);
+    }
+}
