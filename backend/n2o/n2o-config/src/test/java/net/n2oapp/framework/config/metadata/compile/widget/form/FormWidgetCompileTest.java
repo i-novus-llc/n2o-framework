a46d421673bba60cc9b7638cdb14aba03520c560
rgalina <rgalina@i-novus.ru>
2021-02-20 14:46:58 +0300

    [NNO-6013] fix test

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/widget/form/FormWidgetCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/widget/form/FormWidgetCompileTest.java
@@ -21,11 +21,16 @@ import net.n2oapp.framework.api.metadata.meta.widget.RequestMethod;
 import net.n2oapp.framework.api.metadata.meta.widget.form.Form;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.io.page.SimplePageElementIOv2;
+import net.n2oapp.framework.config.io.page.StandardPageElementIOv2;
+import net.n2oapp.framework.config.io.page.v3.SimplePageElementIOv3;
+import net.n2oapp.framework.config.io.page.v3.StandardPageElementIOv3;
+import net.n2oapp.framework.config.io.region.LineRegionIOv1;
 import net.n2oapp.framework.config.metadata.compile.context.ActionContext;
 import net.n2oapp.framework.config.metadata.compile.context.PageContext;
 import net.n2oapp.framework.config.metadata.compile.context.QueryContext;
 import net.n2oapp.framework.config.metadata.compile.context.WidgetContext;
 import net.n2oapp.framework.config.metadata.compile.page.SimplePageCompiler;
+import net.n2oapp.framework.config.metadata.compile.page.StandardPageCompiler;
 import net.n2oapp.framework.config.metadata.pack.*;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import net.n2oapp.framework.config.test.SourceCompileTestBase;
@@ -53,7 +58,9 @@ public class FormWidgetCompileTest extends SourceCompileTestBase {
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
         builder.packs(new N2oAllDataPack(), new N2oFieldSetsPack(), new N2oControlsPack(), new N2oCellsPack(), new N2oActionsPack(),
-                new N2oWidgetsPack(), new N2oPagesPack(), new N2oRegionsPack())
+                new N2oWidgetsPack(), new N2oRegionsPack())
+                .ios(new SimplePageElementIOv3(), new StandardPageElementIOv3(), new SimplePageElementIOv2(), new StandardPageElementIOv2())
+                .compilers(new SimplePageCompiler(), new StandardPageCompiler())
                 .sources(new CompileInfo("net/n2oapp/framework/config/metadata/compile/widgets/testTable4Compile.query.xml"),
                         new CompileInfo("net/n2oapp/framework/config/metadata/compile/stub/utBlank.object.xml"));
     }
@@ -224,7 +231,7 @@ public class FormWidgetCompileTest extends SourceCompileTestBase {
     }
 
     @Test
-    public void testForm () {
+    public void testFormAsFilter () {
         StandardPage page = (StandardPage) compile("net/n2oapp/framework/config/metadata/compile/widgets/testFormAsFilter.page.xml",
                 "net/n2oapp/framework/config/metadata/compile/widgets/testFormAsFilter.query.xml")
                 .get(new PageContext("testFormAsFilter"));
