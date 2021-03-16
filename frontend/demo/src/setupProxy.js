866f14ab9b9e1200cb4531cd7d565eee10b755bc
Григорчук Константин Анатольевич <kgrigorchuk@i-novus.ru>
2021-03-16 14:34:32 +0000

    Merge branch 'fix/NNO-6089' into 'master'
    [BF][NNO-6089] fix HelpPopover для Safari
    
    See merge request open/n2o-framework!2338
    
    (cherry picked from commit dc8e9c9078449502d58fd220057dec490367134e)
    
    6599ab44 fix/NNO-6089 fix HelpPopover для Safari
    1fbce542 fix/NNO-6089 fix HelpPopover для safari

--- frontend/demo/src/setupProxy.js
+++ frontend/demo/src/setupProxy.js
@@ -6,34 +6,34 @@ module.exports = function(app) {
   //   res.send();
   // });
 
-  app.get("/n2o/config", (req, res) => {
-    res.send({
-      user: {
-        username: null,
-        testProperty: "testProperty"
-      },
-      messages: {},
-      menu: {
-        localeSelect: true,
-        brand: "N2O",
-        color: "inverse",
-        fixed: false,
-        collapsed: true,
-        search: false,
-        items: [
-          {
-            id: "menuItem0",
-            label: "Контакты",
-            href: "/proto",
-            linkType: "inner",
-            type: "link"
-          }
-        ],
-        extraItems: []
-      },
-      locale: 'en'
-    });
-  });
+  // app.get("/n2o/config", (req, res) => {
+  //   res.send({
+  //     user: {
+  //       username: null,
+  //       testProperty: "testProperty"
+  //     },
+  //     messages: {},
+  //     menu: {
+  //       localeSelect: true,
+  //       brand: "N2O",
+  //       color: "inverse",
+  //       fixed: false,
+  //       collapsed: true,
+  //       search: false,
+  //       items: [
+  //         {
+  //           id: "menuItem0",
+  //           label: "Контакты",
+  //           href: "/proto",
+  //           linkType: "inner",
+  //           type: "link"
+  //         }
+  //       ],
+  //       extraItems: []
+  //     },
+  //     locale: 'en'
+  //   });
+  // });
 
   app.use(
     proxy("/n2o", {
