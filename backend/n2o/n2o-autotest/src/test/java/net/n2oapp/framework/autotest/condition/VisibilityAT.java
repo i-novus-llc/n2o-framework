22a87997b581223132d463a810355c3f6f67db2d
Соколов Алексей <asokolov@i-novus.ru>
2021-03-25 10:49:54 +0300

    [BF][NNO-5972] добавил параметр для аватокомплита

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/condition/VisibilityAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/condition/VisibilityAT.java
@@ -14,8 +14,8 @@ import net.n2oapp.framework.config.metadata.pack.N2oHeaderPack;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 /**
  * Автотест dependencies-visibility
