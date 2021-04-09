f630c7f9665860716abf806a0fd655489031daa9
rgalina <rgalina@i-novus.ru>
2021-04-09 15:46:44 +0300

    [NNO-6082] Добавление  примеров N2O

--- frontend/docs/sidebars.js
+++ frontend/docs/sidebars.js
@@ -2,14 +2,51 @@ module.exports = {
     docs: [
         {
             type: 'category',
-            label: 'Docusaurus Tutorial',
+            label: 'Общее',
             items: [
-                'getting-started',
-                'create-a-page',
-                'create-a-document',
-                'create-a-blog-post',
-                'markdown-features',
-                'thank-you',
+                'buttons',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Разметка',
+            items: [
+                'pages',
+                'regions',
+                'fieldset',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Навигация',
+            items: [
+                'menu',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Взаимодействие',
+            items: [
+                'alerts',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Ввод данных',
+            items: [
+                'fields',
+                'cells',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Виджеты',
+            items: [
+                'form',
+                'table',
+                'tiles',
+                'cards',
+                'chart',
             ],
         },
     ],
