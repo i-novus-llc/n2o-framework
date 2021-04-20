a90c36ebc6a743869fe972594af1c5cb714d528f
ilebedenko <ilebedenko@i-novus.ru>
2021-04-20 11:14:20 +0300

    [NF][NNO-4843] - автотест для действия set-value

--- backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/N2oComponentLibrary.java
+++ backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/N2oComponentLibrary.java
@@ -79,7 +79,7 @@ public class N2oComponentLibrary implements ComponentLibrary {
                 N2oStandardField.class, N2oIntervalField.class, N2oButtonField.class, N2oImage.class,
                 // controls
                 N2oInputText.class, N2oInputSelect.class, N2oCheckbox.class, N2oSelect.class, N2oDateInterval.class,
-                N2oDateInput.class, N2oCheckboxGroup.class, N2oRadioGroup.class, N2oMaskedInputControl.class,
+                N2oDateInput.class, N2oCheckboxGroup.class, N2oRadioGroup.class, N2oMaskedInput.class,
                 N2oOutputText.class, N2oOutputList.class, N2oTextEditor.class, N2oCodeEditor.class, N2oHtml.class, N2oRating.class,
                 N2oSlider.class, N2oPills.class, N2oText.class, N2oAutoComplete.class, N2oProgress.class, N2oStatus.class, N2oAlert.class,
                 N2oInputMoney.class, N2oPasswordControl.class, N2oTextArea.class, N2oInputSelectTree.class, N2oFileUploadControl.class,
