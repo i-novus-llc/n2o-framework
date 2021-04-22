ffa15194454c5b5fda69a407635cfddc24d96b78
Муравцев Альберт Вячеславович <amuravtsev@i-novus.ru>
2021-04-22 23:57:07 +0300

    [NNO-6255] У Switch не работает лейбл справа.

--- frontend/n2o-framework/src/components/controls/Switch/Switch.jsx
+++ frontend/n2o-framework/src/components/controls/Switch/Switch.jsx
@@ -8,16 +8,18 @@ class N2OSwitch extends React.Component {
    * базовый рендер
    * */
   render() {
-    const { disabled, value, checked, onChange } = this.props;
+    const { disabled, value, checked, onChange, label, id } = this.props;
 
     return (
       <div className="n2o-switch-wrapper">
         <Switch
+          id={id}
           prefixCls="n2o-switch"
           checked={isNil(checked) ? !!value : checked}
           disabled={disabled}
           onChange={onChange}
         />
+        {label && <label className="n2o-switch-label">{label}</label>}
       </div>
     );
   }
@@ -44,6 +46,14 @@ N2OSwitch.propTypes = {
    * Callback на изменение
    */
   onChange: PropTypes.func,
+  /**
+   * Текст
+   */
+  label: PropTypes.string,
+  /**
+   * Идентификатор
+   */
+  id: PropTypes.string,
 };
 
 export default N2OSwitch;
