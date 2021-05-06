fa2dcafec49a77875f9b583dcf84b6173ea48639
rgalina <rgalina@i-novus.ru>
2021-05-06 15:59:29 +0300

    [NNO-6209] fix test

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/page/TopLeftRightPageAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/page/TopLeftRightPageAT.java
@@ -43,8 +43,8 @@ public class TopLeftRightPageAT extends AutoTestBase {
     @Test
     public void testTopLeftRightPage() {
         TopLeftRightPage page = open(TopLeftRightPage.class);
-        page.breadcrumb().titleShouldHaveText("Страница с тремя регионами");
         page.shouldExists();
+        page.breadcrumb().titleShouldHaveText("Страница с тремя регионами");
         page.shouldHaveCssClass("page_class");
         page.shouldHaveStyle("background: blue;");
 
