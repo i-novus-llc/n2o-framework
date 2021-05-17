402847008d8f89d3618d6e32c051c9e9289329df
ilebedenko <ilebedenko@i-novus.ru>
2021-05-17 15:34:22 +0300

    [СF][NNO-6311] - change page title xml folders route

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/page/PageTitleAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/page/PageTitleAT.java
@@ -37,8 +37,8 @@ public class PageTitleAT extends AutoTestBase {
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
         builder.packs(new N2oAllPagesPack(), new N2oHeaderPack());
-        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/page/title/title/index.page.xml"),
-                new CompileInfo("net/n2oapp/framework/autotest/page/title/title/page.page.xml"),
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/page/title/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/page/title/page.page.xml"),
                 new CompileInfo("net/n2oapp/framework/autotest/simple/test.header.xml"));
     }
 
