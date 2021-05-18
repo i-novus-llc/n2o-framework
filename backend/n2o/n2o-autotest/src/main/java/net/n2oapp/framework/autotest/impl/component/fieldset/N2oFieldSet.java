4f3c86415fcaa6155c44e52bcd36c40469eb0475
ilebedenko <ilebedenko@i-novus.ru>
2021-05-18 11:45:18 +0300

    [NF][NNO-6297] - autotest for fieldset description

--- backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/component/fieldset/N2oFieldSet.java
+++ backend/n2o/n2o-autotest/src/main/java/net/n2oapp/framework/autotest/impl/component/fieldset/N2oFieldSet.java
@@ -1,6 +1,7 @@
 package net.n2oapp.framework.autotest.impl.component.fieldset;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import net.n2oapp.framework.autotest.api.component.fieldset.FieldSet;
 import net.n2oapp.framework.autotest.impl.component.N2oComponent;
 
@@ -16,5 +17,19 @@ public abstract class N2oFieldSet extends N2oComponent implements FieldSet {
     @Override
     public void shouldNotBeVisible() {
         element().shouldBe(Condition.hidden);
+    }
+
+    @Override
+    public void shouldHaveDescription(String description) {
+        description().shouldHave(Condition.text(description));
+    }
+
+    @Override
+    public void shouldNotHaveDescription() {
+        description().shouldNot(Condition.exist);
+    }
+
+    protected SelenideElement description() {
+        return element().$(".n2o-fieldset__description");
     }
 }
