7c5f75768affff1f9fc38cc1a3d137ff8945dc36
ilebedenko <ilebedenko@i-novus.ru>
2021-05-24 12:59:33 +0300

    [BF][NNO-6473] - compile full name ref widgetId

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/table/SimpleColumnHeaderCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/table/SimpleColumnHeaderCompiler.java
@@ -15,8 +15,10 @@ import net.n2oapp.framework.api.metadata.meta.widget.table.ColumnHeader;
 import net.n2oapp.framework.api.metadata.meta.widget.toolbar.Condition;
 import net.n2oapp.framework.api.script.ScriptProcessor;
 import net.n2oapp.framework.config.metadata.compile.ComponentScope;
+import net.n2oapp.framework.config.metadata.compile.page.PageScope;
 import net.n2oapp.framework.config.metadata.compile.widget.CellsScope;
 import net.n2oapp.framework.config.metadata.compile.widget.WidgetScope;
+import net.n2oapp.framework.config.util.CompileUtil;
 import net.n2oapp.framework.config.util.StylesResolver;
 import org.springframework.stereotype.Component;
 
@@ -78,7 +80,8 @@ public class SimpleColumnHeaderCompiler<T extends N2oSimpleColumn> extends Abstr
         }
         if (source.getColumnVisibilities() != null) {
             for (AbstractColumn.ColumnVisibility visibility : source.getColumnVisibilities()) {
-                String refWidgetId = p.cast(visibility.getRefWidgetId(), tableId);
+                String refWidgetId = CompileUtil.generateWidgetId(p.getScope(PageScope.class).getPageId(),
+                        p.cast(visibility.getRefWidgetId(), tableId));
                 ReduxModel refModel = p.cast(visibility.getRefModel(), ReduxModel.FILTER);
                 Condition condition = new Condition();
                 condition.setExpression(ScriptProcessor.resolveFunction(visibility.getValue()));
