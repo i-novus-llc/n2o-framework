0cd6754ec7c2873de81067579cd3a3986c1dc161
Григорчук Константин Анатольевич <kgrigorchuk@i-novus.ru>
2021-05-28 13:57:18 +0000

    Merge branch 'bf/NNO-6492' into 'master'
    fix/NNO-6492: удаление кнопок из redux/toolbar при unmount компонента;...
    
    Closes NNO-6492
    
    See merge request open/n2o-framework!2727
    
    (cherry picked from commit cba71f7abcccb6514369008f5382e2c1a4d29ccf)
    
    d0e39688 fix/NNO-6492: удаление кнопок из redux/toolbar при unmount компонента;...
    e8e25b38 fix/NNO-6492: удаление кнопок из redux/toolbar при unmount компонента;...
    cbed1deb fix/NNO-6492: удаление кнопок из redux/toolbar при unmount компонента;...
    45d03246 fix/NNO-6492: удаление кнопок из redux/toolbar при unmount компонента;...

--- frontend/n2o-framework/src/core/factory/FactoryProvider.jsx
+++ frontend/n2o-framework/src/core/factory/FactoryProvider.jsx
@@ -12,8 +12,9 @@ import SecurityCheck from '../auth/SecurityCheck'
 
 import factoryConfigShape from './factoryConfigShape'
 import { NotFoundFactory } from './NotFoundFactory'
+import { ComponentCache } from './ComponentCache'
 
-const blackList = ['dataProvider', 'action', 'actions']
+const ignoreList = ['dataProvider', 'action', 'actions']
 
 export class FactoryProvider extends Component {
     getChildContext() {
@@ -30,6 +31,7 @@ export class FactoryProvider extends Component {
         this.getComponent = this.getComponent.bind(this)
         this.resolveProps = this.resolveProps.bind(this)
         this.checkSecurityAndRender = this.checkSecurityAndRender.bind(this)
+        this.componentCache = new ComponentCache()
     }
 
     checkSecurityAndRender(component = null, config, level) {
@@ -37,18 +39,22 @@ export class FactoryProvider extends Component {
 
         if (isEmpty(config) || securityBlackList.includes(level)) { return component }
 
-        return props => (
-            <SecurityCheck
-                config={config}
-                render={({ permissions }) => {
-                    if (permissions) {
-                        return React.createElement(component, props)
-                    }
+        if (!this.componentCache.has(component, config)) {
+            this.componentCache.set(component, config, props => (
+                <SecurityCheck
+                    config={config}
+                    render={({ permissions }) => {
+                        if (permissions) {
+                            return React.createElement(component, props)
+                        }
 
-                    return null
-                }}
-            />
-        )
+                        return null
+                    }}
+                />
+            ))
+        }
+
+        return this.componentCache.get(component, config)
     }
 
     getComponent(src, level, security) {
@@ -59,17 +65,17 @@ export class FactoryProvider extends Component {
                 level,
             )
         }
-        const findedFactory = []
+        const factories = []
 
         each(this.factories, (group, level) => {
             if (group && group[src]) {
                 const comp = this.checkSecurityAndRender(group[src], security, level)
 
-                findedFactory.push(comp)
+                factories.push(comp)
             }
         })
 
-        return first(findedFactory)
+        return first(factories)
     }
 
     resolveProps(
@@ -81,12 +87,11 @@ export class FactoryProvider extends Component {
 
         if (isObject(props)) {
             Object.keys(props).forEach((key) => {
-                if (isObject(props[key]) && !blackList.includes(key)) {
+                if (isObject(props[key]) && !ignoreList.includes(key)) {
                     obj[key] = this.resolveProps(props[key], defaultComponent, paramName)
                 } else if (key === 'src') {
-                    obj[paramName] =
-            this.getComponent(props[key], null, props.security) ||
-            this.checkSecurityAndRender(defaultComponent, props.security)
+                    obj[paramName] = this.getComponent(props[key], null, props.security) ||
+                        this.checkSecurityAndRender(defaultComponent, props.security)
                 } else {
                     obj[key] = props[key]
                 }
