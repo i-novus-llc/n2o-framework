055a38231dd8f537a098a994e2ac14b2b5d2ad81
Исхаков Салават <siskhakov@i-novus.ru>
2021-06-07 17:44:06 +0300

    adoc to md
    (cherry picked from commit b6e228bbc26c112dcdaded8ccd022f168f34601e)

--- frontend/docs/sidebars.js
+++ frontend/docs/sidebars.js
@@ -1,73 +1,100 @@
 module.exports = {
     docs: [
-        {
-            type: 'category',
-            label: 'Общее',
-            items: [
-                'buttons',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Разметка',
-            items: [
-                'pages',
-                'regions',
-                'fieldset',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Навигация',
-            items: [
-                'menu',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Взаимодействие',
-            items: [
-                'modal',
-                'alerts',
-                'actions',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Ввод данных',
-            items: [
-                'fields',
-                'select',
-                'select_tree',
-                'date_time',
-                'cells',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Интерфейс',
-            items: [
-                'image',
-                'checkbox',
-                'radio',
-                'progress_bar',
-                'slider',
-                'switcher',
-                'rating',
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Виджеты',
-            items: [
-                'table',
-                'chart',
-                'cards',
-                'form',
-                'list',
-                'tree',
-                'tiles',
-            ],
-        },
-    ],
+    {
+        type: 'category',
+        label: 'Компоненты',
+        items: [
+                       {
+                           type: 'category',
+                           label: 'Общее',
+                           items: [
+                               'buttons',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Разметка',
+                           items: [
+                               'pages',
+                               'regions',
+                               'fieldset',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Навигация',
+                           items: [
+                               'menu',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Взаимодействие',
+                           items: [
+                               'modal',
+                               'alerts',
+                               'actions',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Ввод данных',
+                           items: [
+                               'fields',
+                               'select',
+                               'select_tree',
+                               'date_time',
+                               'cells',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Интерфейс',
+                           items: [
+                               'image',
+                               'checkbox',
+                               'radio',
+                               'progress_bar',
+                               'slider',
+                               'switcher',
+                               'rating',
+                           ],
+                       },
+                       {
+                           type: 'category',
+                           label: 'Виджеты',
+                           items: [
+                               'table',
+                               'chart',
+                               'cards',
+                               'form',
+                               'list',
+                               'tree',
+                               'tiles',
+                           ],
+                       },
+                   ],
+    },
+    {
+        type: 'category',
+        label: 'Manual',
+        items: [
+            'manual/index',
+        ]
+    },
+    {
+        type: 'category',
+        label: 'Migration',
+        items: [
+            'migration/migration',
+        ]
+    },
+    {
+        type: 'category',
+        label: 'Xml',
+        items: [
+            'xml/index',
+        ]
+    },
+    ]
 }
