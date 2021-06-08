a59b5565135750bc78a48134b70bccf840ed6dc4
Nikita Deputatov <ndeputatov@i-novus.ru>
2021-06-08 17:15:29 +0300

    [NNO-6509] Add correct proxy

--- frontend/demo/src/setupProxy.js
+++ frontend/demo/src/setupProxy.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
 
   app.use(
     proxy("/n2o", {
-      target: "https://n2oapp.net/sandbox/view/iJm2h/#/",
+      target: "https://n2o.i-novus.ru/next/demo/",
       changeOrigin: true
     })
   );
