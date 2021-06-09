3b4d9d02cc10c9eb43753bc3a2fe18feee70df23
anigmatullin <anigmatullin@i-novus.ru>
2021-06-09 14:49:28 +0300

    [CF][NNO-6607] New radio-group compilation fix

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/RadioGroupCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/RadioGroupCompiler.java
@@ -27,7 +27,7 @@ public class RadioGroupCompiler extends ListControlCompiler<RadioGroup, N2oRadio
     @Override
     public StandardField<RadioGroup> compile(N2oRadioGroup source, CompileContext<?, ?> context, CompileProcessor p) {
         RadioGroup radioGroup = new RadioGroup();
-        radioGroup.setInline(source.getType() == N2oRadioGroup.RadioGroupType.tabs && source.getInline() == null ? Boolean.TRUE : source.getInline());
+        radioGroup.setInline(source.getInline() == null && source.getType() == N2oRadioGroup.RadioGroupType.tabs ? Boolean.TRUE : source.getInline());
         radioGroup.setType(p.cast(source.getType() == N2oRadioGroup.RadioGroupType.n2o ? N2oRadioGroup.RadioGroupType.defaultType : source.getType(), new DomainProcessor().deserializeEnum(p.resolve(property("n2o.api.control.alt.type"), N2oRadioGroup.RadioGroupType.class), N2oRadioGroup.RadioGroupType.class)));
         radioGroup.setSize(p.cast(source.getSize(), p.resolve(property("n2o.api.control.list.size"), Integer.class)));
         StandardField<RadioGroup> result = compileListControl(radioGroup, source, context, p);
