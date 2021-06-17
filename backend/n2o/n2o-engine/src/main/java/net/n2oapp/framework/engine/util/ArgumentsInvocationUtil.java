7b7f8e6b3e330c60156a4ce0cf48cdead3513a28
ilebedenko <ilebedenko@i-novus.ru>
2021-06-17 18:38:30 +0300

    [RF][NNO-6491] - refactoring arguments invocation util class

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/util/ArgumentsInvocationUtil.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/util/ArgumentsInvocationUtil.java
@@ -17,9 +17,9 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static net.n2oapp.framework.engine.util.MappingProcessor.inMap;
 
@@ -28,37 +28,33 @@ import static net.n2oapp.framework.engine.util.MappingProcessor.inMap;
  */
 public class ArgumentsInvocationUtil {
     private final static ExpressionParser writeParser = new SpelExpressionParser(new SpelParserConfiguration(true, true));
-    private final static Set<String> primitiveTypes = Stream.of("java.lang.Boolean", "java.lang.Character", "java.lang.Byte",
-            "java.lang.Short", "java.lang.Integer", "java.lang.Long", "java.lang.Float", "java.lang.Double", "java.util.Date",
-            "java.math.BigDecimal").collect(Collectors.toSet());
+
 
     /**
-     * Собирает аргументы для query, на выход будет массив из одного аргумента типа net.n2oapp.criteria.api.Criteria
+     * Собирает аргументы для действия invocation в выборке
+     *
+     * @param invocation          Вызов действия
+     * @param query               Скомпилированная модель запроса за данными
+     * @param criteria            Критерий фильтрации данных
+     * @param criteriaConstructor Конструктор критериев
+     * @param domainProcessor     Процессор приведения к типу
+     * @return Массив объектов
      */
     public static Object[] mapToArgs(N2oArgumentsInvocation invocation, CompiledQuery query,
                                      N2oPreparedCriteria criteria, CriteriaConstructor criteriaConstructor,
                                      DomainProcessor domainProcessor) {
-        Class<?>[] classesOfArguments = takeClassesOfArguments(invocation);
-        if (classesOfArguments == null || classesOfArguments.length == 0)
+        Object[] argumentInstances = instantiateArguments(invocation.getArguments());
+        if (ArrayUtils.isEmpty(argumentInstances))
             return new Object[0];
-        Object[] argumentInstances = new Object[classesOfArguments.length];
+
         int criteriaIdx = -1;
         for (int i = 0; i < invocation.getArguments().length; i++) {
-            if (classesOfArguments[i] == null)
-                continue;
-            if (invocation.getArguments()[i].getType().equals(Argument.Type.CLASS) || invocation.getArguments()[i].getType().equals(Argument.Type.ENTITY)) {
-                try {
-                    argumentInstances[i] = classesOfArguments[i].newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    throw new IllegalArgumentException(classesOfArguments[i].getName(), e);
-                }
-            }
-
-            if (invocation.getArguments()[i].getType().equals(Argument.Type.CRITERIA)) {
-                argumentInstances[i] = criteriaConstructor.construct(criteria, classesOfArguments[i]);
+            if (Argument.Type.CRITERIA.equals(invocation.getArguments()[i].getType())) {
+                argumentInstances[i] = criteriaConstructor.construct(criteria, argumentInstances[i]);
                 criteriaIdx = i;
             }
         }
+
         if (Arrays.stream(invocation.getArguments()).filter(arg -> (arg.getType().equals(Argument.Type.ENTITY) ||
                 arg.getType().equals(Argument.Type.CRITERIA))).collect(Collectors.toList()).size() > 1)
             throw new IllegalArgumentException("There must be only one argument with Criteria or Entity type ");
@@ -67,46 +63,31 @@ public class ArgumentsInvocationUtil {
             String mapping = filter.getMapping().startsWith("[") ? filter.getMapping() : "[" + criteriaIdx + "]." + filter.getMapping();
             inMap(argumentInstances, mapping, r.getValue());
         }
-        for (int i = 0; i < argumentInstances.length; i++) {
-            String defaultValue = invocation.getArguments()[i].getDefaultValue();
-            if (argumentInstances[i] == null && defaultValue != null)
-                argumentInstances[i] = domainProcessor.deserialize(defaultValue);
-        }
+
+        resolveDefaultValues(invocation.getArguments(), domainProcessor, argumentInstances);
         return argumentInstances;
     }
 
     /**
      * Собирает аргументы для действия invocation
      *
-     * @param invocation Вызов действия
-     * @param inDataSet  Входные данные
-     * @param inMapping  Маппинг входных данных
+     * @param invocation      Вызов действия
+     * @param dataSet         Входные данные
+     * @param inMapping       Маппинг входных данных
+     * @param domainProcessor Процессор приведения к типу
      * @return Массив объектов
      */
-    public static Object[] mapToArgs(N2oArgumentsInvocation invocation, DataSet inDataSet,
+    public static Object[] mapToArgs(N2oArgumentsInvocation invocation, DataSet dataSet,
                                      Map<String, FieldMapping> inMapping, DomainProcessor domainProcessor) {
         inMapping = changeInMappingForEntity(invocation, inMapping);
         if (invocation.getArguments() == null || invocation.getArguments().length == 0)
             return null;
-        return map(inDataSet, inMapping, invocation.getArguments(), domainProcessor);
-    }
 
-    /**
-     * Генерирует список аргументов для вызова метода.
-     *
-     * @param dataSet    Исходные данные
-     * @param mappingMap Правила маппинга
-     * @param arguments  Список аргументов
-     * @return Массив объектов
-     */
-    public static Object[] map(DataSet dataSet, Map<String, FieldMapping> mappingMap, Argument[] arguments,
-                               DomainProcessor domainProcessor) {
-        Object[] result = instantiateArguments(arguments);
-
+        Object[] result = instantiateArguments(invocation.getArguments());
         boolean hasOnlyOneEntity = result.length == 1 && result[0] != null;
         int idx = 0;
 
-        for (Map.Entry<String, FieldMapping> map : mappingMap.entrySet()) {
+        for (Map.Entry<String, FieldMapping> map : inMapping.entrySet()) {
             Object value = dataSet.get(map.getKey());
             String mapping = map.getValue().getMapping();
             if ((mapping != null && !mapping.startsWith("[") && !mapping.endsWith("]")) || value != null) {
@@ -119,14 +100,32 @@ public class ArgumentsInvocationUtil {
             }
             idx++;
         }
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] == null && arguments[i].getDefaultValue() != null) {
-                result[i] = domainProcessor.deserialize(arguments[i].getDefaultValue());
-            }
-        }
+
+        resolveDefaultValues(invocation.getArguments(), domainProcessor, result);
         return result;
     }
 
+    /**
+     * Задание значений по умолчанию данным вызова, если их значение не задано
+     *
+     * @param arguments       Аргументы вызова
+     * @param domainProcessor Процессор приведения к типу
+     * @param results         Данные вызова
+     */
+    private static void resolveDefaultValues(Argument[] arguments, DomainProcessor domainProcessor, Object[] results) {
+        for (int i = 0; i < results.length; i++) {
+            String defaultValue = arguments[i].getDefaultValue();
+            if (results[i] == null && defaultValue != null)
+                results[i] = domainProcessor.deserialize(defaultValue);
+        }
+    }
+
+    /**
+     * Создание массива экземпляров по классам аргументов
+     *
+     * @param arguments Массив аргументов
+     * @return Массив экземпляров
+     */
     private static Object[] instantiateArguments(Argument[] arguments) {
         if (arguments == null) return null;
         Object[] argumentInstances = new Object[arguments.length];
@@ -134,11 +133,12 @@ public class ArgumentsInvocationUtil {
         for (int k = 0; k < argumentInstances.length; k++) {
             String className = arguments[k].getClassName();
             Class argumentClass;
-            if (className == null || primitiveTypes.contains(className)) {
+            if (className == null || arguments[k].getType() == null ||
+                    Argument.Type.PRIMITIVE.equals(arguments[k].getType())) {
                 argumentInstances[k] = null;
             } else {
                 try {
-                    argumentClass = Class.forName(className);
+                    argumentClass = ClassHash.getClass(className);
                     argumentInstances[k] = argumentClass.newInstance();
                 } catch (Exception e) {
                     throw new N2oException("Can't create instance of class " + className, e);
@@ -149,26 +149,12 @@ public class ArgumentsInvocationUtil {
     }
 
     /**
-     * Возвращает классы аргументов вызываемого метода, используя кэширование
+     * Изменение маппинга вызова согласно наличию entity аргумента
+     *
+     * @param invocation Вызов действия
+     * @param inMapping  Маппинг входных данных
+     * @return Измененный маппинг вызова
      */
-    private static Class<?>[] takeClassesOfArguments(N2oArgumentsInvocation invocation) {
-        int argumentCount = invocation.getArguments() != null ? invocation.getArguments().length : 0;
-        Class<?>[] classesOfArguments = new Class[argumentCount];
-        if (invocation.getArguments() == null) return null;
-        try {
-            for (int i = 0; i < argumentCount; i++) {
-                if (invocation.getArguments()[i].getClassName() == null
-                        || invocation.getArguments()[i].getType().equals(Argument.Type.PRIMITIVE))
-                    classesOfArguments[i] = null;
-                else
-                    classesOfArguments[i] = ClassHash.getClass(invocation.getArguments()[i].getClassName());
-            }
-        } catch (Exception e) {
-            throw new N2oException("Class of argument not found", e);
-        }
-        return classesOfArguments;
-    }
-
     private static Map<String, FieldMapping> changeInMappingForEntity(N2oArgumentsInvocation invocation,
                                                                       Map<String, FieldMapping> inMapping) {
         if (ArrayUtils.isEmpty(invocation.getArguments())) {
