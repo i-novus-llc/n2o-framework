441fc449fc1ea313de8b24049c886915dd016d54
Соколов Алексей <asokolov@i-novus.ru>
2021-06-17 14:45:47 +0300

    [NF][NNO-6618] новый Template с кастомизируемыми header sidebar footer

---
+++ frontend/n2o-framework/src/components/core/templates/Template.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import SimpleHeader from '../../../plugins/Header/SimpleHeader/SimpleHeader'
+import Sidebar from '../../../plugins/SideBar/SidebarContainer'
+import Footer from '../../../plugins/Footer/Footer'
+// eslint-disable-next-line import/no-named-as-default
+import MenuContainer from '../../../plugins/Menu/MenuContainer'
+
+function Layout({ children, layout, header, sidebar, footer, ...rest }) {
+    const { fullSizeHeader, fixed } = layout
+
+    const layoutContainerClasses = classNames(
+        'n2o-layout-container',
+        { 'n2o-layout-with-header': header,
+            'n2o-layout-with-sidebar': sidebar,
+            'n2o-layout-full-size-header d-flex flex-column': fullSizeHeader,
+            'n2o-layout-full-size-sidebar d-flex': !fullSizeHeader,
+            'n2o-layout-fixed': fixed,
+        },
+    )
+
+    if (fullSizeHeader) {
+        return (
+            <div className={layoutContainerClasses}>
+                {header && (
+                    <SimpleHeader {...header} {...rest} />
+                )}
+                <div className="w-100 d-flex flex-row overflow-auto">
+                    {sidebar && (
+                        <Sidebar {...sidebar} {...rest} />
+                    )}
+                    <div className={classNames('w-100', { 'application-body-container-fixed': fixed })}>
+                        <div className="application-body container-fluid">{children}</div>
+                        {footer && <Footer {...footer} />}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className={layoutContainerClasses}>
+            {sidebar && (
+                <Sidebar className={classNames({ 'n2o-fixed-sidebar': fixed })} {...sidebar} {...rest} />
+            )}
+            <div className={classNames('w-100 d-flex flex-column', { 'vh-100': fixed })}>
+                {header && (
+                    <SimpleHeader {...header} {...rest} />
+                )}
+                <div className={classNames({ 'd-flex': sidebar, 'application-body-container-fixed': fixed })}>
+                    <div className="application-body container-fluid">{children}</div>
+                </div>
+                {footer && <Footer {...footer} />}
+            </div>
+        </div>
+    )
+}
+
+function Template({ children }) {
+    return (
+        <div className="application">
+            <MenuContainer render={config => <Layout {...config}>{children}</Layout>} />
+        </div>
+    )
+}
+
+Template.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+}
+
+Layout.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+    layout: PropTypes.shape({
+        fullSizeHeader: PropTypes.bool,
+        fixed: PropTypes.bool,
+    }).isRequired,
+    header: PropTypes.object,
+    sidebar: PropTypes.object,
+    footer: PropTypes.object,
+}
+
+export default Template
