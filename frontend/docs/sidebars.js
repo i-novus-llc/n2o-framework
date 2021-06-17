34ab0bcd8f480681fa2713c82069d9fc2f4e7290
rnuravtsev-macbook <muravtsev21@gmail.com>
2021-06-17 14:17:23 +0300

    [NNO-6647] В docusaurus в разделе Компоненты сделать высоту фрейма с примерами в соответствии с высотой компонентов в примере.

--- frontend/docs/sidebars.js
+++ frontend/docs/sidebars.js
@@ -1,109 +1,125 @@
 module.exports = {
     docs: [
-    {
-        type: 'category',
-        label: 'Компоненты',
-        items: [
-                       {
-                           type: 'category',
-                           label: 'Общее',
-                           items: [
-                               'buttons',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Разметка',
-                           items: [
-                               'pages',
-                               'regions',
-                               'fieldset',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Навигация',
-                           items: [
-                               'menu',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Взаимодействие',
-                           items: [
-                               'modal',
-                               'alerts',
-                               'actions',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Ввод данных',
-                           items: [
-                               'fields',
-                               'select',
-                               'select_tree',
-                               'date_time',
-                               'cells',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Интерфейс',
-                           items: [
-                               'image',
-                               'checkbox',
-                               'radio',
-                               'progress_bar',
-                               'slider',
-                               'switcher',
-                               'rating',
-                           ],
-                       },
-                       {
-                           type: 'category',
-                           label: 'Виджеты',
-                           items: [
-                               'table',
-                               'chart',
-                               'cards',
-                               'form',
-                               'list',
-                               'tree',
-                               'tiles',
-                           ],
-                       },
-                   ],
-    },
-    {
-         type: 'category',
-         label: 'Примеры',
-         items: [
-            'uxcases_input',
-            'uxcases_list',
-            'uxcases_list_actions'
-         ]
-    },
-    {
-        type: 'category',
-        label: 'Manual',
-        items: [
-            'manual/index',
-        ]
-    },
-    {
-        type: 'category',
-        label: 'Migration',
-        items: [
-            'migration/migration',
-        ]
-    },
-    {
-        type: 'category',
-        label: 'Xml',
-        items: [
-            'xml/index',
-        ]
-    },
-    ]
+        {
+            type: 'category',
+            label: 'Компоненты',
+            items: [
+                {
+                    type: 'category',
+                    label: 'Общее',
+                    items: [
+                        'buttons',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Разметка',
+                    items: [
+                        'pages',
+                        'regions',
+                        'fieldset',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Навигация',
+                    items: [
+                        'menu',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Взаимодействие',
+                    items: [
+                        'modal',
+                        'alerts',
+                        'actions',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Ввод данных',
+                    items: [
+                        'fields',
+                        'select',
+                        'select_tree',
+                        'date_time',
+                        'cells',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Интерфейс',
+                    items: [
+                        'image',
+                        'checkbox',
+                        'radio',
+                        'progress_bar',
+                        'slider',
+                        'switcher',
+                        'rating',
+                    ],
+                },
+                {
+                    type: 'category',
+                    label: 'Виджеты',
+                    items: [
+                        'table',
+                        'chart',
+                        'cards',
+                        'form',
+                        'list',
+                        'tree',
+                        'tiles',
+                    ],
+                },
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Примеры',
+            items: [
+                'uxcases_input',
+                'uxcases_list',
+                'uxcases_list_actions',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Manual',
+            items: [
+                // 'manual/index',
+                'manual/overview',
+                'manual/getstarted',
+                'manual/configs',
+                'manual/components',
+                'manual/data',
+                'manual/defaultvalue',
+                'manual/dependency',
+                'manual/filters',
+                'manual/validation',
+                'manual/routing',
+                'manual/setting',
+                'manual/context',
+                'manual/dynamic',
+                'manual/processing',
+                'manual/security',
+                'manual/conclusion',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Migration',
+            items: [
+                'migration/migration',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Xml',
+            items: [
+                'xml/index',
+            ],
+        },
+    ],
 }
