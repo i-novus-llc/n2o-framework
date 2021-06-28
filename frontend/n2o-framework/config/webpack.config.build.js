0561fe40ac03645c4e4c5b615668e262f2218dee
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2021-06-28 22:25:28 +0300

    NNO-6688 Замена ссылки на корневой файл с es/index на lib/index, чтобы небыло дублирование кода в бандлах при импортах "n2o-framework" и "n2o-framework/lib/..."

--- frontend/n2o-framework/config/webpack.config.build.js
+++ frontend/n2o-framework/config/webpack.config.build.js
@@ -1,97 +1,97 @@
 /**
- * Created by emamoshin on 01.02.2018.
+ * Конфигурация webpack для сборки ./dist/
  */
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const pkg = require('../package.json');
+
+const path = require('path')
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const extractLess = new ExtractTextPlugin({
-  filename: 'n2o.css',
-  disable: process.env.NODE_ENV === 'development',
-});
+    filename: 'n2o.css',
+    disable: process.env.NODE_ENV === 'development',
+})
 
 module.exports = {
-  context: path.resolve(__dirname, '../src'),
+    context: path.resolve(__dirname, '../src'),
 
-  entry: ['./sass/n2o.scss'],
+    entry: ['./sass/n2o.scss'],
 
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'n2o.js',
-    library: 'N2O',
-    libraryTarget: 'umd',
-  },
-
-  stats: {
-    colors: true,
-    reasons: false,
-    hash: false,
-    version: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    cached: false,
-    cachedAssets: false
-  },
-
-  resolve: {
-    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
-    extensions: ['.js', '.jsx'],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)?$/,
-        include: [path.resolve(__dirname, '../src')],
-        use: ['babel-loader']
-      },
-      {
-        test: /\.scss/,
-        use: extractLess.extract({
-          use: [
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-          // use style-loader in development
-          fallback: 'style-loader',
-        }),
-      },
-      {
-        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/', // where the fonts will go
-              publicPath: './fonts/', // override the default path
-            },
-          },
-        ],
-      },
-    ],
-  },
-
-  externals: {
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'n2o.js',
+        library: 'N2O',
+        libraryTarget: 'umd',
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom'
-    }
-  },
 
-  plugins: [extractLess],
-};
+    stats: {
+        colors: true,
+        reasons: false,
+        hash: false,
+        version: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        cached: false,
+        cachedAssets: false,
+    },
+
+    resolve: {
+        modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+        extensions: ['.js', '.jsx'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                include: [path.resolve(__dirname, '../src')],
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.scss/,
+                use: extractLess.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                        },
+                    ],
+                    // use style-loader in development
+                    fallback: 'style-loader',
+                }),
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[\da-z]+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/', // where the fonts will go
+                            publicPath: './fonts/', // override the default path
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+
+    externals: {
+        react: {
+            root: 'React',
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+        },
+    },
+
+    plugins: [extractLess],
+}
