4b99a4b6f0f856cc6e21b9363a74df38d424d958
ilebedenko <ilebedenko@i-novus.ru>
2021-07-05 13:33:17 +0300

    [CF][NNO-6701] - fix

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/BaseWidgetCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/widget/BaseWidgetCompiler.java
@@ -128,7 +128,7 @@ public abstract class BaseWidgetCompiler<D extends Widget, S extends N2oWidget> 
         }
         WidgetScope widgetScope = p.getScope(WidgetScope.class);
         if (widgetScope != null && widgetScope.getDependsOnWidgetId() != null &&
-                source.getMasterFieldId() != null) {
+                source.getDetailFieldId() != null) {
             //Если есть master/detail зависимость, то для восстановления необходимо в маршруте добавить идентификатор мастер записи
             String selectedId = normalizeParam(p.cast(source.getMasterParam(), widgetScope.getDependsOnWidgetId() + "_id"));
             return normalize(colon(selectedId)) + normalize(source.getId());
