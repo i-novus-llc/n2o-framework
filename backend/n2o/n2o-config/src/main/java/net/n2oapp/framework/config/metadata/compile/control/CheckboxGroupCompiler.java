b231239b1a4ffbf2e37af768319f0291bf061c82
ilebedenko <ilebedenko@i-novus.ru>
2021-07-26 18:06:55 +0300

    [BF][NNO-6838] - fix route param for checkbox group

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/CheckboxGroupCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/CheckboxGroupCompiler.java
@@ -5,8 +5,11 @@ import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.control.multi.N2oCheckboxGroup;
+import net.n2oapp.framework.api.metadata.meta.ModelLink;
 import net.n2oapp.framework.api.metadata.meta.control.CheckboxGroup;
 import net.n2oapp.framework.api.metadata.meta.control.StandardField;
+import net.n2oapp.framework.api.script.ScriptProcessor;
+import net.n2oapp.framework.config.metadata.compile.widget.ModelsScope;
 import org.springframework.stereotype.Component;
 
 import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
@@ -28,6 +31,15 @@ public class CheckboxGroupCompiler extends ListControlCompiler<CheckboxGroup, N2
         checkboxGroup.setType(p.cast(source.getType() == N2oCheckboxGroup.CheckboxGroupType.n2o ? N2oCheckboxGroup.CheckboxGroupType.defaultType : source.getType(), new DomainProcessor().deserializeEnum(p.resolve(property("n2o.api.control.alt.type"), N2oCheckboxGroup.CheckboxGroupType.class), N2oCheckboxGroup.CheckboxGroupType.class)));
         StandardField<CheckboxGroup> result = compileListControl(checkboxGroup, source, context, p);
         return compileFetchDependencies(result, source, p);
+    }
+
+    @Override
+    protected ModelLink compileLinkOnSet(StandardField<CheckboxGroup> control, N2oCheckboxGroup source, ModelsScope modelsScope) {
+        ModelLink onSet = new ModelLink(modelsScope.getModel(), modelsScope.getWidgetId());
+        onSet.setParam(source.getParam());
+        onSet.setSubModelQuery(createSubModel(source, control.getControl().getData()));
+        onSet.setValue(ScriptProcessor.resolveExpression(String.format("{%s*.id}", source.getId())));
+        return onSet;
     }
 
     @Override
