e9ff44c4b8870547012f705d60dd73a6e954a281
azalyaev <azalyaev@i-novus.ru>
2021-07-27 19:37:29 +0300

    Revert "bf/NNO-6853 упростил FullSizeSidebar и FullSizeHeader - теперь это только шаблон вёрстки страницы, в которую вставляются нужные контролы"
    This reverts commit 570ac9e9383fd95bb9f77b06fb628596b4f2aa91.

--- frontend/n2o-framework/src/components/core/templates/utils.jsx
+++ frontend/n2o-framework/src/components/core/templates/utils.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import isEmpty from 'lodash/isEmpty'
 
@@ -11,3 +12,23 @@ export const layoutContainerClasses = (header, sidebar, fullSizeHeader, fixed, s
         'flex-row-reverse': !fullSizeHeader && side === 'right',
     },
 )
+
+export const layoutTypes = {
+    layoutClassName: PropTypes.string,
+    side: PropTypes.string,
+    header: PropTypes.object,
+    sidebar: PropTypes.object,
+    footer: PropTypes.object,
+    rest: PropTypes.object,
+    toggleSidebar: PropTypes.func,
+    sidebarOpen: PropTypes.bool,
+    fixed: PropTypes.bool,
+    layoutChildren: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+}
