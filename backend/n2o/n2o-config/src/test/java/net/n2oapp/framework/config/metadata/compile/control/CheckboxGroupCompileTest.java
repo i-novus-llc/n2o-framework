937c7698106d289d226d1dcf977c251317eab616
Игорь Рябов <iryabov@i-novus.ru>
2021-07-29 05:38:36 +0000

    Merge branch 'fix/NNO-6838' into 'master'
    [BF][NNO-6838] - fix route param for checkbox group
    
    Closes NNO-6838
    
    See merge request open/n2o-framework!3055
    
    (cherry picked from commit cf466d94bc119794f94237aa22f1e9d6f156e0a4)
    
    b231239b [BF][NNO-6838] - fix route param for checkbox group
    481cb2a1 [RF][NNO-6838] - some refactoring
    a87ca22f [CF][NNO-6838] - fix

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/CheckboxGroupCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/CheckboxGroupCompileTest.java
@@ -1,11 +1,14 @@
 package net.n2oapp.framework.config.metadata.compile.control;
 
+import net.n2oapp.framework.api.metadata.meta.ClientDataProvider;
 import net.n2oapp.framework.api.metadata.meta.control.CheckboxGroup;
 import net.n2oapp.framework.api.metadata.meta.control.ListControl;
 import net.n2oapp.framework.api.metadata.meta.control.StandardField;
-import net.n2oapp.framework.api.metadata.meta.ClientDataProvider;
+import net.n2oapp.framework.api.metadata.meta.page.PageRoutes;
+import net.n2oapp.framework.api.metadata.meta.page.SimplePage;
 import net.n2oapp.framework.api.metadata.meta.widget.form.Form;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.compile.context.PageContext;
 import net.n2oapp.framework.config.metadata.compile.context.WidgetContext;
 import net.n2oapp.framework.config.metadata.pack.*;
 import net.n2oapp.framework.config.selective.CompileInfo;
@@ -14,6 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -37,8 +41,9 @@ public class CheckboxGroupCompileTest extends SourceCompileTestBase {
 
     @Test
     public void testCheckboxGroup() {
-        Form form = (Form) compile("net/n2oapp/framework/config/metadata/compile/field/testCheckboxGroup.widget.xml")
-                .get(new WidgetContext("testCheckboxGroup", "/test"));
+        SimplePage page = (SimplePage) compile("net/n2oapp/framework/config/metadata/compile/field/testCheckboxGroup.page.xml")
+               .get(new PageContext("testCheckboxGroup", "/test"));
+        Form form = (Form) page.getWidget();
 
         ClientDataProvider cdp = ((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
                 .get(0).getCols().get(0).getFields().get(0)).getControl()).getDataProvider();
@@ -47,15 +52,21 @@ public class CheckboxGroupCompileTest extends SourceCompileTestBase {
         assertThat(cdp.getQueryMapping().get("noRef").getValue(), is("`someField`"));
         assertThat(cdp.getQueryMapping().get("countries").getValue(), is(Arrays.asList(1, 2, 3)));
 
-        String format =((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
+        String format = ((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
                 .get(0).getCols().get(0).getFields().get(0)).getControl()).getFormat();
         assertThat(format, nullValue());
 
-        format =((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
+        Map<String, PageRoutes.Query> queryMapping = page.getRoutes().getQueryMapping();
+        assertThat(queryMapping.size(), is(1));
+        assertThat(queryMapping.get("genders").getOnSet().getBindLink(), is("models.resolve['test_main']"));
+        assertThat(queryMapping.get("genders").getOnSet().getValue(), is("`gender.map(function(t){return t.id})`"));
+
+
+        format = ((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
                 .get(1).getCols().get(0).getFields().get(0)).getControl()).getFormat();
         assertThat(format, is("`name+'  '+inn`"));
 
-        format =((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
+        format = ((ListControl) ((StandardField) form.getComponent().getFieldsets().get(0).getRows()
                 .get(2).getCols().get(0).getFields().get(0)).getControl()).getFormat();
         assertThat(format, is("password"));
     }
