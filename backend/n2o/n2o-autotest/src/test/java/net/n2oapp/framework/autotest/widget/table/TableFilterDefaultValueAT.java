c7b03a9216e33fd9af43e1e21d70a82c05310523
rgalina <rgalina@i-novus.ru>
2021-08-04 14:17:51 +0300

    [NNO-6783] После обновления страницы перестает работать роутинг параметров

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/TableFilterDefaultValueAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/TableFilterDefaultValueAT.java
@@ -2,7 +2,11 @@ package net.n2oapp.framework.autotest.widget.table;
 
 import com.codeborne.selenide.Selenide;
 import net.n2oapp.framework.autotest.api.component.control.InputText;
+import net.n2oapp.framework.autotest.api.component.control.RadioGroup;
 import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.page.StandardPage;
+import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
 import net.n2oapp.framework.autotest.api.component.widget.table.TableWidget;
 import net.n2oapp.framework.autotest.run.AutoTestBase;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
@@ -118,6 +122,31 @@ public class TableFilterDefaultValueAT extends AutoTestBase {
         table.filters().toolbar().button("Сбросить").click();
         rows.shouldHaveSize(4);
         filter.shouldBeEmpty();
+    }
+
+    @Test
+    public void testFormAsFilter() {
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/widget/table/filters/form_as_filter/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/widget/table/filters/form_as_filter/test.query.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+
+        TableWidget table = page.regions().region(0, SimpleRegion.class).content().widget(1, TableWidget.class);
+        TableWidget.Rows rows = table.columns().rows();
+        rows.shouldHaveSize(1);
+
+        FormWidget form = page.regions().region(0, SimpleRegion.class).content().widget(0, FormWidget.class);
+        RadioGroup period = form.fields().field("Period").control(RadioGroup.class);
+        period.check("Half year");
+        rows.shouldHaveSize(2);
+
+        Selenide.refresh();
+
+        period.shouldBeChecked("Half year");
+        rows.shouldHaveSize(2);
+
+        period.check("5 years");
+        rows.shouldHaveSize(1);
     }
 }
 
