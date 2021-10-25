d93fd55b9b91df7549f5785d1f131a3fbbfa2890
dharlushin <dharlushin@i-novus.ru>
2021-10-25 16:42:32 +0300

    [BF][NNO-7088] добавил сохранение параметров операции в catch блоке N2oException

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oOperationProcessor.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oOperationProcessor.java
@@ -50,9 +50,10 @@ public class N2oOperationProcessor {
                     inParameters,
                     outParameters
             );
+        } catch (N2oException e) {
+            inDataSet.putAll(getFailOutParameters(operation.getFailOutParametersMap(), e));
+            throw e;
         } catch (Exception e) {
-            if (e instanceof N2oException)
-                throw  e;
             inDataSet.putAll(getFailOutParameters(operation.getFailOutParametersMap(), e));
             throw exceptionHandler.handle(operation, inDataSet, e);
         }
