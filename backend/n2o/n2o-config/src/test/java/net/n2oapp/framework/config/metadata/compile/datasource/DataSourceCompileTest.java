b74fee6de8be39ab5dee7579268e7b2072854b17
Игорь Рябов <iryabov@i-novus.ru>
2021-11-16 17:48:41 +0300

    NNO-6921 Добавил тест на submit в datasource

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/datasource/DataSourceCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/datasource/DataSourceCompileTest.java
@@ -1,22 +1,29 @@
 package net.n2oapp.framework.config.metadata.compile.datasource;
 
 import net.n2oapp.framework.api.metadata.ReduxModel;
+import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.datasource.Datasource;
 import net.n2oapp.framework.api.metadata.global.view.page.DefaultValuesMode;
+import net.n2oapp.framework.api.metadata.local.CompiledObject;
 import net.n2oapp.framework.api.metadata.meta.ModelLink;
 import net.n2oapp.framework.api.metadata.meta.page.StandardPage;
+import net.n2oapp.framework.api.metadata.meta.widget.MessagePlacement;
+import net.n2oapp.framework.api.metadata.meta.widget.MessagePosition;
+import net.n2oapp.framework.api.metadata.meta.widget.RequestMethod;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.compile.context.ActionContext;
 import net.n2oapp.framework.config.metadata.compile.context.PageContext;
 import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
 import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import net.n2oapp.framework.config.test.SourceCompileTestBase;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static java.util.Collections.emptyMap;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasEntry;
 
 public class DataSourceCompileTest extends SourceCompileTestBase {
@@ -86,5 +93,49 @@ public class DataSourceCompileTest extends SourceCompileTestBase {
         Datasource ds = page.getDatasources().get("detail");
         assertThat(ds.getDependencies().size(), is(1));
         assertThat(ds.getDependencies().get(0).getOn(), is("models.resolve[\"p_w_a_master\"]"));
+    }
+
+    @Test
+    public void submit() {
+        StandardPage page = (StandardPage)
+                compile("net/n2oapp/framework/config/metadata/compile/datasource/testDSSubmit.page.xml")
+                        .get(new PageContext("testDSSubmit", "p/w/a"));
+
+        //        simple
+        Datasource ds = page.getDatasources().get("ds1");
+        assertThat(ds.getSubmit(), Matchers.notNullValue());
+        assertThat(ds.getSubmit().getUrl(), is("n2o/data/p/w/a/ds1"));
+        assertThat(ds.getSubmit().getSubmitForm(), is(true));
+        assertThat(ds.getSubmit().getMethod(), is(RequestMethod.POST));
+        ActionContext opCtx = ((ActionContext)route("p/w/a/ds1", CompiledObject.class));
+        assertThat(opCtx.getOperationId(), is("update"));
+        assertThat(opCtx.isMessageOnSuccess(), is(false));
+        assertThat(opCtx.isMessageOnFail(), is(true));
+        assertThat(opCtx.getMessagePosition(), is(MessagePosition.fixed));
+        assertThat(opCtx.getMessagePlacement(), is(MessagePlacement.top));
+
+        //        with form-param
+        ds = page.getDatasources().get("ds2");
+        assertThat(ds.getSubmit(), Matchers.notNullValue());
+        assertThat(ds.getSubmit().getSubmitForm(), is(false));
+        assertThat(ds.getSubmit().getFormMapping(), hasEntry("id", new ModelLink(ReduxModel.RESOLVE, "p_w_a_ds2", "id")));
+
+        //        with messages
+        ds = page.getDatasources().get("ds3");
+        assertThat(ds.getSubmit(), Matchers.notNullValue());
+        opCtx = ((ActionContext)route("p/w/a/ds3", CompiledObject.class));
+        assertThat(opCtx.isMessageOnSuccess(), is(true));
+        assertThat(opCtx.isMessageOnFail(), is(true));
+        assertThat(opCtx.getMessagePosition(), is(MessagePosition.fixed));
+        assertThat(opCtx.getMessagePlacement(), is(MessagePlacement.bottom));
+
+        //        with path-param
+        ds = page.getDatasources().get("ds4");
+        assertThat(ds.getSubmit(), Matchers.notNullValue());
+        assertThat(ds.getSubmit().getUrl(), is("n2o/data/p/w/a/:_id/update"));
+        assertThat(ds.getSubmit().getPathMapping(), hasEntry("_id", new ModelLink(ReduxModel.RESOLVE, "p_w_a_ds4", "id")));
+        opCtx = ((ActionContext)route("p/w/a/123/update", CompiledObject.class));
+        assertThat(opCtx, Matchers.notNullValue());
+        assertThat(opCtx.getParams("p/w/a/123/update", emptyMap()), hasEntry("_id", 123));
     }
 }
