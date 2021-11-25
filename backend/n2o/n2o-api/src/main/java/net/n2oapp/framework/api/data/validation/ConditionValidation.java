925d8c6f2711d52f0012ef341b278bc64db03ac3
amorozkin <amorozkin@i-novus.ru>
2021-11-25 10:40:24 +0300

    [BF] [NNO-7149] bugfix processing parameters on the server

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/data/validation/ConditionValidation.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/data/validation/ConditionValidation.java
@@ -5,14 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.n2oapp.criteria.dataset.DataSet;
+import net.n2oapp.framework.api.StringUtils;
 import net.n2oapp.framework.api.data.DomainProcessor;
 import net.n2oapp.framework.api.data.InvocationProcessor;
+import net.n2oapp.framework.api.metadata.global.dao.object.AbstractParameter;
 import net.n2oapp.framework.api.script.ScriptProcessor;
 
 import javax.script.ScriptException;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Клиентская модель валидации условия значений полей
@@ -41,26 +42,28 @@ public class ConditionValidation extends Validation {
     private Set<String> getExpressionsOn() {
         Set<String> res = new HashSet<>();
         if (expressionOn != null && expressionOn.length() > 0) {
-            String[] expressions = expressionOn.split(",");
-            for (String exp : expressions) {
-                res.add(exp.trim());
-            }
+            Arrays.asList(expressionOn.split(",")).forEach(e -> res.add(e.trim()));
         }
         return res;
+    }
+
+    private DataSet getCopiedDataSet (DataSet dataSet) {
+        DataSet copiedDataSet = new DataSet(dataSet);
+        for (String key : getExpressionsOn()) {
+            Object value = dataSet.get(key);
+            if (value instanceof Date) {
+                copiedDataSet.put(key, DomainProcessor.getInstance().serialize(value));
+            }
+        }
+        return copiedDataSet;
     }
 
     @Override
     public void validate(DataSet dataSet, InvocationProcessor serviceProvider, ValidationFailureCallback callback) {
         try {
-            DataSet copiedDataSet = new DataSet(dataSet);
-            for (String key : getExpressionsOn()) {
-                Object value = dataSet.get(key);
-                if (value instanceof Date) {
-                    copiedDataSet.put(key, DomainProcessor.getInstance().serialize(value));
-                }
-            }
+            DataSet copiedDataSet = getCopiedDataSet(dataSet);
             if (!(boolean) ScriptProcessor.getInstance().eval(getExpression(), copiedDataSet))
-                callback.onFail(getJsonMessage());
+                callback.onFail(StringUtils.resolveLinks(getMessage(), copiedDataSet));
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
