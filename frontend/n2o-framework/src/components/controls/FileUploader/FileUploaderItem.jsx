33d3a43fb8a8ebf40b3a54a67e4e59b45a162f7e
Соколов Алексей <asokolov@i-novus.ru>
2021-12-08 12:52:16 +0300

    fix/NNO-7204-release-7.17 added axios cancelToken to withFileUploader
    (cherry picked from commit 84f37aca04f698a9740d02491a330a90b67f2d38)

--- frontend/n2o-framework/src/components/controls/FileUploader/FileUploaderItem.jsx
+++ frontend/n2o-framework/src/components/controls/FileUploader/FileUploaderItem.jsx
@@ -1,86 +1,66 @@
 import React from 'react'
-import Tooltip from 'reactstrap/lib/Tooltip'
-import cn from 'classnames'
+import classNames from 'classnames'
 import Progress from 'reactstrap/lib/Progress'
 import PropTypes from 'prop-types'
 import isEmpty from 'lodash/isEmpty'
 
+import { Tooltip } from '../../snippets/Tooltip/Tooltip'
 import { Spinner } from '../../snippets/Spinner/Spinner'
 
 import { convertSize } from './utils'
 
-class FileUploaderItem extends React.Component {
-    constructor(props) {
-        super(props)
+function FileUploaderItem(props) {
+    const {
+        file,
+        percentage,
+        statusBarColor,
+        onRemove,
+        showSize,
+        disabled,
+        index,
+        loading,
+        autoUpload,
+        deleteIcon,
+    } = props
 
-        this.state = {
-            tooltipOpen: false,
-        }
+    const error = (!isEmpty(file.error) || !isEmpty(file.response)) && (file.response || file.error)
 
-        this.toggle = this.toggle.bind(this)
-    }
-
-    toggle() {
-        const { tooltipOpen } = this.state
-
-        this.setState({
-            tooltipOpen: !tooltipOpen,
-        })
-    }
-
-    render() {
-        const {
-            file,
-            percentage,
-            statusBarColor,
-            onRemove,
-            showSize,
-            disabled,
-            index,
-            loading,
-            autoUpload,
-        } = this.props
-        const { tooltipOpen } = this.state
-
-        return (
-            <div className="n2o-file-uploader-files-item">
-                <span className="n2o-file-uploader-files-item-info">
-                    <a
-                        title={file.name}
-                        href={file.link}
-                        /* eslint-disable-next-line react/jsx-no-target-blank */
-                        target="_blank"
-                        id={`tooltip-${file.id}`}
-                        className={cn('n2o-file-uploader-link', {
-                            'n2o-file-uploader-item-error': file.error,
-                        })}
-                    >
-                        <span className="n2o-file-uploader-file-name">{file.name}</span>
-                        {file.link && (
-                            <i className=" n2o-file-uploader-external-link fa fa-external-link" />
-                        )}
-                    </a>
-                    {(!isEmpty(file.error) || !isEmpty(file.response)) && (
-                        <Tooltip
-                            isOpen={tooltipOpen}
-                            target={`tooltip-${file.id}`}
-                            toggle={this.toggle}
+    return (
+        <div className="n2o-file-uploader-files-item">
+            <span className="n2o-file-uploader-files-item-info">
+                <Tooltip
+                    label={(
+                        <a
+                            title={file.name}
+                            href={file.link}
+                            /* eslint-disable-next-line react/jsx-no-target-blank */
+                            target="_blank"
+                            id={`tooltip-${file.id}`}
+                            className={classNames('n2o-file-uploader-link', {
+                                'n2o-file-uploader-item-error': file.error,
+                            })}
                         >
-                            {file.response || file.error}
-                        </Tooltip>
+                            <span className="n2o-file-uploader-file-name">{file.name}</span>
+                            {file.link && (
+                                <i className=" n2o-file-uploader-external-link fa fa-external-link" />
+                            )}
+                        </a>
                     )}
-                    <span className="n2o-file-uploader-item-size">
-                        {showSize && <span>{convertSize(file.size)}</span>}
-                        {!disabled && !loading && (
-                            <i
-                                onClick={() => onRemove(index, file.id)}
-                                className="n2o-file-uploader-remove fa fa-times ml-2"
-                            />
-                        )}
-                        {loading && <Spinner className="ml-2" type="inline" size="sm" />}
-                    </span>
+                    hint={error}
+                    placement="bottom"
+                />
+                <span className={classNames('n2o-file-uploader-item-size', { showSize })}>
+                    {showSize && <span className="ml-2 n2o-file-uploader__size">{convertSize(file.size)}</span>}
+                    {!disabled && (
+                        <i
+                            onClick={() => onRemove(index, file.id)}
+                            className={classNames('n2o-file-uploader-remove ml-2', deleteIcon || 'fa fa-times')}
+                        />
+                    )}
+                    {loading && <Spinner type="inline" size="sm" />}
                 </span>
-                {loading ||
+            </span>
+            {loading ||
           (!autoUpload && !file.status && (
               <Progress
                   className="n2o-file-uploader-progress-bar"
@@ -89,15 +69,15 @@ class FileUploaderItem extends React.Component {
                   color={statusBarColor}
               />
           ))}
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
 FileUploaderItem.propTypes = {
     file: PropTypes.object,
     percentage: PropTypes.number,
     statusBarColor: PropTypes.string,
+    deleteIcon: PropTypes.string,
     onRemove: PropTypes.func,
     showSize: PropTypes.bool,
     disabled: PropTypes.bool,
