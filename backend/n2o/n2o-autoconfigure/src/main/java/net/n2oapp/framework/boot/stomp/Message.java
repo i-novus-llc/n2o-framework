022f9f4bcede5613de6191ac014fb1853b77b478
DanilKharlushin <danilharlushin12@gmail.com>
2021-12-17 16:05:17 +0300

    [NF][NNO-7260] добавил WebSocketConfig

---
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/stomp/Message.java
@@ -0,0 +1,11 @@
+package net.n2oapp.framework.boot.stomp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+
+    private Integer count;
+}
---
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/stomp/MockCountController.java
@@ -0,0 +1,9 @@
+package net.n2oapp.framework.boot.stomp;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MockCountController {
+
+
+}
