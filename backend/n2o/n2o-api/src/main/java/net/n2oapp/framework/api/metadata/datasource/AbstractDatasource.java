0b88aa3bc895e804c509b77696173aa7cdf41446
DanilKharlushin <danilharlushin12@gmail.com>
2021-12-20 10:16:14 +0300

    [NF][NNO-7246] убрал интерфейс Serializable

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/datasource/AbstractDatasource.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/datasource/AbstractDatasource.java
@@ -7,11 +7,9 @@ import net.n2oapp.framework.api.metadata.Compiled;
 import net.n2oapp.framework.api.metadata.aware.IdAware;
 import net.n2oapp.framework.api.metadata.global.view.page.DefaultValuesMode;
 
-import java.io.Serializable;
-
 @Getter
 @Setter
-public abstract class AbstractDatasource implements Compiled, IdAware, Serializable {
+public abstract class AbstractDatasource implements Compiled, IdAware {
 
     @JsonProperty
     private String id;
