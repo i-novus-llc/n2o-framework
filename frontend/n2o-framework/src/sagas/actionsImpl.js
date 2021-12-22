5661220bc16f7e5d6107109b2fd22b415beff02e
rgalina <rgalina@i-novus.ru>
2021-12-22 11:14:53 +0300

    [NNO-6930] fix actionsImpl

--- frontend/n2o-framework/src/sagas/actionsImpl.js
+++ frontend/n2o-framework/src/sagas/actionsImpl.js
@@ -14,6 +14,7 @@ import merge from 'deepmerge'
 
 import { START_INVOKE } from '../constants/actionImpls'
 import {
+    makeDatasourceIdSelector,
 } from '../ducks/widgets/selectors'
 import { makeGetModelByPrefixSelector } from '../ducks/models/selectors'
 import { validate as validateDatasource } from '../core/datasource/validate'
