db964758d8206cfafe6a1b794a74ec70db7bfb18
dharlushin <dharlushin@i-novus.ru>
2022-01-10 14:57:01 +0300

    [NF][NNO-7319] сделал конфигурацию веб-сокетов опциональной, поменял STOMP endpoint на /n2o/ws

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/websocket/N2oWebSocketAutoConfiguration.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/websocket/N2oWebSocketAutoConfiguration.java
@@ -1,23 +1,20 @@
-package net.n2oapp.framework.boot.stomp;
+package net.n2oapp.framework.boot.websocket;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class N2oWebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
+@ConditionalOnClass(WebSocketMessageBrokerConfigurer.class)
+public class N2oWebSocketAutoConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/n2o/ws")
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
     }
 }
---
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/websocket/UserHandshakeHandler.java
@@ -0,0 +1,18 @@
+package net.n2oapp.framework.boot.websocket;
+
+import com.sun.security.auth.UserPrincipal;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+
+public class UserHandshakeHandler extends DefaultHandshakeHandler {
+
+    @Override
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        return new UserPrincipal(UUID.randomUUID().toString());
+    }
+}
