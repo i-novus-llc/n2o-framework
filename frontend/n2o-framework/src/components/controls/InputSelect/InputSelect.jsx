8a56053c1274b386a81fb2f32fc6e367052237e9
Муравцев Альберт Вячеславович <amuravtsev@i-novus.ru>
2022-01-18 14:08:35 +0300

    [NNO-7320] Обновить зависимость reactstrap до 9.0.0. Убрал ранее используемый хак.
    (cherry picked from commit 01b976e32edcb9df385903c8f85a17207758c285)

--- frontend/n2o-framework/src/components/controls/InputSelect/InputSelect.jsx
+++ frontend/n2o-framework/src/components/controls/InputSelect/InputSelect.jsx
@@ -604,7 +604,6 @@ class InputSelect extends React.Component {
                     </DropdownToggle>
 
                     <DropdownMenu
-                        key={stateValue}
                         className={classNames('n2o-input-select__menu', {
                             'n2o-input-select__menu--autosize': popupAutoSize,
                         })}
