a3ed826c7f55e615ab535f88cf1c32dc6f99ca92
Соколов Алексей <asokolov@i-novus.ru>
2022-01-18 14:32:38 +0300

    [BF][NNO-7307] added sidebar items security check

--- frontend/n2o-framework/src/plugins/Menu/MenuContainer.js
+++ frontend/n2o-framework/src/plugins/Menu/MenuContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { getContext, compose, mapProps } from 'recompose'
-import isArray from 'lodash/isArray'
 import isEqual from 'lodash/isEqual'
 import uniqBy from 'lodash/uniqBy'
 import isEmpty from 'lodash/isEmpty'
@@ -14,20 +13,16 @@ import { withRouter } from 'react-router-dom'
 import withSecurity from '../../core/auth/withSecurity'
 import { SECURITY_CHECK } from '../../core/auth/authTypes'
 
+const initialItems = {
+    headerItems: [],
+    headerExtraItems: [],
+    sidebarItems: [],
+    sidebarExtraItems: [],
+}
+
 export class MenuContainer extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            items: [],
-            extraItems: [],
-        }
-
-        this.mapRenderProps = this.mapRenderProps.bind(this)
-        this.checkItem = this.checkItem.bind(this)
-        this.makeSecure = this.makeSecure.bind(this)
-        this.getItemsWithAccess = this.getItemsWithAccess.bind(this)
-        this.setSubItem = this.setSubItem.bind(this)
+    state = {
+        ...initialItems,
     }
 
     async componentDidMount() {
@@ -35,139 +30,148 @@ export class MenuContainer extends React.Component {
     }
 
     async componentDidUpdate(prevProps) {
-        const { user } = this.props
-        /*
-        * TODO сделать обертку которая передает items
-        *  исбавиться от get
-        */
-        const items = get(this.props, 'header.menu.items')
-        const prevItems = get(prevProps, 'header.menu.items')
-        const extraItems = get(prevProps, 'header.extraMenu.items')
-        const prevExtraItems = get(prevProps, 'header.extraMenu.items')
+        const {
+            user,
+            headerItems,
+            headerExtraItems,
+            sidebarItems,
+            sidebarExtraItems,
+        } = this.props
+
+        const {
+            headerItems: prevHeaderItems,
+            headerExtraItems: prevHeaderExtraItems,
+            sidebarItems: prevSidebarItems,
+            sidebarExtraItems: prevSidebarExtraItems,
+            user: prevPropsUser,
+        } = prevProps
 
         if (
-            !isEqual(items, prevItems) ||
-            !isEqual(extraItems, prevExtraItems) ||
-            !isEqual(user, prevProps.user)
+            !isEqual(headerItems, prevHeaderItems) ||
+            !isEqual(headerExtraItems, prevHeaderExtraItems) ||
+            !isEqual(sidebarItems, prevSidebarItems) ||
+            !isEqual(sidebarExtraItems, prevSidebarExtraItems) ||
+            !isEqual(user, prevPropsUser)
         ) {
             await this.getItemsWithAccess()
         }
     }
 
-    async checkItem(item, type, id = null) {
-        if (item.security) {
-            const { user, authProvider } = this.props
-            const config = item.security
+     checkItem = async (item, type, id = null) => {
+         if (item.security) {
+             const { user, authProvider } = this.props
+             const config = item.security
 
-            try {
-                await authProvider(SECURITY_CHECK, {
-                    config,
-                    user,
-                })
+             try {
+                 await authProvider(SECURITY_CHECK, {
+                     config,
+                     user,
+                 })
 
-                if (!id) {
-                    this.setState(prevState => ({
-                        ...prevState,
-                        [type]: toArray(uniqBy(prevState[type].concat(item), 'id')),
-                    }))
-                }
-            } catch (error) {
-                if (id) {
-                    this.setSubItem(item, type, id)
-                }
-            }
-        } else {
-            if (!id) {
-                this.setState(prevState => ({
-                    ...prevState,
-                    [type]: toArray(uniqBy(prevState[type].concat(item), 'id')),
-                }))
-            }
-            if (item.items) {
-                // eslint-disable-next-line no-restricted-syntax
-                for (const subItem of item.items) {
-                    await this.checkItem(subItem, 'items', item.id)
-                }
-            }
-        }
-    }
+                 if (!id) {
+                     this.setState(prevState => ({
+                         ...prevState,
+                         [type]: toArray(uniqBy(prevState[type].concat(item), 'id')),
+                     }))
+                 }
+             } catch (error) {
+                 if (id) {
+                     this.setSubItem(item, type, id)
+                 }
+             }
+         } else {
+             if (!id) {
+                 this.setState(prevState => ({
+                     ...prevState,
+                     [type]: toArray(uniqBy(prevState[type].concat(item), 'id')),
+                 }))
+             }
+             if (item.items) {
+                 for (const subItem of item.items) {
+                     await this.checkItem(subItem, 'items', item.id)
+                 }
+             }
+         }
+     }
 
-    setSubItem(item, type, id) {
-        const { [type]: stateItem } = this.state
+     setSubItem = (item, type, id) => {
+         const { [type]: stateItem } = this.state
 
-        const parentIndex = findIndex(stateItem, i => i.id === id)
-        const parentItem = get(stateItem, parentIndex.toString())
-        let subItems = get(parentItem, 'items', [])
+         const parentIndex = findIndex(stateItem, i => i.id === id)
+         const parentItem = get(stateItem, parentIndex.toString())
+         let subItems = get(parentItem, 'items', [])
 
-        subItems = filter(subItems, i => i.id !== item.id)
-        parentItem.items = subItems
-        this.setState((prevState) => {
-            const newState = prevState
+         subItems = filter(subItems, i => i.id !== item.id)
+         parentItem.items = subItems
+         this.setState((prevState) => {
+             const newState = prevState
 
-            newState[type][parentIndex].items = subItems
+             newState[type][parentIndex].items = subItems
 
-            return newState
-        })
-    }
+             return newState
+         })
+     }
 
-    async makeSecure(config) {
-        const makeSecure = async (items, type) => {
-            if (isArray(items) && !isEmpty(items)) {
-                // eslint-disable-next-line no-restricted-syntax
-                for (const item of items) {
-                    await this.checkItem(item, type)
-                }
-            }
-        }
-        /*
-        * TODO сделать обертку которая передает items
-        *  исбавиться от get
-        */
+     makeSecure = async (headerItems, headerExtraItems, sidebarItems, sidebarExtraItems) => {
+         const makeSecure = async (items, type) => {
+             if (Array.isArray(items) && !isEmpty(items)) {
+                 for (const item of items) {
+                     await this.checkItem(item, type)
+                 }
+             }
+         }
 
-        const items = get(config, 'header.menu.items')
-        const extraItems = get(config, 'header.extraMenu.items')
+         await makeSecure(headerItems, 'headerItems')
+         await makeSecure(headerExtraItems, 'headerExtraItems')
+         await makeSecure(sidebarItems, 'sidebarItems')
+         await makeSecure(sidebarExtraItems, 'sidebarExtraItems')
+     }
 
-        await makeSecure(items, 'items')
-        await makeSecure(extraItems, 'extraItems')
-    }
+      getItemsWithAccess = async () => {
+          const {
+              headerItems,
+              headerExtraItems,
+              sidebarItems,
+              sidebarExtraItems,
+          } = this.props
 
-    async getItemsWithAccess() {
-        await this.setState({ items: [], extraItems: [] })
-        await this.makeSecure(this.props)
-    }
+          await this.setState({ ...initialItems })
+          await this.makeSecure(headerItems, headerExtraItems, sidebarItems, sidebarExtraItems)
+      }
 
-    mapRenderProps() {
-        const { items, extraItems } = this.state
-        const { header, location } = this.props
+      mapRenderProps = () => {
+          const { headerItems, headerExtraItems, sidebarItems, sidebarExtraItems } = this.state
+          const { header, sidebar, location } = this.props
 
-        if (!header) {
-            return this.props
-        }
+          if (!header && !sidebar) {
+              return this.props
+          }
+          console.log('Yo!!!', this.props)
+          const withSecurityProps = (props, items, extraItems, type) => ({
+              [type]: {
+                  ...props,
+                  location,
+                  menu: {
+                      items: filter(items, i => !i.items || !isEmpty(i.items)),
+                  },
+                  extraMenu: {
+                      items: filter(extraItems, i => !i.items || !isEmpty(i.items)),
+                  },
+              },
+          })
 
-        const headerProps = {
-            header: {
-                ...header,
-                location,
-                menu: {
-                    items: filter(items, i => !i.items || !isEmpty(i.items)),
-                },
-                extraMenu: {
-                    items: filter(extraItems, i => !i.items || !isEmpty(i.items)),
-                },
-            },
-        }
+          return {
+              ...this.props,
+              ...withSecurityProps(header, headerItems, headerExtraItems, 'header'),
+              ...withSecurityProps(sidebar, sidebarItems, sidebarExtraItems, 'sidebar'),
+          }
+      }
 
-        return {
-            ...this.props,
-            ...headerProps,
-        }
-    }
+      render() {
+          const { render } = this.props
 
-    render() {
-        const { render } = this.props
-
-        return render(this.mapRenderProps())
-    }
+          return render(this.mapRenderProps())
+      }
 }
 
 MenuContainer.propTypes = {
@@ -175,7 +179,12 @@ MenuContainer.propTypes = {
     user: PropTypes.any,
     authProvider: PropTypes.any,
     header: PropTypes.object,
+    sidebar: PropTypes.object,
     location: PropTypes.object,
+    headerItems: PropTypes.array,
+    headerExtraItems: PropTypes.array,
+    sidebarItems: PropTypes.array,
+    sidebarExtraItems: PropTypes.array,
 }
 
 MenuContainer.defaultProps = {
@@ -188,12 +197,24 @@ export const ConfigContainer = compose(
     }),
     withRouter,
     withSecurity,
-    mapProps(({ getFromConfig, ...rest }) => ({
-        ...rest,
-        ...(getFromConfig && {
-            ...getFromConfig('menu'),
-        }),
-    })),
+    mapProps(({ getFromConfig, ...rest }) => {
+        let configProps = {}
+
+        if (getFromConfig) {
+            configProps = getFromConfig('menu')
+        }
+
+        const { header, sidebar } = configProps
+
+        return {
+            ...rest,
+            ...configProps,
+            headerItems: get(header, 'menu.items') || [],
+            headerExtraItems: get(header, 'extraMenu.items') || [],
+            sidebarItems: get(sidebar, 'menu.items') || [],
+            sidebarExtraItems: get(sidebar, 'extraMenu.items') || [],
+        }
+    }),
 )(MenuContainer)
 
 export default ConfigContainer
