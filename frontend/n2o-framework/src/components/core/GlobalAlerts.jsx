dafdcab2f2d811dcdda46deda027a3e5ad9eb821
Соколов Алексей <asokolov@i-novus.ru>
2022-02-02 15:10:00 +0300

    sub/NNO-7299 added new params + refactoring Alert

--- frontend/n2o-framework/src/components/core/GlobalAlerts.jsx
+++ frontend/n2o-framework/src/components/core/GlobalAlerts.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import map from 'lodash/map'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-// eslint-disable-next-line import/no-named-as-default
-import Alerts from '../snippets/Alerts/Alerts'
+import { Alerts } from '../snippets/Alerts/Alerts'
 import { GLOBAL_KEY } from '../../ducks/alerts/constants'
 import { alertsByKeySelector, removeAlert } from '../../ducks/alerts/store'
 
@@ -15,30 +13,25 @@ import { alertsByKeySelector, removeAlert } from '../../ducks/alerts/store'
  * @param {function} onDismiss - функция закрытия
  * @returns {JSX.Element}
  */
-export function GlobalAlerts({ alerts, onDismiss }) {
-    const handleDismiss = alertId => alertId && onDismiss(alertId)
-    const mapAlertsProps = () => map(alerts, alert => ({
+
+export function GlobalAlerts({ alerts = [], onDismiss }) {
+    const mappedAlerts = alerts.map(alert => ({
         ...alert,
         key: alert.id,
-        onDismiss: () => handleDismiss(alert.id),
+        onDismiss: () => alert.id && onDismiss(alert.id),
         className: 'd-inline-flex mb-0 p-2 mw-100',
-        details: alert.stacktrace,
         animate: true,
         position: 'fixed',
     }))
 
     return (
-        <Alerts alerts={mapAlertsProps()} />
+        <Alerts alerts={mappedAlerts} />
     )
 }
 
 GlobalAlerts.propTypes = {
     alerts: PropTypes.array,
     onDismiss: PropTypes.func,
-}
-
-GlobalAlerts.defaultProps = {
-    alerts: [],
 }
 
 const mapStateToProps = createStructuredSelector({
---
+++ frontend/n2o-framework/src/components/snippets/Alerts/DefaultAlert.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import classNames from 'classnames'
+import defaultTo from 'lodash/defaultTo'
+
+import HelpPopover from '../../widgets/Form/fields/StandardField/HelpPopover'
+
+import { AlertTypes } from './AlertsTypes'
+
+export const DefaultAlert = ({
+    title,
+    text,
+    color,
+    href,
+    timestamp,
+    closeButton,
+    className,
+    style,
+    icon,
+    stacktrace,
+    onDismiss,
+    help,
+    animate,
+    t,
+    detailsVisible,
+    togglingDetails,
+    formattingDetails,
+}) => {
+    const closeIconClasses = classNames(
+        'close n2o-alert-close n2o-alert-close__icon',
+        { 'with-details': stacktrace },
+    )
+
+    const batchedActionToClose = (e) => {
+        e.preventDefault()
+        onDismiss()
+    }
+
+    const batchedActionToToggling = (e) => {
+        e.preventDefault()
+        togglingDetails()
+    }
+
+    return (
+        <a
+            href={href}
+            className={classNames('n2o-alert', 'alert', className, {
+                [`alert-${color}`]: color,
+                'n2o-alert--animated': animate,
+                'with-details': stacktrace,
+            })}
+            style={style}
+        >
+            <div className="n2o-alert-help">
+                {help && <HelpPopover help={help} />}
+            </div>
+            <div className="n2o-alert-body-container">
+                {title && (
+                    <div className="n2o-alert-header">
+                        {icon && <i className={icon} />}
+                        <h4>{title}</h4>
+                    </div>
+                )}
+                <div className="n2o-alert-body">
+                    <div className="n2o-alert-body-text white-space-pre-line">
+                        {text}
+                    </div>
+                    {stacktrace && (
+                        <a
+                            className="alert-link details-title"
+                            onClick={batchedActionToToggling}
+                        >
+                            {detailsVisible ? t('hide') : t('details')}
+                        </a>
+                    )}
+                    {detailsVisible && (
+                        <div className="details">{formattingDetails(stacktrace)}</div>
+                    )}
+                </div>
+            </div>
+            <span>{timestamp}</span>
+            {defaultTo(closeButton, true) && (
+                <div className={closeIconClasses} onClick={batchedActionToClose}>×</div>
+            )}
+        </a>
+    )
+}
+
+DefaultAlert.propTypes = AlertTypes
