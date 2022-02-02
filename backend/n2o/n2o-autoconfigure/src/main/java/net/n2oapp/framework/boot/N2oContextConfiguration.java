b1e0ebe5fe2e52262480837ae52cb4bd339ed33f
dharlushin <dharlushin@i-novus.ru>
2022-02-02 12:34:45 +0300

    [BF][NNO-7473] изменил декларацию бина handshakeHandler

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oContextConfiguration.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oContextConfiguration.java
@@ -1,5 +1,6 @@
 package net.n2oapp.framework.boot;
 
+import com.sun.security.auth.UserPrincipal;
 import net.n2oapp.framework.api.context.ContextEngine;
 import net.n2oapp.framework.api.user.StaticUserContext;
 import net.n2oapp.framework.ui.context.ConcurrentMapContextEngine;
@@ -9,7 +10,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Конфигурация пользовательского контекста
@@ -32,5 +40,16 @@ public class N2oContextConfiguration {
     @Bean
     public StaticUserContext staticUserContext(ContextEngine contextEngine) {
         return new StaticUserContext(contextEngine);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(DefaultHandshakeHandler.class)
+    public DefaultHandshakeHandler handshakeHandler() {
+        return new DefaultHandshakeHandler() {
+            @Override
+            protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                return new UserPrincipal(UUID.randomUUID().toString());
+            }
+        };
     }
 }
