2d94be4569fc0fec7230da868271dac2092efbc3
dharlushin <dharlushin@i-novus.ru>
2022-02-15 15:22:54 +0300

    [NF][NNO-7536] добавил проверку наличия идентификатора поля в метаданной

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/control/FieldValidator.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/control/FieldValidator.java
@@ -28,6 +28,8 @@ public class FieldValidator implements SourceValidator<N2oField>, SourceClassAwa
 
     @Override
     public void validate(N2oField source, SourceProcessor p) {
+        WidgetScope widgetScope = p.getScope(WidgetScope.class);
+        checkIdExistence(source, widgetScope, p);
         if (p.getScope(FieldsScope.class) != null) {
             FieldsScope scope = p.getScope(FieldsScope.class);
             Boolean sameFieldIdHasDependency = scope.get(source.getId());
@@ -40,9 +42,21 @@ public class FieldValidator implements SourceValidator<N2oField>, SourceClassAwa
         checkDependencies(source);
         DatasourceIdsScope datasourceIdsScope = p.getScope(DatasourceIdsScope.class);
         checkRefDatasource(source, datasourceIdsScope);
-        WidgetScope widgetScope = p.getScope(WidgetScope.class);
         if (widgetScope != null)
             checkWhiteListValidation(source, widgetScope, p);
+    }
+
+    /**
+     * Проверка наличия идентификатора поля
+     * @param source      Поле
+     * @param widgetScope Скоуп виджета, в котором находится поле
+     * @param p           Процессор исходных метаданных
+     */
+    private void checkIdExistence(N2oField source, WidgetScope widgetScope, SourceProcessor p) {
+        p.checkIdExistence(source, String.format(
+                "Для компиляции виджета %s необходимо задать идентификаторы для всех полей",
+                widgetScope != null ? ValidationUtils.getIdOrEmptyString(widgetScope.getWidgetId()) : ""
+         ));
     }
 
     /**
