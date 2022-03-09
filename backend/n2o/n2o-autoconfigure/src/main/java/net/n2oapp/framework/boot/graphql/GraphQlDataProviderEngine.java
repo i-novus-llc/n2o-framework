81059331c45f20856a10d801bcd69ec9393e496f
Лебеденко Игорь <ilebedenko@i-novus.ru>
2022-03-09 15:35:35 +0300

    [RF][NNO-7610] - some refactoring

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/graphql/GraphQlDataProviderEngine.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/graphql/GraphQlDataProviderEngine.java
@@ -8,6 +8,7 @@ import net.n2oapp.framework.engine.data.QueryUtil;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,8 +25,8 @@ public class GraphQlDataProviderEngine implements MapInvocationEngine<N2oGraphQl
     @Setter
     private RestTemplate restTemplate;
 
-    private Pattern variablePattern = Pattern.compile("\\$\\w+");
-    private Pattern placeholderKeyPattern = Pattern.compile("\\{\\{\\w+}}\\s*:");
+    private final Pattern variablePattern = Pattern.compile("\\$\\w+");
+    private final Pattern placeholderKeyPattern = Pattern.compile("\\{\\{\\w+}}\\s*:");
 
 
     @Override
@@ -145,24 +146,33 @@ public class GraphQlDataProviderEngine implements MapInvocationEngine<N2oGraphQl
      * @return Множество переменных
      */
     private Set<String> extractVariables(String query) {
-        Set<String> variables = new HashSet<>();
-        Matcher matcher = variablePattern.matcher(query);
-        while (matcher.find())
-            variables.add(query.substring(matcher.start() + 1, matcher.end()));
-        return variables;
+        return extract(query, variablePattern, (s, m) -> s.substring(m.start() + 1, m.end()));
     }
 
     /**
      * Получение множества ключей-плейсхолдеров из входной строки GraphQl запроса
+     *
      * @param query Строка GraphQl запроса
      * @return Множество ключей-плейсхолдеров
      */
     private Set<String> extractPlaceholderKeys(String query) {
-        Set<String> keys = new HashSet<>();
-        Matcher matcher = placeholderKeyPattern.matcher(query);
+        return extract(query, placeholderKeyPattern, (s, m) -> s.substring(m.start(), m.end() - 1).trim());
+    }
+
+    /**
+     * Получение множества значений из входной строки GraphQl запроса
+     *
+     * @param query    Строка GraphQl запроса
+     * @param pattern  Паттерн, по которому будут находиться значения
+     * @param function Функция, описывающая действия с найденными значениями
+     * @return Множество значений
+     */
+    private Set<String> extract(String query, Pattern pattern, BiFunction<String, Matcher, String> function) {
+        Set<String> result = new HashSet<>();
+        Matcher matcher = pattern.matcher(query);
         while (matcher.find())
-            keys.add(query.substring(matcher.start(), matcher.end() - 1).trim());
-        return keys;
+            result.add(function.apply(query, matcher));
+        return result;
     }
 
     /**
