bd1cfdd8e156360d01d94c7ef22f36377f7b2244
Игорь Рябов <iryabov@i-novus.ru>
2022-03-11 16:31:16 +0300

    NNO-7296 Added testDynamicToolbarCompiler

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/toolbar/ToolbarCompileDynamicTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/toolbar/ToolbarCompileDynamicTest.java
@@ -1,12 +1,21 @@
 package net.n2oapp.framework.config.metadata.compile.toolbar;
 
+import net.n2oapp.framework.api.metadata.compile.CompileContext;
+import net.n2oapp.framework.api.metadata.global.view.action.LabelType;
 import net.n2oapp.framework.api.metadata.global.view.widget.N2oWidget;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.N2oTable;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.N2oButton;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.N2oToolbar;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.ToolbarItem;
 import net.n2oapp.framework.api.metadata.meta.page.SimplePage;
+import net.n2oapp.framework.api.metadata.meta.toolbar.Toolbar;
 import net.n2oapp.framework.api.metadata.meta.toolbar.ToolbarCell;
 import net.n2oapp.framework.api.metadata.meta.widget.table.Table;
+import net.n2oapp.framework.api.metadata.pipeline.CompilePipeline;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.compile.pipeline.N2oPipelineSupport;
 import net.n2oapp.framework.config.metadata.compile.context.PageContext;
+import net.n2oapp.framework.config.metadata.compile.context.WidgetContext;
 import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
 import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
 import net.n2oapp.framework.config.register.JavaInfo;
@@ -42,4 +51,18 @@ public class ToolbarCompileDynamicTest extends SourceCompileTestBase {
         assertThat(((ToolbarCell)((Table)page.getWidget()).getComponent().getCells().get(0)).getToolbar(), notNull());
     }
 
+    @Test
+    public void testDynamicToolbarCompiler() {
+        builder.scan();
+        N2oToolbar toolbar = new N2oToolbar();
+        N2oButton deleteButton = new N2oButton();
+        deleteButton.setId("deleteRecord_r_");
+        deleteButton.setType(LabelType.icon);
+        deleteButton.setIcon("fa fa-pencil");
+        deleteButton.setColor("danger");
+        toolbar.setItems(new ToolbarItem[]{deleteButton});
+        CompilePipeline pipeline = N2oPipelineSupport.compilePipeline(builder.getEnvironment());
+        CompileContext<?, ?> context = new WidgetContext("");
+        Toolbar filterField = pipeline.compile().get(toolbar, context);
+    }
 }
