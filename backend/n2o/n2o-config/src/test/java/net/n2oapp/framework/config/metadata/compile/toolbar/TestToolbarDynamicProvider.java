06dfb42d5bf077fa0b1ece3358157a2ae4b41f39
adel <ammindubaev@i-novus.ru>
2022-03-11 16:06:41 +0300

    [NNO-7607] created test for toolbar dynamic provider

---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/toolbar/TestToolbarDynamicProvider.java
@@ -0,0 +1,47 @@
+package net.n2oapp.framework.config.metadata.compile.toolbar;
+
+import net.n2oapp.framework.api.metadata.SourceMetadata;
+import net.n2oapp.framework.api.metadata.global.view.action.LabelType;
+import net.n2oapp.framework.api.metadata.global.view.widget.table.N2oTable;
+import net.n2oapp.framework.api.metadata.global.view.widget.table.column.AbstractColumn;
+import net.n2oapp.framework.api.metadata.global.view.widget.table.column.N2oSimpleColumn;
+import net.n2oapp.framework.api.metadata.global.view.widget.table.column.cell.N2oToolbarCell;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.N2oButton;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.ToolbarItem;
+import net.n2oapp.framework.api.register.DynamicMetadataProvider;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestToolbarDynamicProvider implements DynamicMetadataProvider {
+
+    public static final String TEST_DYNAMIC = "testDynamic";
+
+    @Override
+    public String getCode() {
+        return TEST_DYNAMIC;
+    }
+
+    @Override
+    public List<? extends SourceMetadata> read(String context) {
+        N2oTable table = new N2oTable();
+        N2oSimpleColumn idColumn = new N2oSimpleColumn();
+        idColumn.setId("id");
+        idColumn.setTextFieldId("id");
+        table.setColumns(new AbstractColumn[]{idColumn});
+        table.setId("testDynamic?Dummy");
+
+        N2oToolbarCell toolbar = new N2oToolbarCell();
+        N2oButton deleteButton = new N2oButton();
+        deleteButton.setId("deleteRecord_r_");
+        deleteButton.setType(LabelType.icon);
+        deleteButton.setIcon("fa fa-pencil");
+        deleteButton.setColor("danger");
+        toolbar.setItems(new ToolbarItem[]{deleteButton});
+        N2oSimpleColumn n2oSimpleColumn = new N2oSimpleColumn();
+        n2oSimpleColumn.setCell(toolbar);
+        table.setColumns(new N2oSimpleColumn[]{n2oSimpleColumn});
+        return Arrays.asList(table);
+    }
+
+}
---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/toolbar/ToolbarCompileDynamicTest.java
@@ -0,0 +1,44 @@
+package net.n2oapp.framework.config.metadata.compile.toolbar;
+
+import net.n2oapp.framework.api.metadata.global.view.widget.table.N2oTable;
+import net.n2oapp.framework.api.metadata.meta.page.SimplePage;
+import net.n2oapp.framework.api.metadata.meta.toolbar.ToolbarCell;
+import net.n2oapp.framework.api.metadata.meta.widget.table.Table;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.compile.context.PageContext;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oAllPagesPack;
+import net.n2oapp.framework.config.register.JavaInfo;
+import net.n2oapp.framework.config.register.dynamic.JavaSourceLoader;
+import net.n2oapp.framework.config.test.SourceCompileTestBase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.notNull;
+
+public class ToolbarCompileDynamicTest extends SourceCompileTestBase {
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(new N2oAllDataPack(), new N2oAllPagesPack())
+                .sources(new JavaInfo("testDynamic", N2oTable.class))
+                .providers(new TestToolbarDynamicProvider())
+                .loaders(new JavaSourceLoader(builder.getEnvironment().getDynamicMetadataProviderFactory()));
+    }
+
+    @Test
+    public void testDynamicPage() {
+        SimplePage page = (SimplePage) compile("net/n2oapp/framework/config/metadata/compile/toolbar/toolbarCompileDynamicTest.page.xml")
+                .get(new PageContext("toolbarCompileDynamicTest"));
+        assertThat(((ToolbarCell)((Table)page.getWidget()).getComponent().getCells().get(0)).getToolbar(), notNull());
+    }
+
+}
