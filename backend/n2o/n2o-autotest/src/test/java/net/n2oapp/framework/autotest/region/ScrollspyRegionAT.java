4cdd3e70315b00adf0dd8c6fb4a242a45d4ed569
DanilKharlushin <danilharlushin12@gmail.com>
2022-03-11 14:30:56 +0300

    [NF][NNO-7525] дополнил тесты

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/region/ScrollspyRegionAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/region/ScrollspyRegionAT.java
@@ -1,8 +1,8 @@
 package net.n2oapp.framework.autotest.region;
 
 import net.n2oapp.framework.autotest.api.component.page.StandardPage;
-import net.n2oapp.framework.autotest.api.component.region.ScrollspyRegion;
-import net.n2oapp.framework.autotest.api.component.region.TabsRegion;
+import net.n2oapp.framework.autotest.api.component.region.*;
+import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
 import net.n2oapp.framework.autotest.run.AutoTestBase;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
@@ -66,6 +66,99 @@ public class ScrollspyRegionAT extends AutoTestBase {
         menu.menuItem("Дополнительная информация").click();
         region.activeContentItemShouldBe("Дополнительная информация");
         region.activeMenuItemShouldBe("Дополнительная информация");
+    }
+
+    @Test
+    public void testContent() {
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/region/scrollspy/content/index.page.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        page.breadcrumb().titleShouldHaveText("Тестирование контента");
+
+        ScrollspyRegion scrollspy = page.regions().region(0, ScrollspyRegion.class);
+        scrollspy.activeMenuItemShouldBe("Элемент1");
+        scrollspy.activeContentItemShouldBe("Элемент1");
+        RegionItems content = scrollspy.contentItem("Элемент1").content();
+
+        TabsRegion tabsRegion = content.region(0, TabsRegion.class);
+        tabsRegion.shouldExists();
+        tabsRegion.shouldHaveSize(3);
+
+        PanelRegion panelRegion = content.region(1, PanelRegion.class);
+        panelRegion.shouldExists();
+        panelRegion.shouldHaveTitle("Регион в виде панелей");
+
+        LineRegion lineRegion = content.region(2, LineRegion.class);
+        lineRegion.shouldExists();
+        lineRegion.shouldHaveLabel("Регион с горизонтальным делителем");
+
+        content.region(3, SimpleRegion.class).shouldExists();
+        FormWidget formWidget = content.region(3, SimpleRegion.class).content().widget(FormWidget.class);
+        formWidget.shouldExists();
+        formWidget.fields().field("Поле1").shouldExists();
+
+        formWidget = scrollspy.contentItem("Элемент2").content().widget(FormWidget.class);
+        formWidget.shouldExists();
+        formWidget.fields().field("Поле2").shouldExists();
+    }
+
+    @Test
+    public void testTabsInScrollspy() {
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/region/scrollspy/tabs_in_scrollspy/index.page.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        page.breadcrumb().titleShouldHaveText("Тестирование табов внутри scrollspy");
+
+        ScrollspyRegion scrollspy = page.regions().region(0, ScrollspyRegion.class);
+        scrollspy.activeMenuItemShouldBe("Элемент1");
+        scrollspy.activeContentItemShouldBe("Элемент1");
+        TabsRegion tabs = scrollspy.contentItem(0).content().region(TabsRegion.class);
+        tabs.tab(0).shouldBeActive();
+
+        tabs.tab(1).click();
+        tabs.tab(1).shouldBeActive();
+        scrollspy.activeMenuItemShouldBe("Элемент1");
+        scrollspy.activeContentItemShouldBe("Элемент1");
+
+        scrollspy.menu().menuItem("Элемент2").click();
+        scrollspy.activeMenuItemShouldBe("Элемент2");
+        scrollspy.activeContentItemShouldBe("Элемент2");
+        tabs.tab(1).shouldBeActive();
+    }
+
+    @Test
+    public void testScrollspyInTabs() {
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/region/scrollspy/scrollspy_in_tabs/index.page.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        page.breadcrumb().titleShouldHaveText("Тестирование scrollspy внутри табов");
+
+        TabsRegion tabs = page.regions().region(0, TabsRegion.class);
+        tabs.tab(0).shouldBeActive();
+        ScrollspyRegion scrollspy1 = tabs.tab(0).content().region(ScrollspyRegion.class);
+        scrollspy1.activeMenuItemShouldBe("Элемент1 в первом табе");
+        scrollspy1.activeContentItemShouldBe("Элемент1 в первом табе");
+
+        scrollspy1.menu().menuItem("Элемент2 в первом табе").click();
+        scrollspy1.activeMenuItemShouldBe("Элемент2 в первом табе");
+        scrollspy1.activeContentItemShouldBe("Элемент2 в первом табе");
+        tabs.tab(0).shouldBeActive();
+
+        tabs.tab(1).click();
+        tabs.tab(1).shouldBeActive();
+        ScrollspyRegion scrollspy2 = tabs.tab(1).content().region(ScrollspyRegion.class);
+        scrollspy2.activeMenuItemShouldBe("Элемент1 во втором табе");
+        scrollspy2.activeContentItemShouldBe("Элемент1 во втором табе");
+
+        scrollspy2.menu().menuItem("Элемент3 во втором табе").click();
+        scrollspy2.activeMenuItemShouldBe("Элемент3 во втором табе");
+        scrollspy2.activeContentItemShouldBe("Элемент3 во втором табе");
+        tabs.tab(1).shouldBeActive();
+
+        tabs.tab(0).click();
+        tabs.tab(0).shouldBeActive();
+        scrollspy1.activeMenuItemShouldBe("Элемент2 в первом табе");
+        scrollspy1.activeContentItemShouldBe("Элемент2 в первом табе");
     }
 
     @Test
