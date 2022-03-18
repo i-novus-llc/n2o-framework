8b0d2ca9cbfe7c9cb337b48a424ec622286535df
rgalina <rgalina@i-novus.ru>
2022-03-18 12:56:46 +0300

    [NNO-7611] fix autotest

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/ObjectMapperConstructor.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/ObjectMapperConstructor.java
@@ -2,6 +2,7 @@ package net.n2oapp.framework.boot;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import net.n2oapp.framework.api.data.DomainProcessor;
@@ -14,13 +15,11 @@ public class ObjectMapperConstructor {
     public static ObjectMapper metaObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setDateFormat(new SimpleDateFormat(DomainProcessor.JAVA_DATE_FORMAT));
-//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-//        objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-//                .withFieldVisibility(JsonAutoDetect.Visibility.NONE)
-//                .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-//                .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-//                .withCreatorVisibility(JsonAutoDetect.Visibility.NONE)
-//                .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE));
+        objectMapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.registerModule(new N2oJacksonModule(new SimpleDateFormat(DomainProcessor.JAVA_DATE_FORMAT)));
         return objectMapper;
