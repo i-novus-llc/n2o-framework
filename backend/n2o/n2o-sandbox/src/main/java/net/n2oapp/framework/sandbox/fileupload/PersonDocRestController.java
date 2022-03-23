da31e7c3fa601f9a245b6ec949b1d8475f6cc940
DanilKharlushin <danilharlushin12@gmail.com>
2022-03-23 18:54:31 +0300

    [RF][NNO-7659] рефакторинг кейса file upload

--- backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/fileupload/PersonDocRestController.java
+++ backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/fileupload/PersonDocRestController.java
@@ -2,15 +2,10 @@ package net.n2oapp.framework.sandbox.fileupload;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 
 /**
@@ -20,45 +15,24 @@ import java.io.IOException;
 @RequestMapping("/persons")
 public class PersonDocRestController {
 
-    private final PersonStorageController personStorageController;
-
-    public PersonDocRestController(@Autowired PersonStorageController personStorageController) {
-        this.personStorageController = personStorageController;
-    }
+    @Autowired
+    private PersonDocRestClient restClient;
 
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST,value = "/{personId}/files")
-    public ResponseEntity<FileModel> uploadFile(@PathVariable("personId") String personId, @RequestParam("file") MultipartFile file, HttpServletRequest request) {
-        FileModel model = personStorageController.storeFile(file, request.getRequestURL().toString());
-        personStorageController.setDoc(personId, model);
-        return new ResponseEntity<>(new FileModel(model.getId(), model.getFileName(), model.getUrl(), model.getSize()), HttpStatus.CREATED);
+    public ResponseEntity<FileModel> uploadFile(@PathVariable("personId") String personId, @RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(restClient.uploadFile(personId, file), HttpStatus.OK);
     }
 
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.DELETE, value = "/{personId}/files/{id}")
     public void deleteFile(@PathVariable String personId, @PathVariable String id) {
-        personStorageController.deleteDoc(personId, id);
+        restClient.deleteFile(personId, id);
     }
 
     @CrossOrigin(origins = "*")
     @GetMapping("/{personId}/files/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        Resource resource = personStorageController.loadFile(fileName);
-
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            //do nothing
-        }
-
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+    public ResponseEntity<Resource> downloadFile(@PathVariable String personId, @PathVariable String fileName) {
+        return new ResponseEntity<>(restClient.downloadFile(personId, fileName), HttpStatus.OK);
     }
 }
