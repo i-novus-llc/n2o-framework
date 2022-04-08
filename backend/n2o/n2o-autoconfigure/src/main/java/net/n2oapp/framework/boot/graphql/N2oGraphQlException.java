23dfea922a5b7d8ec76426cfaa8167b869f60d09
adel <ammindubaev@i-novus.ru>
2022-04-08 12:58:56 +0300

    [NNO-7697] edited extends

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/graphql/N2oGraphQlException.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/graphql/N2oGraphQlException.java
@@ -2,12 +2,11 @@ package net.n2oapp.framework.boot.graphql;
 
 import lombok.Getter;
 import net.n2oapp.criteria.dataset.DataSet;
-import net.n2oapp.framework.api.exception.N2oException;
 
 /**
  * Ошибка обработки GraphQL запроса
  */
-public class N2oGraphQlException extends N2oException {
+public class N2oGraphQlException extends RuntimeException {
 
     @Getter
     private DataSet response;
