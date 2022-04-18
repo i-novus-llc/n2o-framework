137dd48759980de43034395dd61ad9f78018203b
adel <ammindubaev@i-novus.ru>
2022-04-18 15:49:01 +0300

    [NNO-7732] added content element for html

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/io/widget/v4/HtmlWidgetElementIOv4.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/io/widget/v4/HtmlWidgetElementIOv4.java
@@ -14,8 +14,8 @@ public class HtmlWidgetElementIOv4 extends WidgetElementIOv4<N2oHtmlWidget> {
     @Override
     public void io(Element e, N2oHtmlWidget m, IOProcessor p) {
         super.io(e, m, p);
-        p.attribute(e, "src", m::getSrc, m::setSrc);
-        p.text(e, m::getHtml, m::setHtml);
+        p.childAttribute(e, "content", "url", m::getUrl, m::setUrl);
+        p.childrenText(e, "content", m::getHtml, m::setHtml);
     }
 
     @Override
