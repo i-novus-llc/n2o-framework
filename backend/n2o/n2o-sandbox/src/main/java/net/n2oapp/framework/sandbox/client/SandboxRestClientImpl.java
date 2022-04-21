49c8d98a2e7bc456cee8e0b3b20fc32eefb7910d
adel <ammindubaev@i-novus.ru>
2022-04-21 15:25:55 +0300

    [NNO-7756] added sandbox to n2o 7.22 version

---
+++ backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/client/SandboxRestClientImpl.java
@@ -0,0 +1,54 @@
+package net.n2oapp.framework.sandbox.client;
+
+import net.n2oapp.framework.sandbox.client.model.FileModel;
+import net.n2oapp.framework.sandbox.client.model.ProjectModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+import static net.n2oapp.framework.config.register.route.RouteUtil.normalize;
+
+/**
+ * Реализация клиента для отправки запросов на ApiController
+ */
+public class SandboxRestClientImpl implements SandboxRestClient {
+
+    private static final String PROJECT_PREFIX = "/project";
+    private static final String TEMPLATES_PREFIX = "/templates";
+    private static final String SCHEMAS_PREFIX = "/schemas";
+    @Value("${n2o.sandbox.api.url}/project")
+    private String baseApiProjectUrl;
+    private RestTemplate restTemplate;
+
+    public SandboxRestClientImpl() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    @Override
+    public ProjectModel getProject(String projectId, HttpSession session) {
+        return restTemplate.getForObject(baseApiProjectUrl + normalize(projectId), ProjectModel.class);
+    }
+
+    @Override
+    public String getFile(String projectId, String file, HttpSession session) {
+        return restTemplate.getForObject(baseApiProjectUrl + normalize(projectId) + normalize(file), String.class);
+    }
+
+    @Override
+    public boolean isProjectExists(String projectId) {
+        ResponseEntity<ProjectModel> result = restTemplate.exchange(baseApiProjectUrl + normalize(projectId), HttpMethod.HEAD, null, ProjectModel.class);
+        return HttpStatus.OK.equals(result.getStatusCode());
+    }
+
+    @Override
+    public void putFiles(String projectId, List<FileModel> files, HttpSession session) {
+        HttpEntity<List<FileModel>> requestUpdate = new HttpEntity<>(files);
+        restTemplate.put(baseApiProjectUrl + normalize(projectId), requestUpdate);
+    }
+}
