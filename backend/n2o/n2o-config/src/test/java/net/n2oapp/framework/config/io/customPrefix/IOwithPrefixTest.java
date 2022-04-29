381872d48449c52433ef6b9ea03262173e9ca388
aarifullin <aarifullin@i-novus.ru>
2022-04-29 17:10:45 +0300

    [NF][NNO-7804] Доработка записи моделей подключенных с помощью внешних схем

---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/io/customPrefix/IOwithPrefixTest.java
@@ -0,0 +1,30 @@
+package net.n2oapp.framework.config.io.customPrefix;
+
+import net.n2oapp.framework.api.pack.XmlIOBuilder;
+import net.n2oapp.framework.config.io.page.v4.StandardPageElementIOv4;
+import net.n2oapp.framework.config.io.region.v3.PanelRegionIOv3;
+import net.n2oapp.framework.config.io.widget.v5.FormElementIOV5;
+import net.n2oapp.framework.config.test.XmlIOTestBase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Тестирование чтения, записи компонента с указанным префиксом
+ */
+public class IOwithPrefixTest extends XmlIOTestBase {
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(XmlIOBuilder<?> b) {
+        b.ios(new StandardPageElementIOv4(), new PrefixElementIO(), new PanelRegionIOv3(), new FormElementIOV5());
+    }
+
+    @Test
+    public void test() {
+        check("net/n2oapp/framework/config/testPageWithPrefix.page.xml");
+    }
+}
---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/io/customPrefix/N2oTestModel.java
@@ -0,0 +1,58 @@
+package net.n2oapp.framework.config.io.customPrefix;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.n2oapp.framework.api.metadata.SourceComponent;
+import net.n2oapp.framework.api.metadata.global.N2oMetadata;
+
+@Getter
+@Setter
+public class N2oTestModel extends N2oMetadata implements SourceComponent {
+
+    private String schemaPrefix;
+
+    @Override
+    public String getPostfix() {
+        return null;
+    }
+
+    @Override
+    public void setNamespacePrefix(String namespacePrefix) {
+        schemaPrefix = namespacePrefix;
+    }
+
+    @Override
+    public String getNamespacePrefix() {
+        return schemaPrefix;
+    }
+
+    @Override
+    public String getCssClass() {
+        return null;
+    }
+
+    @Override
+    public void setCssClass(String cssClass) {
+
+    }
+
+    @Override
+    public String getStyle() {
+        return null;
+    }
+
+    @Override
+    public void setStyle(String style) {
+
+    }
+
+    @Override
+    public String getSrc() {
+        return null;
+    }
+
+    @Override
+    public void setSrc(String src) {
+
+    }
+}
