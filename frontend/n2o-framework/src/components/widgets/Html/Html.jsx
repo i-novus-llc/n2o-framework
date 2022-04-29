be2769531c15242f057efe21d57e7ddfb2624d93
Alexey <evilsokol@gmail.com>
2022-04-29 15:23:05 +0300

    fix/NNO-7805 fixed Html components

--- frontend/n2o-framework/src/components/widgets/Html/Html.jsx
+++ frontend/n2o-framework/src/components/widgets/Html/Html.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { Html as HtmlSnippet } from '../../snippets/Html/Html'
+import { parseExpression } from '../../../utils/evalExpression'
+
 /**
- * Компонент встаквки html-кода
+ * Компонент встаквки html-кода производит резолв плейсхолдеров
  * @reactProps {string} url - url html, который будет вставляться
  * @reactProps {string} id - id виджета
  * @reactProps {string} html - html строка
@@ -11,32 +14,52 @@ import PropTypes from 'prop-types'
  */
 
 // Принимает html и data
-// прим. html = <h1>User is {name} {surname}</h1> ,data = [{"name" : "testUserName", "surname": "testUserSurname"}],
+// прим. html = <h1>User is +name+ +surname+</h1> ,data = [{"name" : "testUserName", "surname": "testUserSurname"}],
 // заменяет плейсхолдеры в html (прим. {name}, {surname}) на стоотвствующие значения по ключам в data.
-// при отсутствии ключа в data или плейсхолдер === {} заменяет на пустоту
 
 export const replacePlaceholders = (html, data) => {
+    if (!html) {
+        return null
+    }
+
     const keys = Object.keys(data)
 
     keys.forEach((key) => {
     // заменяет плейсхолдеры на соответствующие ключи:значения в data
-        html = html.replace(new RegExp(`{${key}}`, 'gm'), data[key])
+        html = html.replace(new RegExp(`'\\+${key}\\+'`, 'gm'), data[key])
     })
 
-    // удаляет остальные плейсхолдеры, включая {}
-    return html.replace(new RegExp('{.*?}', 'gm'), '')
+    return html
 }
 
-const Html = (props) => {
-    const { html, data, loading = false } = props
+export const Html = (props) => {
+    const { html, data, loading = false, id, className } = props
+
+    let finalHtml = html
+
+    if (data) {
+        finalHtml = replacePlaceholders(html, data)
+
+        if (parseExpression(finalHtml)) {
+            finalHtml = parseExpression(finalHtml)
+        }
+
+        if (finalHtml.startsWith('\'') && finalHtml.endsWith('\'')) {
+            finalHtml = finalHtml.substring(1, finalHtml.length - 1)
+        }
+    }
+
+    /* устраняет мерцания с плейсхолдерами */
+    if (parseExpression(finalHtml) || !html) {
+        return null
+    }
 
     return (
         !loading && (
-            <div
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                    __html: html && data ? replacePlaceholders(html, data) : html,
-                }}
+            <HtmlSnippet
+                html={finalHtml}
+                id={id}
+                className={className}
             />
         )
     )
