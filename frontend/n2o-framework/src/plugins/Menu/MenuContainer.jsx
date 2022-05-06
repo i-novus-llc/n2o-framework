d4b311bf65a42bc8b74869fbdc5ad705d2ee8c5a
Асанов Артур Маратович <aasanov@i-novus.ru>
2022-05-06 14:22:18 +0300

    NNO-7788 Fixed behavior on case with asterisk

--- frontend/n2o-framework/src/plugins/Menu/MenuContainer.jsx
+++ frontend/n2o-framework/src/plugins/Menu/MenuContainer.jsx
@@ -232,6 +232,13 @@ export const ConfigContainer = compose(
         const { location: { pathname } } = rest
 
         const sidebar = (sidebars || [])
+            .sort((a, b) => {
+                if (!a.path && b.path) { return 1 }
+
+                if (a.path && !b.path) { return -1 }
+
+                return 0
+            })
             .map(sidebar => ({ ...sidebar, path: sidebar.path ? sidebar.path.replace('*', '(.*)') : '(.*)' }))
             .find(sidebar => !isEmpty(pathToRegexp(sidebar.path).exec(pathname)))
 
