c28ba93d0a99b331175490ceac1bf05a0d9f3c92
adel <ammindubaev@i-novus.ru>
2022-05-06 12:34:27 +0300

    [NNO-7833] created sidebars validation

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/application/ApplicationValidator.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/application/ApplicationValidator.java
@@ -6,13 +6,19 @@ import net.n2oapp.framework.api.metadata.header.N2oHeader;
 import net.n2oapp.framework.api.metadata.menu.N2oSimpleMenu;
 import net.n2oapp.framework.api.metadata.compile.SourceProcessor;
 import net.n2oapp.framework.api.metadata.validation.TypedMetadataValidator;
+import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Валидатор приложения
  */
 @Component
 public class ApplicationValidator extends TypedMetadataValidator<N2oApplication> {
+
+    private static final String EMPTY_URL = "empty-url";
 
     @Override
     public Class<N2oApplication> getSourceClass() {
@@ -21,21 +27,33 @@ public class ApplicationValidator extends TypedMetadataValidator<N2oApplication>
 
     @Override
     public void validate(N2oApplication application, SourceProcessor p) {
-        if(application.getHeader() != null)
+        if (application.getHeader() != null)
             checkHeader(application.getHeader(), p);
         if (application.getSidebars() != null) {
+            List<String> sidebarsPaths = new ArrayList<>();
             for (N2oSidebar sidebar : application.getSidebars())
-                checkSidebar(sidebar, p);
+                checkSidebar(sidebar, p, sidebarsPaths);
         }
     }
 
-    private void checkSidebar(N2oSidebar sidebar, SourceProcessor p) {
+    private void checkSidebar(N2oSidebar sidebar, SourceProcessor p, List<String> paths) {
         if (sidebar.getMenu() != null) {
             p.checkForExists(sidebar.getMenu().getRefId(), N2oSimpleMenu.class, "Menu {0} doesn't exists for header");
         }
         if (sidebar.getExtraMenu() != null) {
             p.checkForExists(sidebar.getExtraMenu().getRefId(), N2oSimpleMenu.class, "Menu {0} doesn't exists for header");
         }
+
+        String sidebarPath = sidebar.getPath() != null ? sidebar.getPath() : EMPTY_URL;
+        if (paths.contains(sidebarPath)) {
+            String errorMessage = sidebarPath.equals(EMPTY_URL)
+                    ? "More than one sidebar does not contain a path"
+                    : String.format("The {0} path is already taken by one of the sidebars", sidebarPath);
+            throw new N2oMetadataValidationException(errorMessage);
+        } else {
+            paths.add(sidebarPath);
+        }
+
     }
 
     private void checkHeader(N2oHeader header, SourceProcessor p) {
--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/application/ApplicationValidateTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/application/ApplicationValidateTest.java
@@ -5,6 +5,7 @@ import net.n2oapp.framework.api.metadata.menu.N2oSimpleMenu;
 import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.metadata.compile.application.ApplicationIO;
+import net.n2oapp.framework.config.metadata.compile.application.ApplicationIOv3;
 import net.n2oapp.framework.config.metadata.compile.application.ApplicationValidator;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import net.n2oapp.framework.config.test.SourceValidationTestBase;
@@ -24,7 +25,7 @@ public class ApplicationValidateTest extends SourceValidationTestBase {
     @Override
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
-        builder.ios(new ApplicationIO());
+        builder.ios(new ApplicationIO(), new ApplicationIOv3());
         builder.validators(new ApplicationValidator());
         builder.sources(new CompileInfo("menuForHeaderValidation", N2oSimpleMenu.class));
     }
@@ -32,6 +33,21 @@ public class ApplicationValidateTest extends SourceValidationTestBase {
     @Test
     public void testHeaderValidation() {
         validate("net/n2oapp/framework/config/metadata/application/applicationHeaderValidate.application.xml");
+    }
+
+    @Test
+    public void testSidebarsValidation() {
+        validate("net/n2oapp/framework/config/metadata/application/applicationSidebarsValidate.application.xml");
+    }
+
+    @Test(expected = N2oMetadataValidationException.class)
+    public void testEmptySidebarsValidationFail(){
+        validate("net/n2oapp/framework/config/metadata/application/applicationEmptySidebarsValidationFail.application.xml");
+    }
+
+    @Test(expected = N2oMetadataValidationException.class)
+    public void testEqualPathsSidebarsValidationFail(){
+        validate("net/n2oapp/framework/config/metadata/application/applicationEqualPathSidebarsValidationFail.application.xml");
     }
 
     @Test(expected = N2oMetadataValidationException.class)
