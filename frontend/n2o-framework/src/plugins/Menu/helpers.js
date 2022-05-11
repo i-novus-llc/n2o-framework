83f308c3d6c1c49409549e60e6145f1b9d061405
Асанов Артур <aasanov@i-novus.ru>
2022-05-11 18:16:50 +0300

    NNO-7857 Dynamic paths in sidebars - added logic and tests

--- frontend/n2o-framework/src/plugins/Menu/helpers.js
+++ frontend/n2o-framework/src/plugins/Menu/helpers.js
@@ -4,6 +4,17 @@ import pathToRegexp from 'path-to-regexp'
 const asteriskCount = str => str.split('*').length - 1
 
 export const getMatchingSidebar = (sidebars, pathname) => [...sidebars]
-    .map(sidebar => ({ ...sidebar, path: sidebar.path ? sidebar.path.replace('*', '(.*)') : '(.*)' }))
-    .filter(sidebar => !isEmpty(pathToRegexp(sidebar.path).exec(pathname)))
-    .sort((a, b) => asteriskCount(a.path) - asteriskCount(b.path))[0]
+    .map(sidebar => ({ ...sidebar, path: sidebar.path ? sidebar.path.replace(/\*/g, '(.*)') : '/(.*)' }))
+    .filter((sidebar) => {
+        const array = pathToRegexp(sidebar.path).exec(pathname) || []
+        const isValid = array.every((e, i) => {
+            if (i > 0 && e.includes('/') && e.length > 1) {
+                return array[i + 1] !== undefined && array[i + 1].length === 0
+            }
+
+            return true
+        })
+
+        return !isEmpty(array) && isValid
+    })
+    .sort((a, b) => (b.path.length - asteriskCount(b.path)) - (a.path.length - asteriskCount(a.path)))[0]
