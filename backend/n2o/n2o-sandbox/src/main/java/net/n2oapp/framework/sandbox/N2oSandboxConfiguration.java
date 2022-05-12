5473856f9cd3b8b58a87bb0f191ab0a0d82d7cd4
dharlushin <dharlushin@i-novus.ru>
2022-05-12 14:37:18 +0300

    [BF][NNO-7855] moved test static data from sandbox, add TestDataStaticResolver

--- backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/N2oSandboxConfiguration.java
+++ backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/N2oSandboxConfiguration.java
@@ -16,10 +16,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
@@ -45,6 +50,19 @@ public class N2oSandboxConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return ObjectMapperConstructor.metaObjectMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer mvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/view/*/static/**")
+                        .addResourceLocations("/static/")
+                        .resourceChain(true)
+                        .addResolver(new TestDataStaticResolver("META-INF/resources"));
+            }
+        };
     }
 
     @Bean
