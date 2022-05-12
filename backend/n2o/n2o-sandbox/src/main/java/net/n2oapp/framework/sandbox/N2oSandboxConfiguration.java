dc595dccecad8561ddb3409e6f30ad3cb817cfa5
dharlushin <dharlushin@i-novus.ru>
2022-05-12 10:18:49 +0300

    [NF][NNO-7815] implement stands redirect, remove context path /sandbox

--- backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/N2oSandboxConfiguration.java
+++ backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/N2oSandboxConfiguration.java
@@ -14,12 +14,18 @@ import net.n2oapp.framework.ui.controller.action.SetController;
 import net.n2oapp.framework.ui.controller.query.GetController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
@@ -45,6 +51,22 @@ public class N2oSandboxConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return ObjectMapperConstructor.metaObjectMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer mvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Value("${n2o.sandbox.url}")
+            private String sandboxUrl;
+
+            @Value("${n2o.engine.rest.url}")
+            private String standUrl;
+
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/",sandboxUrl + "?url=" + standUrl);
+            }
+        };
     }
 
     @Bean
