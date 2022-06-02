c8c4018f45a4f297f10b635aaf91c8b29963ac9c
adel <ammindubaev@i-novus.ru>
2022-06-02 15:55:11 +0300

    [NNO-7971] fixed npe

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ClearActionCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/ClearActionCompiler.java
@@ -4,8 +4,10 @@ import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.event.action.N2oClearAction;
+import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.N2oButton;
 import net.n2oapp.framework.api.metadata.meta.action.clear.ClearAction;
 import net.n2oapp.framework.api.metadata.meta.saga.MetaSaga;
+import net.n2oapp.framework.config.metadata.compile.ComponentScope;
 import net.n2oapp.framework.config.metadata.compile.page.PageScope;
 import net.n2oapp.framework.config.metadata.compile.widget.WidgetScope;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,9 @@ import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.pr
  */
 @Component
 public class ClearActionCompiler extends AbstractActionCompiler<ClearAction, N2oClearAction> {
+
+    private static final String DEFAULT_MODEL = "resolve";
+
     @Override
     public Class<? extends Source> getSourceClass() {
         return N2oClearAction.class;
@@ -29,7 +34,19 @@ public class ClearActionCompiler extends AbstractActionCompiler<ClearAction, N2o
         compileAction(clearAction, source, p);
         clearAction.setType(p.resolve(property("n2o.api.action.clear.type"), String.class));
         WidgetScope widgetScope = p.getScope(WidgetScope.class);
-        clearAction.getPayload().setPrefixes(p.cast(source.getModel(), new String[]{widgetScope.getModel().getId()}));
+        String[] prefixes = null;
+
+        if (source.getModel() != null) {
+            prefixes = source.getModel();
+        }
+        else if (widgetScope != null) {
+            prefixes = new String[]{widgetScope.getModel().getId()};
+        }
+        else {
+            N2oButton button = p.getScope(ComponentScope.class).unwrap(N2oButton.class);
+            prefixes = button == null ? null : new String[]{button.getModel().getId()};
+        }
+        clearAction.getPayload().setPrefixes(p.cast(prefixes, new String[]{DEFAULT_MODEL}));
         String widgetId = initClientWidgetId(context, p);
         PageScope pageScope = p.getScope(PageScope.class);
         clearAction.getPayload().setKey(
