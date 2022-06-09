b7068d9041b07ec26eeb006248f1c3fb3a024077
Рамазанов Алмаз Асгатович <aramazanov@i-novus.ru>
2022-06-09 13:47:29 +0300

    [NNO-8031] Docusaurus. Не работает переход к примеру в сендбоксе
    (cherry picked from commit edaee9950cb66d07f0da506bc576bc2b52382261)

--- frontend/docs/src/components/Sandbox/utils.js
+++ frontend/docs/src/components/Sandbox/utils.js
@@ -1,20 +1,5 @@
-import { newPath } from '../../constants/constants'
-import CONFIG from '../../ci-config.json'
-
 export function getFileLang(fileName) {
     const fileNameParts = fileName.split('.')
 
     return fileNameParts[fileNameParts.length - 1]
-}
-
-export const openProjectWithRedirect = async (templateId) => {
-    try {
-        const response = await fetch(`${CONFIG.sandboxUrl}${newPath}/${templateId}/`)
-
-        if (response.ok && response.redirected) {
-            return { redirectUrl: response.url }
-        }
-    } catch (error) {
-        console.warn(error)
-    }
 }
