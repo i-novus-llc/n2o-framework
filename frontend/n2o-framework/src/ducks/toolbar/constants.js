2184da09000d4761eb8eb8f8aecdb0fd5faf6752
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2022-07-01 10:52:36 +0000

    Merge branch 'bugfix/NNO-8089' into 'master'
    [NNO-8089] - добавил возможность печати из html с сервера
    
    Closes NNO-8089
    
    See merge request open/n2o-framework!4366
    
    (cherry picked from commit 9022e273226f1658a5c370a7f009e424e6eb71b7)
    
    14d597e1 [NNO-8089] - добавил возможность печати из html с сервера
    23af63c5 [NNO-8089] - добавил возможность печати из html с сервера
    6907fc27 [NNO-8089] - добавил возможность печати из html с сервера

--- frontend/n2o-framework/src/ducks/toolbar/constants.js
+++ frontend/n2o-framework/src/ducks/toolbar/constants.js
@@ -1,2 +1,11 @@
 export const CALL_ACTION_IMPL = 'n2o/actionsImpl/CALL_ACTION_IMPL'
 export const PRINT_BUTTON = 'n2o/toolbar/PRINT_BUTTON'
+
+export const PrintType = {
+    TEXT: 'text',
+    PDF: 'pdf',
+    IMAGE: 'image',
+}
+
+export const DEFAULT_PRINT_ERROR_MESSAGE = 'An error has occurred'
+export const DEFAULT_PRINT_INCOMPATIBLE_BROWSER_MESSAGE = 'Your browser is not supported'
