653674ac29041b84220efc1f88e4d0a575dc8eb3
DanilKharlushin <danilharlushin12@gmail.com>
2022-07-05 15:31:22 +0300

    [NNO-8126] changed parent class

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/exception/N2oSpelException.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/exception/N2oSpelException.java
@@ -2,23 +2,23 @@ package net.n2oapp.framework.engine.exception;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.n2oapp.framework.api.exception.N2oException;
+import net.n2oapp.framework.api.exception.N2oUserException;
 
 @Getter
 @Setter
-public class N2oSpelException extends N2oException {
+public class N2oSpelException extends N2oUserException {
 
     private String file;
     private String mapping;
     private String fieldId;
 
     public N2oSpelException(String mapping, Throwable cause) {
-        super("n2o.summary.spelError", cause);
+        super("n2o.summary.spelError", cause.getLocalizedMessage());
         this.mapping = mapping;
     }
 
     public N2oSpelException(String fieldId, String mapping, Throwable cause) {
-        super("n2o.summary.fieldSpelError", cause);
+        super("n2o.summary.fieldSpelError", cause.getLocalizedMessage());
         this.fieldId = fieldId;
         this.mapping = mapping;
     }
