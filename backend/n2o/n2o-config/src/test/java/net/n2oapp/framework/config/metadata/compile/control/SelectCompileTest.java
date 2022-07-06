71f864874070725d869c7157903e21abc989cccb
Игорь Рябов <iryabov@i-novus.ru>
2022-07-06 10:45:19 +0300

    NNO-8132 Добавлена настройка n2o.api.control.list.cache

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/SelectCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/SelectCompileTest.java
@@ -8,10 +8,7 @@ import net.n2oapp.framework.api.metadata.meta.page.SimplePage;
 import net.n2oapp.framework.api.metadata.meta.widget.form.Form;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.metadata.compile.context.PageContext;
-import net.n2oapp.framework.config.metadata.pack.N2oControlsV2IOPack;
-import net.n2oapp.framework.config.metadata.pack.N2oFieldSetsPack;
-import net.n2oapp.framework.config.metadata.pack.N2oPagesPack;
-import net.n2oapp.framework.config.metadata.pack.N2oWidgetsPack;
+import net.n2oapp.framework.config.metadata.pack.*;
 import net.n2oapp.framework.config.test.SourceCompileTestBase;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,13 +29,15 @@ public class SelectCompileTest extends SourceCompileTestBase {
     @Override
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
-        builder.packs(new N2oPagesPack(), new N2oWidgetsPack(), new N2oFieldSetsPack(), new N2oControlsV2IOPack());
+        builder.properties("n2o.api.control.list.cache=true");
+        builder.packs(new N2oPagesPack(), new N2oWidgetsPack(), new N2oFieldSetsPack(), new N2oControlsV2IOPack(), new N2oQueriesPack());
         builder.compilers(new SelectCompiler());
     }
 
     @Test
     public void testSelect() {
-        SimplePage page = (SimplePage) compile("net/n2oapp/framework/config/metadata/compile/control/testSelect.page.xml")
+        SimplePage page = (SimplePage) compile("net/n2oapp/framework/config/metadata/compile/control/testSelect.page.xml",
+                "net/n2oapp/framework/config/metadata/compile/control/test.query.xml")
                 .get(new PageContext("testSelect"));
         Form form = (Form) page.getWidget();
         Field field = form.getComponent().getFieldsets().get(0).getRows().get(0).getCols().get(0).getFields().get(0);
@@ -67,5 +66,9 @@ public class SelectCompileTest extends SourceCompileTestBase {
         assertThat(select.getData().get(0).get("name"), is("A"));
         assertThat(select.getData().get(1).get("id"), is("type2"));
         assertThat(select.getData().get(1).get("name"), is("B"));
+
+        field = form.getComponent().getFieldsets().get(0).getRows().get(3).getCols().get(0).getFields().get(0);
+        select = (Select) ((StandardField) field).getControl();
+        assertThat(select.getCaching(), is(true));
     }
 }
