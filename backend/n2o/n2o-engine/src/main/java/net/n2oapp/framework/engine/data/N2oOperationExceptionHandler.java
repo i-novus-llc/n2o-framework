7afc62a9d098dad9e3cccf87d6f460240b3fd8cb
DanilKharlushin <danilharlushin12@gmail.com>
2022-07-08 10:13:49 +0300

    [NNO-8126] changed parent class for N2oSpelException to N2oException, removed messages

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oOperationExceptionHandler.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oOperationExceptionHandler.java
@@ -6,6 +6,7 @@ import net.n2oapp.framework.api.data.OperationExceptionHandler;
 import net.n2oapp.framework.api.exception.N2oException;
 import net.n2oapp.framework.api.exception.N2oUserException;
 import net.n2oapp.framework.api.metadata.local.CompiledObject;
+import net.n2oapp.framework.engine.exception.N2oSpelException;
 import net.n2oapp.framework.engine.processor.N2oActionException;
 
 /**
@@ -14,7 +15,7 @@ import net.n2oapp.framework.engine.processor.N2oActionException;
 public class N2oOperationExceptionHandler implements OperationExceptionHandler {
     @Override
     public N2oException handle(CompiledObject.Operation o, DataSet data, Exception e) {
-        if (e instanceof N2oUserException)
+        if (e instanceof N2oUserException || e instanceof N2oSpelException)
             return (N2oException) e;
         N2oException n2oE = new N2oActionException(e);
         if (o.getFailText() != null) {
--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/exception/N2oSpelException.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/exception/N2oSpelException.java
@@ -2,11 +2,11 @@ package net.n2oapp.framework.engine.exception;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.n2oapp.framework.api.exception.N2oUserException;
+import net.n2oapp.framework.api.exception.N2oException;
 
 @Getter
 @Setter
-public class N2oSpelException extends N2oUserException {
+public class N2oSpelException extends N2oException {
 
     private static final String DEFAULT_MESSAGE = "Spel expression conversion error with %s from metadata %s";
     private static final String DEFAULT_FIELD_MESSAGE = "Spel expression conversion error with %s of field %s from metadata %s";
@@ -16,20 +16,18 @@ public class N2oSpelException extends N2oUserException {
     private String fieldId;
 
     public N2oSpelException(N2oSpelException e, String file) {
-        super(e.getFieldId() != null ? "n2o.summary.fieldSpelError" : "n2o.summary.spelError",
-                e.getFieldId() != null
+        super(e.getFieldId() != null
                 ? String.format(DEFAULT_FIELD_MESSAGE, e.getMapping(), e.getFieldId(), file)
                 : String.format(DEFAULT_MESSAGE, e.getMapping(), file), e.getCause());
-        addData(e.getMapping(), file, e.getFieldId());
     }
 
     public N2oSpelException(String mapping, Throwable cause) {
-        super("n2o.summary.spelError", cause);
+        super(cause.getMessage(), cause);
         this.mapping = mapping;
     }
 
     public N2oSpelException(String fieldId, String mapping, Throwable cause) {
-        super("n2o.summary.fieldSpelError", cause);
+        super(cause.getMessage(), cause);
         this.fieldId = fieldId;
         this.mapping = mapping;
     }
