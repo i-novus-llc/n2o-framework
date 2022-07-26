98288af111e878691524ec4086f2c460c0cf2d06
adel <ammindubaev@i-novus.ru>
2022-07-26 15:22:02 +0300

    [NNO-8287] added new attributes for copy action

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/CopyActionCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/CopyActionCompiler.java
@@ -5,11 +5,13 @@ import net.n2oapp.framework.api.metadata.ReduxModel;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
+import net.n2oapp.framework.api.metadata.control.PageRef;
 import net.n2oapp.framework.api.metadata.event.action.N2oCopyAction;
 import net.n2oapp.framework.api.metadata.global.view.widget.toolbar.CopyMode;
 import net.n2oapp.framework.api.metadata.meta.action.copy.CopyAction;
 import net.n2oapp.framework.api.metadata.meta.action.copy.CopyActionPayload;
 import net.n2oapp.framework.api.metadata.meta.saga.MetaSaga;
+import net.n2oapp.framework.config.metadata.compile.context.PageContext;
 import net.n2oapp.framework.config.metadata.compile.widget.WidgetScope;
 import org.springframework.stereotype.Component;
 
@@ -38,7 +40,7 @@ public class CopyActionCompiler extends AbstractActionCompiler<CopyAction, N2oCo
                 getClientDatasourceId(source.getSourceDatasourceId(), p),
                 source.getSourceModel().getId(), source.getSourceFieldId());
         CopyActionPayload.ClientModel targetModel = new CopyActionPayload.ClientModel(
-                getTargetDatasourceId(source, p),
+                getClientTargetDatasourceId(source, context, p),
                 source.getTargetModel().getId(),
                 source.getTargetFieldId());
 
@@ -46,7 +48,7 @@ public class CopyActionCompiler extends AbstractActionCompiler<CopyAction, N2oCo
         copyAction.getPayload().setTarget(targetModel);
         copyAction.getPayload().setMode(source.getMode());
 
-        copyAction.setMeta(compileMeta(p));
+        copyAction.setMeta(compileMeta(source, p));
         return copyAction;
     }
 
@@ -55,19 +57,20 @@ public class CopyActionCompiler extends AbstractActionCompiler<CopyAction, N2oCo
         super.initDefaults(source, context, p);
         source.setMode(p.cast(source.getMode(), CopyMode.merge));
         source.setSourceModel(p.cast(source.getSourceModel(), ReduxModel.resolve));
-        source.setSourceDatasourceId(initSourceDatasource(source, p));
+        source.setSourceDatasourceId(initSourceDatasourceId(source, p));
         source.setTargetModel(p.cast(source.getTargetModel(), ReduxModel.resolve));
-        source.setTargetDatasourceId(initTargetDatasource(source, p));
+        source.setTargetDatasourceId(initTargetDatasourceId(source, p));
     }
 
-    private MetaSaga compileMeta(CompileProcessor p) {
+    private MetaSaga compileMeta(N2oCopyAction source, CompileProcessor p) {
         MetaSaga meta = new MetaSaga();
-        Boolean closeLastModal = p.resolve(property("n2o.api.action.copy.close_on_success"), Boolean.class);
-        meta.setModalsToClose(closeLastModal ? 1 : 0);
+        boolean closeOnSuccess = p.cast(source.getCloseOnSuccess(),
+                p.resolve(property("n2o.api.action.copy.close_on_success"), Boolean.class));
+        meta.setModalsToClose(closeOnSuccess ? 1 : 0);
         return meta;
     }
 
-    private String initSourceDatasource(N2oCopyAction source, CompileProcessor p) {
+    private String initSourceDatasourceId(N2oCopyAction source, CompileProcessor p) {
         if (source.getSourceDatasourceId() != null)
             return source.getSourceDatasourceId();
         String datasource = getDatasourceByComponentScope(p);
@@ -79,16 +82,21 @@ public class CopyActionCompiler extends AbstractActionCompiler<CopyAction, N2oCo
         throw new N2oException(String.format("source-datasource is not undefined in copy action [%s]", source.getId()));
     }
 
-    private String initTargetDatasource(N2oCopyAction source, CompileProcessor p) {
+    private String initTargetDatasourceId(N2oCopyAction source, CompileProcessor p) {
         if (source.getTargetDatasourceId() != null)
             return source.getTargetDatasourceId();
-        return initSourceDatasource(source, p);
+        return initSourceDatasourceId(source, p);
     }
 
-    private String getTargetDatasourceId(N2oCopyAction source, CompileProcessor p) {
+    private String getClientTargetDatasourceId(N2oCopyAction source, CompileContext<?, ?> context, CompileProcessor p) {
         if (source.getTargetClientPageId() != null)
             return getClientDatasourceId(source.getTargetDatasourceId(), source.getTargetClientPageId());
-        else
-            return getClientDatasourceId(source.getTargetDatasourceId(), p);
+        else if (PageRef.PARENT.equals(source.getTargetPage())) {
+            if (context instanceof PageContext)
+                return getClientDatasourceId(source.getTargetDatasourceId(), ((PageContext) context).getParentClientPageId());
+            else
+                throw new N2oException("Action 'copy' has target-page=\"parent\" but PageContext not found");
+        }
+        return getClientDatasourceId(source.getTargetDatasourceId(), p);
     }
 }
