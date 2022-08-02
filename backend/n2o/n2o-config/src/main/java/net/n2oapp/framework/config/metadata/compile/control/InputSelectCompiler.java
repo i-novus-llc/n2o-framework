078bb7dc0e191f71faaa4e22567d92fc36c61463
adel <ammindubaev@i-novus.ru>
2022-08-02 14:59:38 +0300

    [NNO-8199] added new attributes for input-select

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectCompiler.java
@@ -3,12 +3,13 @@ package net.n2oapp.framework.config.metadata.compile.control;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
-import net.n2oapp.framework.api.metadata.compile.building.Placeholders;
 import net.n2oapp.framework.api.metadata.control.list.ListType;
 import net.n2oapp.framework.api.metadata.control.list.N2oInputSelect;
 import net.n2oapp.framework.api.metadata.meta.control.InputSelect;
 import net.n2oapp.framework.api.metadata.meta.control.StandardField;
 import org.springframework.stereotype.Component;
+
+import static net.n2oapp.framework.api.metadata.compile.building.Placeholders.property;
 
 
 @Component
@@ -25,8 +26,12 @@ public class InputSelectCompiler extends ListControlCompiler<InputSelect, N2oInp
         InputSelect inputSelect = new InputSelect();
         inputSelect.setResetOnBlur(p.cast(source.getResetOnBlur(), true));
         inputSelect.setDescriptionFieldId(source.getDescriptionFieldId());
+        inputSelect.setThrottleDelay(p.cast(source.getThrottleDelay(),
+                p.resolve(property("n2o.api.control.input.select.throttle_delay"), Integer.class)));
+        inputSelect.setSearchMinLength(p.cast(source.getSearchMinLength(),
+                p.resolve(property("n2o.api.control.input.select.search_min_length"), Integer.class)));
         inputSelect.setMaxTagTextLength(p.cast(source.getMaxTagTextLength(),
-                p.resolve(Placeholders.property("n2o.api.control.input.select.max_tag_text_length"), Integer.class)));
+                p.resolve(property("n2o.api.control.input.select.max_tag_text_length"), Integer.class)));
         ListType type = p.cast(source.getType(), ListType.single);
         switch (type) {
             case checkboxes:
