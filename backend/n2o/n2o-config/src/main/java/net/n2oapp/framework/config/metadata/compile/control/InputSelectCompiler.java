a754b86437637dccf80203f58c53af5a2e647de8
adel <ammindubaev@i-novus.ru>
2022-08-03 15:11:45 +0300

    [NNO-8199] fixed naming

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/control/InputSelectCompiler.java
@@ -17,7 +17,7 @@ public class InputSelectCompiler extends ListControlCompiler<InputSelect, N2oInp
 
     @Override
     protected String getControlSrcProperty() {
-        return "n2o.api.control.input.select.src";
+        return "n2o.api.control.input_select.src";
     }
 
     @Override
@@ -27,11 +27,11 @@ public class InputSelectCompiler extends ListControlCompiler<InputSelect, N2oInp
         inputSelect.setResetOnBlur(p.cast(source.getResetOnBlur(), true));
         inputSelect.setDescriptionFieldId(source.getDescriptionFieldId());
         inputSelect.setThrottleDelay(p.cast(source.getThrottleDelay(),
-                p.resolve(property("n2o.api.control.input.select.throttle_delay"), Integer.class)));
+                p.resolve(property("n2o.api.control.input_select.throttle_delay"), Integer.class)));
         inputSelect.setSearchMinLength(p.cast(source.getSearchMinLength(),
-                p.resolve(property("n2o.api.control.input.select.search_min_length"), Integer.class)));
+                p.resolve(property("n2o.api.control.input_select.search_min_length"), Integer.class)));
         inputSelect.setMaxTagTextLength(p.cast(source.getMaxTagTextLength(),
-                p.resolve(property("n2o.api.control.input.select.max_tag_text_length"), Integer.class)));
+                p.resolve(property("n2o.api.control.input_select.max_tag_text_length"), Integer.class)));
         ListType type = p.cast(source.getType(), ListType.single);
         switch (type) {
             case checkboxes:
