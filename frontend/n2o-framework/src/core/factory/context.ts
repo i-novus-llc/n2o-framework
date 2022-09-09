615706158cd0644d884a0ac0ae64f92395f56e7e
Alexey <evilsokol@gmail.com>
2022-09-09 12:27:59 +0300

    fix/NNO-8324 factory tooltip

--- frontend/n2o-framework/src/core/factory/context.ts
+++ frontend/n2o-framework/src/core/factory/context.ts
@@ -2,10 +2,8 @@ import { createContext } from 'react'
 
 import { FactoryLevels } from './factoryLevels'
 
-type levels = keyof typeof FactoryLevels
-
 export const FactoryContext = createContext({
     resolveProps(props: object) { return props },
     // eslint-disable-next-line @typescript-eslint/ban-types
-    getComponent<TComponent extends Function>(componentName: string, level: levels): TComponent | void {},
+    getComponent<TComponent extends Function>(componentName: string, level: FactoryLevels): TComponent | void {},
 })
