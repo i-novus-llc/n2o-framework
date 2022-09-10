4ca493076c0d68a672c8718f81431f072f4c0cf7
ilebedenko <ilebedenko@i-novus.ru>
2022-09-10 13:15:45 +0300

    [CF][NNO-8448] - fix test

---
+++ backend/n2o/n2o-test/src/test/java/net/n2oapp/framework/test/engine/TestController.java
@@ -0,0 +1,27 @@
+package net.n2oapp.framework.test.engine;
+
+import net.n2oapp.criteria.api.CollectionPage;
+import net.n2oapp.criteria.api.Direction;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service("testController")
+public class TestController {
+
+    public CollectionPage<TestRow> getCollectionPage(TestCriteria criteria) {
+        Collection<TestRow> result = new ArrayList<>();
+        if (criteria.getId() == null) {
+            assert criteria.getSorting().getField().equals("value");
+            assert criteria.getSorting().getDirection().equals(Direction.DESC);
+            for (int i = 0; i < 10; i++) {
+                result.add(new TestRow(i, "value" + i));
+            }
+            return new CollectionPage<>(15, result, criteria);
+        } else {
+            result.add(new TestRow(criteria.getId(), "value" + criteria.getId()));
+            return new CollectionPage<>(1, result, criteria);
+        }
+    }
+}
