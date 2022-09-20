5a5ed7c98d837e113b2953820ee74ec7a04b3152
Лебеденко Игорь <ilebedenko@i-novus.ru>
2022-09-20 19:32:14 +0300

    [NNO-8503] - remove jsonProperty

--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/widget/table/column/cell/N2oImageCell.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/global/view/widget/table/column/cell/N2oImageCell.java
@@ -1,13 +1,8 @@
 package net.n2oapp.framework.api.metadata.global.view.widget.table.column.cell;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import net.n2oapp.framework.api.metadata.global.view.action.control.Target;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.ShapeType;
-import net.n2oapp.framework.api.metadata.meta.ModelLink;
-
-import java.util.Map;
 
 /**
  * Ячейка с изображением
@@ -15,28 +10,13 @@ import java.util.Map;
 @Getter
 @Setter
 public class N2oImageCell extends N2oActionCell {
-    @JsonProperty
     private String width;
-    @JsonProperty
-    private String url;
-    @JsonProperty
     private ShapeType shape;
-    @JsonProperty
     private String title;
-    @JsonProperty
     private String data;
-    @JsonProperty
     private String description;
-    @JsonProperty
     private Position textPosition;
-    @JsonProperty
     private ImageStatusElement[] statuses;
-    @JsonProperty
-    private Target target;
-    @JsonProperty
-    private Map<String, ModelLink> pathMapping;
-    @JsonProperty
-    private Map<String, ModelLink> queryMapping;
 
     public enum Position {
         top, left, right, bottom
--- backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/cell/ImageCell.java
+++ backend/n2o/n2o-api/src/main/java/net/n2oapp/framework/api/metadata/meta/cell/ImageCell.java
@@ -4,13 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import net.n2oapp.framework.api.metadata.aware.UrlAware;
-import net.n2oapp.framework.api.metadata.global.view.action.control.Target;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.ShapeType;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.column.cell.ImageStatusElement;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.column.cell.N2oImageCell;
-import net.n2oapp.framework.api.metadata.meta.ModelLink;
-
-import java.util.Map;
 
 /**
  * Клиентская модель ячейки с изображением
@@ -20,8 +16,6 @@ import java.util.Map;
 public class ImageCell extends ActionCell implements UrlAware {
     @JsonProperty
     private String width;
-    @JsonProperty
-    private String url;
     @JsonProperty
     private ShapeType shape;
     @JsonProperty
@@ -34,10 +28,4 @@ public class ImageCell extends ActionCell implements UrlAware {
     private N2oImageCell.Position textPosition;
     @JsonProperty
     private ImageStatusElement[] statuses;
-    @JsonProperty
-    private Target target;
-    @JsonProperty
-    private Map<String, ModelLink> pathMapping;
-    @JsonProperty
-    private Map<String, ModelLink> queryMapping;
 }
