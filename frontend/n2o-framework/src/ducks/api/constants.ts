d82680ef358e98f9b306d16da7af92886f5a2414
azalyaev <azalyaev@i-novus.ru>
2022-11-03 00:42:03 +0300

    sub/NNO-8650 edit_list action init

--- frontend/n2o-framework/src/ducks/api/constants.ts
+++ frontend/n2o-framework/src/ducks/api/constants.ts
@@ -1,3 +1,4 @@
 export const API_PREFIX = 'n2o/api/'
 export const ACTIONS_PREFIX = `${API_PREFIX}action/`
 export const ALERTS_PREFIX = `${API_PREFIX}alerts/`
+export const MODELS_PREFIX = `${API_PREFIX}models/`
