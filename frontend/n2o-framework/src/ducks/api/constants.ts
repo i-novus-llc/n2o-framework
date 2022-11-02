a7ee9fc962db00e5583dd08e553f46d6b6f13fcc
azalyaev <azalyaev@i-novus.ru>
2022-11-03 00:42:03 +0300

    sub/NNO-8650 edit_list action init

--- frontend/n2o-framework/src/ducks/api/constants.ts
+++ frontend/n2o-framework/src/ducks/api/constants.ts
@@ -1,2 +1,3 @@
 export const API_PREFIX = 'n2o/api/'
 export const ALERTS_PREFIX = `${API_PREFIX}alerts/`
+export const MODELS_PREFIX = `${API_PREFIX}models/`
--- frontend/n2o-framework/src/ducks/api/sagas.ts
+++ frontend/n2o-framework/src/ducks/api/sagas.ts
@@ -1,6 +1,8 @@
 // @ts-ignore ignore import error from js file
 import { sagas as alertSagas } from './alerts'
+import { sagas as modelsSagas } from './models'
 
 export const sagas = [
     ...alertSagas,
+    ...modelsSagas,
 ]
