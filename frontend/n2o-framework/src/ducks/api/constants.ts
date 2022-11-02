16812d964134f04c4536cd371b75f9e2c821acf6
azalyaev <azalyaev@i-novus.ru>
2022-11-02 20:47:35 +0300

    sub/NNO-8650 Поправил расположение саг для api/models

---
+++ frontend/n2o-framework/src/ducks/api/constants.ts
@@ -0,0 +1,2 @@
+export const API_PREFIX = 'n2o/api/'
+export const ALERTS_PREFIX = `${API_PREFIX}alerts/`
---
+++ frontend/n2o-framework/src/ducks/api/sagas.ts
@@ -0,0 +1,6 @@
+// @ts-ignore ignore import error from js file
+import { sagas as alertSagas } from './alerts'
+
+export const sagas = [
+    ...alertSagas,
+]
