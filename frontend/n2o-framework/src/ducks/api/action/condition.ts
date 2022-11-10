19d12b39f1c4c1eb9f8f81e10ac09ff2982b386a
azalyaev <azalyaev@i-novus.ru>
2022-11-10 18:48:36 +0300

    fix/NNO-8747 Перенёс вызов экшена напрямую в хелпер. Поправил условие проверки ошибки и таймаута

--- frontend/n2o-framework/src/ducks/api/action/condition.ts
+++ frontend/n2o-framework/src/ducks/api/action/condition.ts
@@ -1,15 +1,13 @@
 import { createAction } from '@reduxjs/toolkit'
 import { isEmpty } from 'lodash'
-import { put, select } from 'redux-saga/effects'
+import { select } from 'redux-saga/effects'
 
 import { Action, ErrorAction, Meta } from '../../Action'
 import { ModelPrefix } from '../../../core/datasource/const'
 import evalExpression from '../../../utils/evalExpression'
 import { makeGetModelByPrefixSelector } from '../../models/selectors'
 import { ACTIONS_PREFIX } from '../constants'
-import { mergeMeta } from '../utils/mergeMeta'
 import { waitOperation } from '../utils/waitOperation'
-import { guid } from '../../../utils/id'
 
 export type Payload = {
     datasource: string
@@ -33,15 +31,9 @@ export function* effect({ payload }: Action<string, Payload>) {
     const action = evalExpression(condition, model) ? success : fail
 
     if (!isEmpty(action)) {
-        const operationId = guid()
+        const resultAction: Action | ErrorAction = yield waitOperation(action)
 
-        yield put(mergeMeta(action, {
-            operationId,
-        }))
-
-        const resultAction: Action | ErrorAction = yield waitOperation(operationId)
-
-        if (!resultAction.error) {
+        if (resultAction.error) {
             throw new Error(resultAction.error)
         }
     }
--- frontend/n2o-framework/src/ducks/api/action/sequence.ts
+++ frontend/n2o-framework/src/ducks/api/action/sequence.ts
@@ -1,10 +1,7 @@
 import { createAction } from '@reduxjs/toolkit'
-import { put } from 'redux-saga/effects'
 
 import { Action, ErrorAction, Meta } from '../../Action'
-import { guid } from '../../../utils/id'
 import { ACTIONS_PREFIX } from '../constants'
-import { mergeMeta } from '../utils/mergeMeta'
 import { waitOperation } from '../utils/waitOperation'
 
 export type Payload = {
@@ -23,15 +20,9 @@ export function* effect({ payload }: ReturnType<typeof creator>) {
     const { actions } = payload
 
     for (const action of actions) {
-        const operationId = guid()
+        const resultAction: Action | ErrorAction = yield waitOperation(action)
 
-        yield put(mergeMeta(action, {
-            operationId,
-        }))
-
-        const resultAction: Action | ErrorAction = yield waitOperation(operationId)
-
-        if (!resultAction.error) {
+        if (resultAction.error) {
             throw new Error(resultAction.error)
         }
     }
