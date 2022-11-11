a3196469fd227cf6ba46a006ecf8fed406afad4e
azalyaev <azalyaev@i-novus.ru>
2022-11-11 11:46:52 +0300

    fix/NNO-8714 add open page action

--- frontend/n2o-framework/src/ducks/api/constants.ts
+++ frontend/n2o-framework/src/ducks/api/constants.ts
@@ -2,3 +2,4 @@ export const API_PREFIX = 'n2o/api/'
 export const ACTIONS_PREFIX = `${API_PREFIX}action/`
 export const ALERTS_PREFIX = `${API_PREFIX}alerts/`
 export const MODELS_PREFIX = `${API_PREFIX}models/`
+export const PAGE_PREFIX = `${API_PREFIX}page/`
