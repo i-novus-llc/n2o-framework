5c2db6291483bef86cad4028bd45311f3ea82e42
rgalina <rgalina@i-novus.ru>
2023-01-16 15:45:50 +0300

    [NNO-8950] Некорректная работа списковых полей

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oInvocationProcessor.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/N2oInvocationProcessor.java
@@ -119,15 +119,14 @@ public class N2oInvocationProcessor implements InvocationProcessor, MetadataEnvi
                                           DataSet inDataSet) {
         DataSet resultDataSet = new DataSet();
         // normalize values
-        invocationParameters.forEach(parameter -> {
-            Object value = inDataSet.get(parameter.getId());
-            if (value != null) {
-                if (parameter.getNormalize() != null) {
-                    value = tryToNormalize(value, parameter, resultDataSet, applicationContext);
-                }
-                resultDataSet.put(parameter.getId(), value);
-            }
-        });
+        invocationParameters.stream().filter(parameter -> inDataSet.containsKey(parameter.getId()))
+                .forEach(parameter -> {
+                    Object value = inDataSet.get(parameter.getId());
+                    if (value != null && parameter.getNormalize() != null) {
+                        value = tryToNormalize(value, parameter, resultDataSet, applicationContext);
+                    }
+                    resultDataSet.put(parameter.getId(), value);
+                });
         // remove not enabled data
         invocationParameters.stream().filter(parameter -> !isMappingEnabled(parameter, inDataSet))
                 .forEach(parameter -> resultDataSet.remove(parameter.getId()));
