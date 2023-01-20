d8c69c1b397d8f1a3d725f39152655bd4a306d65
NailMukminov <nmukminov@i-novus.ru>
2023-01-20 14:13:07 +0300

    [NNO-8953] RequestDataAT.testDataRequest was created

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/RequestDataAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/RequestDataAT.java
@@ -1,6 +1,7 @@
 package net.n2oapp.framework.autotest.control;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import net.n2oapp.framework.autotest.api.component.button.Button;
 import net.n2oapp.framework.autotest.api.component.control.*;
 import net.n2oapp.framework.autotest.api.component.page.SimplePage;
@@ -15,6 +16,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Автотест проверяющий отправку запроса данных на сохранение
+ */
 public class RequestDataAT extends AutoTestBase {
     @BeforeAll
     public static void beforeClass() {
@@ -29,6 +33,7 @@ public class RequestDataAT extends AutoTestBase {
 
     @Override
     protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
         builder.packs(new N2oApplicationPack(), new N2oAllPagesPack(), new N2oAllDataPack());
 
         builder.sources(
@@ -38,15 +43,14 @@ public class RequestDataAT extends AutoTestBase {
                 new CompileInfo("net/n2oapp/framework/autotest/control/request_data/test.object.xml"));
     }
 
+    /**
+     * Тест проверяет сохранение изменений, после сохранения и обновления страницы
+     */
     @Test
     public void testDataRequest() {
         SimplePage page = open(SimplePage.class);
         page.shouldExists();
 
-        InputText inputText = page.widget(FormWidget.class).fields().field("input")
-                .control(InputText.class);
-        MaskedInput maskedInput = page.widget(FormWidget.class).fields().field("masked")
-                .control(MaskedInput.class);
         Select select = page.widget(FormWidget.class).fields().field("select")
                 .control(Select.class);
         InputSelect inputSelect = page.widget(FormWidget.class).fields().field("input_select")
@@ -57,76 +61,53 @@ public class RequestDataAT extends AutoTestBase {
                 .control(InputSelect.class);
         AutoComplete autoCompleteMulti = page.widget(FormWidget.class).fields().field("auto_multi")
                 .control(AutoComplete.class);
-        DateInterval dateInterval = page.widget(FormWidget.class).fields().field("Интервал")
-                .control(DateInterval.class);
-        DateInterval dateIntervalNoTime = page.widget(FormWidget.class).fields().field("Интервал без проставленного времени")
-                .control(DateInterval.class);
-        DateInput date = page.widget(FormWidget.class).fields().field("Дата")
-                .control(DateInput.class);
-        DateInput dateNoTime = page.widget(FormWidget.class).fields().field("Дата без проставленного времени")
-                .control(DateInput.class);
-        TimePicker timePicker = page.widget(FormWidget.class).fields().field("time")
-                .control(TimePicker.class);
         Button save = page.widget(FormWidget.class).toolbar().topLeft().button("Сохранить", Button.class);
 
-        inputText.shouldHaveValue("test-input");
-        maskedInput.shouldHaveValue("+7 (212) 312-31-22");
-        select.shouldSelected("test2");
-        inputSelect.shouldSelected("test1");
-        autoComplete.shouldHaveValue("test3");
-        inputSelectMulti.shouldSelectedMulti("test2", "test3", "test5");
-        autoCompleteMulti.shouldHaveTags("test2", "test3", "test5");
-        dateInterval.shouldHaveValue("14.08.2022 — 28.08.2022");
-        dateIntervalNoTime.shouldHaveValue("03.08.2022 — 18.08.2022");
-        date.shouldHaveValue("18.08.2022");
-        dateNoTime.shouldHaveValue("13.08.2022");
-        timePicker.shouldHaveValue("12:30");
+        select.shouldSelected("test4");
+        inputSelect.shouldSelected("test4");
+        autoComplete.shouldBeEmpty();
+        inputSelectMulti.shouldBeEmpty();
+        autoCompleteMulti.shouldBeEmpty();
 
-        inputText.val("test");
-        maskedInput.val("2283221337");
         select.click();
-        select.select(Condition.text("test5"));
-        select.shouldSelected("test5");
+        select.shouldBeOpened();
+        select.clear();
+        select.shouldBeEmpty();
+        select.closePopup();
+
         inputSelect.click();
+        inputSelect.shouldBeOpened();
         inputSelect.clear();
-        inputSelect.click();
-        inputSelect.select(Condition.text("test5"));
+        inputSelect.shouldBeEmpty();
+        inputSelect.select(Condition.text("test6"));
+        inputSelect.shouldHaveValue("test6");
+
         autoComplete.val("test5");
-        inputSelectMulti.click();
-        inputSelectMulti.clearItems("test5");
-        page.breadcrumb().element().click();
+        autoComplete.shouldHaveValue("test5");
+
+        inputSelectMulti.valMulti("test3", "test5");
+        inputSelectMulti.shouldHaveValue("test3");
+        inputSelectMulti.shouldHaveValue("test5");
+
         autoCompleteMulti.click();
         autoCompleteMulti.chooseDropdownOption("test1");
-        page.breadcrumb().element().click();
-        dateInterval.beginVal("10.08.2022");
-        dateInterval.endVal("18.08.2022");
-        dateIntervalNoTime.beginVal("10.08.2022");
-        dateIntervalNoTime.endVal("18.08.2022");
-        date.val("10.08.2022");
-        dateNoTime.val("10.08.2022");
-        timePicker.selectHours("10");
-        timePicker.selectMinutes("30");
+        autoCompleteMulti.click();
+        autoCompleteMulti.chooseDropdownOption("test6");
+        autoCompleteMulti.shouldHaveValue("test1");
+        autoCompleteMulti.shouldHaveValue("test6");
 
-//        inputSelect.expand();
-//        inputSelect.clear();
-//        inputSelect.collapse();
-//
-//        save.click();
-//        inputSelect.shouldExists();
-//        Selenide.refresh();
+        save.click();
+        Selenide.refresh();
 
+        page.shouldExists();
         page.breadcrumb().titleShouldHaveText("Автоматическое сохранение формы");
-        inputText.shouldHaveValue("test");
-        maskedInput.shouldHaveValue("+7 (228) 322-13-37");
-        select.shouldSelected("test5");
-        inputSelect.shouldSelected("");
+
+        select.shouldBeEmpty();
+        inputSelect.shouldHaveValue("test6");
         autoComplete.shouldHaveValue("test5");
-        inputSelectMulti.shouldSelectedMulti("test2", "test3");
-        autoCompleteMulti.shouldHaveTags("test2", "test3", "test5", "test1");
-        dateInterval.shouldHaveValue("10.08.2022 — 18.08.2022");
-        dateIntervalNoTime.shouldHaveValue("10.08.2022 — 18.08.2022");
-        date.shouldHaveValue("10.08.2022");
-        dateNoTime.shouldHaveValue("10.08.2022");
-        timePicker.shouldHaveValue("10:30");
+        inputSelectMulti.shouldHaveValue("test3");
+        inputSelectMulti.shouldHaveValue("test5");
+        autoCompleteMulti.shouldHaveValue("test1");
+        autoCompleteMulti.shouldHaveValue("test6");
     }
 }
