a60a6be467c5d863645d788cf3f2609ecbc35855
Лебеденко Игорь <ilebedenko@i-novus.ru>
2023-02-22 15:17:26 +0300

    [NNO-9102] - fixed blinking at

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/action/BulkOperationAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/action/BulkOperationAT.java
@@ -20,7 +20,6 @@ import net.n2oapp.framework.config.metadata.pack.N2oApplicationPack;
 import net.n2oapp.framework.config.selective.CompileInfo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -46,7 +45,6 @@ public class BulkOperationAT extends AutoTestBase {
     }
 
     @Test
-    @Disabled
     public void bulkOperationTest() {
         builder.sources(new CompileInfo("net/n2oapp/framework/autotest/action/bulk_operation/index.page.xml"),
                 new CompileInfo("net/n2oapp/framework/autotest/action/bulk_operation/setName.page.xml"),
@@ -150,6 +148,7 @@ public class BulkOperationAT extends AutoTestBase {
         Button deleteManyButton = table.toolbar().topLeft().button("Удалить выбранные");
         deleteManyButton.shouldExists();
         deleteManyButton.click();
+        page.alerts(Alert.Placement.top).alert(0).shouldHaveText("Данные сохранены");
         table.columns().rows().shouldHaveSize(1);
     }
 }
