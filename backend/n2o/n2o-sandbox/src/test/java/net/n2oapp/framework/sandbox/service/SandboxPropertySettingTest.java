1ce214e97ef4d94cce0bb1f7aecc884040a2c5d8
Лебеденко Игорь <ilebedenko@i-novus.ru>
2023-03-27 20:17:14 +0300

    Merge branch 'fix/NNO-9263' into 'master'
    fix/NNO-9263 docs fix
    
    See merge request open/n2o-framework!5248
    
    (cherry picked from commit 9a6d53f9656f0a84d662b1992aa2bd9054156789)
    
    f530c517 fix/NNO-9263 docs fix
    ef66d9d3 [NNO-9263] fix example url
    d4857a3a [NNO-9263] fix tests

--- backend/n2o/n2o-sandbox/src/test/java/net/n2oapp/framework/sandbox/service/SandboxPropertySettingTest.java
+++ backend/n2o/n2o-sandbox/src/test/java/net/n2oapp/framework/sandbox/service/SandboxPropertySettingTest.java
@@ -8,6 +8,7 @@ import net.n2oapp.framework.api.metadata.meta.page.Page;
 import net.n2oapp.framework.api.metadata.meta.page.SimplePage;
 import net.n2oapp.framework.sandbox.client.SandboxRestClientImpl;
 import net.n2oapp.framework.sandbox.resource.XsdSchemaParser;
+import net.n2oapp.framework.sandbox.templates.ProjectTemplateHolder;
 import net.n2oapp.framework.sandbox.view.SandboxApplicationBuilderConfigurer;
 import net.n2oapp.framework.sandbox.view.SandboxContext;
 import net.n2oapp.framework.sandbox.view.SandboxPropertyResolver;
@@ -36,7 +37,7 @@ import static org.hamcrest.Matchers.is;
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = {ViewController.class, SandboxPropertyResolver.class, SandboxRestClientImpl.class,
-                SandboxContext.class, XsdSchemaParser.class, SandboxApplicationBuilderConfigurer.class},
+                SandboxContext.class, XsdSchemaParser.class, SandboxApplicationBuilderConfigurer.class, ProjectTemplateHolder.class},
         properties = {"n2o.sandbox.url=http://${n2o.sandbox.api.host}:${n2o.sandbox.api.port}"})
 @PropertySource("classpath:sandbox.properties")
 @EnableAutoConfiguration
