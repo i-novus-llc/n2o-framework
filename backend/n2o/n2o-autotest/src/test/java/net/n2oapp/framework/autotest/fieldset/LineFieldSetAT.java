836d8f07b336b1459a43f8db1044f9c720f5cce5
Andrey Nikolaev <anikolaev@i-novus.ru>
2023-03-29 16:35:24 +0300

    NNO-9149 пример к баджу в line филдсете

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/fieldset/LineFieldSetAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/fieldset/LineFieldSetAT.java
@@ -149,5 +149,7 @@ public class LineFieldSetAT extends AutoTestBase {
         fieldsets.fieldset(2, LineFieldSet.class).shouldHaveText("27");
         fieldsets.fieldset(2, LineFieldSet.class).fields().field("count").control(InputText.class).setValue("54");
         fieldsets.fieldset(2, LineFieldSet.class).shouldHaveText("54");
+        fieldsets.fieldset(2, LineFieldSet.class).fields().field("count").control(InputText.class).clear();
+        fieldsets.fieldset(2, LineFieldSet.class).shouldHaveText("");
     }
 }
