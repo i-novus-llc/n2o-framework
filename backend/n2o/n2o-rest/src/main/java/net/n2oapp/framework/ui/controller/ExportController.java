6b312cfa08cba426b4d370b3dcaa5a61ea61d977
NailMukminov <nmukminov@i-novus.ru>
2023-04-05 15:43:05 +0400

    [NNO-9284] ViewController.export was created

--- backend/n2o/n2o-rest/src/main/java/net/n2oapp/framework/ui/controller/ExportController.java
+++ backend/n2o/n2o-rest/src/main/java/net/n2oapp/framework/ui/controller/ExportController.java
@@ -1,5 +1,6 @@
 package net.n2oapp.framework.ui.controller;
 
+import com.opencsv.CSVWriter;
 import net.n2oapp.criteria.dataset.DataSet;
 import net.n2oapp.framework.api.MetadataEnvironment;
 import net.n2oapp.framework.api.register.route.MetadataRouter;
@@ -7,33 +8,46 @@ import net.n2oapp.framework.api.rest.ExportResponse;
 import net.n2oapp.framework.api.rest.GetDataResponse;
 import net.n2oapp.framework.api.user.UserContext;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static java.nio.charset.StandardCharsets.*;
 
 public class ExportController extends AbstractController {
 
+    private static final String FILES_DIRECTORY_NAME = "src/main/resources/META-INF/resources/export_files/";
+    private static final String CSV_FILE_NAME = "export.csv";
     private final DataController dataController;
-
-    public ExportController(MetadataEnvironment environment,
-                            DataController dataController) {
-        super(environment);
-        this.dataController = dataController;
-    }
 
     public ExportController(MetadataEnvironment environment, MetadataRouter router, DataController dataController) {
         super(environment, router);
         this.dataController = dataController;
     }
 
-    public ExportResponse export(List<DataSet> body, String format, String charset) {
+    public ExportResponse export(List<DataSet> data, String format, String charset) {
         ExportResponse response = new ExportResponse();
+        byte[] fileBytes = null;
 
-        byte[] file = createScv(null, "export.csv");
+        switch (format) {
+            case ("csv"):
+                fileBytes = createScv(data, charset);
+                response.setFileName(CSV_FILE_NAME);
+        }
 
-        response.setFile(file);
+        if (fileBytes == null)
+           response.setStatus(500);
+
+        response.setFile(fileBytes);
         response.setCharset(charset);
         response.setFormat(format);
-        response.setFileName("export.csv");
         return response;
     }
 
@@ -41,7 +55,64 @@ public class ExportController extends AbstractController {
         return dataController.getData(path, parameters, user);
     }
 
-    private byte[] createScv(String body, String fileName) {
-        return null;
+    private byte[] createScv(List<DataSet> data, String charset) {
+        byte[] fileBytes = null;
+
+        try {
+            FileWriter fileWriter = new FileWriter(FILES_DIRECTORY_NAME + CSV_FILE_NAME, resolveCharset(charset));
+
+            CSVWriter writer = new CSVWriter(fileWriter, ';',
+                    CSVWriter.NO_QUOTE_CHARACTER,
+                    CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                    CSVWriter.DEFAULT_LINE_END);
+
+            List<String[]> csvData = resolveToCsvFormat(data);
+            writer.writeAll(csvData);
+
+            writer.close();
+
+            fileBytes = Files.readAllBytes(Path.of(FILES_DIRECTORY_NAME + CSV_FILE_NAME));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileBytes;
+    }
+
+    private List<String[]> resolveToCsvFormat(List<DataSet> data) {
+        Set<String> keys = data.get(0).flatKeySet();
+        int columnCount = keys.size();
+        String[] headers = new String[columnCount];
+
+        Iterator<String> keysIterator = keys.iterator();
+
+        for (int i = 0; i < columnCount; i++) {
+            headers[i] = keysIterator.next();
+        }
+
+        List<String[]> csvData = new ArrayList<>();
+        csvData.add(headers);
+
+        for (DataSet str : data) {
+            String[] csvStr = new String[columnCount];
+            int i = 0;
+            for (String key : keys) {
+                Object value = str.get(key);
+                if (value instanceof List)
+                    csvStr[i] = str.getList(key).toString();
+                else
+                    csvStr[i] = str.getString(key);
+                i++;
+            }
+
+            csvData.add(csvStr);
+        }
+
+        return csvData;
+    }
+
+    private Charset resolveCharset(String charset) {
+        return UTF_8;
     }
 }
