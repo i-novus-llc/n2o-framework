d6d01fe76622f242f6046b4b1f9941768b10a18e
Andrey Nikolaev <anikolaev@i-novus.ru>
2023-04-28 17:14:59 +0300

    NNO-9321 Автотест для проверки автосохранения

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/StandardFieldSubmitAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/control/StandardFieldSubmitAT.java
@@ -6,6 +6,8 @@ import net.n2oapp.framework.autotest.api.component.control.DateInterval;
 import net.n2oapp.framework.autotest.api.component.control.InputText;
 import net.n2oapp.framework.autotest.api.component.control.Select;
 import net.n2oapp.framework.autotest.api.component.page.SimplePage;
+import net.n2oapp.framework.autotest.api.component.page.StandardPage;
+import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
 import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
 import net.n2oapp.framework.autotest.run.AutoTestBase;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
@@ -32,27 +34,96 @@ public class StandardFieldSubmitAT extends AutoTestBase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-
-        SimplePage page = open(SimplePage.class);
-        page.shouldExists();
-        fields = page.widget(FormWidget.class).fields();
-        fields.shouldHaveSize(3);
     }
 
     @Override
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
-        builder.packs(new N2oPagesPack(), new N2oApplicationPack(), new N2oWidgetsPack(),
-                new N2oFieldSetsPack(), new N2oControlsPack(), new N2oAllDataPack());
+        builder.packs(new N2oApplicationPack(), new N2oAllPagesPack(), new N2oAllDataPack());
+    }
+
+    @Test
+    public void testSubmit() {
         setJsonPath("net/n2oapp/framework/autotest/control/submit");
         builder.sources(
                 new CompileInfo("net/n2oapp/framework/autotest/control/submit/index.page.xml"),
                 new CompileInfo("net/n2oapp/framework/autotest/control/submit/test.query.xml"),
                 new CompileInfo("net/n2oapp/framework/autotest/control/submit/test.object.xml"));
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+        fields = page.widget(FormWidget.class).fields();
+        fields.shouldHaveSize(3);
+        InputText inputText = fields.field("Имя").control(InputText.class);
+        inputText.shouldExists();
+        inputText.shouldHaveValue("Joe");
+
+        Select select = fields.field("Пол").control(Select.class);
+        select.shouldHaveValue("Мужской");
+
+        DateInterval dateInterval = fields.field("Даты отпуска").control(DateInterval.class);
+        dateInterval.shouldBeClosed();
+        dateInterval.beginShouldHaveValue("15.01.2020");
+        dateInterval.endShouldHaveValue("30.01.2020");
+
+        // изменяем обычное текстовое поле
+        inputText.click();
+        inputText.setValue("Ann");
+        // обновляем страницу и проверяем значения всех полей
+        // ожидание отправки поля
+        Selenide.sleep(500);
+        inputText.shouldHaveValue("Ann");
+        Selenide.refresh();
+        inputText.shouldHaveValue("Ann");
+        select.shouldHaveValue("Мужской");
+        dateInterval.shouldBeClosed();
+        dateInterval.beginShouldHaveValue("15.01.2020");
+        dateInterval.endShouldHaveValue("30.01.2020");
+
+        // изменяем списковое поле
+        select.openPopup();
+        select.dropdown().selectItem(1);
+        Selenide.sleep(500);
+        select.shouldHaveValue("Женский");
+        Selenide.refresh();
+        inputText.shouldHaveValue("Ann");
+        select.shouldHaveValue("Женский");
+        dateInterval.shouldBeClosed();
+        dateInterval.beginShouldHaveValue("15.01.2020");
+        dateInterval.endShouldHaveValue("30.01.2020");
+
+        // изменяем интервальное поле
+        dateInterval.setValueInBegin("18.01.2020");
+        Selenide.sleep(500);
+        dateInterval.beginShouldHaveValue("18.01.2020");
+        Selenide.refresh();
+        inputText.shouldHaveValue("Ann");
+        select.shouldHaveValue("Женский");
+        dateInterval.shouldBeClosed();
+        dateInterval.beginShouldHaveValue("18.01.2020");
+        dateInterval.endShouldHaveValue("30.01.2020");
+
+        dateInterval.setValueInEnd("24.01.2020");
+        Selenide.sleep(500);
+        dateInterval.endShouldHaveValue("24.01.2020");
+        Selenide.refresh();
+        inputText.shouldHaveValue("Ann");
+        select.shouldHaveValue("Женский");
+        dateInterval.shouldBeClosed();
+        dateInterval.beginShouldHaveValue("18.01.2020");
+        dateInterval.endShouldHaveValue("24.01.2020");
     }
 
     @Test
-    public void testSubmit() {
+    public void testDifferentNameFormDatasource() {
+        setJsonPath("net/n2oapp/framework/autotest/control/submit");
+        builder.sources(
+                new CompileInfo("net/n2oapp/framework/autotest/control/submit/different_name_form_datasource/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/control/submit/test.query.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/control/submit/test.object.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+        fields = page.regions().region(0, SimpleRegion.class).content().widget(FormWidget.class).fields();
+        fields.shouldHaveSize(3);
         InputText inputText = fields.field("Имя").control(InputText.class);
         inputText.shouldExists();
         inputText.shouldHaveValue("Joe");
