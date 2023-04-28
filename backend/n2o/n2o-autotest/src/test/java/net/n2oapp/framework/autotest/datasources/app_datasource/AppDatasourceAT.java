a6fc25532e8c7c57b8ab898ac7ab1644af17374d
rgalina <rgalina@i-novus.ru>
2023-04-28 11:21:26 +0300

    [NNO-9399] Автотесты на атрибут source-datsource в parent/app datasource

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/datasources/app_datasource/AppDatasourceAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/datasources/app_datasource/AppDatasourceAT.java
@@ -1,10 +1,14 @@
 package net.n2oapp.framework.autotest.datasources.app_datasource;
 
+import net.n2oapp.framework.autotest.N2oSelenide;
 import net.n2oapp.framework.autotest.api.component.application.Sidebar;
+import net.n2oapp.framework.autotest.api.component.button.StandardButton;
 import net.n2oapp.framework.autotest.api.component.control.InputText;
+import net.n2oapp.framework.autotest.api.component.modal.Modal;
 import net.n2oapp.framework.autotest.api.component.page.StandardPage;
 import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
 import net.n2oapp.framework.autotest.api.component.widget.FormWidget;
+import net.n2oapp.framework.autotest.api.component.widget.table.TableWidget;
 import net.n2oapp.framework.autotest.run.AutoTestBase;
 import net.n2oapp.framework.config.N2oApplicationBuilder;
 import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
@@ -35,17 +39,16 @@ public class AppDatasourceAT extends AutoTestBase {
     protected void configure(N2oApplicationBuilder builder) {
         super.configure(builder);
         builder.packs(new N2oApplicationPack(), new N2oAllPagesPack(), new N2oAllDataPack());
-
-        setJsonPath("net/n2oapp/framework/autotest/datasources/app_datasource");
-        builder.sources(
-                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/side.application.xml"),
-                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/index.page.xml"),
-                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/page2.page.xml"),
-                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/test.query.xml"));
     }
 
     @Test
     public void testAppDS() {
+        setJsonPath("net/n2oapp/framework/autotest/datasources/app_datasource/simple");
+        builder.sources(
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/simple/side.application.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/simple/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/simple/page2.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/simple/test.query.xml"));
         StandardPage page = open(StandardPage.class);
         page.shouldExists();
         page.breadcrumb().crumb(0).shouldHaveLabel("App datasource. Ссылка на источник данных, объявленный в application.xml");
@@ -65,5 +68,56 @@ public class AppDatasourceAT extends AutoTestBase {
         sidebar.nav().anchor(0).click();
         id.shouldHaveValue("1");
         name.shouldHaveValue("test1");
+    }
+
+    /**
+     * Тестирование Прокидывание разных app-datasource из родительской страницы
+     */
+    @Test
+    public void testSourceDatasource() {
+        setJsonPath("net/n2oapp/framework/autotest/datasources/app_datasource/source_datasource");
+        builder.sources(
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/source_datasource/default.application.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/source_datasource/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/source_datasource/modal.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/app_datasource/source_datasource/test.query.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+
+        TableWidget table1 = page.regions().region(0, SimpleRegion.class).content()
+                .widget(1, TableWidget.class);
+        table1.shouldExists();
+        StandardButton showModalBtn = table1.toolbar().topLeft().button("Открыть");
+        showModalBtn.shouldExists();
+        showModalBtn.click();
+        Modal modalPage = N2oSelenide.modal();
+        modalPage.shouldExists();
+
+        FormWidget formWidget = modalPage.content(StandardPage.class).regions().region(0, SimpleRegion.class).content()
+                .widget(0,FormWidget.class);
+        formWidget.fields().field("id").control(InputText.class).setValue("5");
+        formWidget.fields().field("name").control(InputText.class).setValue("Mary");
+        modalPage.toolbar().bottomRight().button("Сохранить").click();
+
+        table1.columns().rows().shouldHaveSize(5);
+        table1.columns().rows().row(4).cell(0).shouldHaveText("5");
+
+        TableWidget table2 = page.regions().region(0, SimpleRegion.class).content()
+                .widget(2, TableWidget.class);
+        table1.shouldExists();
+        showModalBtn = table2.toolbar().topLeft().button("Открыть");
+        showModalBtn.shouldExists();
+        showModalBtn.click();
+        modalPage = N2oSelenide.modal();
+        modalPage.shouldExists();
+
+        formWidget = modalPage.content(StandardPage.class).regions().region(0, SimpleRegion.class).content()
+                .widget(0,FormWidget.class);
+        formWidget.fields().field("id").control(InputText.class).setValue("3");
+        formWidget.fields().field("name").control(InputText.class).setValue("Mary");
+        modalPage.toolbar().bottomRight().button("Сохранить").click();
+
+        table2.columns().rows().shouldHaveSize(3);
+        table2.columns().rows().row(2).cell(0).shouldHaveText("3");
     }
 }
--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/datasources/parent_datasource/ParentDatasourceAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/datasources/parent_datasource/ParentDatasourceAT.java
@@ -2,6 +2,7 @@ package net.n2oapp.framework.autotest.datasources.parent_datasource;
 
 import net.n2oapp.framework.autotest.N2oSelenide;
 import net.n2oapp.framework.autotest.api.component.button.StandardButton;
+import net.n2oapp.framework.autotest.api.component.control.InputText;
 import net.n2oapp.framework.autotest.api.component.modal.Modal;
 import net.n2oapp.framework.autotest.api.component.page.StandardPage;
 import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
@@ -66,31 +67,53 @@ public class ParentDatasourceAT extends AutoTestBase {
         rows.row(2).cell(0).shouldHaveText("3");
     }
 
-//    /**
-//     * Тестирование Прокидывание разных parent-datasource из родительской страницы
-//     */
-//    @Test
-//    public void testSourceDatasource() {
-//        setJsonPath("net/n2oapp/framework/autotest/datasources/parent_datasource/filter_by_parent");
-//        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/filter_by_parent/index.page.xml"),
-//                new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/filter_by_parent/modal.page.xml"),
-//                new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/filter_by_parent/test.query.xml"));
-//        StandardPage page = open(StandardPage.class);
-//        page.shouldExists();
-//
-//        StandardButton showModalBtn = page.regions().region(0, SimpleRegion.class).content()
-//                .widget(0, FormWidget.class).toolbar().topLeft().button("История изменений");
-//        showModalBtn.shouldExists();
-//        showModalBtn.click();
-//        Modal modalPage = N2oSelenide.modal();
-//        modalPage.shouldExists();
-//        modalPage.shouldHaveTitle("История изменений");
-//        TableWidget.Rows rows = modalPage.content(StandardPage.class).regions().region(0, SimpleRegion.class).content()
-//                .widget(1, TableWidget.class).columns().rows();
-//        rows.shouldHaveSize(3);
-//        rows.row(0).cell(0).shouldHaveText("1");
-//        rows.row(1).cell(0).shouldHaveText("2");
-//        rows.row(2).cell(0).shouldHaveText("3");
-//    }
+    /**
+     * Тестирование Прокидывание разных parent-datasource из родительской страницы
+     */
+    @Test
+    public void testSourceDatasource() {
+        setJsonPath("net/n2oapp/framework/autotest/datasources/parent_datasource/source_datasource");
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/source_datasource/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/source_datasource/modal.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/datasources/parent_datasource/source_datasource/test.query.xml"));
+        StandardPage page = open(StandardPage.class);
+        page.shouldExists();
+
+        TableWidget table1 = page.regions().region(0, SimpleRegion.class).content()
+                .widget(1, TableWidget.class);
+        table1.shouldExists();
+        StandardButton showModalBtn = table1.toolbar().topLeft().button("Открыть");
+        showModalBtn.shouldExists();
+        showModalBtn.click();
+        Modal modalPage = N2oSelenide.modal();
+        modalPage.shouldExists();
+
+        FormWidget formWidget = modalPage.content(StandardPage.class).regions().region(0, SimpleRegion.class).content()
+                .widget(0,FormWidget.class);
+        formWidget.fields().field("id").control(InputText.class).setValue("5");
+        formWidget.fields().field("name").control(InputText.class).setValue("Mary");
+        modalPage.toolbar().bottomRight().button("Сохранить").click();
+
+        table1.columns().rows().shouldHaveSize(5);
+        table1.columns().rows().row(4).cell(0).shouldHaveText("5");
+
+        TableWidget table2 = page.regions().region(0, SimpleRegion.class).content()
+                .widget(2, TableWidget.class);
+        table1.shouldExists();
+        showModalBtn = table2.toolbar().topLeft().button("Открыть");
+        showModalBtn.shouldExists();
+        showModalBtn.click();
+        modalPage = N2oSelenide.modal();
+        modalPage.shouldExists();
+
+        formWidget = modalPage.content(StandardPage.class).regions().region(0, SimpleRegion.class).content()
+                .widget(0,FormWidget.class);
+        formWidget.fields().field("id").control(InputText.class).setValue("3");
+        formWidget.fields().field("name").control(InputText.class).setValue("Mary");
+        modalPage.toolbar().bottomRight().button("Сохранить").click();
+
+        table2.columns().rows().shouldHaveSize(3);
+        table2.columns().rows().row(2).cell(0).shouldHaveText("3");
+    }
 }
 
