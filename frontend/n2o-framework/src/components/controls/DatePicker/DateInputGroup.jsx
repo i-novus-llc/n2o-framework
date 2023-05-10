7ef6dc3a6a230d5d58674efa325141fa56574338
aramazanov <aramazanov@i-novus.ru>
2023-05-10 19:32:11 +0300

    [NNO-9315] Баг в moment в связке с vite

--- frontend/n2o-framework/src/components/controls/DatePicker/DateInputGroup.jsx
+++ frontend/n2o-framework/src/components/controls/DatePicker/DateInputGroup.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
+import moment from 'moment/moment'
 import pick from 'lodash/pick'
 
 // eslint-disable-next-line import/no-cycle
