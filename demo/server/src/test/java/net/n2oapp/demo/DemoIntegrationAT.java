6de41cc215b38e2a7a431793bab08d3bd41a816e
rgalina <rgalina@i-novus.ru>
2023-05-11 23:41:21 +0300

    [NNO-9305] fix demo tests

--- demo/server/src/test/java/net/n2oapp/demo/DemoIntegrationAT.java
+++ demo/server/src/test/java/net/n2oapp/demo/DemoIntegrationAT.java
@@ -581,7 +581,7 @@ public class DemoIntegrationAT {
         protoPage.deleteClientFromTableCell(row);
 
         protoPage.shouldBeDialog("Предупреждение");
-        protoPage.shouldDialogHaveText("Предупреждение", "Вы уверены, что хотите удалить клиента " + surnames.get(row));
+        protoPage.shouldDialogHaveText("Предупреждение", "Нажмите \"Да\", если Вы уверены в совершаемом действии. Или \"Нет\", если ещё хотите обдумать совершаемое действие.");
         protoPage.acceptDialog("Предупреждение");
 
         protoPage.alertColorShouldBe(Colors.SUCCESS);
@@ -604,7 +604,7 @@ public class DemoIntegrationAT {
         protoPage.deleteClientFromTableToolBar();
 
         protoPage.shouldBeDialog("Предупреждение");
-        protoPage.shouldDialogHaveText("Предупреждение", "Вы уверены, что хотите удалить клиента " + surnames.get(row));
+        protoPage.shouldDialogHaveText("Предупреждение", "Вы уверены, что хотите удалить клиента?");
         protoPage.acceptDialog("Предупреждение");
 
         protoPage.alertColorShouldBe(Colors.SUCCESS);
