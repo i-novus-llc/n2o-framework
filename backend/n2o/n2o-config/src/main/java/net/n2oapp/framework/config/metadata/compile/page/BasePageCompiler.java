c34ba1671e1dee330720df2a89bf79e1644bb4c4
rgalina <rgalina@i-novus.ru>
2023-05-18 14:19:51 +0300

    Неправильно генерируется id датасорса страницы

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/page/BasePageCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/page/BasePageCompiler.java
@@ -123,8 +123,16 @@ public abstract class BasePageCompiler<S extends N2oBasePage, D extends Standard
         page.setEvents(initEvents(source, context, p, metaActions, pageScope, pageIndexScope,
                 datasourcesScope, appDatasourceIdsScope, parentDatasourceIdsScope));
 
-        if (source.getDatasourceId() != null)
-            page.getPageProperty().setDatasource(getClientDatasourceId(source.getDatasourceId(), page.getId(), p));
+        if (source.getDatasourceId() != null) {
+            N2oAbstractDatasource pageDs = datasourcesScope.get(source.getDatasourceId());
+            if (pageDs instanceof N2oApplicationDatasource) {
+                page.getPageProperty().setDatasource(source.getDatasourceId());
+            } else if (pageDs instanceof N2oParentDatasource) {
+                page.getPageProperty().setDatasource(parentDatasourceIdsScope.get(source.getDatasourceId()));
+            } else {
+                page.getPageProperty().setDatasource(getClientDatasourceId(source.getDatasourceId(), page.getId(), p));
+            }
+        }
 
         return page;
     }
