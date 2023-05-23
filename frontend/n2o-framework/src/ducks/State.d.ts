65b733e107c37d8c02c8885ac5776a19dde00b5d
Соколов Алексей <asokolov@i-novus.ru>
2023-05-23 16:30:00 +0300

    fix/NNO-9162 restore query filters

--- frontend/n2o-framework/src/ducks/State.d.ts
+++ frontend/n2o-framework/src/ducks/State.d.ts
@@ -1,7 +1,9 @@
 import type { State as DataSourceState } from './datasource/DataSource'
 import type { State as ModelsState } from './models/Models'
+import { TGlobalState } from './global/TGlobalState'
 
 export interface State {
     datasource: DataSourceState
     models: ModelsState
+    global: TGlobalState
 }
