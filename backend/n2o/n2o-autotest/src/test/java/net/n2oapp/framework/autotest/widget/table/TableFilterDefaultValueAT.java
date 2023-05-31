fe89c0b7a2a7cf5a904f1947015cf0b5d60d74a9
anikolaev <anikolaev@i-novus.ru>
2023-05-31 16:35:39 +0300

    NNO-9553 Автотест. Востановление фильтров при возврате назад

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/TableFilterDefaultValueAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/widget/table/TableFilterDefaultValueAT.java
@@ -1,8 +1,12 @@
 package net.n2oapp.framework.autotest.widget.table;
 
 import com.codeborne.selenide.Selenide;
+import net.n2oapp.framework.autotest.N2oSelenide;
+import net.n2oapp.framework.autotest.api.component.button.Button;
 import net.n2oapp.framework.autotest.api.component.control.InputText;
 import net.n2oapp.framework.autotest.api.component.control.RadioGroup;
+import net.n2oapp.framework.autotest.api.component.drawer.Drawer;
+import net.n2oapp.framework.autotest.api.component.modal.Modal;
 import net.n2oapp.framework.autotest.api.component.page.SimplePage;
 import net.n2oapp.framework.autotest.api.component.page.StandardPage;
 import net.n2oapp.framework.autotest.api.component.region.SimpleRegion;
@@ -184,6 +188,71 @@ public class TableFilterDefaultValueAT extends AutoTestBase {
         table.filters().toolbar().button("Сбросить").click();
         rows.shouldHaveSize(4);
         filter.shouldBeEmpty();*/
+    }
+
+    @Test
+    public void testRestoreFiltersAfterClose() {
+        setJsonPath("net/n2oapp/framework/autotest/widget/table/filters/restore_filters_after_close");
+        builder.sources(new CompileInfo("net/n2oapp/framework/autotest/widget/table/filters/restore_filters_after_close/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/widget/table/filters/restore_filters_after_close/open.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/widget/table/filters/restore_filters_after_close/test.query.xml"));
+        SimplePage page = open(SimplePage.class);
+        StandardPage secPage = open(StandardPage.class);
+        Modal modal = N2oSelenide.modal();
+        Drawer drawer = N2oSelenide.drawer();
+        page.shouldExists();
+
+        TableWidget table = page.widget(TableWidget.class);
+        Button search = table.filters().toolbar().button("Найти");
+        InputText filter = table.filters().fields().field("name").control(InputText.class);
+        Button openPage = table.toolbar().topLeft().button("open-page");
+        Button showModal = table.toolbar().topLeft().button("show-modal");
+        Button openDrawer = table.toolbar().topLeft().button("open-drawer");
+
+        Button close = secPage.toolbar().bottomRight().button("Закрыть");
+
+        table.shouldExists();
+        table.columns().rows().shouldHaveSize(9);
+        filter.setValue("TEST2");
+        search.click();
+        table.columns().rows().shouldHaveSize(1);
+
+        openPage.click();
+        secPage.shouldExists();
+        close.click();
+        page.shouldExists();
+        filter.shouldHaveValue("TEST2");
+        table.columns().rows().shouldHaveSize(1);
+
+        openPage.click();
+        secPage.shouldExists();
+        Selenide.back();
+        page.shouldExists();
+        filter.shouldHaveValue("TEST2");
+        table.columns().rows().shouldHaveSize(1);
+
+        showModal.click();
+        modal.shouldExists();
+        modal.toolbar().bottomRight().button("Закрыть").click();
+        modal.shouldNotExists();
+        page.shouldExists();
+        filter.shouldHaveValue("TEST2");
+        table.columns().rows().shouldHaveSize(1);
+
+        openDrawer.click();
+        drawer.shouldExists();
+        drawer.toolbar().bottomRight().button("Закрыть").click();
+        drawer.shouldNotExists();
+        page.shouldExists();
+        filter.shouldHaveValue("TEST2");
+        table.columns().rows().shouldHaveSize(1);
+
+        Selenide.back();
+        secPage.shouldExists();
+        Selenide.forward();
+        page.shouldExists();
+        filter.shouldHaveValue("TEST2");
+        table.columns().rows().shouldHaveSize(1);
     }
 }
 
