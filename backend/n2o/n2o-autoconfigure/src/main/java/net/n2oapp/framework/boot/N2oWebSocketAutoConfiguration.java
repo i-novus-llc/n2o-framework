6c8b3de93ee0d71e55446f535eb76b54eb4ae8a6
Лебеденко Игорь <ilebedenko@i-novus.ru>
2023-06-15 19:30:24 +0300

    Merge branch 'fix/NNO-9671' into 'master'
    [NNO-9671] Исправил работы вебсокет уведомлений
    
    Closes NNO-9671
    
    See merge request open/n2o-framework!5682
    
    (cherry picked from commit bba523e21f7d7e35f756cadeab7492fd7437e9aa)
    
    de4cf486 [NNO-9671] Исправил работы вебсокет уведомлений
    e6adaafe [NNO-9671] return destination check

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oWebSocketAutoConfiguration.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oWebSocketAutoConfiguration.java
@@ -2,7 +2,6 @@ package net.n2oapp.framework.boot;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.security.auth.UserPrincipal;
 import net.n2oapp.framework.api.MetadataEnvironment;
 import net.n2oapp.framework.api.metadata.jackson.SingletonTypeIdHandlerInstantiator;
 import net.n2oapp.framework.boot.stomp.N2oWebSocketController;
@@ -10,24 +9,19 @@ import net.n2oapp.framework.boot.stomp.WebSocketController;
 import net.n2oapp.framework.config.compile.pipeline.N2oPipelineSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
-import java.security.Principal;
-import java.util.Map;
-import java.util.UUID;
-
 @Configuration
 @EnableWebSocketMessageBroker
 @ConditionalOnClass(WebSocketMessageBrokerConfigurer.class)
+@Import(N2oWebSocketHandshakeHandlerConfiguration.class)
 public class N2oWebSocketAutoConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Autowired
@@ -43,17 +37,6 @@ public class N2oWebSocketAutoConfiguration implements WebSocketMessageBrokerConf
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setUserDestinationPrefix("/user");
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(DefaultHandshakeHandler.class)
-    public DefaultHandshakeHandler handshakeHandler() {
-        return new DefaultHandshakeHandler() {
-            @Override
-            protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-                return new UserPrincipal(UUID.randomUUID().toString());
-            }
-        };
     }
 
     @Bean
