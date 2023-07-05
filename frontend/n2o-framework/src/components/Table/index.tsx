ec7235e24311b0ef1d8278a5f19549e27b32970c
xuryrg2034@gmail.com <stalker0234>
2023-07-05 13:23:27 +0300

    nf/NNO-9369 - Добавлен экспорт базовых компонетов таблицы

--- frontend/n2o-framework/src/components/Table/index.tsx
+++ frontend/n2o-framework/src/components/Table/index.tsx
@@ -1,3 +1,4 @@
 export { TableContainer } from './components/TableContainer'
+export { Table as BasicTable } from './components/basic'
 export { ChildrenToggleState, SelectionType, ActionType, TableActions } from './enum'
 export { useTableActions } from './provider/TableActions'
