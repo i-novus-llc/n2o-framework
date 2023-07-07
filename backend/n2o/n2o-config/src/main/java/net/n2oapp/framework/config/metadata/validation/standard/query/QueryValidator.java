864462f9637fe4d3da4a08a2f0c187080474a5fc
Николаев Андрей Валерьевич <anikolaev@i-novus.ru>
2023-07-07 12:13:23 +0300

    Resolve NNO-9606 "Sub/"

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/query/QueryValidator.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/query/QueryValidator.java
@@ -3,11 +3,15 @@ package net.n2oapp.framework.config.metadata.validation.standard.query;
 import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.aware.SourceClassAware;
 import net.n2oapp.framework.api.metadata.compile.SourceProcessor;
+import net.n2oapp.framework.api.metadata.global.dao.object.AbstractParameter;
 import net.n2oapp.framework.api.metadata.global.dao.object.N2oObject;
+import net.n2oapp.framework.api.metadata.global.dao.object.field.ObjectReferenceField;
 import net.n2oapp.framework.api.metadata.global.dao.query.AbstractField;
 import net.n2oapp.framework.api.metadata.global.dao.query.N2oQuery;
+import net.n2oapp.framework.api.metadata.global.dao.query.field.QueryReferenceField;
 import net.n2oapp.framework.api.metadata.validate.SourceValidator;
 import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
+import net.n2oapp.framework.config.metadata.validation.standard.ValidationUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -26,8 +30,10 @@ public class QueryValidator implements SourceValidator<N2oQuery>, SourceClassAwa
     public void validate(N2oQuery n2oQuery, SourceProcessor p) {
         if (n2oQuery.getObjectId() != null)
             checkForExistsObject(n2oQuery.getId(), n2oQuery.getObjectId(), p);
-        if (n2oQuery.getFields() != null)
+        if (n2oQuery.getFields() != null) {
+            p.safeStreamOf(n2oQuery.getFields()).forEach(field -> checkFieldIdExistence(field, n2oQuery.getId(), p));
             checkForUniqueFields(n2oQuery.getFields(), n2oQuery.getId(), p);
+        }
         if (n2oQuery.getFilters() != null) {
             checkForUniqueFilterFields(n2oQuery.getFilters(), n2oQuery.getId());
             checkForExistsFiltersInSelections(n2oQuery);
@@ -44,7 +50,21 @@ public class QueryValidator implements SourceValidator<N2oQuery>, SourceClassAwa
      */
     private void checkForExistsObject(String queryId, String objectId, SourceProcessor p) {
         p.checkForExists(objectId, N2oObject.class,
-                String.format("Выборка '%s' ссылается на несуществующий объект %s", queryId, objectId));
+                String.format("Выборка %s ссылается на несуществующий объект %s",
+                        ValidationUtils.getIdOrEmptyString(queryId),
+                        ValidationUtils.getIdOrEmptyString(objectId)));
+    }
+
+    /**
+     * Проверка полей на наличие id
+     *
+     * @param field   Исходная metadata поля
+     * @param p       Процессор исходных метаданных
+     */
+    public static void checkFieldIdExistence(AbstractField field, String queryId, SourceProcessor p) {
+        if (field instanceof QueryReferenceField)
+            p.safeStreamOf(((QueryReferenceField) field).getFields()).forEach(childField -> checkFieldIdExistence(childField, queryId, p));
+        p.checkIdExistence(field,  String.format("Одно из полей выборки %s не имеет 'id'", ValidationUtils.getIdOrEmptyString(queryId)));
     }
 
     /**
@@ -68,7 +88,9 @@ public class QueryValidator implements SourceValidator<N2oQuery>, SourceClassAwa
         Set<String> filterFields = new HashSet<>();
         for (N2oQuery.Filter filter : filters) {
             if (filter.getFilterId() != null && !filterFields.add(filter.getFilterId())) {
-                throw new N2oMetadataValidationException(String.format("Фильтр %s в выборке %s повторяется", filter.getFilterId(), queryId));
+                throw new N2oMetadataValidationException(String.format("Фильтр %s в выборке %s повторяется",
+                        ValidationUtils.getIdOrEmptyString(filter.getFilterId()),
+                        ValidationUtils.getIdOrEmptyString(queryId)));
             }
         }
     }
@@ -80,18 +102,21 @@ public class QueryValidator implements SourceValidator<N2oQuery>, SourceClassAwa
      */
     private void checkForExistsFiltersInSelections(N2oQuery query) {
         Set<String> filterFields = Arrays.stream(query.getFilters()).map(N2oQuery.Filter::getFilterId).collect(Collectors.toSet());
-        checkFiltersExistInSelectionType(query.getLists(), filterFields, "list");
-        checkFiltersExistInSelectionType(query.getUniques(), filterFields, "unique");
-        checkFiltersExistInSelectionType(query.getCounts(), filterFields, "count");
+        checkFiltersExistInSelectionType(query.getLists(), filterFields, "list", query.getId());
+        checkFiltersExistInSelectionType(query.getUniques(), filterFields, "unique", query.getId());
+        checkFiltersExistInSelectionType(query.getCounts(), filterFields, "count", query.getId());
     }
 
-    private void checkFiltersExistInSelectionType(N2oQuery.Selection[] selections, Set<String> filterFields, String selectionType) {
+    private void checkFiltersExistInSelectionType(N2oQuery.Selection[] selections, Set<String> filterFields, String selectionType, String queryId) {
         if (selections != null) {
             for (N2oQuery.Selection s : selections) {
                 if (s.getFilters() != null) {
                     for (String filter : s.getFilters()) {
                         if (!filterFields.contains(filter))
-                            throw new N2oMetadataValidationException(String.format("<%s> ссылается на несуществующий фильтр %s", selectionType, filter));
+                            throw new N2oMetadataValidationException(String.format("<%s> ссылается на несуществующий фильтр %s в выборке %s",
+                                    selectionType,
+                                    ValidationUtils.getIdOrEmptyString(filter),
+                                    ValidationUtils.getIdOrEmptyString(queryId)));
                     }
                 }
             }
