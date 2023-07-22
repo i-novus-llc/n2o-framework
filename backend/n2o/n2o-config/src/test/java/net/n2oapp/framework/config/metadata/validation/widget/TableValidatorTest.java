924b624a53cae8ed379cc2744d5e86023ad6b2ef
Мукминов Наиль Фаритович <nmukminov@i-novus.ru>
2023-07-23 00:52:47 +0300

    [NNO-9760] validated

---
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/validation/widget/TableValidatorTest.java
@@ -0,0 +1,56 @@
+package net.n2oapp.framework.config.metadata.validation.widget;
+
+import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
+import net.n2oapp.framework.config.N2oApplicationBuilder;
+import net.n2oapp.framework.config.metadata.pack.N2oAllDataPack;
+import net.n2oapp.framework.config.metadata.pack.N2oPagesPack;
+import net.n2oapp.framework.config.metadata.pack.N2oRegionsPack;
+import net.n2oapp.framework.config.metadata.pack.N2oWidgetsPack;
+import net.n2oapp.framework.config.metadata.validation.standard.widget.TableValidator;
+import net.n2oapp.framework.config.test.SourceValidationTestBase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TableValidatorTest extends SourceValidationTestBase {
+
+    @Override
+    @BeforeEach
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void configure(N2oApplicationBuilder builder) {
+        super.configure(builder);
+        builder.packs(
+                new N2oPagesPack(),
+                new N2oRegionsPack(),
+                new N2oWidgetsPack(),
+                new N2oAllDataPack()
+        );
+        builder.validators(
+                new TableValidator()
+        );
+    }
+
+    @Test
+    void testDuplicateColumnIds() {
+        N2oMetadataValidationException exception = assertThrows(
+                N2oMetadataValidationException.class,
+                () -> validate("net/n2oapp/framework/config/metadata/validation/widget/testDuplicateColumnIds.widget.xml")
+        );
+        assertEquals("Таблица 'testDuplicateColumnIds' содержит повторяющиеся значения id=\"id1\" в <column>", exception.getMessage());
+    }
+
+    @Test
+    void testDuplicateColumnTextFieldId() {
+        N2oMetadataValidationException exception = assertThrows(
+                N2oMetadataValidationException.class,
+                () -> validate("net/n2oapp/framework/config/metadata/validation/widget/testDuplicateColumnTextFieldId.widget.xml")
+        );
+        assertEquals("Таблица 'testDuplicateColumnTextFieldId' содержит повторяющиеся значения text-field-id=\"c1\" в <column>", exception.getMessage());
+    }
+}
