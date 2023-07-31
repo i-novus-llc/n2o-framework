605b29ccfdd0e75c5eb35f31adb21a5de0999f52
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2023-07-31 13:33:09 +0300

    Merge branch 'bf/NNO-9852' into 'master'
    bf/NNO-9852 - Не применяется кастомный src к строкам таблицы
    
    Closes NNO-9852
    
    See merge request open/n2o-framework!5887
    
    (cherry picked from commit 3755e1a4b9bcc83523bc4ebccd8548f3d0decf28)
    
    2c2b1d2a bf/NNO-9852 - Добалена обработка кастомной строки в таблице

--- frontend/n2o-framework/src/components/Table/components/RowResolver.tsx
+++ frontend/n2o-framework/src/components/Table/components/RowResolver.tsx
@@ -4,12 +4,12 @@ import { RowResolverProps } from '../types/props'
 import { SelectionType } from '../enum'
 import { useTableActions } from '../provider/TableActions'
 
-import Table from './basic'
 import { CellContainer } from './CellContainer'
+import { DataRow } from './DataRow'
 
 export const RowResolver: VFC<RowResolverProps> = (props) => {
     const {
-        component: CustomRowComponent,
+        component: RowComponent = DataRow,
         elementAttributes,
         data,
         treeDeepLevel,
@@ -39,70 +39,41 @@ export const RowResolver: VFC<RowResolverProps> = (props) => {
         setFocusOnRow(data.id, data)
     }, [setFocusOnRow])
 
-    const onRowClick = useCallback((data) => {
-        if (hasSelection) {
-            onSelection(data)
-        }
-
-        if (hasRowAction) {
-            onClickRowAction(data)
-        }
-    }, [hasSelection, hasRowAction, onClickRowAction, onSelection])
-
     const mergedStyle = useMemo(() => ({
         '--deep-level': treeDeepLevel,
         ...style,
     }), [treeDeepLevel, style])
 
-    const cellsComponents = useMemo(() => (
-        cells.map(({
-            elementAttributes,
-            ...cellProps
-        }, index) => (
-            <CellContainer
-                key={cellProps.id}
-                cellIndex={index}
-                hasExpandedButton={hasExpandedButton}
-                isSelectedRow={isSelectedRow}
-                model={data}
-                rowValue={rowValue}
-                isTreeExpanded={isTreeExpanded}
-                {...elementAttributes}
-                {...cellProps}
-            />
-        ))
-    ), [cells, data, hasExpandedButton, isSelectedRow, isTreeExpanded, rowValue])
-    const dataParams = useMemo(() => ({
-        'data-focused': isFocused,
-        'data-has-click': hasSelection || hasRowAction,
-        'data-deep-level': treeDeepLevel,
-    }), [hasRowAction, hasSelection, isFocused, treeDeepLevel])
-
-    return CustomRowComponent ? (
-        <CustomRowComponent
+    return (
+        <RowComponent
             {...otherProps}
+            {...otherElementAttributes}
             selection={selection}
             data={data}
-            elementAttributes={otherElementAttributes}
             onClick={hasRowAction ? onClickRowAction : undefined}
             onSelection={hasSelection ? onSelection : undefined}
             style={mergedStyle}
-            isSelectedRow={isSelectedRow}
-            isFocused={isFocused}
-            treeDeepLevel={treeDeepLevel}
-            dataParams={dataParams}
-        >
-            {cellsComponents}
-        </CustomRowComponent>
-    ) : (
-        <Table.Row
-            {...otherElementAttributes}
-            onClick={() => onRowClick(data)}
-            style={mergedStyle}
+            data-focused={isFocused}
+            data-has-click={hasSelection || hasRowAction}
+            data-deep-level={treeDeepLevel}
             data-selected={isSelectedRow}
-            {...dataParams}
         >
-            {cellsComponents}
-        </Table.Row>
+            {cells.map(({
+                elementAttributes,
+                ...cellProps
+            }, index) => (
+                <CellContainer
+                    key={cellProps.id}
+                    cellIndex={index}
+                    hasExpandedButton={hasExpandedButton}
+                    isSelectedRow={isSelectedRow}
+                    model={data}
+                    rowValue={rowValue}
+                    isTreeExpanded={isTreeExpanded}
+                    {...elementAttributes}
+                    {...cellProps}
+                />
+            ))}
+        </RowComponent>
     )
 }
