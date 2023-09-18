06fdc447a88c019525aae8cb1dd1edc902b1663b
Бурнашов Павел Константинович <pburnashov@i-novus.ru>
2023-09-18 11:22:36 +0300

    cf/NNO-9749 - Отказ от отдельного хранилища для колонок

--- frontend/n2o-framework/src/components/widgets/AdvancedTable/index.jsx
+++ frontend/n2o-framework/src/components/widgets/AdvancedTable/index.jsx
@@ -5,6 +5,7 @@ import omit from 'lodash/omit'
 import set from 'lodash/set'
 import get from 'lodash/get'
 import { compose } from 'recompose'
+import { createContext, useContext as useContextSelector } from 'use-context-selector'
 
 import { N2OPagination } from '../Table/N2OPagination'
 import WidgetLayout from '../StandardWidget'
@@ -13,7 +14,6 @@ import { WidgetHOC } from '../../../core/widget/WidgetHOC'
 import { FactoryContext } from '../../../core/factory/context'
 import { dataSourceModelByPrefixSelector } from '../../../ducks/datasource/selectors'
 import { ModelPrefix } from '../../../core/datasource/const'
-import { getContainerColumns } from '../../../ducks/columns/selectors'
 import { Selection, TableActions, TableContainer } from '../../Table'
 import { useCheckAccess } from '../../../core/auth/SecurityController'
 import { withSecurityList } from '../../../core/auth/withSecurity'
@@ -21,8 +21,11 @@ import { EMPTY_ARRAY } from '../../../utils/emptyTypes'
 
 import { useExpandAllRows } from './hooks/useExpandAllRows'
 import { useResolveCellsVisible } from './hooks/useResolveCellsVisible'
-import { useRegisterColumns } from './hooks/useRegisterColumns'
+import { useColumnsState } from './hooks/useColumnsState'
 import { useTableActionReactions } from './hooks/useTableActionReactions'
+import { VoidResolveColumnConditions } from './voidComponents/VoidResolveColumnConditions'
+
+const tableWidgetContext = createContext(null)
 
 const EmptyComponent = () => (
     <div className="d-flex justify-content-center text-muted">Нет данных для отображения</div>
@@ -34,8 +37,10 @@ const AdvancedTableContainer = (props) => {
         style, paging, filter, table, size, count, page, sorting, children, hasNext, isInit,
         setResolve,
     } = props
+    const { resolveProps } = useContext(FactoryContext)
+
     const [expandedRows, setExpandedRows] = useState([])
-    const columnsState = useSelector(getContainerColumns(id))
+
     const datasourceModel = useSelector(dataSourceModelByPrefixSelector(datasource, ModelPrefix.source))
     const selectedRows = useSelector((state) => {
         const models = dataSourceModelByPrefixSelector(datasource, ModelPrefix.selected)(state) || EMPTY_ARRAY
@@ -46,19 +51,19 @@ const AdvancedTableContainer = (props) => {
 
         return EMPTY_ARRAY
     })
-
     const focusedRowValue = useSelector((state) => {
         const model = dataSourceModelByPrefixSelector(datasource, ModelPrefix.active)(state)
 
         return model ? model.id : null
     })
 
-    const { resolveProps } = useContext(FactoryContext)
-    const resolvedFilter = useMemo(() => resolveProps(filter, StandardFieldset), [filter, resolveProps])
     const cells = useMemo(() => ({
         header: table.header.cells.map(cell => resolveProps(cell)),
         body: table.body.cells.map(cell => resolveProps(cell)),
     }), [table.header.cells, table.body.cells, resolveProps])
+    const resolvedFilter = useMemo(() => resolveProps(filter, StandardFieldset), [filter, resolveProps])
+
+    const [columnsState, changeColumnParam] = useColumnsState(cells.header)
     const resolvedCells = useResolveCellsVisible(cells, columnsState)
 
     const tableConfig = useMemo(() => {
@@ -74,7 +79,7 @@ const AdvancedTableContainer = (props) => {
         return config
     }, [resolveProps, table])
 
-    const paginationVisible = useMemo(() => Object.values(columnsState).some(column => column.visible), [columnsState])
+    const paginationVisible = useMemo(() => columnsState.some(column => column.visible), [columnsState])
     const hasSecurityAccess = useCheckAccess(tableConfig.body?.row?.security)
 
     const { place = 'bottomLeft' } = paging
@@ -144,40 +149,45 @@ const AdvancedTableContainer = (props) => {
         }
     }, [datasourceModel, setResolve, isNeedSetResolveModel])
 
-    useRegisterColumns(id, cells.header)
     useExpandAllRows(setExpandedRows, children, datasourceModel)
 
     return (
-        <WidgetLayout
-            disabled={disabled}
-            widgetId={id}
-            datasource={datasource}
-            toolbar={toolbar}
-            filter={resolvedFilter}
-            className={className}
-            style={style}
-            fetchData={fetchData}
-            loading={loading}
-            {...pagination}
-        >
-            {isInit ? (
-                <TableContainer
-                    actionListener={actionListener}
-                    hasSecurityAccess={hasSecurityAccess}
-                    childrenToggleState={children}
-                    sorting={sorting}
-                    data={datasourceModel}
-                    cells={resolvedCells}
-                    tableConfig={tableConfig}
-                    id={id}
-                    isTextWrap={table.textWrap}
-                    selectedRows={selectedRows}
-                    expandedRows={expandedRows}
-                    focusedRowValue={focusedRowValue}
-                    EmptyContent={<EmptyComponent />}
-                />
-            ) : null}
-        </WidgetLayout>
+        <tableWidgetContext.Provider value={{ changeColumnParam, columnsState }}>
+            <VoidResolveColumnConditions
+                columnsState={columnsState}
+                changeColumnParam={changeColumnParam}
+            />
+            <WidgetLayout
+                disabled={disabled}
+                widgetId={id}
+                datasource={datasource}
+                toolbar={toolbar}
+                filter={resolvedFilter}
+                className={className}
+                style={style}
+                fetchData={fetchData}
+                loading={loading}
+                {...pagination}
+            >
+                {isInit ? (
+                    <TableContainer
+                        actionListener={actionListener}
+                        hasSecurityAccess={hasSecurityAccess}
+                        childrenToggleState={children}
+                        sorting={sorting}
+                        data={datasourceModel}
+                        cells={resolvedCells}
+                        tableConfig={tableConfig}
+                        id={id}
+                        isTextWrap={table.textWrap}
+                        selectedRows={selectedRows}
+                        expandedRows={expandedRows}
+                        focusedRowValue={focusedRowValue}
+                        EmptyContent={<EmptyComponent />}
+                    />
+                ) : null}
+            </WidgetLayout>
+        </tableWidgetContext.Provider>
     )
 }
 
@@ -186,3 +196,13 @@ AdvancedTableContainer.displayName = 'AdvancedTableWidget'
 export const AdvancedTableWidget = compose(
     WidgetHOC,
 )(withSecurityList(AdvancedTableContainer, 'table.header.cells'))
+
+export const useTableWidget = () => {
+    const context = useContextSelector(tableWidgetContext)
+
+    if (!context) {
+        throw new Error('useTableWidget must be used with tableWidgetContext')
+    }
+
+    return context
+}
