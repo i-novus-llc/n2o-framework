6ac0813a71c28437bf66dffe80e9ae47977e73bb
Соколов Алексей <asokolov@i-novus.ru>
2023-10-05 14:29:41 +0300

    Resolve NNO-9828 "Fix/ r 7.24"

--- frontend/n2o-framework/src/components/controls/withListContainer.jsx
+++ frontend/n2o-framework/src/components/controls/withListContainer.jsx
@@ -47,8 +47,9 @@ function withListContainer(WrappedComponent) {
          * Совершает вызов апи с параметрами
          * @param optionalParams {object} - дополнительные параметра запроса
          * @param concat {boolean} - флаг добавления новых данных к текущим
+         * @param cacheReset {boolean} - флаг принудительного сбрасывания cache в withFetchData
          */
-        const callApiWithParams = (optionalParams = {}, concat = false) => {
+        const callApiWithParams = (optionalParams = {}, concat = false, cacheReset = false) => {
             const sortId = sortFieldId || valueFieldId || labelFieldId
 
             const params = {
@@ -58,7 +59,7 @@ function withListContainer(WrappedComponent) {
                 ...optionalParams,
             }
 
-            _fetchData(params, concat)
+            _fetchData(params, concat, cacheReset)
         }
 
         /**
