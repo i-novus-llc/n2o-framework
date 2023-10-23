ac6c8c6485cd5066b86fa4b9b01c840cb4e52c71
Alexey <evilsokol@gmail.com>
2023-10-23 13:34:37 +0300

    fix/NNO-10029 added check icon to the word wrap button

--- frontend/n2o-framework/src/components/buttons/WordWrap/WordWrap.jsx
+++ frontend/n2o-framework/src/components/buttons/WordWrap/WordWrap.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useStore } from 'react-redux'
+import get from 'lodash/get'
+import classNames from 'classnames'
 
 import StandardButton from '../StandardButton/StandardButton'
 
 export function WordWrap(props) {
-    return <StandardButton {...props} />
+    const { className } = props
+    const id = get(props, 'action.payload.id')
+
+    const { getState } = useStore()
+    const state = getState()
+
+    const textWrap = get(state, `widgets.${id}.table.textWrap`)
+
+    const icon = textWrap ? 'fa fa-check word-wrap-btn__check' : null
+
+    return <StandardButton {...props} className={classNames(className, 'word-wrap-btn')} icon={icon} />
 }
