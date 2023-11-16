88ae9d76f02daca4d3c645bab87a43f81be08ad6
Лебеденко Игорь <ilebedenko@i-novus.ru>
2023-11-16 23:11:34 +0300

    Merge branch 'sub/NNO-10115' into 'master'

---
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/data/rest/N2oRestLoggingHandler.java
@@ -0,0 +1,16 @@
+package net.n2oapp.framework.engine.data.rest;
+
+import net.n2oapp.framework.api.rest.RestLoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+
+public class N2oRestLoggingHandler implements RestLoggingHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringRestDataProviderEngine.class);
+    @Override
+    public void handleError(Exception e, HttpMethod method, String query, HttpHeaders headers) {
+        log.error("Execution error with REST query: " + query, e);
+    }
+}
