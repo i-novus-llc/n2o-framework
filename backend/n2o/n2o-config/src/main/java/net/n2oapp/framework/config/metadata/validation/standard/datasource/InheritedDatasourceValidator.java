510cea6b6ae8398643fde118bb01066451cb3ead
Мукминов Наиль Фаритович <nmukminov@i-novus.ru>
2023-12-06 12:59:06 +0300

    [NNO-10174] source-datasource attribute existed validation is added + test

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/datasource/InheritedDatasourceValidator.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/datasource/InheritedDatasourceValidator.java
@@ -4,7 +4,10 @@ import net.n2oapp.framework.api.metadata.Source;
 import net.n2oapp.framework.api.metadata.compile.SourceProcessor;
 import net.n2oapp.framework.api.metadata.global.view.page.datasource.N2oInheritedDatasource;
 import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
+import net.n2oapp.framework.config.metadata.validation.standard.ValidationUtils;
 import org.springframework.stereotype.Component;
+
+import static net.n2oapp.framework.config.metadata.validation.standard.ValidationUtils.getIdOrEmptyString;
 
 /**
  * Валидатор источника данных, получающего данные из другого источника данных
@@ -20,19 +23,28 @@ public class InheritedDatasourceValidator extends DatasourceValidator<N2oInherit
     @Override
     public void validate(N2oInheritedDatasource source, SourceProcessor p) {
         super.validate(source, p);
-        checkSourceDatasource(source);
+        checkSourceDatasource(source, p);
     }
 
-    private void checkSourceDatasource(N2oInheritedDatasource source) {
+    private void checkSourceDatasource(N2oInheritedDatasource source, SourceProcessor p) {
         String sourceDatasource = source.getSourceDatasource();
         if (sourceDatasource == null)
             throw new N2oMetadataValidationException(
-                    String.format("В источнике данных '%s' не задан атрибут 'source-datasource'", source.getId())
+                    String.format("В источнике данных %s не задан атрибут 'source-datasource'", getIdOrEmptyString(source.getId()))
             );
         if (sourceDatasource.equals(source.getId()))
             throw new N2oMetadataValidationException(
-                    String.format("Атрибут 'source-datasource' источника данных '%s' совпадает с 'id'",
-                            source.getId())
+                    String.format("Атрибут 'source-datasource' источника данных %s совпадает с 'id'",
+                            getIdOrEmptyString(source.getId()))
             );
+        ValidationUtils.checkDatasourceExistence(
+                sourceDatasource,
+                p,
+                String.format(
+                        "В источнике данных %s атрибут 'source-datasource' ссылается на несуществующий источник данных %s",
+                        getIdOrEmptyString(source.getId()),
+                        getIdOrEmptyString(source.getSourceDatasource())
+                )
+        );
     }
 }
