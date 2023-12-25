910df26be4306a3c472d11fa83c1db9b328007b8
Лебеденко Игорь <ilebedenko@i-novus.ru>
2023-12-25 14:26:58 +0300

    [NNO-10231] - remove old classes

--- backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/modules/stack/DataProcessingStack.java
+++ backend/n2o/n2o-engine/src/main/java/net/n2oapp/framework/engine/modules/stack/DataProcessingStack.java
@@ -18,57 +18,53 @@ public abstract class DataProcessingStack {
     Logger logger = LoggerFactory.getLogger(getClass());
     private volatile List<DataProcessing> stack;
 
-    public void processAction(final ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, final DataSet dataSet) {
+    public void processAction(ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, DataSet dataSet) {
         process(module -> {
             logger.debug("Processing in data-set");
             module.processAction(requestInfo, responseInfo, dataSet);
         });
     }
 
-    public void processActionError(final ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, final DataSet dataSet, N2oException exception) {
+    public void processActionError(ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, DataSet dataSet, N2oException exception) {
         process(module -> {
             logger.debug("Processing action error");
             module.processActionError(requestInfo, responseInfo, dataSet, exception);
         });
     }
 
-    public void processActionResult(final ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, final DataSet dataSet) {
+    public void processActionResult(ActionRequestInfo<DataSet> requestInfo, ActionResponseInfo responseInfo, DataSet dataSet) {
         process(module -> {
             logger.debug("Processing out data-set");
             module.processActionResult(requestInfo, responseInfo, dataSet);
         });
     }
 
-    public void processQuery(final QueryRequestInfo requestInfo, QueryResponseInfo responseInfo) {
+    public void processQuery(QueryRequestInfo requestInfo, QueryResponseInfo responseInfo) {
         process(module -> {
             logger.debug("Processing query");
             module.processQuery(requestInfo, responseInfo);
         });
     }
 
-    public void processQueryError(final QueryRequestInfo requestInfo, QueryResponseInfo responseInfo, final N2oException exception) {
+    public void processQueryError(QueryRequestInfo requestInfo, QueryResponseInfo responseInfo, N2oException exception) {
         process(module -> {
             logger.debug("Processing query error");
             module.processQueryError(requestInfo, responseInfo, exception);
         });
     }
 
-    public void processQueryResult(final QueryRequestInfo requestInfo, QueryResponseInfo responseInfo, final CollectionPage<DataSet> page) {
+    public void processQueryResult(QueryRequestInfo requestInfo, QueryResponseInfo responseInfo, CollectionPage<DataSet> page) {
         process(module -> {
             logger.debug("Processing query-result");
             module.processQueryResult(requestInfo, responseInfo, page);
         });
     }
 
-    private void process(final DataProcessingCallback callback) {
+    private void process(DataProcessingCallback callback) {
         if (stack == null)
             stack = findModules();
         for (final DataProcessing module : stack)
             callback.process(module);
-    }
-
-    private synchronized void initStack() {
-        stack = findModules();
     }
 
     protected abstract List<DataProcessing> findModules();
