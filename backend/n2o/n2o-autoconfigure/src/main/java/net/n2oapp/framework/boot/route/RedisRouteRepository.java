3ea9769a052a78684599e67bb8b529f5d30f1155
Лебеденко Игорь <ilebedenko@i-novus.ru>
2024-03-12 18:19:53 +0300

    [NNO-10520] - fix RedisRouteRepository logger

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/route/RedisRouteRepository.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/route/RedisRouteRepository.java
@@ -1,10 +1,9 @@
-package net.n2oapp.framework.boot.route.jdbc;
+package net.n2oapp.framework.boot.route;
 
+import lombok.extern.slf4j.Slf4j;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.register.route.RouteInfoKey;
 import net.n2oapp.framework.config.register.ConfigRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.HashOperations;
@@ -17,9 +16,8 @@ import java.util.stream.Collectors;
 /**
  * Сохранение и загрузка данных RouteRegister в Redis (key-value) БД
  */
+@Slf4j
 public class RedisRouteRepository implements ConfigRepository<RouteInfoKey, CompileContext> {
-
-    private static final Logger logger = LoggerFactory.getLogger(JDBCRouteRepository.class);
 
     @Autowired
     private RedisTemplate redisTemplate;
@@ -41,7 +39,7 @@ public class RedisRouteRepository implements ConfigRepository<RouteInfoKey, Comp
         hashOperations.put(HASH_KEY, key.getUrlMatching(), value);
 
         String action = contextInHash == null ? "Inserted" : "Updated";
-        logger.info(String.format("%s route: '%s' to [%s]", action, value, key.getUrlMatching()));
+        log.info(String.format("%s route: '%s' to [%s]", action, value, key.getUrlMatching()));
 
         return value;
     }
@@ -53,7 +51,7 @@ public class RedisRouteRepository implements ConfigRepository<RouteInfoKey, Comp
                         row -> new RouteInfoKey(row.getKey(), row.getValue().getCompiledClass()),
                         Map.Entry::getValue, (a, b) -> b)
                 );
-        logger.info(String.format("Returned %s routes.", result.size()));
+        log.info(String.format("Returned %s routes.", result.size()));
         return result;
     }
 
