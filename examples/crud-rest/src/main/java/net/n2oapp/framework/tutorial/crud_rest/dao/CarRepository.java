2eefe75ed192a7bea88433e981f994d0f8e74336
Лебеденко Игорь <ilebedenko@i-novus.ru>
2024-07-09 14:16:13 +0300

    fix crud-rest example

--- examples/crud-rest/src/main/java/net/n2oapp/framework/tutorial/crud_rest/dao/CarRepository.java
+++ examples/crud-rest/src/main/java/net/n2oapp/framework/tutorial/crud_rest/dao/CarRepository.java
@@ -4,6 +4,7 @@ import net.n2oapp.framework.tutorial.crud_rest.model.Car;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
