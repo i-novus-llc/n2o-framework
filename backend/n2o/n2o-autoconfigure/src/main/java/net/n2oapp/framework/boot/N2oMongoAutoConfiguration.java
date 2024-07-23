f7288055458fbc7198d0f6b373a941c1c7a9ef80
Лебеденко Игорь <ilebedenko@i-novus.ru>
2024-07-23 20:14:58 +0300

    fix date serializing

--- backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oMongoAutoConfiguration.java
+++ backend/n2o/n2o-autoconfigure/src/main/java/net/n2oapp/framework/boot/N2oMongoAutoConfiguration.java
@@ -1,13 +1,11 @@
 package net.n2oapp.framework.boot;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import jakarta.annotation.PreDestroy;
 import net.n2oapp.framework.boot.mongodb.MongoDbDataProviderEngine;
-import net.n2oapp.framework.engine.data.rest.RestEngineTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -15,7 +13,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
-import java.text.SimpleDateFormat;
+import static net.n2oapp.framework.boot.ObjectMapperConstructor.dataObjectMapper;
 
 @AutoConfiguration
 @ConditionalOnClass(MongoClient.class)
@@ -24,9 +22,6 @@ public class N2oMongoAutoConfiguration {
     @Value("${n2o.engine.mongodb.connection_url:}")
     private String connectionUrl;
 
-    @Value("${n2o.engine.mongodb.dateformat.serialize}")
-    private String serializingFormat;
-
 
     @Deprecated
     private MongoClient mongo;
@@ -34,8 +29,7 @@ public class N2oMongoAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public MongoDbDataProviderEngine mongoDbDataProviderEngine() {
-        MongoDbDataProviderEngine mongoDbDataProviderEngine = new MongoDbDataProviderEngine(mongoObjectMapper());
-        return mongoDbDataProviderEngine;
+        return new MongoDbDataProviderEngine(dataObjectMapper());
     }
 
     @PreDestroy
@@ -54,13 +48,5 @@ public class N2oMongoAutoConfiguration {
                 .build();
         mongo = MongoClients.create(mongoClientSettings);
         return mongo;
-    }
-
-    private ObjectMapper mongoObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setDateFormat(new SimpleDateFormat(serializingFormat));
-        RestEngineTimeModule module = new RestEngineTimeModule();
-        objectMapper.registerModules(module);
-        return objectMapper;
     }
 }
