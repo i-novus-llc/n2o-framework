3b88c44d40fa9f5576164ffc0617fef7001512f6
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2024-08-15 13:14:38 +0300

    fix/NNO-10902

--- frontend/n2o-framework/src/ducks/models/sagas.ts
+++ frontend/n2o-framework/src/ducks/models/sagas.ts
@@ -1,101 +1,66 @@
 import { put, select, takeEvery } from 'redux-saga/effects'
 import get from 'lodash/get'
-import set from 'lodash/set'
-import values from 'lodash/values'
-import includes from 'lodash/includes'
-import merge from 'lodash/merge'
-import entries from 'lodash/entries'
 import isObject from 'lodash/isObject'
 
 import { EffectWrapper } from '../api/utils/effectWrapper'
 import { executeExpression } from '../../core/Expression/execute'
 
-import type { State as ModelsState } from './Models'
 import { copyModel, setModel, updateModel } from './store'
-import { getModelByPrefixAndNameSelector, modelsSelector } from './selectors'
+import { getModelByPrefixAndNameSelector, Model } from './selectors'
 import { CopyAction } from './Actions'
 
 export function* copyAction({ payload, meta = {} }: CopyAction) {
     const { target, source, mode = 'replace', sourceMapper: expression } = payload
-    const state: ModelsState = yield select(modelsSelector)
-    let sourceModel = get(state, values(source).join('.'))
-    const selectedTargetModel: object[] = yield select(getModelByPrefixAndNameSelector(target.prefix, target.key))
-    const targetModel = selectedTargetModel || []
+
+    const targetModel: Model | Model[] = yield select(getModelByPrefixAndNameSelector(target.prefix, target.key))
+    let targetField = target.field ? get(targetModel, target.field) : targetModel
+    const sourceModel: Model | Model[] = yield select(getModelByPrefixAndNameSelector(source.prefix, source.key))
+    let sourceField = source.field ? get(sourceModel, source.field) : sourceModel
+
     let newModel
-
-    const { field } = target
-    const targetModelField = get(targetModel, field, [])
-
-    const treePath = includes(target.field, '.')
 
     if (expression) {
         const { evalContext } = meta
 
-        sourceModel = executeExpression(expression, sourceModel, evalContext)
+        sourceField = executeExpression(expression, sourceField, evalContext)
     }
 
-    if (mode === 'merge') {
-        if (!target.field) {
-            newModel = { ...targetModel, ...sourceModel }
-        } else if (isObject(sourceModel) || Array.isArray(sourceModel)) {
-            newModel = {
-                ...targetModel,
-                [target.field]: {
-                    ...targetModelField,
-                    ...sourceModel,
-                },
-            }
-        } else {
-            newModel = {
-                ...targetModel,
-                [target.field]: sourceModel,
-            }
+    switch (mode) {
+        case 'merge': {
+            newModel = isObject(sourceField)
+                ? { ...targetField, ...sourceField }
+                : sourceField
+
+            break
         }
-    } else if (mode === 'add') {
-        if (!Array.isArray(sourceModel) || !Array.isArray(targetModelField)) {
-            // eslint-disable-next-line no-console
-            console.warn('Source or target is not an array!')
-        }
-
-        if (!Array.isArray(sourceModel)) {
-            /* the key by which the data is copied to the target model */
-            if (field) {
-                newModel = { ...targetModel, [field]: targetModelField.concat(sourceModel) }
-            } else {
-                newModel = targetModel.concat(sourceModel)
+        case 'add': {
+            if (!targetField) {
+                targetField = []
             }
-        } else {
-            sourceModel = Object.values(sourceModel)
+            if (!Array.isArray(targetField)) {
+                // eslint-disable-next-line no-console
+                console.warn('Source or target is not an array!')
 
-            newModel = target.field
-                ? {
-                    ...targetModel,
-                    [target.field]: [...targetModelField, ...sourceModel],
-                }
-                : [...targetModelField, ...sourceModel]
-        }
-    } else if (treePath) {
-        newModel = merge({}, targetModel)
-        set(newModel, target.field, sourceModel)
-    } else {
-        newModel = target.field
-            ? {
-                ...targetModel,
-                [target.field]: sourceModel,
+                return
             }
-            : sourceModel
-    }
 
-    yield put(setModel(target.prefix, target.key, newModel))
+            newModel = Array.isArray(sourceField)
+                ? [...targetField, ...sourceField]
+                : [...targetField, sourceField]
 
-    // костыль, тк я убрал резолв зависимостей на redux-form/INITIALIZE из-за беспорядочных вызовов,
-    // которые приводили к зацикливанию при applyOnInit: true и переинициализации, зависимости перестали вызываться
-    // после копирования
-    for (const [field, value] of entries(newModel)) {
-        if (get(targetModel, field) !== value) {
-            yield put(updateModel(target.prefix, target.key, field, value))
+            break
+        }
+        case 'replace':
+        default: {
+            newModel = sourceField
         }
     }
+
+    yield put(
+        target.field
+            ? updateModel(target.prefix, target.key, target.field, newModel)
+            : setModel(target.prefix, target.key, newModel),
+    )
 }
 
 export const modelSagas = [
