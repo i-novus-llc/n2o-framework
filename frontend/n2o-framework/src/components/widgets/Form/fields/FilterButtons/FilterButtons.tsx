d47cacb56c72b0f722810ff31ce70b281fe1bd88
Alexei Sokolov <asokolov@i-novus.ru>
2024-09-03 14:39:29 +0300

    fix/NNO-10938 filter clear button fix

--- frontend/n2o-framework/src/components/widgets/Form/fields/FilterButtons/FilterButtons.tsx
+++ frontend/n2o-framework/src/components/widgets/Form/fields/FilterButtons/FilterButtons.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import isEmpty from 'lodash/isEmpty'
 import { ButtonGroup } from 'reactstrap'
+
+import { isEmptyModel } from '../../../../../utils/isEmptyModel'
 
 import { FilterSearchButton, Props as SearchProps } from './FilterSearchButton/FilterSearchButton'
 import { FilterClearButton, Props as ClearProps } from './FilterClearButton/FilterClearButton'
@@ -18,7 +19,7 @@ export function FilterButtons({ search, clear, model, visible = true }: Props) {
     return (
         <ButtonGroup>
             <FilterSearchButton {...search} />
-            <FilterClearButton {...clear} disabled={isEmpty(model)} />
+            <FilterClearButton {...clear} disabled={isEmptyModel(model)} />
         </ButtonGroup>
     )
 }
