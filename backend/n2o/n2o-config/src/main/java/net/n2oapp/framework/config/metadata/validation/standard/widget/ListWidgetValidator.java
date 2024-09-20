8447f9d4c199377b94442a49a07b3ba6cd9b39be
Жегалова Екатерина Александровна <ezhegalova@i-novus.ru>
2024-09-20 16:09:13 +0300

    fix/NNO-10922 Исправлена валидация пейджинга. Добавлена проверка на null

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/widget/ListWidgetValidator.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/validation/standard/widget/ListWidgetValidator.java
@@ -2,6 +2,7 @@ package net.n2oapp.framework.config.metadata.validation.standard.widget;
 
 import net.n2oapp.framework.api.metadata.compile.SourceProcessor;
 import net.n2oapp.framework.api.metadata.global.view.widget.N2oAbstractListWidget;
+import net.n2oapp.framework.api.metadata.global.view.widget.table.N2oPagination;
 import net.n2oapp.framework.api.metadata.global.view.widget.table.ShowCountType;
 import net.n2oapp.framework.api.metadata.validation.exception.N2oMetadataValidationException;
 
@@ -10,9 +11,10 @@ public abstract class ListWidgetValidator<T extends N2oAbstractListWidget> exten
     @Override
     public void validate(T source, SourceProcessor p) {
         super.validate(source, p);
-        if (source.getPagination() != null &&
-                Boolean.FALSE.equals(source.getPagination().getShowLast()) &&
-                ShowCountType.ALWAYS == source.getPagination().getShowCount())
+        N2oPagination pagination = source.getPagination();
+        if (pagination != null &&
+                Boolean.FALSE.equals(pagination.getShowLast()) &&
+                (pagination.getShowCount() == ShowCountType.ALWAYS || pagination.getShowCount() == null))
             throw new N2oMetadataValidationException(
                     String.format("Используется некорректная комбинация атрибутов 'show-last=\"false\"' и 'show-count=\"always\"' пагинации виджета '%s'", source.getId()));
     }
