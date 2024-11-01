93f4c26574ead07ec6f50540c0025a477f420253
Соколов Алексей <asokolov@i-novus.ru>
2024-11-01 12:33:03 +0300

    fix/NNO-11071 responseFieldId fix

---
+++ frontend/n2o-framework/src/components/controls/FileUploader/File.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames'
+import React, { LegacyRef } from 'react'
+
+export interface Props {
+    forwardedRef?: LegacyRef<HTMLSpanElement>
+    name: string
+    link: string
+    id: string
+    error: string
+}
+
+export function File({ forwardedRef, name, link, id, error }: Props) {
+    return (
+        <a
+            title={name}
+            href={link}
+            /* eslint-disable-next-line react/jsx-no-target-blank */
+            target="_blank"
+            id={`tooltip-${id}`}
+            className={classNames('n2o-file-uploader-link', { 'n2o-file-uploader-item-error': error })}
+            rel="noreferrer"
+        >
+            <span ref={forwardedRef} className="n2o-file-uploader-file-name" title="">{name}</span>
+            {link && <i className=" n2o-file-uploader-external-link fa fa-external-link" />}
+        </a>
+    )
+}
