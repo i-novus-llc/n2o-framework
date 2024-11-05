8b3108a9dcdcf2d3d3f45d59d183fb6520b2be43
Лебеденко Игорь <ilebedenko@i-novus.ru>
2024-11-05 13:22:28 +0300

    Add access properties for simple-page too

--- backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/view/SandboxPermissionApi.java
+++ backend/n2o/n2o-sandbox/src/main/java/net/n2oapp/framework/sandbox/view/SandboxPermissionApi.java
@@ -1,7 +1,6 @@
 package net.n2oapp.framework.sandbox.view;
 
 import net.n2oapp.framework.access.simple.PermissionApi;
-import net.n2oapp.framework.api.context.ContextProcessor;
 import net.n2oapp.framework.api.user.UserContext;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +9,16 @@ import java.util.List;
 @Service
 public class SandboxPermissionApi implements PermissionApi {
 
-    private ContextProcessor contextProcessor;
-
     @Override
+    @SuppressWarnings("unchecked")
     public boolean hasPermission(UserContext userContext, String permission) {
-        return ((List<String>)userContext.get("permissions")).contains(permission);
+        return ((List<String>) userContext.get("permissions")).contains(permission);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean hasRole(UserContext userContext, String roles) {
-        return ((List<String>)userContext.get("roles")).contains(roles);
+        return ((List<String>) userContext.get("roles")).contains(roles);
     }
 
     @Override
