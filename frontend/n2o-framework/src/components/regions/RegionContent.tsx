b7b1a58a9cdb75fc457682ea52255e2d0311277d
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2025-01-21 14:49:18 +0300

    Merge branch 'fix/NNO-11234' into 'master'

--- frontend/n2o-framework/src/components/regions/RegionContent.tsx
+++ frontend/n2o-framework/src/components/regions/RegionContent.tsx
@@ -66,7 +66,7 @@ export function RegionContent({
                         className={className}
                         fetchOnInit={fetchOnInit}
                         fetch={fetch}
-                        parent={parent || { regionId, tabId }}
+                        parent={parent || (tabId ? { regionId, tabId } : {})}
                     />
                 )
             })}
