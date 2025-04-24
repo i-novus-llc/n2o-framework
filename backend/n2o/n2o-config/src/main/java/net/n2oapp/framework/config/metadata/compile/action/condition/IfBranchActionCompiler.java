1e358370309f03d5b1fad3bb9e547723e12c3ebe
Барышева Розалина Илдаровна <rgalina@i-novus.ru>
2025-04-24 18:43:04 +0300

    NNO-11563 Неправильно формируется ссылка на parent-datasource для действия if

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/condition/IfBranchActionCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/action/condition/IfBranchActionCompiler.java
@@ -2,13 +2,12 @@ package net.n2oapp.framework.config.metadata.compile.action.condition;
 
 import net.n2oapp.framework.api.exception.N2oException;
 import net.n2oapp.framework.api.metadata.Source;
-import net.n2oapp.framework.api.metadata.compile.CompileContext;
-import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.action.ifelse.N2oConditionBranch;
 import net.n2oapp.framework.api.metadata.action.ifelse.N2oIfBranchAction;
+import net.n2oapp.framework.api.metadata.compile.CompileContext;
+import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
 import net.n2oapp.framework.api.metadata.meta.action.condition.ConditionActionPayload;
 import net.n2oapp.framework.config.metadata.compile.PageIndexScope;
-import net.n2oapp.framework.config.metadata.compile.page.PageScope;
 import org.springframework.stereotype.Component;
 
 import static net.n2oapp.framework.api.metadata.local.util.CompileUtil.castDefault;
@@ -35,7 +34,8 @@ public class IfBranchActionCompiler extends BaseConditionActionCompiler<N2oIfBra
     }
 
     private void initDatasource(ConditionActionPayload payload, N2oIfBranchAction source, CompileProcessor p) {
-        payload.setDatasource(getClientDatasourceId(castDefault(source.getDatasourceId(), () -> getLocalDatasourceId(p)), p.getScope(PageScope.class).getPageId()));
+        String datasourceId = castDefault(source.getDatasourceId(), () -> getLocalDatasourceId(p));
+        payload.setDatasource(getClientDatasourceId(datasourceId, p));
         if (payload.getDatasource() == null) {
             throw new N2oException(String.format("Источник данных не определен для \"<if-branch>\" с атрибутом 'test=%s'",
                     source.getTest()));
