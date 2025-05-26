745ffbac0bb16e6f40487137be996758560bcd22
Alexei Sokolov <asokolov@i-novus.ru>
2025-05-26 13:44:24 +0300

    fix/NNO-11710 edit cell layout fix

--- frontend/n2o-framework/src/components/widgets/Table/cells/EditableCell/VIew.tsx
+++ frontend/n2o-framework/src/components/widgets/Table/cells/EditableCell/VIew.tsx
@@ -4,10 +4,7 @@ import React, { FC, HTMLAttributes } from 'react'
 type ViewProps = Pick<HTMLAttributes<HTMLDivElement>, 'onClick' | 'className'>
 
 export const View: FC<ViewProps> = ({ children, className, onClick }) => (
-    <div
-        className={classNames('n2o-editable-cell-text', className)}
-        onClick={onClick}
-    >
-        {children}
+    <div className={classNames('n2o-editable-cell-text', className)} onClick={onClick}>
+        <span className="n2o-snippet">{children}</span>
     </div>
 )
