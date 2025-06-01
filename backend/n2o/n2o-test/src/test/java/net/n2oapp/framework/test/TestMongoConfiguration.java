427cbf8b1269d5bc26e840f47a09079e1e49d5af
Арифуллин Артур Фаридович <aarifullin@i-novus.ru>
2025-06-01 12:06:41 +0300

    enable MongodbDataProviderEngineTest

--- backend/n2o/n2o-test/src/test/java/net/n2oapp/framework/test/TestMongoConfiguration.java
+++ backend/n2o/n2o-test/src/test/java/net/n2oapp/framework/test/TestMongoConfiguration.java
@@ -1,13 +1,29 @@
 package net.n2oapp.framework.test;
 
+import de.flapdoodle.embed.mongo.packageresolver.*;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
+import de.flapdoodle.embed.mongo.transitions.ImmutableMongod;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.PackageOfCommandDistribution;
+import de.flapdoodle.embed.process.config.store.FileSet;
+import de.flapdoodle.embed.process.config.store.FileType;
+import de.flapdoodle.embed.process.config.store.ImmutableFileSet;
+import de.flapdoodle.embed.process.distribution.ArchiveType;
+import de.flapdoodle.os.BitSize;
+import de.flapdoodle.os.CPUType;
+import de.flapdoodle.os.CommonOS;
 import net.n2oapp.criteria.dataset.DataList;
 import net.n2oapp.framework.boot.N2oMongoAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 import java.util.StringJoiner;
+
+import static de.flapdoodle.embed.mongo.packageresolver.AbstractPackageFinder.match;
 
 @TestConfiguration
 @Import({N2oMongoAutoConfiguration.class,
@@ -20,5 +36,45 @@ public class TestMongoConfiguration {
         for (Object o : ids)
             res.add("new ObjectId('" + o + "')");
         return res.toString();
+    }
+
+    @Bean
+    @Primary
+    public Mongod mongodCustom(Mongod mongod, @Value("${de.flapdoodle.mongodb.embedded.version}") String version) {
+
+        ImmutableFileSet fileSet = FileSet.builder()
+                .addEntry(FileType.Executable, Command.MongoD.commandName())
+                .build();
+
+
+        PackageFinderRules mongodPackageRules = PackageFinderRules.builder()
+                .addRules(PackageFinderRule.builder()
+                        .match(match(CommonOS.Linux, BitSize.B64, CPUType.X86)
+                                .andThen(
+                                        DistributionMatch.any(VersionRange.of(version))
+                                ))
+                        .finder(UrlTemplatePackageFinder.builder()
+                                .fileSet(fileSet)
+                                .archiveType(ArchiveType.TGZ)
+                                .urlTemplate("/linux/mongodb-linux-x86_64-ubuntu2204-{version}.tgz")
+                                .isDevVersion(true)
+                                .build())
+                        .build())
+                .addRules(PackageFinderRule.builder()
+                        .match(PlatformMatch.withOs(CommonOS.Windows))
+                        .finder(new WindowsPackageFinder(Command.MongoD))
+                        .build())
+                .build();
+
+        return ((ImmutableMongod) mongod).withPackageOfDistribution(PackageOfCommandDistribution.withDefaults()
+                .withCommandPackageResolver(command -> distribution -> {
+                    switch (command) {
+                        case MongoD:
+                            return mongodPackageRules.packageFor(distribution)
+                                    .orElseThrow(() -> new IllegalArgumentException("could not find package for " + distribution));
+                        default:
+                            throw new IllegalArgumentException("not implemented");
+                    }
+                }));
     }
 }
