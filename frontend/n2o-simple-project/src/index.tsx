b78a14ee575a91e93182b242596394c9334ae39b
Соколов Алексей <asokolov@i-novus.ru>
2025-06-26 14:06:50 +0300

    sub/NNO-11715 deps upd

--- frontend/n2o-simple-project/src/index.tsx
+++ frontend/n2o-simple-project/src/index.tsx
@@ -4,7 +4,7 @@ import N2O from 'n2o-framework'
 import createFactoryConfig from "n2o-framework/lib/core/factory/createFactoryConfig"
 import authProvider from "n2o-framework/lib/core/auth/authProvider"
 
-import 'n2o-framework/dist/n2o.css'
+import 'n2o-framework/dist/styles/n2o.scss'
 import './index.css'
 
 const config = {
