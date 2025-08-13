ec6c3d9e3d0b8086c268dbc9940dcc27dcb6c21a
Соколов Алексей <asokolov@i-novus.ru>
2025-08-13 21:19:00 +0300

    fix/NNO-11975 rating fixes

--- frontend/n2o-components/src/inputs/Rating.tsx
+++ frontend/n2o-components/src/inputs/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { UncontrolledTooltip } from 'reactstrap'
 import classNames from 'classnames'
 import isInteger from 'lodash/isInteger'
@@ -7,13 +7,12 @@ import round from 'lodash/round'
 
 import { TBaseProps } from '../types'
 import { id as generateId } from '../utils/id'
+import { NOOP_FUNCTION } from '../utils/emptyTypes'
 
 import { mapToNum } from './utils'
 
-// Получение численного значения
 const prepareValue = (rating: number | string, half: boolean): number => {
     if (half) {
-        // округление до .5
         return Math.round(+rating / 0.5) * 0.5
     }
 
@@ -21,171 +20,129 @@ const prepareValue = (rating: number | string, half: boolean): number => {
 }
 
 export type Props = TBaseProps & {
-    half: boolean,
-    max: number | string,
-    onChange(value: number): void,
-    rating: number | string,
-    readonly: boolean,
-    showTooltip: boolean,
-    value?: number | string
+    half?: boolean;
+    max?: number | string;
+    onChange?(value: number): void;
+    rating?: number | string;
+    readonly?: boolean;
+    showTooltip?: boolean;
+    value?: number | string;
 }
 
-type RatingState = {
-    rating: Props['rating'],
-    value: number
-}
+export const Rating = ({
+    half = false,
+    max = 5,
+    onChange = NOOP_FUNCTION,
+    rating = 0,
+    readonly = false,
+    showTooltip = false,
+    value = 0,
+    ...rest
+}: Props) => {
+    const idRef = useRef(generateId())
+    const id = idRef.current
 
-// TODO отказаться от rating в параметрах в пользу value для единообразия со всеми полями ввода
-export class Rating extends Component<Props, RatingState> {
-    id: string
+    const initialRating = rating || value
+    const [currentValue, setCurrentValue] = useState<number>(
+        prepareValue(initialRating, half),
+    )
 
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            rating: props.rating,
-            value: prepareValue(props.rating, props.half),
-        }
-        this.id = generateId()
+    useEffect(() => {
+        const externalRating = rating || value
+
+        setCurrentValue(prepareValue(externalRating, half))
+    }, [rating, value, half])
+
+    const handleChange = (newValue: string) => {
+        const numericValue = Number(newValue)
+
+        setCurrentValue(numericValue)
+        onChange(numericValue)
     }
 
-    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<RatingState>): void {
-        const { value } = this.state
-        const { value: propsValue } = this.props
-
-        if (value && propsValue === null) { this.setState({ value: 0 }) }
-    }
-
-    static getDerivedStateFromProps(props: Props, state: RatingState) {
-        const { value } = props
-        const rating = props.rating || value
-
-        if (rating && (rating !== state.rating)) {
-            return { rating, value: prepareValue(rating, props.half) }
-        }
-
-        return null
-    }
-
-    onChangeAndSetState = (value: string) => {
-        const newValue = Number(value)
-        const { onChange } = this.props
-
-        this.setState({ value: newValue, rating: newValue })
-        onChange(newValue)
-    }
-
-    renderTooltip = () => {
-        const { showTooltip } = this.props
-
+    const renderTooltip = () => {
         if (!showTooltip) { return null }
 
-        const { rating } = this.state
-
         return (
-            <UncontrolledTooltip placement="top" target={this.id}>
-                {round(+rating, 2)}
+            <UncontrolledTooltip placement="top" target={id}>
+                {round(currentValue, 2)}
             </UncontrolledTooltip>
         )
     }
 
-    renderNullStar = () => {
-        const { value } = this.state
-        const { readonly } = this.props
+    const renderNullStar = () => (
+        <>
+            <input
+                className="rating__input rating__input--none"
+                name={`rating-0-${id}`}
+                id={`rating-0-${id}`}
+                value="0"
+                type="radio"
+                onClick={() => !readonly && handleChange('0')}
+                checked={eq(0, currentValue)}
+                readOnly={readonly}
+            />
+            <label
+                className={classNames('rating__label', {
+                    'rating__label--no-pointer': readonly,
+                })}
+                htmlFor={`rating-0-${id}`}
+                aria-label="null-rating"
+            >
+                &nbsp;
+            </label>
+        </>
+    )
 
-        /* eslint-disable react/jsx-one-expression-per-line */
-        return (
-            <>
-                <input
-                    className="rating__input rating__input--none"
-                    name={`rating-0-${this.id}`}
-                    id={`rating-0-${this.id}`}
-                    value="0"
-                    type="radio"
-                    onClick={(e) => {
-                        if (!readonly) { this.onChangeAndSetState((e.target as HTMLInputElement).value) }
-                    }}
-                    checked={eq(0, value)}
-                />
-                <label
-                    className={classNames('rating__label', {
-                        'rating__label--no-pointer': readonly,
+    const renderStar = (index: number) => (
+        <React.Fragment key={index}>
+            <input
+                className={readonly ? 'rating__input--readonly' : 'rating__input'}
+                name={`rating-${index}-${id}`}
+                id={`rating-${index}-${id}`}
+                value={index}
+                type="radio"
+                onClick={() => !readonly && handleChange(String(index))}
+                checked={eq(index, currentValue)}
+                readOnly={readonly}
+            />
+            <label
+                className={classNames('rating__label', {
+                    'rating__label--half': !isInteger(index),
+                    'rating__label--no-pointer': readonly,
+                })}
+                aria-label="rating"
+                htmlFor={`rating-${index}-${id}`}
+            >
+                <i
+                    className={classNames('rating__icon rating__icon--star fa', {
+                        'fa-star-half': !isInteger(index),
+                        'fa-star': isInteger(index),
                     })}
-                    htmlFor={`rating-0-${this.id}`}
-                    aria-label="null-rating"
-                >
+                />
+            </label>
+        </React.Fragment>
+    )
 
-                    &nbsp;
-                </label>
-            </>
-        )
+    const starOptions = {
+        increment: half ? 0.5 : 1,
+        start: half ? 0.5 : 1,
+        inclusive: true,
     }
 
-    renderStars = (index: number) => {
-        const { value } = this.state
-        const { readonly } = this.props
-
-        return (
-            <>
-                <label
-                    className={classNames('rating__label', {
-                        'rating__label--half': !isInteger(index),
-                        'rating__label--no-pointer': readonly,
-                    })}
-                    aria-label="rating"
-                    htmlFor={`rating-${index}-${this.id}`}
-                >
-                    <i
-                        className={classNames('rating__icon rating__icon--star fa', {
-                            'fa-star-half': !isInteger(index),
-                            'fa-star': isInteger(index),
-                        })}
-                    />
-                </label>
-                <input
-                    className={readonly ? 'rating__input--readonly' : 'rating__input'}
-                    name={`rating-${index}-${this.id}`}
-                    id={`rating-${index}-${this.id}`}
-                    value={index}
-                    type="radio"
-                    onClick={(e) => {
-                        if (!readonly) { this.onChangeAndSetState((e.target as HTMLInputElement).value) }
-                    }}
-                    checked={eq(index, value)}
-                />
-            </>
-        )
-    }
-
-    render() {
-        const { max, half, readonly } = this.props
-
-        const options = {
-            increment: half ? 0.5 : 1,
-            start: half ? 0.5 : 1,
-            inclusive: true,
-        }
-
-        return (
-            <div className="n2o-rating-stars">
-                <div
-                    className={readonly ? 'rating-group--readonly' : 'rating-group'}
-                    id={this.id}
-                >
-                    {this.renderNullStar()}
-                    {mapToNum(max, this.renderStars, options)}
-                </div>
-                {this.renderTooltip()}
+    return (
+        <div className="n2o-rating-stars" {...rest}>
+            <div
+                className={classNames({
+                    'rating-group': !readonly,
+                    'rating-group--readonly': readonly,
+                })}
+                id={id}
+            >
+                {renderNullStar()}
+                {mapToNum(max, renderStar, starOptions)}
             </div>
-        )
-    }
-
-    static defaultProps = {
-        value: 0,
-        max: 5,
-        half: false,
-        rating: 0,
-        showTooltip: false,
-        readonly: false,
-        onChange: () => {},
-    } as Props
+            {renderTooltip()}
+        </div>
+    )
 }
