f686c496194bf3ce6d2e042c0634489970f27f65
Лебеденко Игорь <ilebedenko@i-novus.ru>
2025-08-21 14:09:00 +0300

    fix/NNO-12003 При использовании invoke в events в урл попадает null. Задавать...

--- backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/events/BaseEventCompiler.java
+++ backend/n2o/n2o-config/src/main/java/net/n2oapp/framework/config/metadata/compile/events/BaseEventCompiler.java
@@ -1,10 +1,13 @@
 package net.n2oapp.framework.config.metadata.compile.events;
 
-import net.n2oapp.framework.api.metadata.meta.event.Event;
-import net.n2oapp.framework.api.metadata.event.N2oAbstractEvent;
 import net.n2oapp.framework.api.metadata.compile.CompileContext;
 import net.n2oapp.framework.api.metadata.compile.CompileProcessor;
+import net.n2oapp.framework.api.metadata.event.N2oAbstractEvent;
+import net.n2oapp.framework.api.metadata.meta.event.Event;
 import net.n2oapp.framework.config.metadata.compile.BaseSourceCompiler;
+import net.n2oapp.framework.config.metadata.compile.IndexScope;
+
+import static net.n2oapp.framework.api.metadata.local.util.CompileUtil.castDefault;
 
 /**
  * Базовая компиляция события
@@ -12,7 +15,9 @@ import net.n2oapp.framework.config.metadata.compile.BaseSourceCompiler;
 public abstract class BaseEventCompiler<S extends N2oAbstractEvent, D extends Event>
     implements BaseSourceCompiler<D, S, CompileContext<?, ?>> {
 
-    protected void initEvents(Event event, S source, CompileContext<?, ?> context, CompileProcessor p) {
+    protected void initEvents(Event event, S source, CompileProcessor p) {
+        IndexScope idx = p.getScope(IndexScope.class);
+        source.setId(castDefault(source.getId(), () -> "ev" + idx.get()));
         event.setId(source.getId());
     }
 }
