b1ec8aa3ad65331ba3f3e2c002966a8575a2ceec
Alexei Sokolov <asokolov@i-novus.ru>
2025-10-14 16:01:42 +0300

    fix/NNO-12157 encodeURIComponent to the export url

--- frontend/n2o-framework/src/ducks/api/utils/escapeUrl.ts
+++ frontend/n2o-framework/src/ducks/api/utils/escapeUrl.ts
@@ -1,10 +1,3 @@
-const ESCAPED_SPACE = '%26'
-const ESCAPED_LEFT_BRACKET = '%5B'
-const ESCAPED_RIGHT_BRACKET = '%5D'
-
 export function escapeUrl(url: string) {
-    return url
-        .replaceAll('&', ESCAPED_SPACE)
-        .replaceAll('[', ESCAPED_LEFT_BRACKET)
-        .replaceAll(']', ESCAPED_RIGHT_BRACKET)
+    return encodeURIComponent(url)
 }
