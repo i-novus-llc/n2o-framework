466781e04dedfc2e94643bcdb19836193ba30d78
Соколов Алексей <asokolov@i-novus.ru>
2025-12-23 13:14:47 +0300

    fix/NNO-12370 dangerouslySetInnerHTML sanitise

--- frontend/n2o-components/src/display/Html.tsx
+++ frontend/n2o-components/src/display/Html.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import DOMPurify from 'dompurify'
 
 import { TBaseProps } from '../types'
 
@@ -9,4 +10,4 @@ export type Props = TBaseProps & {
 }
 
 // eslint-disable-next-line react/no-danger
-export const Html = ({ id, html, className }: Props) => <div id={id} className={classNames('n2o-html-snippet n2o-snippet', className)} dangerouslySetInnerHTML={{ __html: html }} />
+export const Html = ({ id, html, className }: Props) => <div id={id} className={classNames('n2o-html-snippet n2o-snippet', className)} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }} />
--- frontend/n2o-framework/src/utils/useHtmlResolver.ts
+++ frontend/n2o-framework/src/utils/useHtmlResolver.ts
@@ -1,10 +1,17 @@
 import isEmpty from 'lodash/isEmpty'
 import { useContext } from 'react'
+import DOMPurify from 'dompurify'
 
 import { Model } from '../components/widgets/Form/fields/MarkdownField/helpers'
 import { ExpressionContext } from '../core/Expression/Context'
 import { parseExpression } from '../core/Expression/parse'
 import { executeExpression } from '../core/Expression/execute'
+import { N2O_BUTTON_TAG, N2O_BUTTON_ATTR } from '../components/widgets/Form/fields/MarkdownField/MappedComponents'
+
+export const SANITIZE_CONFIG = {
+    ADD_TAGS: [N2O_BUTTON_TAG],
+    ADD_ATTR: [...N2O_BUTTON_ATTR],
+}
 
 export const useHtmlResolver = (html: string, model: Model) => {
     const evalContext = useContext(ExpressionContext)
@@ -13,9 +20,13 @@ export const useHtmlResolver = (html: string, model: Model) => {
 
     const parsedExpression = parseExpression(html)
 
-    if (!parsedExpression) { return html }
+    if (!parsedExpression) {
+        return DOMPurify.sanitize(html, SANITIZE_CONFIG)
+    }
 
     if (isEmpty(model)) { return null }
 
-    return executeExpression(parsedExpression.replace(/\n/g, '\\n'), model, evalContext)
+    const resolvedHtml = executeExpression(parsedExpression.replace(/\n/g, '\\n'), model, evalContext) as string
+
+    return DOMPurify.sanitize(resolvedHtml, SANITIZE_CONFIG)
 }
