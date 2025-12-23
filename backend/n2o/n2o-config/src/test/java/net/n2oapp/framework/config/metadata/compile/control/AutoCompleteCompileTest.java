c2b2b68f79ea398aee877178d14241b980971e40
ezhegalova <ezhegalova@i-novus.ru>
2025-12-23 16:30:33 +0300

    story/NNO-12379 Добавить динамику для атрибутов `<options>`

--- backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/AutoCompleteCompileTest.java
+++ backend/n2o/n2o-config/src/test/java/net/n2oapp/framework/config/metadata/compile/control/AutoCompleteCompileTest.java
@@ -15,7 +15,6 @@ import net.n2oapp.framework.config.test.SourceCompileTestBase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -45,10 +44,10 @@ class AutoCompleteCompileTest extends SourceCompileTestBase {
     void testAutoComplete() {
         StandardPage page = (StandardPage) compile("net/n2oapp/framework/config/metadata/compile/field/testAutoComplete.page.xml")
                 .get(new PageContext("testAutoComplete"));
-        Form form = (Form) page.getRegions().get("single").get(0).getContent().get(1);
+        Form form = (Form) page.getRegions().get("single").getFirst().getContent().get(1);
 
-        StandardField field = (StandardField) form.getComponent().getFieldsets().get(0).getRows().get(0)
-                .getCols().get(0).getFields().get(0);
+        StandardField<?> field = (StandardField<?>) form.getComponent().getFieldsets().getFirst().getRows().getFirst()
+                .getCols().getFirst().getFields().getFirst();
         AutoComplete autoComplete = (AutoComplete) field.getControl();
         assertThat(autoComplete.getSrc(), is("AutoComplete"));
         assertThat(autoComplete.getDataProvider().getUrl(), is("n2o/data/test"));
@@ -61,31 +60,30 @@ class AutoCompleteCompileTest extends SourceCompileTestBase {
         Map<String, ModelLink> queryMapping = autoComplete.getDataProvider().getQueryMapping();
         assertThat(queryMapping.size(), is(2));
         assertThat(queryMapping.get("id").getValue(), is("`org_id`"));
-        assertThat(queryMapping.get("id").getValue(), is("`org_id`"));
         assertThat(queryMapping.get("id").getLink(), is("models.resolve['testAutoComplete_test']"));
         assertThat(queryMapping.get("id").getDatasource(), is("testAutoComplete_test"));
         assertThat(queryMapping.get("name").getParam(), is("org_name"));
 
         List<ControlDependency> dependencies = field.getDependencies();
         assertThat(dependencies.size(), is(2));
-        assertThat(dependencies.get(0).getType(), is(ValidationTypeEnum.FETCH));
-        assertThat(dependencies.get(0).getOn(), is(Arrays.asList("auto2")));
+        assertThat(dependencies.getFirst().getType(), is(ValidationTypeEnum.FETCH));
+        assertThat(dependencies.getFirst().getOn(), is(List.of("auto2")));
         assertThat(dependencies.get(1).getType(), is(ValidationTypeEnum.RESET));
-        assertThat(dependencies.get(1).getOn(), is(Arrays.asList("org_id")));
+        assertThat(dependencies.get(1).getOn(), is(List.of("org_id")));
 
 
-        autoComplete = (AutoComplete) ((StandardField) form.getComponent().getFieldsets().get(0).getRows().get(1)
-                .getCols().get(0).getFields().get(0)).getControl();
+        autoComplete = (AutoComplete) ((StandardField<?>) form.getComponent().getFieldsets().getFirst().getRows().get(1)
+                .getCols().getFirst().getFields().getFirst()).getControl();
         assertThat(autoComplete.getSrc(), is("AutoComplete"));
-        assertThat(autoComplete.getData().get(0).get("name"), is("test1"));
+        assertThat(autoComplete.getData().getFirst().get("name"), is("`test1`"));
         assertThat(autoComplete.getData().get(1).get("name"), is("test2"));
         assertThat(autoComplete.getValueFieldId(), is("name"));
         assertThat(autoComplete.getTags(), is(false));
         assertThat(autoComplete.getMaxTagTextLength(), is(15));
 
 
-        autoComplete = (AutoComplete) ((StandardField) form.getComponent().getFieldsets().get(0).getRows().get(2)
-                .getCols().get(0).getFields().get(0)).getControl();
+        autoComplete = (AutoComplete) ((StandardField<?>) form.getComponent().getFieldsets().getFirst().getRows().get(2)
+                .getCols().getFirst().getFields().getFirst()).getControl();
         assertThat(autoComplete.getDatasource(), is("testAutoComplete_test"));
         assertThat(autoComplete.getData(), nullValue());
     }
