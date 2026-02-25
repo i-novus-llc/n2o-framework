11bc24a47f52e3c6effaf1a0a79aa23037d2cc06
Жегалова Екатерина <ezhegalova@i-novus.ru>
2026-02-25 10:35:23 +0300

    story/NNO-12568 ValidateActionAT добавить кейсы с вложенным мультисетом

--- backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/action/ValidateActionAT.java
+++ backend/n2o/n2o-autotest/src/test/java/net/n2oapp/framework/autotest/action/ValidateActionAT.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 class ValidateActionAT extends AutoTestBase {
 
     public static final String SHOULD_HAVE_VALUE = "Следует заполнить поле";
+    public static final String SUCCESS_MESSAGE = "Имя и название заполнены верно";
 
     @BeforeAll
     static void beforeClass() {
@@ -80,7 +81,7 @@ class ValidateActionAT extends AutoTestBase {
 
         checkBtn.click();
 
-        page.alerts(Alert.PlacementEnum.TOP).alert(0).shouldHaveText("Имя и название заполнены верно");
+        page.alerts(Alert.PlacementEnum.TOP).alert(0).shouldHaveText(SUCCESS_MESSAGE);
 
         saveBtn.click();
 
@@ -95,7 +96,7 @@ class ValidateActionAT extends AutoTestBase {
         nameField.shouldHaveValidationMessage(Condition.empty);
         birthdayField.shouldHaveValidationMessage(Condition.empty);
         page.alerts(Alert.PlacementEnum.TOP).alert(0).shouldHaveText(SUCCESS_ALERT_MESSAGE);
-        page.alerts(Alert.PlacementEnum.TOP).alert(1).shouldHaveText("Имя и название заполнены верно");
+        page.alerts(Alert.PlacementEnum.TOP).alert(1).shouldHaveText(SUCCESS_MESSAGE);
     }
 
     @Test
@@ -220,5 +221,162 @@ class ValidateActionAT extends AutoTestBase {
 
         button.click();
         alert.shouldHaveText(SUCCESS_ALERT_MESSAGE);
+    }
+
+    @Test
+    void testValidateNestedMultiSet() {
+        builder.sources(
+                new CompileInfo("net/n2oapp/framework/autotest/action/validate/nested_multiset/index.page.xml"),
+                new CompileInfo("net/n2oapp/framework/autotest/action/validate/nested_multiset/test.object.xml")
+        );
+
+        SimplePage page = open(SimplePage.class);
+        page.shouldExists();
+
+        // Валидация всех элементов внешнего списка
+        validateAllOuterBtn(page);
+        Selenide.refresh();
+        // Валидация только первого элемента внешнего списка
+        validateFirstOuterBtn(page);
+        Selenide.refresh();
+        // Валидация только первого элемента внутреннего списка
+        validateFirstInnerBtn(page);
+    }
+
+    private void validateAllOuterBtn(SimplePage page) {
+        FormWidget form = page.widget(FormWidget.class);
+        MultiFieldSet multiSet = form.fieldsets().fieldset(0, MultiFieldSet.class);
+        multiSet.clickAddButton("Добавить во внешний список");
+        multiSet.clickAddButton("Добавить во внешний список");
+
+        StandardField outerField0 = multiSet.item(0).fields().field("0. Внешнее поле");
+        MultiFieldSet innerMultiSet0 = multiSet.item(0).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet0.clickAddButton();
+        innerMultiSet0.clickAddButton();
+        StandardField innerField00 = innerMultiSet0.item(0).fields().field("0.0. Обязательное внутреннее поле");
+        StandardField innerField01 = innerMultiSet0.item(1).fields().field("0.1. Обязательное внутреннее поле");
+
+        StandardField outerField1 = multiSet.item(3).fields().field("1. Внешнее поле");
+        MultiFieldSet innerMultiSet1 = multiSet.item(3).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet1.clickAddButton();
+        innerMultiSet1.clickAddButton();
+        StandardField innerField10 = innerMultiSet1.item(0).fields().field("1.0. Обязательное внутреннее поле");
+        StandardField innerField11 = innerMultiSet1.item(1).fields().field("1.1. Обязательное внутреннее поле");
+
+        StandardButton validateAllOuterBtn = form.toolbar().topLeft().button("validate outerList.*");
+        validateAllOuterBtn.click();
+
+        outerField0.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField00.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField01.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        outerField1.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField10.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField11.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+
+        outerField0.control(InputText.class).setValue("Тест");
+        innerField00.control(InputText.class).setValue("Тест");
+        innerField01.control(InputText.class).setValue("Тест");
+        outerField1.control(InputText.class).setValue("Тест");
+        innerField10.control(InputText.class).setValue("Тест");
+        innerField11.control(InputText.class).setValue("Тест");
+        validateAllOuterBtn.click();
+
+        Alert alert = page.alerts(Alert.PlacementEnum.TOP).alert(0);
+        alert.shouldHaveText(SUCCESS_MESSAGE);
+        outerField0.shouldHaveValidationMessage(Condition.empty);
+        innerField00.shouldHaveValidationMessage(Condition.empty);
+        innerField01.shouldHaveValidationMessage(Condition.empty);
+        outerField1.shouldHaveValidationMessage(Condition.empty);
+        innerField10.shouldHaveValidationMessage(Condition.empty);
+        innerField11.shouldHaveValidationMessage(Condition.empty);
+    }
+
+    private void validateFirstOuterBtn(SimplePage page) {
+        FormWidget form = page.widget(FormWidget.class);
+        MultiFieldSet multiSet = form.fieldsets().fieldset(0, MultiFieldSet.class);
+        multiSet.clickAddButton("Добавить во внешний список");
+        multiSet.clickAddButton("Добавить во внешний список");
+
+        StandardField outerField0 = multiSet.item(0).fields().field("0. Внешнее поле");
+        MultiFieldSet innerMultiSet0 = multiSet.item(0).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet0.clickAddButton();
+        innerMultiSet0.clickAddButton();
+        StandardField innerField00 = innerMultiSet0.item(0).fields().field("0.0. Обязательное внутреннее поле");
+        StandardField innerField01 = innerMultiSet0.item(1).fields().field("0.1. Обязательное внутреннее поле");
+
+        StandardField outerField1 = multiSet.item(3).fields().field("1. Внешнее поле");
+        MultiFieldSet innerMultiSet1 = multiSet.item(3).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet1.clickAddButton();
+        innerMultiSet1.clickAddButton();
+        StandardField innerField10 = innerMultiSet1.item(0).fields().field("1.0. Обязательное внутреннее поле");
+        StandardField innerField11 = innerMultiSet1.item(1).fields().field("1.1. Обязательное внутреннее поле");
+
+        StandardButton validateFirstOuterBtn = form.toolbar().topLeft().button("validate outerList[0].*");
+        validateFirstOuterBtn.click();
+
+        outerField0.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField00.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField01.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        outerField1.shouldHaveValidationMessage(Condition.empty);
+        innerField10.shouldHaveValidationMessage(Condition.empty);
+        innerField11.shouldHaveValidationMessage(Condition.empty);
+
+        outerField0.control(InputText.class).setValue("Тест");
+        innerField00.control(InputText.class).setValue("Тест");
+        innerField01.control(InputText.class).setValue("Тест");
+        validateFirstOuterBtn.click();
+
+        Alert alert = page.alerts(Alert.PlacementEnum.TOP).alert(0);
+        alert.shouldHaveText(SUCCESS_MESSAGE);
+        outerField0.shouldHaveValidationMessage(Condition.empty);
+        innerField00.shouldHaveValidationMessage(Condition.empty);
+        innerField01.shouldHaveValidationMessage(Condition.empty);
+        outerField1.shouldHaveValidationMessage(Condition.empty);
+        innerField10.shouldHaveValidationMessage(Condition.empty);
+        innerField11.shouldHaveValidationMessage(Condition.empty);
+    }
+
+    private void validateFirstInnerBtn(SimplePage page) {
+        FormWidget form = page.widget(FormWidget.class);
+
+        MultiFieldSet multiSet = form.fieldsets().fieldset(0, MultiFieldSet.class);
+        multiSet.clickAddButton("Добавить во внешний список");
+        multiSet.clickAddButton("Добавить во внешний список");
+
+        StandardField outerField0 = multiSet.item(0).fields().field("0. Внешнее поле");
+        MultiFieldSet innerMultiSet0 = multiSet.item(0).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet0.clickAddButton();
+        innerMultiSet0.clickAddButton();
+        StandardField innerField00 = innerMultiSet0.item(0).fields().field("0.0. Обязательное внутреннее поле");
+        StandardField innerField01 = innerMultiSet0.item(1).fields().field("0.1. Обязательное внутреннее поле");
+
+        StandardField outerField1 = multiSet.item(3).fields().field("1. Внешнее поле");
+        MultiFieldSet innerMultiSet1 = multiSet.item(3).fieldsets().fieldset(0, MultiFieldSet.class);
+        innerMultiSet1.clickAddButton();
+        innerMultiSet1.clickAddButton();
+        StandardField innerField10 = innerMultiSet1.item(0).fields().field("1.0. Обязательное внутреннее поле");
+        StandardField innerField11 = innerMultiSet1.item(1).fields().field("1.1. Обязательное внутреннее поле");
+
+        StandardButton validateFirstInnerBtn = form.toolbar().topLeft().button("validate outerList[0].innerList[0].*");
+        validateFirstInnerBtn.click();
+
+        outerField0.shouldHaveValidationMessage(Condition.empty);
+        innerField00.shouldHaveValidationMessage(Condition.text(REQUIRED_VALIDATION_MESSAGE));
+        innerField01.shouldHaveValidationMessage(Condition.empty);
+        outerField1.shouldHaveValidationMessage(Condition.empty);
+        innerField10.shouldHaveValidationMessage(Condition.empty);
+        innerField11.shouldHaveValidationMessage(Condition.empty);
+
+        innerField00.control(InputText.class).setValue("Тест");
+        validateFirstInnerBtn.click();
+
+        Alert alert = page.alerts(Alert.PlacementEnum.TOP).alert(0);
+        alert.shouldHaveText(SUCCESS_MESSAGE);
+        outerField0.shouldHaveValidationMessage(Condition.empty);
+        innerField00.shouldHaveValidationMessage(Condition.empty);
+        innerField01.shouldHaveValidationMessage(Condition.empty);
+        outerField1.shouldHaveValidationMessage(Condition.empty);
+        innerField10.shouldHaveValidationMessage(Condition.empty);
+        innerField11.shouldHaveValidationMessage(Condition.empty);
     }
 }
