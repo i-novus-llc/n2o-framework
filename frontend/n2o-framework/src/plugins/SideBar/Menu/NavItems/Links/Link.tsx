09eebbb936e6c4d5fc0b384eab42c210ce719bf8
Соколов Алексей <asokolov@i-novus.ru>
2026-05-18 11:18:59 +0300

    sub/NNO-12710 :key param processing removed

--- frontend/n2o-framework/src/plugins/SideBar/Menu/NavItems/Links/Link.tsx
+++ frontend/n2o-framework/src/plugins/SideBar/Menu/NavItems/Links/Link.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Link as LinkComponent } from '@i-novus/n2o-components/lib/navigation/Link'
 import classNames from 'classnames'
-import isEmpty from 'lodash/isEmpty'
 
 import { Item } from '../../../../CommonMenuTypes'
 import { Tooltip } from '../../../../../components/snippets/Tooltip/TooltipHOC'
 import { useLink } from '../../../../../components/core/router/useLink'
 import { useLinkPropsResolver } from '../../../../../components/navigation/useLinkPropsResolver'
-import { ModelPrefix } from '../../../../../core/models/types'
 
 import { LinkBody } from './LinkBody'
 
@@ -19,32 +17,8 @@ interface LinkProps {
     showContent: boolean
 }
 
-/*
- * Костыль для SidebarsAT.testSidebarWithDatasource
- * TODO: Разобраться на сколько нужная фича
- */
-const setDefaultLink = (item: Item) => {
-    const { datasource, model: prefix = ModelPrefix.active, pathMapping } = item
-
-    if (!datasource || !pathMapping || isEmpty(pathMapping)) { return item }
-
-    return {
-        ...item,
-        pathMapping: Object.fromEntries(Object.entries(pathMapping).map(([key, mapping]) => [
-            key,
-            mapping.link
-                ? mapping
-                : {
-                    ...mapping,
-                    link: `models.${prefix}['${datasource}']`,
-                    value: mapping.value.replace(/^:(.*)/i, '`$1`'),
-                },
-        ])),
-    }
-}
-
 export function Link({ item, sidebarOpen, isMiniView, isStaticView, showContent }: LinkProps) {
-    const { url: href, title, disabled, target, visible } = useLinkPropsResolver({ ...setDefaultLink(item), url: item.href })
+    const { url: href, title, disabled, target, visible } = useLinkPropsResolver({ ...item, url: item.href })
 
     const hint = isMiniView ? title : null
     const { active, ...linkProps } = useLink({ href, disabled, target })
