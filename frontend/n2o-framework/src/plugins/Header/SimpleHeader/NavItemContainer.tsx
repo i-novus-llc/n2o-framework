5ae99ef9f6f92c21d45b0bab9e487f522ca8c524
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2026-06-04 17:00:40 +0300

    Merge branch 'fix/NNO-12914' into 'master'

--- frontend/n2o-framework/src/plugins/Header/SimpleHeader/NavItemContainer.tsx
+++ frontend/n2o-framework/src/plugins/Header/SimpleHeader/NavItemContainer.tsx
@@ -5,10 +5,10 @@ import classNames from 'classnames'
 
 import { FactoryContext } from '../../../core/factory/context'
 import { FactoryLevels } from '../../../core/factory/factoryLevels'
-import { Item, FactoryComponent } from '../../CommonMenuTypes'
+import { type Item, type FactoryComponent } from '../../CommonMenuTypes'
 import { getModelByPrefixAndNameSelector } from '../../../ducks/models/selectors'
 import { ModelPrefix } from '../../../core/models/types'
-import { propsResolver } from '../../../core/Expression/propsResolver'
+import { useResolved } from '../../../core/Expression/useResolver'
 
 interface NavItemContainerProps {
     itemProps: Item
@@ -20,28 +20,40 @@ interface NavItemContainerProps {
     nested?: boolean
 }
 
-const NavItemContainer = ({
-    itemProps,
-    className,
-    ...props
-}: NavItemContainerProps) => {
+const hasVisibleChildrenRecursive = (items: Item['items']) => {
+    for (const item of items) {
+        const { visible = true } = item
+
+        if (visible) { return true }
+
+        if (item.items && hasVisibleChildrenRecursive(item.items)) {
+            return true
+        }
+    }
+
+    return false
+}
+
+const NavItemContainer = ({ itemProps, className, ...rest }: NavItemContainerProps) => {
     const { datasource, model: prefix = ModelPrefix.active } = itemProps
     const model = useSelector(getModelByPrefixAndNameSelector(prefix, datasource))
-    const item = propsResolver(itemProps, model, model as Record<string, unknown>, ['items'])
+    const item = useResolved(itemProps, model, ['items'])
 
-    const { src, visible } = item
+    const { src, visible, items } = item
+
+    const hasVisibleChildren = items ? hasVisibleChildrenRecursive(items) : true
 
     const { getComponent } = useContext(FactoryContext)
     const FactoryComponent: FactoryComponent = getComponent(src, FactoryLevels.HEADER_ITEMS)
 
-    if (!FactoryComponent || !visible) { return null }
+    if (!FactoryComponent || !visible || !hasVisibleChildren) { return null }
 
     return (
         <FactoryComponent
             item={item}
             from="HEADER"
             className={classNames('nav-item', className)}
-            {...props}
+            {...rest}
         />
     )
 }
