e8589b54d695345440d986889a82e9000d1cc7f2
Заляев Айрат Вазихович <azalyaev@i-novus.ru>
2026-06-16 11:28:54 +0300

    sub/NNO-12956

--- frontend/n2o-components/src/display/Layout/types.ts
+++ frontend/n2o-components/src/display/Layout/types.ts
@@ -1,29 +1,17 @@
 import { CSSProperties, ReactNode, HTMLAttributes } from 'react'
 
-export enum Align {
-    /** Вертикальное выравнивание по верхнему краю (по умолчанию) */
-    TOP = 'top',
-    /** Вертикальное выравнивание по центру */
-    MIDDLE = 'middle',
-    /** Вертикальное выравнивание по нижнему краю */
-    BOTTOM = 'bottom',
-    /** Растягивание содержимого на всю высоту (если допустимо) */
-    STRETCH = 'stretch',
-}
+export type RowItemAlign  = 'top' | 'middle' | 'bottom' | 'stretch'
 
-export enum Justify {
-    /** Горизонтальное выравнивание по началу (по умолчанию) */
-    START = 'start',
-    /** Горизонтальное выравнивание по концу */
-    END = 'end',
-    /** Горизонтальное выравнивание по центру */
-    CENTER = 'center',
-    /** Равномерное распределение с отступами вокруг элементов */
-    SPACE_AROUND = 'space-around',
-    /** Равномерное распределение с отступами между элементами */
-    SPACE_BETWEEN = 'space-between',
-    /** Равномерное распределение с одинаковыми отступами вокруг и между элементами */
-    SPACE_EVENLY = 'space-evenly',
+type ExcludeString<T> = T extends T
+    ? (string extends T ? never : T)
+    : never
+type AlignItems = ExcludeString<CSSProperties['alignItems']>
+
+export const AlignItemsMap: Record<RowItemAlign, AlignItems> = {
+    top: 'start',
+    middle: 'center',
+    bottom: 'end',
+    stretch: 'stretch',
 }
 
 /** Style param в DOM для активации колоночной системы */
@@ -34,28 +22,10 @@ export interface RowProps {
     id?: string
     children?: ReactNode
     wrap?: boolean
-    align?: Align
-    justify?: Justify
+    align?: RowItemAlign
+    justify?: CSSProperties['justifyContent']
     className?: string
     style?: CSSProperties
-}
-
-// Маппинг значений выравнивания на классы
-export const ALIGN_CLASS_MAP = {
-    [Align.TOP]: 'layout-row--align-top',
-    [Align.MIDDLE]: 'layout-row--align-middle',
-    [Align.BOTTOM]: 'layout-row--align-bottom',
-    [Align.STRETCH]: 'layout-row--align-stretch',
-}
-
-// Маппинг значений распределения на классы
-export const JUSTIFY_CLASS_MAP = {
-    [Justify.START]: 'layout-row--justify-start',
-    [Justify.END]: 'layout-row--justify-end',
-    [Justify.CENTER]: 'layout-row--justify-center',
-    [Justify.SPACE_AROUND]: 'layout-row--justify-around',
-    [Justify.SPACE_BETWEEN]: 'layout-row--justify-between',
-    [Justify.SPACE_EVENLY]: 'layout-row--justify-evenly',
 }
 
 /** Style param в DOM для управления колонками */
